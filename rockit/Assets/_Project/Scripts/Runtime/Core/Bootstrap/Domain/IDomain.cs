4535e18594a2777004d95b2266100177282c921f
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 23:26:20 +0300

    Expand IDomain interface with TryInstallFeature method

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/IDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/IDomain.cs
@@ -1,7 +1,9 @@
-﻿namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Features;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
 {
     public interface IDomain
     {
-        
+        void TryInstallFeature<T>() where T : IFeatureInstaller;
     }
 }
