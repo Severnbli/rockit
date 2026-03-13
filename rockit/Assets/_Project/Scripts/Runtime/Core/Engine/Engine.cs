78786c2dd7df369de74c474f2ef76f3e0ad08fbd
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 22:56:56 +0300

    Create Engine class that implement IEngine interface

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Engine/Engine.cs
@@ -0,0 +1,27 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Engine
+{
+    public class Engine : IEngine
+    {
+        public void Init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FixedRun()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Destroy()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
