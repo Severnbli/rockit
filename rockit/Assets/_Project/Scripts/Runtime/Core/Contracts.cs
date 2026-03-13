7b90b5ebd8eec5e4f644c66af79e45f5185456a5
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 15:12:29 +0300

    Create Contracts static class for constants

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Contracts.cs
@@ -0,0 +1,8 @@
+﻿namespace _Project.Scripts.Runtime.Core
+{
+    public static class Contracts
+    {
+        public const string RunSystems = "run_systems";
+        public const string FixedRunSystems = "fixed_run_systems";
+    }
+}
