631de38988aabb859c6a5d0b4f66a2ef2342ceff
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 20:22:32 +0300

    Create BaseFeatureInstaller that implements IFeatureInstaller

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Features/BaseFeatureInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Leopotam.EcsProto;
+using Zenject;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Features
+{
+    public abstract class BaseFeatureInstaller : IFeatureInstaller
+    {
+        public void InstallBindings(DiContainer container)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddSystems(ProtoSystems systems)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
