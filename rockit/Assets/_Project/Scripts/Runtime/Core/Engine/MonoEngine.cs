a84b2f933a58ed7bf9ccdc06a9e4bff464a16fdc
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 14:17:36 +0300

    Define MonoEngine class that implement IEngine interface

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Engine/MonoEngine.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Engine
+{
+    public class MonoEngine : MonoBehaviour, IEngine
+    {
+        protected virtual void Start()
+        {
+            Init();
+        }
+        
+        public void Init()
+        {
+            ;
+        }
+
+        protected virtual void Update()
+        {
+            Run();
+        }
+
+        public void Run()
+        {
+            ;
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            FixedRun();
+        }
+
+        public void FixedRun()
+        {
+            ;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            Destroy();
+        }
+
+        public void Destroy()
+        {
+            ;
+        }
+    }
+}
