f24bdab2cc3f3468062defd6b46342d7621bc6c2
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 14:18:25 +0300

    Define world and systems fields, its assifnification and usage

--- rockit/Assets/_Project/Scripts/Runtime/Core/Engine/MonoEngine.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Engine/MonoEngine.cs
@@ -1,9 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto;
+using UnityEngine;
+using Zenject;
 
 namespace _Project.Scripts.Runtime.Core.Engine
 {
     public class MonoEngine : MonoBehaviour, IEngine
     {
+        private ProtoWorld _world;
+        private ProtoSystems _runSystems;
+        private ProtoSystems _fixedRunSystems;
+
+        [Inject]
+        public void Construct(ProtoWorld world, List<IRunSystems> runSystems, List<IFixedRunSystems> fixedRunSystems)
+        {
+            _world = world;
+
+            _runSystems = new ProtoSystems(_world);
+            foreach (var runSystem in runSystems)
+            {
+                _runSystems.AddSystem(runSystem);
+            }
+            
+            _fixedRunSystems = new ProtoSystems(_world);
+            foreach (var fixedRunSystem in fixedRunSystems)
+            {
+                _fixedRunSystems.AddSystem(fixedRunSystem);
+            }
+        }
+
         protected virtual void Start()
         {
             Init();
@@ -11,7 +37,7 @@ namespace _Project.Scripts.Runtime.Core.Engine
         
         public void Init()
         {
-            ;
+            _runSystems?.Init();
         }
 
         protected virtual void Update()
@@ -21,7 +47,7 @@ namespace _Project.Scripts.Runtime.Core.Engine
 
         public void Run()
         {
-            ;
+            _runSystems?.Run();
         }
 
         protected virtual void FixedUpdate()
@@ -31,7 +57,7 @@ namespace _Project.Scripts.Runtime.Core.Engine
 
         public void FixedRun()
         {
-            ;
+            _fixedRunSystems?.Run();
         }
 
         protected virtual void OnDestroy()
@@ -41,7 +67,11 @@ namespace _Project.Scripts.Runtime.Core.Engine
 
         public void Destroy()
         {
-            ;
+            _runSystems?.Destroy();
+            _runSystems = null;
+            
+            _fixedRunSystems?.Destroy();
+            _fixedRunSystems = null;
         }
     }
 }
