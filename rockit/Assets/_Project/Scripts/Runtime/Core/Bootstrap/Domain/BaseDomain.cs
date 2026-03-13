d97ba29bd0b0cc625887c23c15200b0a9277054b
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 23:27:16 +0300

    Inherit BaseDomain from IDomain

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/BaseDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/BaseDomain.cs
@@ -1,11 +1,12 @@
-﻿using _Project.Scripts.Runtime.Core.Engine;
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Features;
+using _Project.Scripts.Runtime.Core.Engine;
 using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
 using Zenject;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
 {
-    public abstract class BaseDomain : MonoInstaller
+    public abstract class BaseDomain : MonoInstaller, IDomain
     {
         protected readonly ProtoWorld World;
         protected readonly EcsSystems Systems;
@@ -21,6 +22,11 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
             Container.Bind<ProtoWorld>().FromInstance(World).AsSingle();
             Container.Bind<EcsSystems>().FromInstance(Systems).AsSingle();
             Container.Bind<MonoEngine>().FromNewComponentOn(gameObject).AsSingle().NonLazy();
+        }
+
+        public void TryInstallFeature<T>() where T : IFeatureInstaller
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
