6b2e2024247b9add302af90888e42f9c4d91815a
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 20:21:15 +0300

    Create IFeatureInstaller interface with InstallBindings and AddSystems methods

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Features/IFeatureInstaller.cs
@@ -0,0 +1,11 @@
+﻿using Leopotam.EcsProto;
+using Zenject;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Features
+{
+    public interface IFeatureInstaller
+    {
+        void InstallBindings(DiContainer container);
+        void AddSystems(ProtoSystems systems);
+    }
+}
