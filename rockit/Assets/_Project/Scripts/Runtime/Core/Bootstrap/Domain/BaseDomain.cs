34ba78ffe8b46d33254484bedd106722371ac50e
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 23:16:34 +0300

    Add bind of MonoEngine at BaseDomain

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/BaseDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/BaseDomain.cs
@@ -1,9 +1,13 @@
-﻿using Zenject;
+﻿using _Project.Scripts.Runtime.Core.Engine;
+using Zenject;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
 {
     public class BaseDomain : MonoInstaller
     {
-        
+        public sealed override void InstallBindings()
+        {
+            Container.Bind<MonoEngine>().FromNewComponentOn(gameObject).AsSingle().NonLazy();
+        }
     }
 }
