9cb5b4ad5f30146106a89bbe63d9a32ba208a104
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 23:19:03 +0300

    Add field world of type ProtoWorld

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/BaseDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/BaseDomain.cs
@@ -1,10 +1,13 @@
 ﻿using _Project.Scripts.Runtime.Core.Engine;
+using Leopotam.EcsProto;
 using Zenject;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
 {
     public class BaseDomain : MonoInstaller
     {
+        protected ProtoWorld World;
+        
         public sealed override void InstallBindings()
         {
             Container.Bind<MonoEngine>().FromNewComponentOn(gameObject).AsSingle().NonLazy();
