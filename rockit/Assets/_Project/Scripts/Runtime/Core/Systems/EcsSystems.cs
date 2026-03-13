1806a0565d75e7fb8548d94d04876f06ae9e3bf4
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 21:37:42 +0300

    Create EcsSystems class that inherited from ProtoSystems

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Systems/EcsSystems.cs
@@ -0,0 +1,11 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Systems
+{
+    public class EcsSystems : ProtoSystems
+    {
+        public EcsSystems(ProtoWorld defaultWorld) : base(defaultWorld)
+        {
+        }
+    }
+}
