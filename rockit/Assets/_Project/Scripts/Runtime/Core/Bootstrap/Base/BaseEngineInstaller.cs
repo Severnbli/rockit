d46ac2d8151b71aefc419a3ca44bb0e9f4d02cea
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 13:05:08 +0300

    Implement IEngineInstall into BaseEngineInstaller

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Base/BaseEngineInstaller.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Base/BaseEngineInstaller.cs
@@ -2,8 +2,16 @@
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Base
 {
-    public class BaseEngineInstaller : MonoInstaller
+    public class BaseEngineInstaller : MonoInstaller, IEngineInstaller
     {
-        
+        public void AddFeatureInstaller(IFeatureInstaller installer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RegisterBindings(DiContainer container)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
