f7b1255cfa9df6b1dd1636816923f1b617ac6aab
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-13 23:49:25 +0300

    Add protected readonly fields container, systems and pausablesystemssolver

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Features/BaseFeatureInstaller.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Features/BaseFeatureInstaller.cs
@@ -1,7 +1,21 @@
-﻿namespace _Project.Scripts.Runtime.Core.Bootstrap.Features
+﻿using _Project.Scripts.Runtime.Core.Systems;
+using Zenject;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Features
 {
     public abstract class BaseFeatureInstaller : IFeatureInstaller
     {
+        protected readonly DiContainer Container;
+        protected readonly EcsSystems Systems;
+        protected readonly PausableSystemsSolver PausableSystemsSolver;
+        
+        public BaseFeatureInstaller(DiContainer container, EcsSystems systems, PausableSystemsSolver pausableSystemsSolver)
+        {
+            Container = container;
+            Systems = systems;
+            PausableSystemsSolver = pausableSystemsSolver;
+        }
+        
         public void InstallBindings()
         {
             BindServices();
