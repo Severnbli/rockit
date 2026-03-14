4ec423a76aa14dc152fab3acd3d7b5fccb14bea9
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-14 13:03:23 +0300

    Expand IState with OnEnter and OnLeave methods with UniTask return type

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/StateMachine/IState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/StateMachine/IState.cs
@@ -1,7 +1,10 @@
-﻿namespace _Project.Scripts.Runtime.Core.Bootstrap.StateMachine
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.StateMachine
 {
     public interface IState
     {
-        
+        UniTask OnEnter();
+        UniTask OnLeave();
     }
 }
