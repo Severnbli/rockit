41e9345a88705ecc517a2b368cf45d62029fc731
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-14 14:24:49 +0300

    Create ProjectDomain inherited from BaseDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Project/ProjectDomain.cs
@@ -0,0 +1,11 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Project
+{
+    public class ProjectDomain : BaseDomain
+    {
+        public ProjectDomain(ProtoWorld world) : base(world)
+        {
+        }
+    }
+}
