601469a0c2c880c6bfed2a3c6730770ab03d238c
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-14 15:47:54 +0300

    Make SceneDomain abstract

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/SceneDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/SceneDomain.cs
@@ -1,15 +1,9 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes;
-using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
 {
-    public class SceneDomain : BaseDomain
+    public abstract class SceneDomain : BaseDomain
     {
-        protected override ProtoWorld ConstructWorld()
-        {
-            throw new System.NotImplementedException();
-        }
-
         protected override void RegisterBindings()
         {
             base.RegisterBindings();
