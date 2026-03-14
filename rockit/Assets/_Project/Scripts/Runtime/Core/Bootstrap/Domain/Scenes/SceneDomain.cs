baa72746dd8590c0234595d41cf3423c80511df0
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-14 15:26:57 +0300

    Create SceneDomain inherited from BaseDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/SceneDomain.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
+{
+    public class SceneDomain : BaseDomain
+    {
+        protected override ProtoWorld ConstructWorld()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
