6bd13f17e0be63c3a9a8565d8b194f33549ab0dd
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-14 15:11:07 +0300

    Create ProjectCloseState that implements IProjectState

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Project/ProjectCloseState.cs
@@ -0,0 +1,17 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Project
+{
+    public class ProjectCloseState : IProjectState
+    {
+        public async UniTask OnEnter()
+        {
+            await UniTask.CompletedTask;
+        }
+
+        public async UniTask OnLeave()
+        {
+            await UniTask.CompletedTask;
+        }
+    }
+}
