127a8ee3c26d5b6b83223e1775b41e10125f430d
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-14 15:55:55 +0300

    Create MenuSceneDomain that inherits from SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/MenuSceneDomain.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
+{
+    public class MenuSceneDomain : SceneDomain
+    {
+        protected override ProtoWorld ConstructWorld()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
