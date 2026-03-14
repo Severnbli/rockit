ee9f70b14f4bfd2092668a479a97cd3540b04df2
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-14 13:09:20 +0300

    Expand IStateMachine with ChangeState generic method (T is limited to be inherited from IState)

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/StateMachine/IStateMachine.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/StateMachine/IStateMachine.cs
@@ -1,7 +1,9 @@
-﻿namespace _Project.Scripts.Runtime.Core.Bootstrap.StateMachine
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.StateMachine
 {
     public interface IStateMachine
     {
-        
+        UniTask ChangeState<T>() where T : IState;
     }
 }
