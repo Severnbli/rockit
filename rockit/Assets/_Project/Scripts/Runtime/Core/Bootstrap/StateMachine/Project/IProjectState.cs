4299cbb5c8e29c900b64193e6696bb11cf9691ed
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-14 14:31:00 +0300

    Rename StateMachine folder to States

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/StateMachine/Project/IProjectState.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _Project.Scripts.Runtime.Core.Bootstrap.StateMachine.Project
-{
-    public interface IProjectState : IState
-    {
-        
-    }
-}
--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/IStateMachine.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/IStateMachine.cs
@@ -1,6 +1,6 @@
 ﻿using Cysharp.Threading.Tasks;
 
-namespace _Project.Scripts.Runtime.Core.Bootstrap.StateMachine
+namespace _Project.Scripts.Runtime.Core.Bootstrap.States
 {
     public interface IStateMachine
     {
