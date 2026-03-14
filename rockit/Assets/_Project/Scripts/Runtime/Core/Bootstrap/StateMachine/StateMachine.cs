2b2f60fd7848fe4df14f26ce40b99e01d3eaa6b9
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-14 13:13:55 +0300

    Add ActiveState field to StateMachine

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/StateMachine/StateMachine.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/StateMachine/StateMachine.cs
@@ -4,6 +4,8 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.StateMachine
 {
     public class StateMachine : IStateMachine
     {
+        protected IState ActiveState;
+        
         public UniTask ChangeState<T>() where T : IState
         {
             throw new System.NotImplementedException();
