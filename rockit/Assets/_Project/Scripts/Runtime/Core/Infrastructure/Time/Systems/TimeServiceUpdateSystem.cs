a274d3f7b865ae02ae672507e4c02d21cecd40bc
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-18 23:23:14 +0300

    Create TimeServiceUpdateSystem that implements IProtoRunSystem and IProtoFixedRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Time/Systems/TimeServiceUpdateSystem.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Time.Systems
+{
+    public sealed class TimeServiceUpdateSystem : IProtoRunSystem, IProtoFixedRunSystem
+    {
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FixedRun()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
