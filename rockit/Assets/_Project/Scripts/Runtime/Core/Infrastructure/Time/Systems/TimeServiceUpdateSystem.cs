e3261b08eb7e17a47aa1bcef625c11c828a1f995
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-18 23:24:52 +0300

    Add private field of type TimeService and initialize it at constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Time/Systems/TimeServiceUpdateSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Time/Systems/TimeServiceUpdateSystem.cs
@@ -1,10 +1,18 @@
-﻿using _Project.Scripts.Runtime.Core.Systems;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Time.Systems
 {
     public sealed class TimeServiceUpdateSystem : IProtoRunSystem, IProtoFixedRunSystem
     {
+        private TimeService _timeService;
+
+        public TimeServiceUpdateSystem(TimeService timeService)
+        {
+            _timeService = timeService;
+        }
+
         public void Run()
         {
             throw new System.NotImplementedException();
