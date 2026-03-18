692a10abb4c89004fc182b2dea790959edb4e0ca
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-18 23:21:50 +0300

    Add public fields to TimeService such as Time, FixedTime, DeltaTime, FixedDeltaTime, UnscaledTime, UnscaledFixedTime, UnscaledDeltaTime, UnscaledFixedDeltaTime

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Time/Services/TimeService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Time/Services/TimeService.cs
@@ -2,6 +2,13 @@
 {
     public sealed class TimeService
     {
-        
+        public float Time;
+        public float FixedTime;
+        public float DeltaTime;
+        public float FixedDeltaTime;
+        public float UnscaledTime;
+        public float UnscaledFixedTime;
+        public float UnscaledDeltaTime;
+        public float UnscaledFixedDeltaTime;
     }
 }
