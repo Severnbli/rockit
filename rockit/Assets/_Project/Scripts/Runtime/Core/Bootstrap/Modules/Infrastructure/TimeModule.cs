e09646a1ca53f0b994f39147c159ef80253c0793
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-18 23:33:30 +0300

    Create TimeModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/TimeModule.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
+{
+    public sealed class TimeModule : BaseModuleInstaller<TimeModule>
+    {
+        public TimeModule(EcsSystems systems, PausableSystemsSolver pausableSystemsSolver) : base(systems,
+            pausableSystemsSolver)
+        {
+        }
+    }
+}
