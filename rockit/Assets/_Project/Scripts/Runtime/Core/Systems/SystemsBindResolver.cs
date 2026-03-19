cca7129f3796977b0ad1c9a5607e9ba5b628a264
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 09:34:33 +0300

    Move inject of EcsSystems to method Construct

--- rockit/Assets/_Project/Scripts/Runtime/Core/Systems/SystemsBindResolver.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Systems/SystemsBindResolver.cs
@@ -6,8 +6,13 @@ namespace _Project.Scripts.Runtime.Core.Systems
 {
     public class SystemsBindResolver
     {
-        [Inject] private EcsSystems _systems;
         [Inject(Id = Contracts.NonPausableSystemsId)] private List<IProtoSystem> _nonPausableSystems;
         [Inject(Id = Contracts.PausableSystemsId)] private List<IProtoSystem> _pausableSystems;
+
+        [Inject]
+        private void Construct(EcsSystems systems)
+        {
+            
+        }
     }
 }
