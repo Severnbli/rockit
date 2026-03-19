18fd6510ce0ae4a15400200803e5d291be50c22e
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 13:49:38 +0300

    Add ProtoPool<ActiveRequestTag> to RequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -7,5 +8,6 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
     public class RequestsAspect : ProtoAspectInject
     {
         public ProtoPool<RequestComponent> RequestComponentPool;
+        public ProtoPool<ActiveRequestTag> ActiveRequestTagPool;
     }
 }
