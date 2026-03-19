393066c4de7de8f6eb3586316883a211ae6c4a0b
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 14:55:33 +0300

    Expand ISystemsContainerProvider with GetSystemsContainer method

--- rockit/Assets/_Project/Scripts/Tests/Systems/SystemsContainerProvider.cs
+++ rockit/Assets/_Project/Scripts/Tests/Systems/SystemsContainerProvider.cs
@@ -1,7 +1,12 @@
-﻿namespace _Project.Scripts.Tests.Systems
+﻿using _Project.Scripts.Runtime.Core.Systems;
+
+namespace _Project.Scripts.Tests.Systems
 {
     public class SystemsContainerProvider : ISystemsContainerProvider
     {
-        
+        public EcsSystems GetSystemsContainer()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
