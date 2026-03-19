ef1ea9941b5ca4f30277cb925896ffc1f4e2dd99
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 14:04:24 +0300

    Create ActivateRequestsSystem that implements IProtoRunSystem and IProtoFixedRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/ActivateRequestsSystem.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.Systems
+{
+    public class ActivateRequestsSystem : IProtoRunSystem, IProtoFixedRunSystem
+    {
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FixedRun()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
