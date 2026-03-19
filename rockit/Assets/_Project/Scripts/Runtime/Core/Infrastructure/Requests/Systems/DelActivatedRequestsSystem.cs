7c0e4015a3ac8623389b6c55a20feb90416c25ad
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 14:10:44 +0300

    Create DelActivatedRequestsSystem that implements IProtoRunSystem and IProtoFixedRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/DelActivatedRequestsSystem.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.Systems
+{
+    public class DelActivatedRequestsSystem : IProtoRunSystem, IProtoFixedRunSystem
+    {
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FixedRun()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
