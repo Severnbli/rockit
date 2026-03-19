0559b390d4e1656cc450b0bba2c14bd37beea120
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 14:18:16 +0300

    Define RequestsModule BindSystems method by binding DelActivatedRequestsSystem and ActivateRequestsSystem

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/RequestsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/RequestsModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
 {
@@ -6,6 +7,14 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
     {
         public RequestsModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<DelActivatedRequestsSystem>();
+            BindSystem<ActivateRequestsSystem>();
         }
     }
 }
