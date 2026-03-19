e8f0538f9920c89e401cca436d50b49897579977
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 15:06:38 +0300

    Delete EcsSystems field

--- rockit/Assets/_Project/Scripts/Tests/Systems/InfrastructureTests.cs
+++ rockit/Assets/_Project/Scripts/Tests/Systems/InfrastructureTests.cs
@@ -1,10 +1,7 @@
-﻿using _Project.Scripts.Runtime.Core.Systems;
-
-namespace _Project.Scripts.Tests.Systems
+﻿namespace _Project.Scripts.Tests.Systems
 {
     public class InfrastructureTests
     {
-        private EcsSystems _systems;
         private ISystemsContainerProvider _systemsContainerProvider;
     }
 }
