6c0307a0a5f298530acf44121bb9cfe96d3b7db9
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 14:00:29 +0300

    Add ProtoIt RunActivated field

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
@@ -13,5 +13,6 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
         public ProtoPool<FixedRunRequestTag> FixedRunRequestTagPool;
         public ProtoItExc RunNotActivated = new(It.Inc<RequestComponent, RunRequestTag>(), It.Exc<ActiveRequestTag>());
         public ProtoItExc FixedRunNotActivated = new(It.Inc<RequestComponent, FixedRunRequestTag>(), It.Exc<ActiveRequestTag>());
+        public ProtoIt RunActivated = new(It.Inc<RequestComponent, RunRequestTag, ActiveRequestTag>());
     }
 }
