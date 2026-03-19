48f5c5e2e9212d847ed1fdeb1ae1c7e19a7fc0f6
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 13:38:13 +0300

    Add Entity field

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Components/RequestComponent.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Components/RequestComponent.cs
@@ -1,7 +1,9 @@
-﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components
 {
     public struct RequestComponent
     {
-        
+        public ProtoPackedEntity Entity;
     }
 }
