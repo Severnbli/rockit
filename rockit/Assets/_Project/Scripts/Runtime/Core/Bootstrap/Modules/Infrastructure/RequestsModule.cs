ea1e6c7aeb0678c482c1c80fa5f50ad9b3e582e2
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 14:16:37 +0300

    Create RequestsModule that inherited from BaseModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/RequestsModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
+{
+    public sealed class RequestsModule : BaseModule<RequestsModule>
+    {
+        public RequestsModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
