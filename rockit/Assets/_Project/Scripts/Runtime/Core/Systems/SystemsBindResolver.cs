f9141b204aacbfa53c8d84fb7843e228ef06d644
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 09:36:19 +0300

    Change list fields type to array

--- rockit/Assets/_Project/Scripts/Runtime/Core/Systems/SystemsBindResolver.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Systems/SystemsBindResolver.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections.Generic;
-using Leopotam.EcsProto;
+﻿using Leopotam.EcsProto;
 using Zenject;
 
 namespace _Project.Scripts.Runtime.Core.Systems
 {
     public class SystemsBindResolver
     {
-        [Inject(Id = Contracts.NonPausableSystemsId)] private List<IProtoSystem> _nonPausableSystems;
-        [Inject(Id = Contracts.PausableSystemsId)] private List<IProtoSystem> _pausableSystems;
+        [Inject(Id = Contracts.NonPausableSystemsId)] private IProtoSystem[] _nonPausableSystems;
+        [Inject(Id = Contracts.PausableSystemsId)] private IProtoSystem[] _pausableSystems;
 
         [Inject]
         private void Construct(EcsSystems systems, PausableSystemsSolver solver)
