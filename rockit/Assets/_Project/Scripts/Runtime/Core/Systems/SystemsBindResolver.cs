e87c35575956e5e4dd9e9b6b4905af9d167a81fb
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 09:33:15 +0300

    Add private fields to inject pausable and non pausable systems through id

--- rockit/Assets/_Project/Scripts/Runtime/Core/Systems/SystemsBindResolver.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Systems/SystemsBindResolver.cs
@@ -1,9 +1,13 @@
-﻿using Zenject;
+﻿using System.Collections.Generic;
+using Leopotam.EcsProto;
+using Zenject;
 
 namespace _Project.Scripts.Runtime.Core.Systems
 {
     public class SystemsBindResolver
     {
         [Inject] private EcsSystems _systems;
+        [Inject(Id = Contracts.NonPausableSystemsId)] private List<IProtoSystem> _nonPausableSystems;
+        [Inject(Id = Contracts.PausableSystemsId)] private List<IProtoSystem> _pausableSystems;
     }
 }
