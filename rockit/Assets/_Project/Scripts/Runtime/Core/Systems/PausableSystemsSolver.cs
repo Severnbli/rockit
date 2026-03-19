7706374000a50c0a9d4d7cad3caaa11cfd4c69cb
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 11:31:26 +0300

    Make Paused property private for set, add Pause and Resume method to operate with this property

--- rockit/Assets/_Project/Scripts/Runtime/Core/Systems/PausableSystemsSolver.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Systems/PausableSystemsSolver.cs
@@ -4,8 +4,11 @@ namespace _Project.Scripts.Runtime.Core.Systems
 {
     public class PausableSystemsSolver : IConditionalSystemSolver
     {
-        public bool Paused { get; set; } = false;
+        public bool Paused { get; private set; } = false;
         
         public bool Solve() => !Paused;
+        
+        public void Pause() => Paused = true;
+        public void Resume() => Paused = false;
     }
 }
