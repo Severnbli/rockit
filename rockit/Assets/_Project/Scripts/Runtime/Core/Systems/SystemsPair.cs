24480e7f7bfb4dcd7a4d67639cad76a16e36b226
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 09:09:31 +0300

    Add public fields UnPausableSystem and PausavleSystem of type Type

--- rockit/Assets/_Project/Scripts/Runtime/Core/Systems/SystemsPair.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Systems/SystemsPair.cs
@@ -1,7 +1,10 @@
-﻿namespace _Project.Scripts.Runtime.Core.Systems
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Core.Systems
 {
     public sealed class SystemsPair
     {
-        
+        public Type UnPausableSystem;
+        public Type PausableSystem;
     }
 }
