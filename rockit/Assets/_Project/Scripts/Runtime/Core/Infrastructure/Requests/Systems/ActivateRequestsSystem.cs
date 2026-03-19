4742a87ddab69c16db5e2e854e55b9e311c07511
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-19 14:07:21 +0300

    Add RequestsAspect field that initialized with DI attribute

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/ActivateRequestsSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/ActivateRequestsSystem.cs
@@ -1,10 +1,13 @@
 ﻿using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.Systems
 {
     public class ActivateRequestsSystem : IProtoRunSystem, IProtoFixedRunSystem
     {
+        [DI] private RequestsAspect _requestsAspect;
+        
         public void Run()
         {
             throw new System.NotImplementedException();
