d93dfcd4d6c36b36dab28d64c53edbacf706625c
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 14:08:26 +0300

    Add "_monoEngine" field and assign it at constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/MonoEngineCancellationTokenSourceProvider.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/MonoEngineCancellationTokenSourceProvider.cs
@@ -1,9 +1,17 @@
 ﻿using System.Threading;
+using _Project.Scripts.Runtime.Core.Engine;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
 {
     public class MonoEngineCancellationTokenSourceProvider : IDomainCancellationTokenSourceProvider
     {
+        private MonoEngine _monoEngine;
+
+        public MonoEngineCancellationTokenSourceProvider(MonoEngine monoEngine)
+        {
+            _monoEngine = monoEngine;
+        }
+
         public CancellationTokenSource GetCancellationTokenSource()
         {
             return null;
