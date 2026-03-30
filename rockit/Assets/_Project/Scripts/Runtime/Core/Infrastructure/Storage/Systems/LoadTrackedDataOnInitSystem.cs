9974e5dac3c8eed9ffab3c77d2bdd3336ddd0878
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 15:45:36 +0300

    Create LoadTrackedDataOnInitSystem that implements IProtoInitSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Systems/LoadTrackedDataOnInitSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Systems
+{
+    public class LoadTrackedDataOnInitSystem : IProtoInitSystem
+    {
+        public void Init(IProtoSystems systems)
+        {
+            
+        }
+    }
+}
