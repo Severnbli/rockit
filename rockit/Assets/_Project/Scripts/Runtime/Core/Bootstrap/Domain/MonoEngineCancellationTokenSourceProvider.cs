7522217a315435356fff2c329d2031227ae794f5
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 14:06:46 +0300

    Create MonoEngineCancellationTokenSourceProvider that implements IDomainCancellationTokenSourceProvider

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/MonoEngineCancellationTokenSourceProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
+{
+    public class MonoEngineCancellationTokenSourceProvider : IDomainCancellationTokenSourceProvider
+    {
+        public CancellationTokenSource GetCancellationTokenSource()
+        {
+            return null;
+        }
+    }
+}
