51d5769888b46eff9152f1c03e482919c8ec89be
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 14:05:45 +0300

    Create IDomainCancellationTokenSourceProvider with "GetCancellationTokenSource" method

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/IDomainCancellationTokenSourceProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
+{
+    public interface IDomainCancellationTokenSourceProvider
+    {
+        CancellationTokenSource GetCancellationTokenSource();
+    }
+}
