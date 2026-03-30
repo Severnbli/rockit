a70dffd540cca6bff28aa8855e6d29087391dcbb
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 14:18:06 +0300

    Add "_ct" field of type CancellationToken and initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/BaseDataStorage.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/BaseDataStorage.cs
@@ -1,14 +1,17 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage
 {
     public class BaseDataStorage : IDataStorage
     {
         private readonly IDataStorageKeyProvider _keyProvider;
+        private readonly CancellationToken _ct;
 
-        public BaseDataStorage(IDataStorageKeyProvider keyProvider)
+        public BaseDataStorage(IDataStorageKeyProvider keyProvider, CancellationToken ct)
         {
             _keyProvider = keyProvider;
+            _ct = ct;
         }
 
         public async UniTask Load<T>()
