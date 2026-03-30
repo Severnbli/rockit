6aeb7ac2dc74b067408c8b14c1a90ff2f4cefb7b
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 18:55:37 +0300

    Create SceneSwitcher that implements ISceneSwitcher

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/SceneSwitcher.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes
+{
+    public class SceneSwitcher : ISceneSwitcher
+    {
+        public async UniTask SwitchScene(string sceneName)
+        {
+            await UniTask.CompletedTask;
+        }
+    }
+}
