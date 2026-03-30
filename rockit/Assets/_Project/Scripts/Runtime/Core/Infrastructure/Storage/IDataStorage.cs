d7b4b7333601d01d28c627158b24aedda22d59ff
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 13:42:49 +0300

    Create IDataStorage with generic methods "Load" and "Save"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/IDataStorage.cs
@@ -0,0 +1,10 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage
+{
+    public interface IDataStorage
+    {
+        UniTask Load<T>();
+        UniTask Save<T>(T item);
+    }
+}
