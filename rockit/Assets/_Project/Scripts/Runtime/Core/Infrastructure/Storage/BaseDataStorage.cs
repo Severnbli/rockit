4b0b7026d8d5c1a37621e220e976573e86ecd857
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 13:55:25 +0300

    Add "_keyProvider" field and initialize its from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/BaseDataStorage.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/BaseDataStorage.cs
@@ -4,6 +4,13 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage
 {
     public class BaseDataStorage : IDataStorage
     {
+        private readonly IDataStorageKeyProvider _keyProvider;
+
+        public BaseDataStorage(IDataStorageKeyProvider keyProvider)
+        {
+            _keyProvider = keyProvider;
+        }
+
         public async UniTask Load<T>()
         {
             
