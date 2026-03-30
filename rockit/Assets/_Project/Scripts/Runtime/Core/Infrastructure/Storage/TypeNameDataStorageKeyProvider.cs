337f36547d8b66b7bd6e984fb15c125f9f98e697
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 13:53:47 +0300

    Create TypeNameDataStorageKeyProvider that implement IDataStorageKeyProvider

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/TypeNameDataStorageKeyProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage
+{
+    public class TypeNameDataStorageKeyProvider : IDataStorageKeyProvider
+    {
+        public string GetKey<T>()
+        {
+            return "";
+        }
+    }
+}
