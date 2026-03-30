d0bcd4c20e3031422c53623d0134fc80fe9241bc
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 13:50:35 +0300

    Create BaseDataStorage that implement IDataStorage

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/BaseDataStorage.cs
@@ -0,0 +1,17 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage
+{
+    public class BaseDataStorage : IDataStorage
+    {
+        public async UniTask Load<T>()
+        {
+            
+        }
+
+        public async UniTask Save<T>(T item)
+        {
+            
+        }
+    }
+}
