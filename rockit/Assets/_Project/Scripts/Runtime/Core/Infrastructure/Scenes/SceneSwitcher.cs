c77cacf36e4429ee0fd2b768f9e8851efdd805ee
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 22:49:27 +0300

    Add "_ct" field of type CancellationToken an initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/SceneSwitcher.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/SceneSwitcher.cs
@@ -1,9 +1,17 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes
 {
     public class SceneSwitcher : ISceneSwitcher
     {
+        private CancellationToken _ct;
+
+        public SceneSwitcher(CancellationToken ct)
+        {
+            _ct = ct;
+        }
+
         public async UniTask SwitchScene(string sceneName)
         {
             await UniTask.CompletedTask;
