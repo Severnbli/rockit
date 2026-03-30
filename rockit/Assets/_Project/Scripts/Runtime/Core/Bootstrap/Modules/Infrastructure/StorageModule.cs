1604185d2343a50ef96c31833bcd3389baabcc70
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 13:56:07 +0300

    Create StorageModule that inherited from BaseModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/StorageModule.cs
@@ -0,0 +1,13 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
+{
+    public class StorageModule : BaseModule<StorageModule>
+    {
+        public StorageModule(IDomain domain) : base(domain)
+        {
+        }
+        
+        
+    }
+}
