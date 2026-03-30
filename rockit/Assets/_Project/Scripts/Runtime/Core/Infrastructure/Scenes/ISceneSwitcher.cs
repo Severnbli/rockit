adae53c68eaca7d806b898ca606b8d3a5a6c2778
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 18:54:32 +0300

    Create ISceneSwitcher with "SwithScene" method

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/ISceneSwitcher.cs
@@ -0,0 +1,9 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes
+{
+    public interface ISceneSwitcher
+    {
+        UniTask SwitchScene(string sceneName);
+    }
+}
