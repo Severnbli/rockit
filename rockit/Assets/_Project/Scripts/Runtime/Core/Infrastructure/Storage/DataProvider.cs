61aaed937790871cf52ad65d14d10ba12eb7c4a8
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-30 15:35:12 +0300

    Create DataProvider that inherited from BaseDataProvider

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/DataProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage
+{
+    public class DataProvider : BaseDataProvider
+    {
+        public DataProvider(IDataStorageKeyProvider keyProvider, IDataStorage dataStorage) : base(keyProvider, dataStorage)
+        {
+        }
+    }
+}
