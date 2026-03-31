0f28c22619b8cd14349fcbd8198469f550c96ed0
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:57:49 +0300

    Add "_timeService" field and initialize it at constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Escort;
+using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
@@ -8,11 +9,13 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Loader
     {
         private readonly SceneSwitcherService _switcherService;
         private readonly ISceneLoadingEscort _loadingEscort;
+        private readonly TimeService _timeService;
 
-        public SceneLoader(SceneSwitcherService switcherService, ISceneLoadingEscort loadingEscort)
+        public SceneLoader(SceneSwitcherService switcherService, ISceneLoadingEscort loadingEscort, TimeService timeService)
         {
             _switcherService = switcherService;
             _loadingEscort = loadingEscort;
+            _timeService = timeService;
         }
 
         public async UniTask<bool> TryLoadScene(string sceneName, AsyncOperation loadingOperation)
