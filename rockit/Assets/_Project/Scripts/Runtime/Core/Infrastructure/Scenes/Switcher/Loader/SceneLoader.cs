020a6af3124d8bd627738c09f89987178f45541d
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:54:17 +0300

    Add "_loadingEscort" field and initialize it at constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
@@ -1,4 +1,5 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Escort;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Loader
@@ -6,10 +7,12 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Loader
     public class SceneLoader : ISceneLoader
     {
         private SceneSwitcherService _switcherService;
+        private readonly ISceneLoadingEscort _loadingEscort;
 
-        public SceneLoader(SceneSwitcherService switcherService)
+        public SceneLoader(SceneSwitcherService switcherService, ISceneLoadingEscort loadingEscort)
         {
             _switcherService = switcherService;
+            _loadingEscort = loadingEscort;
         }
 
         public async UniTask<bool> TryLoadScene(string sceneName, AsyncOperation loadingOperation)
