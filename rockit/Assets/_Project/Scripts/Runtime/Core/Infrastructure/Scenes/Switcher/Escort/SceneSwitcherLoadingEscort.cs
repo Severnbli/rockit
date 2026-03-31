3c8ce1b01427b7f5ef83549e7fdb05f77dd4f1e8
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:17:37 +0300

    Add "_ct" field and initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Escort/SceneSwitcherLoadingEscort.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Escort/SceneSwitcherLoadingEscort.cs
@@ -1,10 +1,18 @@
 ﻿using System.ComponentModel;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Escort
 {
     public class SceneSwitcherLoadingEscort : ISceneSwitcherLoadingEscort
     {
+        private CancellationToken _ct;
+
+        public SceneSwitcherLoadingEscort(CancellationToken ct)
+        {
+            _ct = ct;
+        }
+
         public async UniTask EscortLoading(AsyncOperation operation, SceneSwitcherService service)
         {
             await UniTask.CompletedTask;
