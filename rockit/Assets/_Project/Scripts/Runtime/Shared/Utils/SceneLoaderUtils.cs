5b83155ab5c20a62bdeb07966bb0e889714871bf
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:46:51 +0300

    Define TryStartLoading method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/SceneLoaderUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/SceneLoaderUtils.cs
@@ -1,7 +1,24 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Utils
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Runtime.Shared.Utils
 {
     public static class SceneLoaderUtils
     {
-        
+        public static bool TryStartLoading(string sceneName, AsyncOperation loadingOperation)
+        {
+            if (loadingOperation is not null)
+            {
+                LogUtils.LogWarning($"Detect start loading {sceneName} scene invocation while another loading " +
+                                    $"operation is still running");
+                return false;
+            }
+            
+            loadingOperation = SceneManager.LoadSceneAsync(sceneName);
+            if (loadingOperation is not null) return true;
+            
+            LogUtils.LogError($"Scene {sceneName} failed to load");
+            return false;
+        }
     }
 }
