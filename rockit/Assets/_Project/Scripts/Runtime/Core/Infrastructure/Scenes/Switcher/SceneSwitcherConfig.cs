5e9372db706a76b1c99b4a63624b866358b49dc3
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 18:06:50 +0300

    Add "_simulationLoadingDuration" field with its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/SceneSwitcherConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/SceneSwitcherConfig.cs
@@ -1,9 +1,12 @@
 ﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher
 {
     public class SceneSwitcherConfig : ScriptableObjectAutoInstaller<SceneSwitcherConfig>
     {
+        [SerializeField] private float _simulationLoadingDuration = 2f;
         
+        public float SimulationLoadingDuration => _simulationLoadingDuration;
     }
 }
