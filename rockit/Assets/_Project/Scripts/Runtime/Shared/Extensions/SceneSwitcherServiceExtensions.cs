85ed534d6e8640fb02c19161ef05b0818c2459fd
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 18:29:34 +0300

    Define "SetProgress" method that set service progress fields based on AsyncOperation object

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/SceneSwitcherServiceExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/SceneSwitcherServiceExtensions.cs
@@ -1,7 +1,18 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Extensions
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions
 {
     public static class SceneSwitcherServiceExtensions
     {
         private const float Progress01Progress0100Multiplier = 100f;
+
+        public static void SetProgress(this SceneSwitcherService service, AsyncOperation operation)
+        {
+            if (operation is null) return;
+
+            service.Progress01 = operation.progress;
+            service.Progress0100 = service.Progress01 * Progress01Progress0100Multiplier;
+        }
     }
 }
