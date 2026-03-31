51a96ae4a0ba88fe3c6dcf0013cf072ffe893ac3
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 22:03:27 +0300

    Add "_service" field of type SceneSwitcherService and initialize it at constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Escort/SceneLoadingEscort.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Escort/SceneLoadingEscort.cs
@@ -8,10 +8,12 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Escort
     public class SceneLoadingEscort : ISceneLoadingEscort
     {
         private readonly CancellationToken _ct;
+        private readonly SceneSwitcherService _service;
 
-        public SceneLoadingEscort(CancellationToken ct)
+        public SceneLoadingEscort(CancellationToken ct, SceneSwitcherService service)
         {
             _ct = ct;
+            _service = service;
         }
 
         public async UniTask EscortLoading(AsyncOperation operation)
