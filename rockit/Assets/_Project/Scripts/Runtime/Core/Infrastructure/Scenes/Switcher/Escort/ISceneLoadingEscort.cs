6a7f2e8925d224fa7e35d0a1e9478373628a7536
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:53:13 +0300

    Fix Escort items names

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Escort/ISceneLoadingEscort.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Escort/ISceneLoadingEscort.cs
@@ -3,7 +3,7 @@ using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Escort
 {
-    public interface ISceneSwitcherLoadingEscort
+    public interface ISceneLoadingEscort
     {
         UniTask EscortLoading(AsyncOperation operation, SceneSwitcherService service);
     }
