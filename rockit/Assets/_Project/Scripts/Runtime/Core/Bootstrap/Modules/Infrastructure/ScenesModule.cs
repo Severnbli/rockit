71bca5657c069dc23ca8622abb57cee35f332704
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:01:07 +0300

    Create ScenesModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/ScenesModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
+{
+    public sealed class ScenesModule : BaseModule<ScenesModule>
+    {
+        public ScenesModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
