e626496e2e67d5c468c2a7cb36a38b4c1567c5b8
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 18:15:05 +0300

    Add "_config" field of type SceneSwitcherConfig and initialize its at constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/SceneSwitcher.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/SceneSwitcher.cs
@@ -7,11 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher
     public class SceneSwitcher : ISceneSwitcher
     {
         private CancellationToken _ct;
+        private SceneSwitcherConfig _config;
         private AsyncOperation _loadingOperation;
 
-        public SceneSwitcher(CancellationToken ct)
+        public SceneSwitcher(CancellationToken ct, SceneSwitcherConfig config)
         {
             _ct = ct;
+            _config = config;
         }
 
         public async UniTask SwitchScene(string sceneName)
