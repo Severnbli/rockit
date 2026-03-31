68fb39a56ffed33b09819a8581542de6332d0d5d
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 19:30:00 +0300

    Add "GetFuncSpentTime" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/TimeServiceExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/TimeServiceExtensions.cs
@@ -1,7 +1,16 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Extensions
+﻿using System;
+using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions
 {
     public static class TimeServiceExtensions
     {
-        
+        public static async UniTask<float> GetFuncSpentTime(this TimeService timeService, Func<UniTask> func)
+        {
+            var startTime = timeService.UnscaledTime;
+            await func();
+            return timeService.UnscaledTime - startTime;
+        }
     }
 }
