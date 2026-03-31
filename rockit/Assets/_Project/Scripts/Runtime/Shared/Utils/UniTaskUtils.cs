8b1f75668b0ddc6a0ba2ddaa297a820f4509a662
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 19:38:04 +0300

    Add "WaitForRemainingTime" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/UniTaskUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/UniTaskUtils.cs
@@ -1,7 +1,15 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Utils
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Shared.Utils
 {
     public static class UniTaskUtils
     {
-        
+        public static async UniTask WaitForRemainingTime(float spentTime, float requiredTime, CancellationToken ct = default)
+        {
+            var remainingTime = requiredTime - spentTime;
+            if (remainingTime <= 0f) return;
+            await UniTask.WaitForSeconds(remainingTime, cancellationToken: ct);
+        }
     }
 }
