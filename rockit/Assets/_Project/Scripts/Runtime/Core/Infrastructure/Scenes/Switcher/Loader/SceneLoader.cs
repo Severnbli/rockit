45050c70cd7285cc784c393f983992c5d2f8d77a
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:40:16 +0300

    Change "LoadScene" method signature: add "loadingOperation" parameter

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
@@ -1,10 +1,11 @@
 ﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Loader
 {
     public class SceneLoader : ISceneLoader
     {
-        public async UniTask LoadScene(string sceneName)
+        public async UniTask LoadScene(string sceneName, AsyncOperation loadingOperation)
         {
             await UniTask.CompletedTask;
         }
