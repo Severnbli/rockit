9f8b8fb32c20b4f449217f0e639d8c9eae724955
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 18:05:30 +0300

    Create SceneSwitcherConfig that inherited from ScriptableObjectAutoInstaller

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/SceneSwitcherConfig.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Modules.Zenject;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher
+{
+    public class SceneSwitcherConfig : ScriptableObjectAutoInstaller<SceneSwitcherConfig>
+    {
+        
+    }
+}
