5532482431b0664b33f2837f14475638a577b396
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:54:48 +0300

    Make "_switcherService" field readonly

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
@@ -6,7 +6,7 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Loader
 {
     public class SceneLoader : ISceneLoader
     {
-        private SceneSwitcherService _switcherService;
+        private readonly SceneSwitcherService _switcherService;
         private readonly ISceneLoadingEscort _loadingEscort;
 
         public SceneLoader(SceneSwitcherService switcherService, ISceneLoadingEscort loadingEscort)
