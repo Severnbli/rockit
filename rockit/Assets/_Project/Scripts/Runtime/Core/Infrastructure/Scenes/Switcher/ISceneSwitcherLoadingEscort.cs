1ebde9a5a89d0103baf576341c771fff83cd757a
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:14:27 +0300

    Create ISceneSwitcherLoadingEscort with "EscortLoading" method

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/ISceneSwitcherLoadingEscort.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher
+{
+    public interface ISceneSwitcherLoadingEscort
+    {
+        UniTask EscortLoading(AsyncOperation operation, SceneSwitcherService service);
+    }
+}
