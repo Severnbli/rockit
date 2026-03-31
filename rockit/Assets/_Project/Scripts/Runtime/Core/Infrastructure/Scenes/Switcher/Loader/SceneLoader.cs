465ed8b53a2ce7da358da1f5a1ccdad9205c3c2c
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 22:11:03 +0300

    Add "_config" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
@@ -7,15 +7,17 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Loader
 {
     public class SceneLoader : ISceneLoader
     {
+        private readonly SceneLoaderConfig _config;
         private readonly SceneSwitcherService _switcherService;
         private readonly ISceneLoadingEscort _loadingEscort;
         private readonly TimeService _timeService;
 
-        public SceneLoader(SceneSwitcherService switcherService, ISceneLoadingEscort loadingEscort, TimeService timeService)
+        public SceneLoader(SceneSwitcherService switcherService, ISceneLoadingEscort loadingEscort, TimeService timeService, SceneLoaderConfig config)
         {
             _switcherService = switcherService;
             _loadingEscort = loadingEscort;
             _timeService = timeService;
+            _config = config;
         }
 
         public async UniTask<bool> TryLoadScene(string sceneName, AsyncOperation loadingOperation)
