c62fae098167a331ab6c1a9ea85c40fc628d3094
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 18:09:11 +0300

    Add "_simulateLoading" field with its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/SceneSwitcherConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/SceneSwitcherConfig.cs
@@ -5,8 +5,10 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher
 {
     public class SceneSwitcherConfig : ScriptableObjectAutoInstaller<SceneSwitcherConfig>
     {
+        [SerializeField] private bool _simulateLoading = false;
         [SerializeField] private float _simulationLoadingDuration = 2f;
         
+        public bool SimulateLoading => _simulateLoading;
         public float SimulationLoadingDuration => _simulationLoadingDuration;
     }
 }
