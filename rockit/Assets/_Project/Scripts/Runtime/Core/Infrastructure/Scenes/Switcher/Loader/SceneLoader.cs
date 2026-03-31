f55fd21c46b52e7fd768dce9d0a1ac55b7ebc801
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-31 21:51:27 +0300

    Add "_switcherService" field and initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Switcher/Loader/SceneLoader.cs
@@ -5,6 +5,13 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Loader
 {
     public class SceneLoader : ISceneLoader
     {
+        private SceneSwitcherService _switcherService;
+
+        public SceneLoader(SceneSwitcherService switcherService)
+        {
+            _switcherService = switcherService;
+        }
+
         public async UniTask<bool> TryLoadScene(string sceneName, AsyncOperation loadingOperation)
         {
             await UniTask.CompletedTask;
