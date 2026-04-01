3ffea8a6f14ca66d9ade89ed692e9fc1a0d38a11
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 00:14:16 +0300

    Define "OneTimeSetUp" method

--- rockit/Assets/_Project/Scripts/Tests/NonSystems/Scenes/Switcher/SceneSwitcherTests.cs
+++ rockit/Assets/_Project/Scripts/Tests/NonSystems/Scenes/Switcher/SceneSwitcherTests.cs
@@ -1,7 +1,25 @@
-﻿namespace _Project.Scripts.Tests.NonSystems.Scenes.Switcher
+﻿using System.Threading;
+using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher;
+using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Escort;
+using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Loader;
+using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using UnityEngine;
+
+namespace _Project.Scripts.Tests.NonSystems.Scenes.Switcher
 {
     public class SceneSwitcherTests
     {
-        
+        private void OneTimeSetUp()
+        {
+            var switcherService = new SceneSwitcherService();
+            var timeService = new TimeService();
+            var loaderConfig = ScriptableObject.CreateInstance<SceneLoaderConfig>();
+            
+            var cts = new CancellationTokenSource();
+            
+            var loadingEscort = new SceneLoadingEscort(cts.Token, switcherService);
+            var loader = new SceneLoader(switcherService, loadingEscort, timeService, loaderConfig, cts.Token);
+            var sceneSwitcher = new SceneSwitcher(switcherService, loader);
+        }
     }
 }
