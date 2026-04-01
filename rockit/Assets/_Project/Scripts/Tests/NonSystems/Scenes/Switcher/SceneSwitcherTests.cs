fa20c30e7d40a476056d9c56c9c03e93a1c8aab0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 00:17:57 +0300

    Add "OneTimeSetUp" attribute

--- rockit/Assets/_Project/Scripts/Tests/NonSystems/Scenes/Switcher/SceneSwitcherTests.cs
+++ rockit/Assets/_Project/Scripts/Tests/NonSystems/Scenes/Switcher/SceneSwitcherTests.cs
@@ -3,6 +3,7 @@ using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher;
 using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Escort;
 using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher.Loader;
 using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using NUnit.Framework;
 using UnityEngine;
 
 namespace _Project.Scripts.Tests.NonSystems.Scenes.Switcher
@@ -17,6 +18,7 @@ namespace _Project.Scripts.Tests.NonSystems.Scenes.Switcher
         private ISceneLoader _loader;
         private ISceneSwitcher _switcher;
         
+        [OneTimeSetUp]
         private void OneTimeSetUp()
         {
             _switcherService = new SceneSwitcherService();
