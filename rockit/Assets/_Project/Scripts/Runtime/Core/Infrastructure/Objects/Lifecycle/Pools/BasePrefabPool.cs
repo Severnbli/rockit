eb3217d92891e1a400ed5419e0df9120f3dbf5a3
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 23:06:32 +0300

    Create BasePrefabPool that inherited from BasePrefabFactory and implements IPrefabPool

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/BasePrefabPool.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools
+{
+    public abstract class BasePrefabPool<T> : BasePrefabFactory<T>, IPrefabPool<T> where T : Component
+    {
+        public T Spawn(Transform at = null)
+        {
+            return Create(at);
+        }
+
+        public void Despawn(T instance)
+        {
+            ;
+        }
+    }
+}
