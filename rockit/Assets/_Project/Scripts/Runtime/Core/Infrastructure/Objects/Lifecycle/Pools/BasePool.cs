747a49eebde21ccff5fcdd7df0d8e801bb4ebcb4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 22:54:19 +0300

    Create generic BasePool that inherited from BaseFactory and implements IPool

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/BasePool.cs
@@ -0,0 +1,17 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools
+{
+    public class BasePool<T> : BaseFactory<T>, IPool<T> where T : new()
+    {
+        public T Spawn()
+        {
+            return Create();
+        }
+
+        public void Despawn(T instance)
+        {
+            ;
+        }
+    }
+}
