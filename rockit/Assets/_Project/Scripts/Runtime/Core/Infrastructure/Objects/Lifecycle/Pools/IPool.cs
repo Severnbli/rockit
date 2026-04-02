57c6856162fadca8316dc9308efd01c55761fa7d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 22:50:09 +0300

    Create generic IPool with generic "Spawn" and "Despawn" methods

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/IPool.cs
@@ -0,0 +1,8 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools
+{
+    public interface IPool<T> where T : new()
+    {
+        T Spawn();
+        void Despawn(T instance);
+    }
+}
