92ac9e2c2109f219279f63c159c2fbcba749522c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 22:45:02 +0300

    Split factories into 2 instances: base and prefab factory

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Factories/BasePrefabFactory.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Factories/BasePrefabFactory.cs
@@ -3,16 +3,28 @@ using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories
 {
-    public class BasePrefabFactory<T> : BaseFactory<T> where T: Component, new ()
+    public class BasePrefabFactory<T> : IPrefabFactory<T> where T: Component
     {
-        protected override T CreateInstance()
+        public T Create(Transform at = null)
         {
-            var gameObject = GetPrefab();
+            PreCreate();
+            var instance = CreateInstance(at);
+            PostCreate(instance);
+            return instance;
+        }
+        
+        protected virtual void PreCreate() {}
+
+        protected virtual T CreateInstance(Transform at = null)
+        {
+            var gameObject = Object.Instantiate(GetPrefab(), at);
             gameObject.TryGet(out T component);
             return component;
         }
-
-        public virtual GameObject GetPrefab()
+        
+        protected virtual void PostCreate(T instance) {}
+        
+        protected virtual GameObject GetPrefab()
         {
             return new GameObject();
         }
