8addc39571fe34809aca0972200773091241a0e3
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 23:09:30 +0300

    Create "PreDespawn", "DespawnInstance" and "PostDespawn" methods to manage despawn lifecycle

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/BasePrefabPool.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/BasePrefabPool.cs
@@ -24,7 +24,15 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools
 
         public void Despawn(T instance)
         {
-            ;
+            PreDespawn(instance);
+            DespawnInstance(instance);
+            PostDespawn(instance);
         }
+        
+        protected virtual void PreDespawn(T instance) {}
+        
+        protected virtual void DespawnInstance(T instance) {}
+        
+        protected virtual void PostDespawn(T instance) {}
     }
 }
