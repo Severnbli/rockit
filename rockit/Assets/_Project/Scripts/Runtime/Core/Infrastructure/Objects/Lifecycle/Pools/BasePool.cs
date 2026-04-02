d8f0b8f80af645bb04a8049339c51cc386360d5f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 22:55:35 +0300

    Add "Instance" field

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/BasePool.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/BasePool.cs
@@ -1,9 +1,12 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories;
+﻿using System.Collections.Generic;
+using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools
 {
     public class BasePool<T> : BaseFactory<T>, IPool<T> where T : new()
     {
+        protected readonly HashSet<T> Instances = new ();
+        
         public T Spawn()
         {
             return Create();
