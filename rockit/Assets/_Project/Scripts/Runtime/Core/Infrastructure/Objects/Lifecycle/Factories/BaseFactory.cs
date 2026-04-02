a6aa578642638bff898492ad7cd157c004148069
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 21:48:29 +0300

    Create BaseFactory that inherit IFactory

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Factories/BaseFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories
+{
+    public class BaseFactory<T>: IFactory<T>
+    {
+        public T Create()
+        {
+            return default;
+        }
+    }
+}
