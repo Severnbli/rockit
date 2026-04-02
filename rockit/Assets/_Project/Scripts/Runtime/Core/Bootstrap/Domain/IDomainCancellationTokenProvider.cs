85dc6d91943bda1dc2927288c8718f138748a046
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 20:47:23 +0300

    Direct bind domain's cancellation token instead of using IDomainCancellationTokenProvider

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/IDomainCancellationTokenProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading;
-
-namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
-{
-    public interface IDomainCancellationTokenProvider
-    {
-        CancellationToken GetCancellationToken();
-    }
-}
