04b8be40bf531f90dd89f28e3e9786a7c0fe5a58
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 22:18:27 +0300

    Add "new ()" limitation to generic type

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Factories/BaseFactory.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Factories/BaseFactory.cs
@@ -1,6 +1,6 @@
 ﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories
 {
-    public class BaseFactory<T>: IFactory<T>
+    public class BaseFactory<T>: IFactory<T> where T : new ()
     {
         public T Create()
         {
--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Factories/IFactory.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Factories/IFactory.cs
@@ -1,6 +1,6 @@
 ﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories
 {
-    public interface IFactory<out T>
+    public interface IFactory<out T> where T : new ()
     {
         T Create();
     }
