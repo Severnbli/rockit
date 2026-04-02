76d942ef1139796eb6755b0ba214042c3b12a477
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-02 21:59:54 +0300

    Add limitation for template type: "where T : MonoBehaviour"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Factories/BasePrefabFactory.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Factories/BasePrefabFactory.cs
@@ -1,6 +1,8 @@
-﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Factories
 {
-    public class BasePrefabFactory<T> : BaseFactory<T>
+    public class BasePrefabFactory<T> : BaseFactory<T> where T : MonoBehaviour
     {
         
     }
