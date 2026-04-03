435fbee17299642365ab176bc0f8aa07fd56ea5d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 11:13:05 +0300

    Add "PostDespawn" overriding that clear instance

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/BaseCollectionPool.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/BaseCollectionPool.cs
@@ -4,6 +4,11 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools
 {
     public class BaseCollectionPool<T, K> : BasePool<T> where T : ICollection<K>, new()
     {
-        
+        protected override void PostDespawn(T instance)
+        {
+            base.PostDespawn(instance);
+            
+            instance.Clear();
+        }
     }
 }
