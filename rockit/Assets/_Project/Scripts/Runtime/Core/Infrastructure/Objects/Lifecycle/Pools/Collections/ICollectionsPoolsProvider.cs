69f5408e42ed37e9b83130ba4a1cd7c9b1f06981
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 11:35:32 +0300

    Add generic "GetCollectionPool" method

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/ICollectionsPoolsProvider.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/ICollectionsPoolsProvider.cs
@@ -1,7 +1,10 @@
-﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.Collections
+﻿using System.Collections.Generic;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.Collections
 {
     public interface ICollectionsPoolsProvider
     {
-        
+        BaseCollectionPool<TCollection, TItem> GetCollectionPool<TCollection, TItem>()
+            where TCollection : ICollection<TItem>, new();
     }
 }
