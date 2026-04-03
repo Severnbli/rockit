85f0b7b6463ec96b431204cb8aa984de410324e9
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 18:50:10 +0300

    Add "Instances" dictionary field

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Domain/ObjectDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Domain/ObjectDomain.cs
@@ -1,7 +1,12 @@
-﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Domain
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Domain
 {
     public class ObjectDomain : IObjectDomain
     {
+        protected readonly Dictionary<Type, object> Instances = new ();
+        
         public TValue Get<TValue>()
         {
             return default;
