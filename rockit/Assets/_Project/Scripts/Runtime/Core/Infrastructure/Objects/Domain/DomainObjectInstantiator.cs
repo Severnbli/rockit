cea6a6d0bf2267a79ac191848fa8c2b558a2bf4b
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 18:49:20 +0300

    Create DomainObjectInstantiator that implements IDomainObjectInstantiator

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Domain/DomainObjectInstantiator.cs
@@ -0,0 +1,10 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Domain
+{
+    public class DomainObjectInstantiator : IDomainObjectInstantiator
+    {
+        public TValue Instantiate<TValue>()
+        {
+            return default;
+        }
+    }
+}
