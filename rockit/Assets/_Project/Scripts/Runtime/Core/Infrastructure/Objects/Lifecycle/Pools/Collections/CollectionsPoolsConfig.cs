b6d440f9fe0ebead74e90d1490f462faf0831e21
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 11:16:44 +0300

    Add "_initSize" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/CollectionsPoolsConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/CollectionsPoolsConfig.cs
@@ -1,9 +1,12 @@
 ﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.Collections
 {
     public class CollectionsPoolsConfig : ScriptableObjectAutoInstaller<CollectionsPoolsConfig>
     {
+        [SerializeField] private int _initSize;
         
+        public int InitSize => _initSize;
     }
 }
