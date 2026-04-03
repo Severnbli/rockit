42bc67830934bd1ba4b2bdfad3b36fbb8f981d36
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 11:36:29 +0300

    Create CollectionsPoolsProvider that implements ICollectionsPoolsProvider

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/CollectionsPoolsProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.Collections
+{
+    public class CollectionsPoolsProvider : ICollectionsPoolsProvider
+    {
+        public BaseCollectionPool<TCollection, TItem> GetCollectionPool<TCollection, TItem>() where TCollection : ICollection<TItem>, new()
+        {
+            return null;
+        }
+    }
+}
