1962432ba190d79f4762a6a27afe033e1e16928e
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 18:50:54 +0300

    Add "Instantiator" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Domain/ObjectDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Domain/ObjectDomain.cs
@@ -6,7 +6,13 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Domain
     public class ObjectDomain : IObjectDomain
     {
         protected readonly Dictionary<Type, object> Instances = new ();
-        
+        protected readonly IDomainObjectInstantiator Instantiator;
+
+        public ObjectDomain(IDomainObjectInstantiator instantiator)
+        {
+            Instantiator = instantiator;
+        }
+
         public TValue Get<TValue>()
         {
             return default;
