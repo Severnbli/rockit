30bfd88efaa80fbdaa45dcd751bfbe8171fc0912
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 18:44:36 +0300

    Create ObjectDomain that implements IObjectDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Domain/ObjectDomain.cs
@@ -0,0 +1,10 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Domain
+{
+    public class ObjectDomain : IObjectDomain
+    {
+        public TValue Get<TValue>()
+        {
+            return default;
+        }
+    }
+}
