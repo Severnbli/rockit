ce85872a154fd1526e992ad246a3f5d92018fda4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 14:56:44 +0300

    Update lining of method "GetCollectionPool" signature

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/CollectionsPoolsProvider.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/CollectionsPoolsProvider.cs
@@ -6,14 +6,15 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.C
     public class CollectionsPoolsProvider : ICollectionsPoolsProvider
     {
         protected readonly Dictionary<Type, object> Pools = new();
-        
-        public BaseCollectionPool<TCollection, TItem> GetCollectionPool<TCollection, TItem>() where TCollection : ICollection<TItem>, new()
         protected readonly CollectionsPoolsConfig Config;
 
         public CollectionsPoolsProvider(CollectionsPoolsConfig config)
         {
             Config = config;
         }
+
+        public BaseCollectionPool<TCollection, TItem> GetCollectionPool<TCollection, TItem>()
+            where TCollection : ICollection<TItem>, new()
         {
             return null;
         }
