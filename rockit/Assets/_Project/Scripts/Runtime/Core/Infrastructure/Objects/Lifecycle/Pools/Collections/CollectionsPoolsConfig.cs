5f95bfc8ab407f8dfa7dde4d0e779f14965dd1bc
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 11:16:00 +0300

    Create CollectionsPoolsConfig that inherited from ScriptableObjectAutoInstaller

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/CollectionsPoolsConfig.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Modules.Zenject;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.Collections
+{
+    public class CollectionsPoolsConfig : ScriptableObjectAutoInstaller<CollectionsPoolsConfig>
+    {
+        
+    }
+}
