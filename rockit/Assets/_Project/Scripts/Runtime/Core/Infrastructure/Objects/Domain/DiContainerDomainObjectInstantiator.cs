5846875e61371cbbfcd6376ba1131710cf3eba21
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 18:54:31 +0300

    Add "Container" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Domain/DiContainerDomainObjectInstantiator.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Domain/DiContainerDomainObjectInstantiator.cs
@@ -1,7 +1,16 @@
-﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Domain
+﻿using Zenject;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Domain
 {
     public class DiContainerDomainObjectInstantiator : IDomainObjectInstantiator
     {
+        protected readonly DiContainer Container;
+
+        public DiContainerDomainObjectInstantiator(DiContainer container)
+        {
+            Container = container;
+        }
+
         public TValue Instantiate<TValue>()
         {
             return default;
