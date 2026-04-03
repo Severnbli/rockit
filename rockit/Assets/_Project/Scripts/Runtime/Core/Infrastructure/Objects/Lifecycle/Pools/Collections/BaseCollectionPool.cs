9b3ae9febf94267f66c2baf951157381676738a7
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 19:11:18 +0300

    Remove "ICollection<T>" type limitation

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/BaseCollectionPool.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/BaseCollectionPool.cs
@@ -1,21 +1,12 @@
-﻿using System.Collections.Generic;
-
-namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.Collections
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.Collections
 {
-    public class BaseCollectionPool<TCollection, TItem> : BasePool<TCollection> where TCollection : ICollection<TItem>, new()
+    public class BaseCollectionPool<TCollection> : BasePool<TCollection> where TCollection : new()
     {
         protected readonly CollectionsPoolsConfig Config;
 
         public BaseCollectionPool(CollectionsPoolsConfig config)
         {
             Config = config;
-        }
-
-        protected override void PostDespawn(TCollection instance)
-        {
-            base.PostDespawn(instance);
-            
-            instance.Clear();
         }
     }
 }
