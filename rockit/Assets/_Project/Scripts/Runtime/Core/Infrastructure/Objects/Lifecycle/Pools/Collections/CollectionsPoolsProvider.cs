ccd8d5d6d5522b9c17b88371ea9b26e2e8ccb07e
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 11:39:51 +0300

    Add "Pools" dictionary field

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/CollectionsPoolsProvider.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/Collections/CollectionsPoolsProvider.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.Collections
 {
     public class CollectionsPoolsProvider : ICollectionsPoolsProvider
     {
+        protected readonly Dictionary<Type, object> Pools = new();
+        
         public BaseCollectionPool<TCollection, TItem> GetCollectionPool<TCollection, TItem>() where TCollection : ICollection<TItem>, new()
         {
             return null;
