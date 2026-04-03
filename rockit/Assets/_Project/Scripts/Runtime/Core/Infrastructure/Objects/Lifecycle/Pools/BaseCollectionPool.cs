2726264d7cdd7478c40757054728981c55774fc3
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-03 11:12:06 +0300

    Create BaseCollectionPool that inherited from BasePool and implements ICollection

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/Pools/BaseCollectionPool.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools
+{
+    public class BaseCollectionPool<T, K> : BasePool<T> where T : ICollection<K>, new()
+    {
+        
+    }
+}
