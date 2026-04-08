279575547b75cb495ad33909a7a666a1485a540a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 12:26:41 +0300

    Create SendDisablePlayerInputRequestOnDestroySystem that implements IProtoDestroySystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/SendDisablePlayerInputRequestOnDestroySystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Input.Systems
+{
+    public sealed class SendDisablePlayerInputRequestOnDestroySystem : IProtoDestroySystem
+    {
+        public void Destroy()
+        {
+            
+        }
+    }
+}
