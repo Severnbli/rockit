47f4cbbf28f04f50f908aa93724bea2c442c5d76
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 12:27:33 +0300

    Add "_domainAspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/SendDisablePlayerInputRequestOnDestroySystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/SendDisablePlayerInputRequestOnDestroySystem.cs
@@ -1,9 +1,13 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Input.Systems
 {
     public sealed class SendDisablePlayerInputRequestOnDestroySystem : IProtoDestroySystem
     {
+        [DI] private readonly DomainAspect _domainAspect;
+        
         public void Destroy()
         {
             
