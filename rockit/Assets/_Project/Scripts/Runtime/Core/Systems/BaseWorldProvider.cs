1fe7e70d8a86177c3185cd81c0bc738867c59b69
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:33:16 +0300

    Create BaseWorldProvider that implements IWorldProvider

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Systems/BaseWorldProvider.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Systems
+{
+    public class BaseWorldProvider : IWorldProvider
+    {
+        public ProtoWorld GetWorld()
+        {
+            return null;
+        }
+    }
+}
