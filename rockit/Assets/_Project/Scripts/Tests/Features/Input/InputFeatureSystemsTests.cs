fa9b8fac47023dea67bd12aa21141a937388c179
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 19:50:40 +0300

    Add "AssertRequestsEmpty" method

--- rockit/Assets/_Project/Scripts/Tests/Features/Input/InputFeatureSystemsTests.cs
+++ rockit/Assets/_Project/Scripts/Tests/Features/Input/InputFeatureSystemsTests.cs
@@ -1,7 +1,16 @@
-﻿namespace _Project.Scripts.Tests.Features.Input
+﻿using NUnit.Framework;
+
+namespace _Project.Scripts.Tests.Features.Input
 {
     public class InputFeatureSystemsTests : BaseSystemsTests
     {
-        
+
+        private void AssertRequestsEmpty()
+        {
+            Assert.True(RequestsWorldAspect.InputAspect.EnablePlayerInputRequests.IsEmptySlow());
+            Assert.True(RequestsWorldAspect.InputAspect.DisablePlayerInputRequests.IsEmptySlow());
+            Assert.True(RequestsWorldAspect.InputAspect.EnablePlatformsInputRequests.IsEmptySlow());
+            Assert.True(RequestsWorldAspect.InputAspect.DisablePlatformsInputRequests.IsEmptySlow());
+        }
     }
 }
