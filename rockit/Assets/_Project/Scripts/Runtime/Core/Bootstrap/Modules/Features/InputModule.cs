2714efddada1ad493f168d1663d3759371a8d8ae
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:32:40 +0300

    Create InputModule that inheriteed from BaseModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/InputModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features
+{
+    public sealed class InputModule : BaseModule<InputModule>
+    {
+        public InputModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
