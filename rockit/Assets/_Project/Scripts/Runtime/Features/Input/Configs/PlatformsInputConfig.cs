5b33a811b6063fec41dc75830bcd4460fcc1f8bf
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 11:51:35 +0300

    Add "_position" field and it property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Configs/PlatformsInputConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Configs/PlatformsInputConfig.cs
@@ -1,9 +1,13 @@
 ﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Project.Scripts.Runtime.Features.Input.Configs
 {
     public class PlatformsInputConfig : ScriptableObjectAutoInstaller<PlatformsInputConfig>
     {
+        [SerializeField] private InputActionReference _position;
         
+        public InputActionReference Position => _position;
     }
 }
