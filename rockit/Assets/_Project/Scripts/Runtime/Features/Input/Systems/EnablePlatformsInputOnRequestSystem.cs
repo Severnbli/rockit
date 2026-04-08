9f0b6b9cec475d776a6287e7bcc87f17ad154e5c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:58:01 +0300

    Add "_inputAspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/EnablePlatformsInputOnRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/EnablePlatformsInputOnRequestSystem.cs
@@ -1,9 +1,12 @@
 ﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Input.Systems
 {
     public sealed class EnablePlatformsInputOnRequestSystem : IProtoRunSystem
     {
+        [DI] private readonly InputAspect _inputAspect;
+        
         public void Run()
         {
             
