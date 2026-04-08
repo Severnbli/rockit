35ddf1618da163603ca9b57b020afb27836b91d2
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:37:19 +0300

    Inherit RequestsWorldProvider from BaseWorldProvider

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorldProvider.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorldProvider.cs
@@ -1,19 +1,9 @@
 ﻿using _Project.Scripts.Runtime.Core.Systems;
-using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
 {
-    public class RequestsWorldProvider : IWorldProvider
+    public class RequestsWorldProvider : BaseWorldProvider
     {
-        private readonly RequestsAspect _aspect;
-        private readonly ProtoWorld _world;
-
-        public RequestsWorldProvider(RequestsAspect aspect)
-        {
-            _aspect = aspect;
-            _world = new ProtoWorld(aspect);
-        }
-
-        public ProtoWorld GetWorld() => _world;
+        public RequestsWorldProvider(RequestsAspect aspect) : base(aspect) {}
     }
 }
