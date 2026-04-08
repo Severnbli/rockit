726acdf40c6c1ad0e0022d9fecf20eed5d0dd5ac
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:27:06 +0300

    Add "_aspect" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorldInstaller.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorldInstaller.cs
@@ -1,9 +1,17 @@
 ﻿using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
 {
     public class RequestsWorldInstaller : IWorldInstaller
     {
+        private readonly RequestsWorldAspect _aspect;
+
+        public RequestsWorldInstaller(RequestsWorldAspect aspect)
+        {
+            _aspect = aspect;
+        }
+
         public void Install(EcsSystems systems)
         {
             
