d53431a4dd03adaff1c081caa73af69ad3abd475
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 17:59:24 +0300

    Create RequestsWorld that inherited from ProtoWorld

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorld.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
+{
+    public class RequestsWorld : ProtoWorld
+    {
+        public RequestsWorld() : base(new DomainAspect())
+        {
+        }
+    }
+}
