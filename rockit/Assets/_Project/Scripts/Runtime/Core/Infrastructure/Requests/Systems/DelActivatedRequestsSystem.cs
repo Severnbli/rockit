085c03043d08260bd70a715b765c4ff8823322aa
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 19:25:29 +0300

    Move DIRequestsAttribute into World folder

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/DelActivatedRequestsSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/DelActivatedRequestsSystem.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Core.Systems;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.Systems
