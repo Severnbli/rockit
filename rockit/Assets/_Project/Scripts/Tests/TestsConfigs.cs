0f1af6a3b0417920235c3d651d563759157dfc46
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 19:59:43 +0300

    Add "_platformsInputConfig" field and its property getter

--- rockit/Assets/_Project/Scripts/Tests/TestsConfigs.cs
+++ rockit/Assets/_Project/Scripts/Tests/TestsConfigs.cs
@@ -6,7 +6,9 @@ namespace _Project.Scripts.Tests
     public class TestsConfigs : ScriptableObject
     {
         [SerializeField] private PlayerInputConfig _playerInputConfig;
+        [SerializeField] private PlatformsInputConfig _platformsInputConfig;
         
         public PlayerInputConfig PlayerInputConfig => _playerInputConfig;
+        public PlatformsInputConfig PlatformsInputConfig => _platformsInputConfig;
     }
 }
