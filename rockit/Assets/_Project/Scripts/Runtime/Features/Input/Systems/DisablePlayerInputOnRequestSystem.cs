a48e5372722e4c6bb965d1c5e3cc2327cf6a333a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:37:32 +0300

    Add "_inputAspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/DisablePlayerInputOnRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/DisablePlayerInputOnRequestSystem.cs
@@ -1,9 +1,12 @@
 ﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Input.Systems
 {
     public class DisablePlayerInputOnRequestSystem : IProtoRunSystem
     {
+        [DI] private readonly InputAspect _inputAspect;
+        
         public void Run()
         {
             
