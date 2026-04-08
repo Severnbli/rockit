417a1e0a9516ba2beb846b896cf6803b015fec77
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:43:35 +0300

    Add "EnableInput" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/PlayerInputUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/PlayerInputUtils.cs
@@ -1,7 +1,16 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Utils
+﻿using _Project.Scripts.Runtime.Features.Input.Configs;
+using _Project.Scripts.Runtime.Features.Input.Services;
+
+namespace _Project.Scripts.Runtime.Shared.Utils
 {
     public static class PlayerInputUtils
     {
-        
+        public static void EnableInput(PlayerInputService service, PlayerInputConfig config)
+        {
+            service.Enabled = true;
+            config.Walk.Enable();
+            config.Jump.Enable();
+            config.Dash.Enable();
+        }
     }
 }
