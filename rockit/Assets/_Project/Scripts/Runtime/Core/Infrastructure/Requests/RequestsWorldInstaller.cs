8670c4a3a958da9e0c82f22212c7c86d2ef7d5e0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:23:15 +0300

    Create RequestsWorldInstaller that implements IWorldInstaller

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorldInstaller.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
+{
+    public class RequestsWorldInstaller : IWorldInstaller
+    {
+        public void Install(EcsSystems systems)
+        {
+            
+        }
+    }
+}
