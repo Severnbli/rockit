140fe1fa8a4662083c79e60b51b93379ccaf6b61
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:32:00 +0300

    Add "_world" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorldProvider.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorldProvider.cs
@@ -6,10 +6,12 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
     public class RequestsWorldProvider : IWorldProvider
     {
         private readonly RequestsAspect _aspect;
+        private readonly ProtoWorld _world;
 
         public RequestsWorldProvider(RequestsAspect aspect)
         {
             _aspect = aspect;
+            _world = new ProtoWorld(aspect);
         }
 
         public ProtoWorld GetWorld()
