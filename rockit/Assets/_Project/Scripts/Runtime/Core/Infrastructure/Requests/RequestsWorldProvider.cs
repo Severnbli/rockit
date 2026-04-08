899ffa0727162cc92c57ce50a009430640c34513
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:19:08 +0300

    Create RequestsWorldProvider that implements IWorldProvider

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorldProvider.cs
@@ -0,0 +1,13 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
+{
+    public class RequestsWorldProvider : IWorldProvider
+    {
+        public ProtoWorld GetWorld()
+        {
+            return null;
+        }
+    }
+}
