9e4660ec5d2533e29761f4a933b78172f82d84ab
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 11:46:51 +0300

    Add "_walk" field and it property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Configs/PlayerInputConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Configs/PlayerInputConfig.cs
@@ -1,9 +1,13 @@
 ﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Project.Scripts.Runtime.Features.Input.Configs
 {
     public class PlayerInputConfig : ScriptableObjectAutoInstaller<PlayerInputConfig>
     {
+        [SerializeField] private InputActionReference _walk;
         
+        public InputActionReference Walk => _walk;
     }
 }
