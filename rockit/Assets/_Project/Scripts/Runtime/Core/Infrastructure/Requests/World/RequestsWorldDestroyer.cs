1a91c00dee66b5bfb948a506e1aa8517f629800a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:49:47 +0300

    Add "_worldProvider" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/World/RequestsWorldDestroyer.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/World/RequestsWorldDestroyer.cs
@@ -4,6 +4,13 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.World
 {
     public class RequestsWorldDestroyer : IDisposable
     {
+        private readonly RequestsWorldProvider _worldProvider;
+
+        public RequestsWorldDestroyer(RequestsWorldProvider worldProvider)
+        {
+            _worldProvider = worldProvider;
+        }
+
         public void Dispose()
         {
             
