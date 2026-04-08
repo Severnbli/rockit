f3e16cf66c2e9977fc36a17a13a7b1343103942f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 12:29:01 +0300

    Create SendEnablePlatformsInputRequestOnInitSystem that implements IProtoInitSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/SendEnablePlatformsInputRequestOnInitSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Input.Systems
+{
+    public sealed class SendEnablePlatformsInputRequestOnInitSystem : IProtoInitSystem
+    {
+        public void Init(IProtoSystems systems)
+        {
+            
+        }
+    }
+}
