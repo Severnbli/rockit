8d550b10d1e76deaa63e27d0a73d567a9981aea8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 11:51:52 +0300

    Add "_rotation" field and it property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Configs/PlatformsInputConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Configs/PlatformsInputConfig.cs
@@ -7,7 +7,9 @@ namespace _Project.Scripts.Runtime.Features.Input.Configs
     public class PlatformsInputConfig : ScriptableObjectAutoInstaller<PlatformsInputConfig>
     {
         [SerializeField] private InputActionReference _position;
+        [SerializeField] private InputActionReference _rotation;
         
         public InputActionReference Position => _position;
+        public InputActionReference Rotation => _rotation;
     }
 }
