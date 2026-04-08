0ad2310b20c89951aec67e69e5f42d5a58d0569f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 12:23:44 +0300

    Create SendEnablePlayerInputRequestOnInitSystem that implements IProtoInitSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/SendEnablePlayerInputRequestOnInitSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Input.Systems
+{
+    public sealed class SendEnablePlayerInputRequestOnInitSystem : IProtoInitSystem
+    {
+        public void Init(IProtoSystems systems)
+        {
+            
+        }
+    }
+}
