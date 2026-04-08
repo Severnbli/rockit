1f5af32d39bc701b14adac7e63ccbc9df4d16327
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 19:59:08 +0300

    Add "_playerInputConfig" field and its property getter

--- rockit/Assets/_Project/Scripts/Tests/TestsConfigs.cs
+++ rockit/Assets/_Project/Scripts/Tests/TestsConfigs.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using _Project.Scripts.Runtime.Features.Input.Configs;
+using UnityEngine;
 
 namespace _Project.Scripts.Tests
 {
     public class TestsConfigs : ScriptableObject
     {
+        [SerializeField] private PlayerInputConfig _playerInputConfig;
         
+        public PlayerInputConfig PlayerInputConfig => _playerInputConfig;
     }
 }
