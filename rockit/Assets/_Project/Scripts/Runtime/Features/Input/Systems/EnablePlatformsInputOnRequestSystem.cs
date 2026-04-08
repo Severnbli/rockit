98ef0cdff9c44f26f3f8fba911ddbf73718c87e5
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:58:50 +0300

    Add "_config" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/EnablePlatformsInputOnRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/EnablePlatformsInputOnRequestSystem.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Features.Input.Services;
+﻿using _Project.Scripts.Runtime.Features.Input.Configs;
+using _Project.Scripts.Runtime.Features.Input.Services;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -8,10 +9,12 @@ namespace _Project.Scripts.Runtime.Features.Input.Systems
     {
         [DI] private readonly InputAspect _inputAspect;
         private readonly PlatformsInputService _service;
+        private readonly PlatformsInputConfig _config;
 
-        public EnablePlatformsInputOnRequestSystem(PlatformsInputService service)
+        public EnablePlatformsInputOnRequestSystem(PlatformsInputService service, PlatformsInputConfig config)
         {
             _service = service;
+            _config = config;
         }
 
         public void Run()
