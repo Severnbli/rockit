7ab4a7cc38c3c2c0041248e9bb1d95b0cf55ed19
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 12:13:08 +0300

    Add "DisablePlayerInputRequests" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/InputAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/InputAspect.cs
@@ -13,5 +13,6 @@ namespace _Project.Scripts.Runtime.Features.Input
         public ProtoPool<EnablePlatformsInputRequest> EnablePlatformsInputRequestPool;
         public ProtoPool<DisablePlatformsInputRequest> DisablePlatformsInputRequestPool;
         public ProtoIt EnablePlayerInputRequests = new (It.Inc<RequestComponent, ActiveRequestTag, EnablePlayerInputRequest>());
+        public ProtoIt DisablePlayerInputRequests = new (It.Inc<RequestComponent, ActiveRequestTag, DisablePlayerInputRequest>());
     }
 }
