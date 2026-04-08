7d7511136f5ad25c5456bddf3653ab41ae27d428
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 17:02:37 +0300

    Add "_service" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/DisablePlatformsInputOnRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/DisablePlatformsInputOnRequestSystem.cs
@@ -1,4 +1,5 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Features.Input.Services;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Input.Systems
@@ -6,7 +7,13 @@ namespace _Project.Scripts.Runtime.Features.Input.Systems
     public sealed class DisablePlatformsInputOnRequestSystem : IProtoRunSystem
     {
         [DI] private readonly InputAspect _inputAspect;
-        
+        private readonly PlatformsInputService _service;
+
+        public DisablePlatformsInputOnRequestSystem(PlatformsInputService service)
+        {
+            _service = service;
+        }
+
         public void Run()
         {
             
