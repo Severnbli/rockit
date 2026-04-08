c0e8473575404d0e044bd4dc9b68c1deb5ca9fbe
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:55:54 +0300

    Add "EnableInput" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Input/PlatformsInputUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Input/PlatformsInputUtils.cs
@@ -1,4 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Input.Configs;
+using _Project.Scripts.Runtime.Features.Input.Services;
 using _Project.Scripts.Runtime.Shared.Extensions;
 using Leopotam.EcsProto;
 
@@ -6,6 +8,14 @@ namespace _Project.Scripts.Runtime.Shared.Utils.Input
 {
     public static class PlatformsInputUtils
     {
+        public static void EnableInput(PlatformsInputService service, PlatformsInputConfig config)
+        {
+            service.Enabled = true;
+            config.Position.Enable();
+            config.Rotation.Enable();
+            config.Scale.Enable();
+        }
+        
         public static ProtoEntity CreateEnableRequest(DomainAspect domainAspect)
         {
             var entity = domainAspect.RequestsAspect.CreateRequest();
