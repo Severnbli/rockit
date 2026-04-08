1a37d35861095bf34e73d3bb974a1fb00a30d6dd
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 17:08:27 +0300

    Copy-paste SetUp and TearDown functionalities from InfrastructureSystemsTests

--- rockit/Assets/_Project/Scripts/Tests/SystemsTestsBase.cs
+++ rockit/Assets/_Project/Scripts/Tests/SystemsTestsBase.cs
@@ -1,7 +1,36 @@
-﻿namespace _Project.Scripts.Tests
+﻿using _Project.Scripts.Runtime.Core.Systems;
+using NUnit.Framework;
+
+namespace _Project.Scripts.Tests
 {
     public class SystemsTestsBase
     {
-        
+        protected ISystemsContainerProvider SystemsContainerProvider;
+        protected EcsSystems Systems;
+
+        [OneTimeSetUp]
+        public virtual void OneTimeSetUp()
+        {
+            SystemsContainerProvider = new SystemsContainerProvider();
+        }
+
+        [OneTimeTearDown]
+        public virtual void OneTimeTearDown()
+        {
+            SystemsContainerProvider = null;
+        }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            Systems = SystemsContainerProvider.GetSystemsContainer();
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            Systems?.Destroy();
+            Systems = null;
+        }
     }
 }
