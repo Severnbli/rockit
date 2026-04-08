134f7bb90b1a3fe4d47470ac3ddc8907ee84af12
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:58:32 +0300

    Add "_service" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/EnablePlatformsInputOnRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/EnablePlatformsInputOnRequestSystem.cs
@@ -1,4 +1,5 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Features.Input.Services;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Input.Systems
@@ -6,7 +7,13 @@ namespace _Project.Scripts.Runtime.Features.Input.Systems
     public sealed class EnablePlatformsInputOnRequestSystem : IProtoRunSystem
     {
         [DI] private readonly InputAspect _inputAspect;
-        
+        private readonly PlatformsInputService _service;
+
+        public EnablePlatformsInputOnRequestSystem(PlatformsInputService service)
+        {
+            _service = service;
+        }
+
         public void Run()
         {
             
