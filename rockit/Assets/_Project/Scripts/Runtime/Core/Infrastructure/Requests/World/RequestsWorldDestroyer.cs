ad5828a413192501069c1e164b913d961776c650
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:49:09 +0300

    Create RequestsWorldDestroyer that implements IDisposable

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/World/RequestsWorldDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.World
+{
+    public class RequestsWorldDestroyer : IDisposable
+    {
+        public void Dispose()
+        {
+            
+        }
+    }
+}
