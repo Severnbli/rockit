3234edb76f8c2ea1c8188bb1e7e452b9206973c5
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:14:20 +0300

    Move from "DI" attribute to "DIRequests"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/ActivateRequestsSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/ActivateRequestsSystem.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using _Project.Scripts.Runtime.Core.Systems;
+﻿using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
-using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.Systems
 {
     public class ActivateRequestsSystem : IProtoRunSystem, IProtoFixedRunSystem
     {
-        [DI] private RequestsAspect _requestsAspect;
+        [DIRequests] private RequestsAspect _requestsAspect;
         
         public void Run()
         {
--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/DelActivatedRequestsSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/Systems/DelActivatedRequestsSystem.cs
@@ -1,12 +1,11 @@
 ﻿using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
-using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests.Systems
 {
     public class DelActivatedRequestsSystem : IProtoRunSystem, IProtoFixedRunSystem
     {
-        [DI] private RequestsAspect _requestsAspect;
+        [DIRequests] private RequestsAspect _requestsAspect;
         
         public void Run()
         {
