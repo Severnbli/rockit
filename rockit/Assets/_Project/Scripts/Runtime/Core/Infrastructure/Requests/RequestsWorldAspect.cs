9542f4118acef5dba9d44b01317d9935f7eb9f20
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:25:45 +0300

    Create RequestsWorldAspect that inherited from DomainAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsWorldAspect.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
+{
+    public class RequestsWorldAspect : DomainAspect
+    {
+        
+    }
+}
