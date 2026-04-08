651129b48bf42bd860b464a1471409dae5a8bea9
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:33:32 +0300

    Bind PlayerInputService

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/InputModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/InputModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Input.Services;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features
     {
         public InputModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindServices()
+        {
+            base.BindServices();
+            
+            BindService<PlayerInputService>();
         }
     }
 }
