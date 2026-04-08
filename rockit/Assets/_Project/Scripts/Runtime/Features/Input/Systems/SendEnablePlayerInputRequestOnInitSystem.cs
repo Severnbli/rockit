8d2ec345638740d6396a2d1fadc7c4f572431a40
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 12:24:26 +0300

    Add "_domainAspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/SendEnablePlayerInputRequestOnInitSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/SendEnablePlayerInputRequestOnInitSystem.cs
@@ -1,9 +1,13 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Input.Systems
 {
     public sealed class SendEnablePlayerInputRequestOnInitSystem : IProtoInitSystem
     {
+        [DI] private DomainAspect _domainAspect;
+        
         public void Init(IProtoSystems systems)
         {
             
