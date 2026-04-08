ca0ccde4ba20aedcf545a4a8f61b94de801d496c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 12:32:32 +0300

    Create EnablePlayerInputOnRequestSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/EnablePlayerInputOnRequestSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Input.Systems
+{
+    public class EnablePlayerInputOnRequestSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
