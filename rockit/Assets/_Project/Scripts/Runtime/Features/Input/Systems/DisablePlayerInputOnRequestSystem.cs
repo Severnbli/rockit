135e4ce152217a1f763c0e22a58183d01a882ecb
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:37:02 +0300

    Create DisablePlayerInputOnRequestSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/DisablePlayerInputOnRequestSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Input.Systems
+{
+    public class DisablePlayerInputOnRequestSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
