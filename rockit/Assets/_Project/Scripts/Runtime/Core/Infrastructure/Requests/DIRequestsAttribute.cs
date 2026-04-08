466f61ef0a6688d0adaca0c266cef0addc45e6ce
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 18:05:11 +0300

    Create DIRequestsAttribute

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/DIRequestsAttribute.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
+{
+    public class DIRequestsAttribute : DIAttribute
+    {
+        public DIRequestsAttribute() : base(RequestsContracts.RequestsWorld) {}
+    }
+}
