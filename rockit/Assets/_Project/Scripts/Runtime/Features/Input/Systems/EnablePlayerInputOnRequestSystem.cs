1208f02f325f4f89352e6c93456e2cdc31ed7027
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 16:31:48 +0300

    Define Run method

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/EnablePlayerInputOnRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/EnablePlayerInputOnRequestSystem.cs
@@ -19,7 +19,12 @@ namespace _Project.Scripts.Runtime.Features.Input.Systems
 
         public void Run()
         {
-            
+            if (_service.Enabled || _inputAspect.EnablePlayerInputRequests.IsEmptySlow()) return;
+
+            _service.Enabled = true;
+            _config.Walk.Enable();
+            _config.Jump.Enable();
+            _config.Dash.Enable();
         }
     }
 }
