f345c53f475b54196b3f043e6790f99e7bb12fe9
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-08 12:19:44 +0300

    Add "CreateEnablePlayerInputRequest" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/InputUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/InputUtils.cs
@@ -1,7 +1,16 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Utils
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Shared.Extensions;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils
 {
     public static class InputUtils
     {
-        
+        public static ProtoEntity CreateEnablePlayerInputRequest(DomainAspect domainAspect)
+        {
+            var entity = domainAspect.RequestsAspect.CreateRequest();
+            domainAspect.InputAspect.EnablePlayerInputRequestPool.Add(entity);
+            return entity;
+        }
     }
 }
