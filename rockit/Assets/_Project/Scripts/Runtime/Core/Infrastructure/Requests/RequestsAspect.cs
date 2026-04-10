425fc12f434e767a40219571f8ccbab04b08ff42
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-10 16:01:30 +0300

    Add "CoreRequestsAspect" field

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
@@ -4,6 +4,6 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
 {
     public class RequestsAspect : ProtoAspectInject
     {
-        
+        public CoreRequestsAspect CoreRequestsAspect;
     }
 }
