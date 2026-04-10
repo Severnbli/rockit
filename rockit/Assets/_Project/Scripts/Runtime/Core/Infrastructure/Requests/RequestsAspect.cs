4531ccc127d80c4549cd7f3f6ff1aa2b24fca0ca
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-10 16:02:13 +0300

    Add "InputRequestsAspect" field

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Input;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
 {
     public class RequestsAspect : ProtoAspectInject
     {
         public CoreRequestsAspect CoreRequestsAspect;
+        public InputRequestsAspect InputRequestsAspect;
     }
 }
