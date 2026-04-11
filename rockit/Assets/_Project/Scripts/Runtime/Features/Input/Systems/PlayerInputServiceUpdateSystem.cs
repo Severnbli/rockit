185e108f44ac4fa24216d502fab59f81bf7b6740
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-11 15:04:12 +0300

    Create PlayerInputServiceUpdateSystem that inherited from IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/PlayerInputServiceUpdateSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Input.Systems
+{
+    public sealed class PlayerInputServiceUpdateSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
