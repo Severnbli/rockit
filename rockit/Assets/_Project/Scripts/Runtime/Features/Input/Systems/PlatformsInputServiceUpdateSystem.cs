162eaefc0c112dd847b0c5961ae769682592fdc8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-11 14:54:56 +0300

    Add "_config" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/PlatformsInputServiceUpdateSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/PlatformsInputServiceUpdateSystem.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Features.Input.Services;
+﻿using _Project.Scripts.Runtime.Features.Input.Configs;
+using _Project.Scripts.Runtime.Features.Input.Services;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Input.Systems
@@ -6,10 +7,12 @@ namespace _Project.Scripts.Runtime.Features.Input.Systems
     public sealed class PlatformsInputServiceUpdateSystem : IProtoRunSystem
     {
         private readonly PlatformsInputService _service;
+        private readonly PlatformsInputConfig _config;
 
-        public PlatformsInputServiceUpdateSystem(PlatformsInputService service)
+        public PlatformsInputServiceUpdateSystem(PlatformsInputService service, PlatformsInputConfig config)
         {
             _service = service;
+            _config = config;
         }
 
         public void Run()
