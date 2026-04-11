1f0c33b193f628dfeab3b87443212b904a978948
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-11 14:58:07 +0300

    Add "TryResetOnDisabled" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PlatformsInputServiceExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PlatformsInputServiceExtensions.cs
@@ -1,7 +1,17 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Extensions
+﻿using _Project.Scripts.Runtime.Features.Input.Services;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions
 {
     public static class PlatformsInputServiceExtensions
     {
-        
+        public static bool TryResetOnDisabled(this PlatformsInputService service)
+        {
+            if (service.Enabled) return false;
+            
+            service.PositionPerformed = false;
+            service.RotationPerformed = false;
+            service.ScalePerformed = false;
+            return true;
+        }
     }
 }
