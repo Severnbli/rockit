3e894d3c0c8f3b5f874247f63569efe57ef660c0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-11 15:08:49 +0300

    Add "_service" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/PlayerInputServiceUpdateSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/PlayerInputServiceUpdateSystem.cs
@@ -1,9 +1,17 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Features.Input.Services;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Input.Systems
 {
     public sealed class PlayerInputServiceUpdateSystem : IProtoRunSystem
     {
+        private readonly PlayerInputService _service;
+
+        public PlayerInputServiceUpdateSystem(PlayerInputService service)
+        {
+            _service = service;
+        }
+
         public void Run()
         {
             
