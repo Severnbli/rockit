bba68168492d423807613dded69a015e85bdee1b
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-11 15:08:10 +0300

    Add "TryResetOnDisabled" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PlayerInputServiceExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PlayerInputServiceExtensions.cs
@@ -1,7 +1,18 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Extensions
+﻿using _Project.Scripts.Runtime.Features.Input.Services;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions
 {
     public static class PlayerInputServiceExtensions
     {
-        
+        public static bool TryResetOnDisabled(this PlayerInputService service)
+        {
+            if (service.Enabled) return false;
+            
+            service.WalkTriggered = false;
+            service.Walk = 0;
+            service.JumpTriggered = false;
+            service.DashTriggered = false;
+            return true;
+        }
     }
 }
