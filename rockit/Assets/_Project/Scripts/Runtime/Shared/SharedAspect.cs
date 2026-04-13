3ef9a5c930fa4f4fb14c48bf8f32bc878c068af7
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-14 02:38:22 +0300

    Add "PlayerTagPool" field

--- rockit/Assets/_Project/Scripts/Runtime/Shared/SharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/SharedAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Shared.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Shared
 {
     public class SharedAspect : ProtoAspectInject
     {
-        
+        public ProtoPool<PlayerTag> PlayerTagPool;
     }
 }
