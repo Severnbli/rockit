41fe1f3c1eb0b2fbda20dd4ad9ba6d4c9f8837b8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-14 02:54:07 +0300

    Add "CreateWalkRequest" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/MovingUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/MovingUtils.cs
@@ -1,7 +1,16 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Utils
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils
 {
     public static class MovingUtils
     {
-        
+        public static ProtoEntity CreateWalkRequest(RequestsAspect aspect)
+        {
+            var entity = aspect.CreateRequest();
+            aspect.MovingRequestsAspect.WalkRequestPool.Add(entity);
+            return entity;
+        }
     }
 }
