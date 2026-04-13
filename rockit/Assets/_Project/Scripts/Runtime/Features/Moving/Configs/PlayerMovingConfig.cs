39411be4244299054bedfe20e383a66c168b6124
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-14 02:14:30 +0300

    Add "_jumpPower" field with default "4f" value and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/PlayerMovingConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/PlayerMovingConfig.cs
@@ -6,7 +6,9 @@ namespace _Project.Scripts.Runtime.Features.Moving.Configs
     public class PlayerMovingConfig : ScriptableObjectAutoInstaller<PlayerMovingConfig>
     {
         [SerializeField] private float _walkSpeed;
+        [SerializeField] private float _jumpPower = 4f;
         
         public float WalkSpeed => _walkSpeed;
+        public float JumpPower => _jumpPower;
     }
 }
