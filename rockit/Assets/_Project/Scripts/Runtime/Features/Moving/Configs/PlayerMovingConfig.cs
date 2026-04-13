50cca7e9ec52d9fe237d991eeb19de0f9db6356e
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-14 02:12:29 +0300

    Add "_walkSpeed" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/PlayerMovingConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/PlayerMovingConfig.cs
@@ -1,9 +1,12 @@
 ﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Configs
 {
     public class PlayerMovingConfig : ScriptableObjectAutoInstaller<PlayerMovingConfig>
     {
+        [SerializeField] private float _walkSpeed;
         
+        public float WalkSpeed => _walkSpeed;
     }
 }
