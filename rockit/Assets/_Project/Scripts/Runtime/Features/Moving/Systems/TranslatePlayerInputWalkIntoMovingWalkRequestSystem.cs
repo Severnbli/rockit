8c7e9ea6cd1a7fc1ad19eb1c002a3321a4d50454
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-14 03:08:58 +0300

    Create TranslatePlayerInputWalkIntoMovingWalkRequestSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputWalkIntoMovingWalkRequestSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Moving.Systems
+{
+    public class TranslatePlayerInputWalkIntoMovingWalkRequestSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
