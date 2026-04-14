49ee04b71732d4d9446f0b18949bfd11ae7ab3b5
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-14 03:09:33 +0300

    Add "_service" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputWalkIntoMovingWalkRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputWalkIntoMovingWalkRequestSystem.cs
@@ -1,9 +1,17 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Features.Input.Services;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Systems
 {
     public class TranslatePlayerInputWalkIntoMovingWalkRequestSystem : IProtoRunSystem
     {
+        private readonly PlayerInputService _service;
+
+        public TranslatePlayerInputWalkIntoMovingWalkRequestSystem(PlayerInputService service)
+        {
+            _service = service;
+        }
+
         public void Run()
         {
             
