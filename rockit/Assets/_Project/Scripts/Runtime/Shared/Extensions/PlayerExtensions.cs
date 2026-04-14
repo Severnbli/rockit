b1c252d27c9ee997bb24cb9a85499b4119852147
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-14 03:03:04 +0300

    Add "AddPlayerTagToRequest" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PlayerExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PlayerExtensions.cs
@@ -1,7 +1,14 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Extensions
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions
 {
     public static class PlayerExtensions
     {
-        
+        public static ProtoEntity AddPlayerTagToRequest(this ProtoEntity entity, RequestsAspect aspect)
+        {
+            aspect.SharedAspect.PlayerTagPool.Add(entity);
+            return entity;
+        }
     }
 }
