b3cbfc1a3cc5f6485d3900f6d9919f6be94eaf26
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 02:21:11 +0300

    Add "_service" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputDashToDashRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputDashToDashRequestSystem.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Input.Services;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Systems
@@ -7,7 +8,13 @@ namespace _Project.Scripts.Runtime.Features.Moving.Systems
     public sealed class TranslatePlayerInputDashToDashRequestSystem : IProtoRunSystem
     {
         [DIRequests] private readonly RequestsAspect _aspect;
-        
+        private readonly PlayerInputService _service;
+
+        public TranslatePlayerInputDashToDashRequestSystem(PlayerInputService service)
+        {
+            _service = service;
+        }
+
         public void Run()
         {
             
