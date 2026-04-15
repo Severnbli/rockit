3c7dffaa8386f99ff47c7b215e366115475d7346
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 01:58:22 +0300

    Update "CreateWalkRequest" method signature: add "prepared" parameter. Assign it parameter to new created request

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/MovingUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/MovingUtils.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Moving.Requests;
 using _Project.Scripts.Runtime.Shared.Extensions;
 using Leopotam.EcsProto;
 
@@ -6,10 +7,11 @@ namespace _Project.Scripts.Runtime.Shared.Utils
 {
     public static class MovingUtils
     {
-        public static ProtoEntity CreateWalkRequest(RequestsAspect aspect)
+        public static ProtoEntity CreateWalkRequest(RequestsAspect aspect, WalkRequest prepared = default)
         {
             var entity = aspect.CreateRequest(fixedRun: true);
-            aspect.MovingRequestsAspect.WalkRequestPool.Add(entity);
+            ref var request = ref aspect.MovingRequestsAspect.WalkRequestPool.Add(entity);
+            request = prepared;
             return entity;
         }
         
