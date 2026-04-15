ab5c5d37920a835ba71e7edd7057dd1d1fff9551
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 02:19:44 +0300

    Create TranslatePlayerInputDashToDashRequestSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputDashToDashRequestSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Moving.Systems
+{
+    public sealed class TranslatePlayerInputDashToDashRequestSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
