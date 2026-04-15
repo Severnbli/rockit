1661a97c250ef3a05521f088f46af3e30e5bb854
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 02:55:52 +0300

    Update create requests methods signature: add "targetEntity" parameter

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/MovingUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/MovingUtils.cs
@@ -2,29 +2,33 @@
 using _Project.Scripts.Runtime.Features.Moving.Requests;
 using _Project.Scripts.Runtime.Shared.Extensions;
 using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Shared.Utils
 {
     public static class MovingUtils
     {
-        public static ProtoEntity CreateWalkRequest(RequestsAspect aspect, WalkRequest prepared = default)
+        public static ProtoEntity CreateWalkRequest(RequestsAspect aspect,
+            ProtoPackedEntityWithWorld targetEntity = default, WalkRequest prepared = default)
         {
-            var entity = aspect.CreateRequest(aspect.MovingRequestsAspect.WalkRequestPool, fixedRun: true,
-                prepared: prepared);
+            var entity = aspect.CreateRequest(aspect.MovingRequestsAspect.WalkRequestPool, targetEntity, true,
+                prepared);
             return entity;
         }
-        
-        public static ProtoEntity CreateJumpRequest(RequestsAspect aspect, JumpRequest prepared = default)
+
+        public static ProtoEntity CreateJumpRequest(RequestsAspect aspect,
+            ProtoPackedEntityWithWorld targetEntity = default, JumpRequest prepared = default)
         {
-            var entity = aspect.CreateRequest(aspect.MovingRequestsAspect.JumpRequestPool, fixedRun: true,
-                prepared: prepared);
+            var entity = aspect.CreateRequest(aspect.MovingRequestsAspect.JumpRequestPool, targetEntity, true,
+                prepared);
             return entity;
         }
-        
-        public static ProtoEntity CreateDashRequest(RequestsAspect aspect, DashRequest prepared = default)
+
+        public static ProtoEntity CreateDashRequest(RequestsAspect aspect,
+            ProtoPackedEntityWithWorld targetEntity = default, DashRequest prepared = default)
         {
-            var entity = aspect.CreateRequest(aspect.MovingRequestsAspect.DashRequestPool, fixedRun: true,
-                prepared: prepared);
+            var entity = aspect.CreateRequest(aspect.MovingRequestsAspect.DashRequestPool, targetEntity, true,
+                prepared);
             return entity;
         }
     }
