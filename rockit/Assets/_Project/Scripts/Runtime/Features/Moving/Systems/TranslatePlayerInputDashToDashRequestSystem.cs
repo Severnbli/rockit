a54c980249dcb6babcd5db556c41f9101910648f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 02:20:28 +0300

    Add "_aspect" field with "DIRequests" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputDashToDashRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputDashToDashRequestSystem.cs
@@ -1,9 +1,13 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Systems
 {
     public sealed class TranslatePlayerInputDashToDashRequestSystem : IProtoRunSystem
     {
+        [DIRequests] private readonly RequestsAspect _aspect;
+        
         public void Run()
         {
             
