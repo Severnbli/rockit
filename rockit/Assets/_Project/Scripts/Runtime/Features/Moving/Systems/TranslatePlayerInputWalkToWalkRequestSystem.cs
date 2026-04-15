1a512cbe9a01979592f5f15d3c8a6299af1d1c0f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 01:39:59 +0300

    Add "_aspect" field with "DIRequests" attrirbute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputWalkToWalkRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/TranslatePlayerInputWalkToWalkRequestSystem.cs
@@ -1,10 +1,13 @@
-﻿using _Project.Scripts.Runtime.Features.Input.Services;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Input.Services;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Systems
 {
     public class TranslatePlayerInputWalkToWalkRequestSystem : IProtoRunSystem
     {
+        [DIRequests] private readonly RequestsAspect _aspect;
         private readonly PlayerInputService _service;
 
         public TranslatePlayerInputWalkToWalkRequestSystem(PlayerInputService service)
