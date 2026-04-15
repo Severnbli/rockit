64ef298062c52085014646af30ac4bfb46a9f980
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 02:24:55 +0300

    Bind TranslatePlayerInputJumpToJumpRequestSystem

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/MovingModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/MovingModule.cs
@@ -14,6 +14,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features
             base.BindSystems();
             
             BindSystem<TranslatePlayerInputWalkToWalkRequestSystem>();
+            BindSystem<TranslatePlayerInputJumpToJumpRequestSystem>();
         }
     }
 }
