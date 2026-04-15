e87af5845464dfd9b34d04c53e71b28f4cd214b2
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 01:37:41 +0300

    Add "Rigidbody2D" field

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Components/Rigidbody2DComponent.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Components/Rigidbody2DComponent.cs
@@ -1,7 +1,9 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Components
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Shared.Components
 {
     public struct Rigidbody2DComponent
     {
-        
+        public Rigidbody2D Rigidbody2D;
     }
 }
