e7bcd504ec50414f87324963ec0e526048f0ec43
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 13:52:36 +0300

    Create GroundCheckUpdateSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/GroundCheckUpdateSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Moving.Systems
+{
+    public sealed class GroundCheckUpdateSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
