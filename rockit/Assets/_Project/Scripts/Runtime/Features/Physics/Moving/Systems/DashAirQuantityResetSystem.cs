2ebf15b97bb76af025e3864fcf296a82ef78af27
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 02:20:49 +0300

    Define "Run" method of DashAirQuantityResetSystem

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/DashAirQuantityResetSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/DashAirQuantityResetSystem.cs
@@ -9,7 +9,15 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
         
         public void Run()
         {
-            
+            foreach (var e in _mAspect.DashGroundCheckResults)
+            {
+                ref var gcResult = ref _mAspect.GroundCheckResultComponentPool.Get(e);
+
+                if (!gcResult.Grounded) continue;
+                
+                ref var dComponent = ref _mAspect.DashComponentPool.Get(e);
+                dComponent.AirQuantity = 0;
+            }
         }
     }
 }
