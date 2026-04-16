631db1a1f45a5457efa765f6712ea7b62e7415a0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 15:26:06 +0300

    Create ApplyGroundVelocityToGroundedObjectSystem that implements IProtoFixedRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/ApplyGroundVelocityToGroundedObjectSystem.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+
+namespace _Project.Scripts.Runtime.Features.Moving.Systems
+{
+    public sealed class ApplyGroundVelocityToGroundedObjectSystem : IProtoFixedRunSystem
+    {
+        public void FixedRun()
+        {
+            
+        }
+    }
+}
