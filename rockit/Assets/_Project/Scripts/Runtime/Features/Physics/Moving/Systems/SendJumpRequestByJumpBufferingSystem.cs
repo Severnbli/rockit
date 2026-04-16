c4d1af6f7f587a49c31bf48061e0b654ff4c807e
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 01:31:29 +0300

    Define "Run" method of SendJumpRequestByJumpBufferingSystem

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SendJumpRequestByJumpBufferingSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SendJumpRequestByJumpBufferingSystem.cs
@@ -2,6 +2,7 @@
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using _Project.Scripts.Runtime.Features.Physics.Moving.Configs;
+using _Project.Scripts.Runtime.Shared.Utils;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -28,7 +29,23 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 
         public void Run()
         {
-            
+            foreach (var e in _mAspect.JumpBufferingGroundCheckResults)
+            {
+                ref var gcResult = ref _mAspect.GroundCheckResultComponentPool.Get(e);
+                ref var jbComponent = ref _mAspect.JumpBufferingComponentPool.Get(e);
+
+                if (gcResult.Grounded)
+                {
+                    jbComponent.Request.Buffered = true;
+                    MovingUtils.CreateJumpRequest(_rAspect, _world.PackEntityWithWorld(e), jbComponent.Request);
+                }
+                else
+                {
+                    if (!MovingUtils.JumpBufferingTimeExpired(jbComponent, _tService, _smConfig)) continue;
+                }
+                
+                _mAspect.JumpBufferingComponentPool.Del(e);
+            }
         }
     }
 }
