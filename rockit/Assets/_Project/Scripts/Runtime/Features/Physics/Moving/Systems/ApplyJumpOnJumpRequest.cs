139d48fc9176fffd35e15d5c84fbf369f728bcd6
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 00:02:52 +0300

    Add "_mrAspect" field of type MovingRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequest.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequest.cs
@@ -1,9 +1,12 @@
-﻿using _Project.Scripts.Runtime.Core.Systems;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Systems;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class ApplyJumpOnJumpRequest : IProtoFixedRunSystem
     {
+        [DIRequests] private readonly MovingRequestsAspect _mrAspect;
+        
         public void FixedRun()
         {
             
