c9eebd15293f5d470bc873d5764fa194f0c39a1c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 15:40:56 +0300

    Add "_sharedAspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/ApplyGroundVelocityToGroundedObjectSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/ApplyGroundVelocityToGroundedObjectSystem.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Systems;
+using _Project.Scripts.Runtime.Shared;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Systems
@@ -6,6 +7,7 @@ namespace _Project.Scripts.Runtime.Features.Moving.Systems
     public sealed class ApplyGroundVelocityToGroundedObjectSystem : IProtoFixedRunSystem
     {
         [DI] private readonly MovingAspect _movingAspect;
+        [DI] private readonly SharedAspect _sharedAspect;
         
         public void FixedRun()
         {
