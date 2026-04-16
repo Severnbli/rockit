97fd5353c26b20842a4b4ca20cd367b4a4b3e0f9
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 14:14:12 +0300

    Create SharedMovingConfig that inherited from ScriptableObjectAutoInstaller

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/SharedMovingConfig.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Modules.Zenject;
+
+namespace _Project.Scripts.Runtime.Features.Moving.Configs
+{
+    public class SharedMovingConfig : ScriptableObjectAutoInstaller<SharedMovingConfig>
+    {
+        
+    }
+}
