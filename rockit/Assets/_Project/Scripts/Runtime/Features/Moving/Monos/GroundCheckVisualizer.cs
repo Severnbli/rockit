f8c5bcf2d1a7a44178d10189954deba3b429e7eb
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 13:30:34 +0300

    Define "OnDrawGizmos" method

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Monos/GroundCheckVisualizer.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Monos/GroundCheckVisualizer.cs
@@ -1,11 +1,22 @@
-﻿using _Project.Scripts.Runtime.Shared.Monos;
+﻿using _Project.Scripts.Runtime.Features.Moving.Components;
+using _Project.Scripts.Runtime.Shared.Extensions;
+using _Project.Scripts.Runtime.Shared.Monos;
 using _Project.Scripts.Runtime.Shared.Tools;
+using _Project.Scripts.Runtime.Shared.Utils;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Monos
 {
     [DebugOnlyBehaviour]
     public class GroundCheckVisualizer : DebugBehaviour
     {
-        
+        private void OnDrawGizmos()
+        {
+            if (!gameObject.TryGetAuthored(out GroundCheckComponent component)) return;
+            
+            Gizmos.DrawWireSphere(
+                MovingUtils.GetGroundCheckPosition(gameObject.transform.position, component.Position),
+                component.Radius);
+        }
     }
 }
