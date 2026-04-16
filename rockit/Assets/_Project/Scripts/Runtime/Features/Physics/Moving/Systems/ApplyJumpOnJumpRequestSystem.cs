def11f9a1eb5f270ea857214ca0caa3799af96d7
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 21:21:56 +0300

    Add "_timeService" field, initialize it from contstructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequestSystem.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using _Project.Scripts.Runtime.Core.Systems;
 using _Project.Scripts.Runtime.Features.Physics.Shared;
 using Leopotam.EcsProto;
@@ -14,7 +15,13 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
         [DIRequests] private readonly MovingRequestsAspect _movingRequestsAspect;
         [DIRequests] private readonly CoreRequestsAspect _coreRequestsAspect;
         private ProtoWorld _world;
-        
+        private readonly TimeService _timeService;
+
+        public ApplyJumpOnJumpRequestSystem(TimeService timeService)
+        {
+            _timeService = timeService;
+        }
+
         public void Init(IProtoSystems systems)
         {
             _world = _movingAspect.World();
