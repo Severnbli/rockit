9b22f05821f632233343a6987aff5280e73b864a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 16:23:32 +0300

    Create PhysicsSharedModule that inherited from BaseModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/PhysicsSharedModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics
+{
+    public sealed class PhysicsSharedModule : BaseModule<PhysicsSharedModule>
+    {
+        public PhysicsSharedModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
