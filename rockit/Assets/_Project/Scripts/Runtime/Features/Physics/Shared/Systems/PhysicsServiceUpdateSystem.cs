a22afe3bf7b1d117265f25aa410aad156b335e2c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 16:27:12 +0300

    Add "_physicsSharedAspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/PhysicsServiceUpdateSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/PhysicsServiceUpdateSystem.cs
@@ -1,9 +1,12 @@
 ﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
 {
     public sealed class PhysicsServiceUpdateSystem : IProtoRunSystem
     {
+        [DI] private readonly PhysicsSharedAspect _physicsSharedAspect;
+        
         public void Run()
         {
             
