0f5c52a7bf4851e9188e8aabe14b01c1133e9442
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 01:52:36 +0300

    Add "DashTimeoutComponentPool" field of type ProtoPool

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/MovingAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/MovingAspect.cs
@@ -11,6 +11,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving
         public readonly ProtoPool<GroundCheckComponent> GroundCheckComponentPool;
         public readonly ProtoPool<GroundCheckResultComponent> GroundCheckResultComponentPool;
         public readonly ProtoPool<JumpBufferingComponent> JumpBufferingComponentPool;
+        public readonly ProtoPool<DashTimeoutComponent> DashTimeoutComponentPool;
         public readonly ProtoIt GroundCheckable = new (It.Inc<TransformComponent, GroundCheckComponent>());
         public readonly ProtoIt Rigidbody2DGroundCheckResults = new (It.Inc<Rigidbody2DComponent, GroundCheckResultComponent>());
         public readonly ProtoIt JumpBufferingGroundCheckResults = new (It.Inc<JumpBufferingComponent, GroundCheckResultComponent>());
