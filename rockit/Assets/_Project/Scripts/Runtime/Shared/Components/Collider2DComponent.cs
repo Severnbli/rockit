2ed545c8ade40fb48e21cce6f8b5568712c0e3ac
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 15:31:34 +0300

    Add "Serializable, ProtoUnityAuthoring" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Components/Collider2DComponent.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Components/Collider2DComponent.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using Leopotam.EcsProto.Unity;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Shared.Components
 {
+    [Serializable, ProtoUnityAuthoring]
     public struct Collider2DComponent
     {
         public Collider2D Collider;
