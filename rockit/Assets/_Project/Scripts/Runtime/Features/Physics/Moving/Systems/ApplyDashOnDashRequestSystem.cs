90b8184208b40d3e876a4670af58be21874175c2
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 02:02:31 +0300

    Implement IProtoInitSystem into ApplyDashOnDashRequestSystem

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashOnDashRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashOnDashRequestSystem.cs
@@ -1,13 +1,19 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
-    public sealed class ApplyDashOnDashRequestSystem : IProtoFixedRunSystem
+    public sealed class ApplyDashOnDashRequestSystem : IProtoInitSystem, IProtoFixedRunSystem
     {
         [DIRequests] private readonly MovingRequestsAspect _mrAspect;
         [DIRequests] private readonly CoreRequestsAspect _crAspect;
+        
+        public void Init(IProtoSystems systems)
+        {
+            
+        }
         
         public void FixedRun()
         {
