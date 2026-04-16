d63ca5ef3dac8996d7aa7a8f2b168460e6508416
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 01:50:11 +0300

    Add "_tService" field of type TimeService, initialize from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/DashTimeoutExpireSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/DashTimeoutExpireSystem.cs
@@ -1,4 +1,5 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
@@ -6,7 +7,13 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
     public sealed class DashTimeoutExpireSystem : IProtoRunSystem
     {
         [DI] private readonly MovingAspect _mAspect;
-        
+        private readonly TimeService _tService;
+
+        public DashTimeoutExpireSystem(TimeService tService)
+        {
+            _tService = tService;
+        }
+
         public void Run()
         {
             
