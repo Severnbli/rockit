9a61d01f85c8dfb6b7f246599ae2e040b29cca5a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 20:00:18 +0300

    Add "_jumpBufferingTime" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Configs/SharedMovingConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Configs/SharedMovingConfig.cs
@@ -6,7 +6,9 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Configs
     public class SharedMovingConfig : ScriptableObjectAutoInstaller<SharedMovingConfig>
     {
         [SerializeField] private float _coyoteTime = 0.1f;
+        [SerializeField] private float _jumpBufferingTime = 0.1f;
         
         public float CoyoteTime => _coyoteTime;
+        public float JumpBufferingTime => _jumpBufferingTime;
     }
 }
