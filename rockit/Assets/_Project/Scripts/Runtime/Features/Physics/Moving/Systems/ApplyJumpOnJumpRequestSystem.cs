e21db62a3580eefa0c5a4e589fc8a62a187d0ff8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 20:59:21 +0300

    Add "_physicsSharedAspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequestSystem.cs
@@ -1,6 +1,7 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Systems;
+using _Project.Scripts.Runtime.Features.Physics.Shared;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -9,6 +10,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
     public sealed class ApplyJumpOnJumpRequestSystem : IProtoInitSystem, IProtoFixedRunSystem
     {
         [DI] private readonly MovingAspect _movingAspect;
+        [DI] private readonly PhysicsSharedAspect _physicsSharedAspect;
         [DIRequests] private readonly MovingRequestsAspect _movingRequestsAspect;
         [DIRequests] private readonly CoreRequestsAspect _coreRequestsAspect;
         private ProtoWorld _world;
