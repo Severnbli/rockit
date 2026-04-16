20e9ff2d873872d1ce1b5460640d9ab6816e0c7b
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 12:47:24 +0300

    Add "TryGetByType" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/GameObjectUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/GameObjectUtils.cs
@@ -1,7 +1,20 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Utils
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Shared.Utils
 {
     public static class GameObjectUtils
     {
-        
+        public static bool TryGetByType<T>(out T component, bool logNull = true) where T : Component
+        {
+            component = Object.FindFirstObjectByType<T>();
+
+            if (component != null) return true;
+
+            if (logNull)
+            {
+                LogUtils.LogError($"Not found object of type {typeof(T)}");
+            }
+            return false;
+        }
     }
 }
