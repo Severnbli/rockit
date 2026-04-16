976f6acb71a82e5e1c550c54e8f560f4c0b0bc0a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 00:09:07 +0300

    Implement IProtoInitSystem to ApplyJumpOnJumpRequest

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequest.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequest.cs
@@ -1,14 +1,20 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
-    public sealed class ApplyJumpOnJumpRequest : IProtoFixedRunSystem
+    public sealed class ApplyJumpOnJumpRequest : IProtoInitSystem, IProtoFixedRunSystem
     {
         [DIRequests] private readonly MovingRequestsAspect _mrAspect;
         [DI] private readonly MovingAspect _mAspect;
 
+        public void Init(IProtoSystems systems)
+        {
+            
+        }
+        
         public void FixedRun()
         {
             
