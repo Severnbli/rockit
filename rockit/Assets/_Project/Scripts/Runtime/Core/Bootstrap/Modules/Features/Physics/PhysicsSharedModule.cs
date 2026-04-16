d17bc521a46a64710bb00e20ec956f243643cd93
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 16:24:09 +0300

    Bind PhysicsService to PhysicsSharedModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/PhysicsSharedModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/PhysicsSharedModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Physics.Shared.Services;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics
     {
         public PhysicsSharedModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindServices()
+        {
+            base.BindServices();
+            
+            BindService<PhysicsService>();
         }
     }
 }
