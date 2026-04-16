b0f77e56250a195d60243dd8a0c8229c2949147d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 12:03:04 +0300

    Add "Position" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Components/GroundCheckComponent.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Components/GroundCheckComponent.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Leopotam.EcsProto.Unity;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Components
 {
     [Serializable, ProtoUnityAuthoring]
     public struct GroundCheckComponent
     {
-        
+        public Vector3 Position;
     }
 }
