5ec5f3df67c7d2f8e0589173c832370a43a12e50
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 11:45:47 +0300

    Add "_dashTimeout" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/MovingConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/MovingConfig.cs
@@ -8,9 +8,11 @@ namespace _Project.Scripts.Runtime.Features.Moving.Configs
         [SerializeField] private float _walkSpeed = 5f;
         [SerializeField] private float _jumpPower = 4f;
         [SerializeField] private float _dashPower = 4f;
+        [SerializeField] private float _dashTimeout = 0.5f;
         
         public float WalkSpeed => _walkSpeed;
         public float JumpPower => _jumpPower;
         public float DashPower => _dashPower;
+        public float DashTimeout => _dashTimeout;
     }
 }
