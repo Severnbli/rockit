2bf4940cf8ddc555ac021bc5b1b9e5a13afb71de
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 01:42:00 +0300

    Add "JumpBuffers" field of type ProtoIt

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/MovingAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/MovingAspect.cs
@@ -14,5 +14,6 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving
         public readonly ProtoIt GroundCheckable = new (It.Inc<TransformComponent, GroundCheckComponent>());
         public readonly ProtoIt Rigidbody2DGroundCheckResults = new (It.Inc<Rigidbody2DComponent, GroundCheckResultComponent>());
         public readonly ProtoIt JumpBufferingGroundCheckResults = new(It.Inc<JumpBufferingComponent, GroundCheckResultComponent>());
+        public readonly ProtoIt JumpBuffers = new(It.Inc<JumpBufferingComponent>());
     }
 }
