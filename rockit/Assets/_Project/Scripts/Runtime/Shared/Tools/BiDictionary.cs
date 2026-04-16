030c211c93253e43a68400852d48b95b6ad83065
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 16:05:21 +0300

    Add "_forward" and "_reverse" dict fields

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Tools/BiDictionary.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Tools/BiDictionary.cs
@@ -1,9 +1,12 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Tools
+﻿using System.Collections.Generic;
+
+namespace _Project.Scripts.Runtime.Shared.Tools
 {
     public class BiDictionary<TFirst, TSecond> 
         where TFirst : notnull 
         where TSecond : notnull
     {
-        
+        private readonly Dictionary<TFirst, TSecond> _forward = new ();
+        private readonly Dictionary<TSecond, TFirst> _reverse = new ();
     }
 }
