c0f7c5c6fb6710a8f7f4bda74e8e8a48a6d7da0d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 12:11:23 +0300

    Create LayersConfig that inherited from ScriptableObjectAutoInstaller

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Configs/LayersConfig.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Modules.Zenject;
+
+namespace _Project.Scripts.Runtime.Shared.Configs
+{
+    public class LayersConfig : ScriptableObjectAutoInstaller<LayersConfig>
+    {
+        
+    }
+}
