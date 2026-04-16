1687b40d4a4195aa7bddd5e9bf73f475834b455f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 21:12:15 +0300

    Update "TryCompareRigidbody2DRequest" method behaviour

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PhysicsExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PhysicsExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Features.Physics.Shared;
+using _Project.Scripts.Runtime.Features.Physics.Shared.Components;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Shared.Extensions
@@ -7,10 +8,20 @@ namespace _Project.Scripts.Runtime.Shared.Extensions
     public static class PhysicsExtensions
     {
         public static bool TryCompareRigidbody2DRequest(this PhysicsSharedAspect physicsSharedAspect,
-            CoreRequestsAspect coreRequestsAspect, ProtoEntity entity, ProtoWorld world, out ProtoEntity targetEntity)
+            CoreRequestsAspect coreRequestsAspect, ProtoEntity entity, ProtoWorld world, out ProtoEntity targetEntity, 
+            out Rigidbody2DComponent rigidbody2DComponent)
         {
-            return coreRequestsAspect.TryCompareRequestWorld(entity, world, out targetEntity) 
-                   && physicsSharedAspect.Rigidbody2DComponentPool.Has(entity);
+            rigidbody2DComponent = default;
+            
+            var compared = coreRequestsAspect.TryCompareRequestWorld(entity, world, out targetEntity) 
+                           && physicsSharedAspect.Rigidbody2DComponentPool.Has(targetEntity);
+            
+            if (compared)
+            {
+                rigidbody2DComponent = physicsSharedAspect.Rigidbody2DComponentPool.Get(targetEntity);
+            }
+            
+            return compared;
         }
     }
 }
