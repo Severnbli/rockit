1ca1d1ceeb81988b6053c94e7cfe78ed656a4d51
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 16:26:07 +0300

    Create PhysicsServiceUpdateSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/PhysicsServiceUpdateSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
+{
+    public sealed class PhysicsServiceUpdateSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
