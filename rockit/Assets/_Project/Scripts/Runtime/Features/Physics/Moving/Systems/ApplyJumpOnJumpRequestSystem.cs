bbfc9c11592bcea207cc3df7e874f7a1d54e8e55
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 20:25:59 +0300

    Add "_movingAspect" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequestSystem.cs
@@ -1,10 +1,12 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class ApplyJumpOnJumpRequestSystem : IProtoFixedRunSystem
     {
+        [DI] private readonly MovingAspect _movingAspect;
         [DIRequests] private readonly MovingRequestsAspect _movingRequestsAspect;
         
         public void FixedRun()
