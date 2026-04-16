2a055852f977abc978bb6349b1b5451d74f9d5d4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 11:45:03 +0300

    Extract moving configs from PlayerMovingConfig to base MovingConfig

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/MovingConfig.cs
@@ -0,0 +1,16 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Moving.Configs
+{
+    public class MovingConfig<T> : ScriptableObjectAutoInstaller<T> where T : MovingConfig<T>
+    {
+        [SerializeField] private float _walkSpeed = 5f;
+        [SerializeField] private float _jumpPower = 4f;
+        [SerializeField] private float _dashPower = 4f;
+        
+        public float WalkSpeed => _walkSpeed;
+        public float JumpPower => _jumpPower;
+        public float DashPower => _dashPower;
+    }
+}
