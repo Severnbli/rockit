e5895ae1dfdf39bcbd8a5b5edb9dc13a79d3b7d2
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 13:07:22 +0300

    Define "Awake" method to destroy component on "!DEBUG" directive if DebugOnlyBehaviourAttribute used

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Monos/DebugBehaviour.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Monos/DebugBehaviour.cs
@@ -1,9 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using _Project.Scripts.Runtime.Shared.Tools;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Shared.Monos
 {
     public class DebugBehaviour : MonoBehaviour
     {
+        private void Awake()
+        {
+#if !DEBUG
+            if (Attribute.IsDefined(GetType(), typeof(DebugOnlyBehaviourAttribute))) {
+                Destroy(this);
+            }
+#endif
+            OnAwake();
+        }
         
+        protected virtual void OnAwake() {}
     }
 }
