534f6495e33dc43a6530aa027d82d6ab1a2ca859
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 12:01:28 +0300

    Add "Serializable, ProtoUnityAuthoring" attributes to Rigidbody2DComponent

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Components/Rigidbody2DComponent.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Components/Rigidbody2DComponent.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using Leopotam.EcsProto.Unity;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Shared.Components
 {
+    [Serializable, ProtoUnityAuthoring]
     public struct Rigidbody2DComponent
     {
         public Rigidbody2D Rigidbody2D;
