c38bedc4d93283d29ff7be26fc243128e546e264
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 12:12:21 +0300

    Add "_groundLayer" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Configs/LayersConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Configs/LayersConfig.cs
@@ -1,9 +1,12 @@
 ﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Shared.Configs
 {
     public class LayersConfig : ScriptableObjectAutoInstaller<LayersConfig>
     {
+        [SerializeField] private LayerMask _groundLayer;
         
+        public LayerMask GroundLayer => _groundLayer;
     }
 }
