bfca4c0fead388f8aa83d77b35368403742fb39e
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 16:39:33 +0300

    Replace "_objectDomain" field with "_collider2DSetPool"

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/PhysicsServiceUpdateSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/PhysicsServiceUpdateSystem.cs
@@ -1,7 +1,9 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Domain;
+using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools.Collections;
 using _Project.Scripts.Runtime.Features.Physics.Shared.Services;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
 {
@@ -9,12 +11,12 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
     {
         [DI] private readonly PhysicsSharedAspect _physicsSharedAspect;
         private readonly PhysicsService _service;
-        private readonly ObjectDomain _objectDomain;
+        private readonly HashSetPool<Collider2D> _collider2DSetPool;
 
         public PhysicsServiceUpdateSystem(PhysicsService service, ObjectDomain objectDomain)
         {
             _service = service;
-            _objectDomain = objectDomain;
+            objectDomain.Get(out _collider2DSetPool);
         }
 
         public void Run()
