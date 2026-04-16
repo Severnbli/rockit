c9d03e29a7bfea0b9605181dd1de3828e8aba3a7
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 12:59:59 +0300

    Move DebugOnlyBehaviourAttribute into Tools folder

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Monos/GroundCheckVisualizer.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Monos/GroundCheckVisualizer.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Shared.Tools.Behaviour;
+﻿using _Project.Scripts.Runtime.Shared.Tools;
+using _Project.Scripts.Runtime.Shared.Tools.Behaviour;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Monos
--- rockit/Assets/_Project/Scripts/Runtime/Shared/Tools/DebugOnlyBehaviourAttribute.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Tools/DebugOnlyBehaviourAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace _Project.Scripts.Runtime.Shared.Tools.Behaviour
+namespace _Project.Scripts.Runtime.Shared.Tools
 {
     [AttributeUsage(AttributeTargets.Class)]
     public class DebugOnlyBehaviourAttribute : Attribute
