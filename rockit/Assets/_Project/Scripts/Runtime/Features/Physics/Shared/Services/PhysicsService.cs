0dbe9176ab453d05abd962b7aafb8d7f16c71339
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 16:19:49 +0300

    Add "PhysicsMatcher" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
@@ -1,7 +1,10 @@
-﻿namespace _Project.Scripts.Runtime.Features.Physics.Shared.Services
+﻿using _Project.Scripts.Runtime.Shared.Tools;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Shared.Services
 {
     public sealed class PhysicsService
     {
-        
+        public BiDictionary<Collider2D, Rigidbody2D> PhysicsMatcher = new ();
     }
 }
