14c0c583d6e57cf57d27636c4e9ffa39be4bbe11
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 01:49:28 +0300

    Add "_mAspect" field of type MovingAspect with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/DashTimeoutExpireSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/DashTimeoutExpireSystem.cs
@@ -1,9 +1,12 @@
 ﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class DashTimeoutExpireSystem : IProtoRunSystem
     {
+        [DI] private readonly MovingAspect _mAspect;
+        
         public void Run()
         {
             
