89bdbff9c998ad040f53db7fd0f41c8ad6d8b2fc
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 16:27:41 +0300

    Add "_service" field, initialize it from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/PhysicsServiceUpdateSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/PhysicsServiceUpdateSystem.cs
@@ -1,4 +1,5 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Features.Physics.Shared.Services;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
@@ -6,7 +7,13 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
     public sealed class PhysicsServiceUpdateSystem : IProtoRunSystem
     {
         [DI] private readonly PhysicsSharedAspect _physicsSharedAspect;
-        
+        private readonly PhysicsService _service;
+
+        public PhysicsServiceUpdateSystem(PhysicsService service)
+        {
+            _service = service;
+        }
+
         public void Run()
         {
             
