937cb117a6602a3d7e161a5113ad16cd8bea6443
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 00:34:52 +0300

    Bind ApplyJumpOnJumpRequest to MovingModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/Moving/MovingModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/Moving/MovingModule.cs
@@ -15,6 +15,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics.Movin
             
             BindSystem<GroundCheckUpdateSystem>();
             BindSystem<ApplyGroundVelocityToGroundedObjectSystem>();
+            BindSystem<ApplyJumpOnJumpRequest>();
         }
     }
 }
