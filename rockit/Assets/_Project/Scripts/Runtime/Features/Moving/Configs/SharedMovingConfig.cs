3d2680f1d0053637ce0f8a6395af2782e9cb07b8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 14:14:51 +0300

    Add "_coyoteTime" field and its property getter with default "0.1f" value

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/SharedMovingConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/SharedMovingConfig.cs
@@ -1,9 +1,12 @@
 ﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Configs
 {
     public class SharedMovingConfig : ScriptableObjectAutoInstaller<SharedMovingConfig>
     {
+        [SerializeField] private float _coyoteTime = 0.1f;
         
+        public float CoyoteTime => _coyoteTime;
     }
 }
