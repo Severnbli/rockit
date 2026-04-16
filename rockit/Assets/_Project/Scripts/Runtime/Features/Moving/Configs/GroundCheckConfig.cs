3270d4c382aea41b67a59bbf53c5da2ee1c9addb
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 12:05:08 +0300

    Remove GroundCheckConfig

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Configs/GroundCheckConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using _Project.Scripts.Modules.Zenject;
-
-namespace _Project.Scripts.Runtime.Features.Moving.Configs
-{
-    public class GroundCheckConfig : ScriptableObjectAutoInstaller<GroundCheckConfig>
-    {
-        
-    }
-}
