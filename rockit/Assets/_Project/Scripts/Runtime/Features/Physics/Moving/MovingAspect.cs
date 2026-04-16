36df348327b99a7a476516731eb1c652c9d45687
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 20:25:02 +0300

    Add "JumpBufferingComponentPool" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/MovingAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/MovingAspect.cs
@@ -10,6 +10,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving
     {
         public readonly ProtoPool<GroundCheckComponent> GroundCheckComponentPool;
         public readonly ProtoPool<GroundCheckResultComponent> GroundCheckResultComponentPool;
+        public readonly ProtoPool<JumpBufferingComponent> JumpBufferingComponentPool;
         public readonly ProtoIt GroundCheckable = new (It.Inc<TransformComponent, GroundCheckComponent>());
         public readonly ProtoIt Rigidbody2DGroundCheckResults = new (It.Inc<Rigidbody2DComponent, GroundCheckResultComponent>());
     }
