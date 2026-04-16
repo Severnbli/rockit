4fc488124ca0d2fb3254376e37ad062c6e7ddfb4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 13:17:17 +0300

    Update "TryGet" signature: add "logNull" parameter and its usage

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/GameObjectExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/GameObjectExtensions.cs
@@ -5,13 +5,16 @@ namespace _Project.Scripts.Runtime.Shared.Extensions
 {
     public static class GameObjectExtensions
     {
-        public static bool TryGet<T>(this GameObject gameObject, out T component) where T : Component
+        public static bool TryGet<T>(this GameObject gameObject, out T component, bool logNull = true) where T : Component
         {
             component = gameObject.GetComponent<T>();
 
             if (component != null) return true;
-            
-            LogUtils.LogError($"Component {typeof(T).Name} not found on {gameObject.name}");
+
+            if (logNull)
+            { 
+                LogUtils.LogError($"Component {typeof(T).Name} not found on {gameObject.name}");
+            }
             return false;
         }
     }
