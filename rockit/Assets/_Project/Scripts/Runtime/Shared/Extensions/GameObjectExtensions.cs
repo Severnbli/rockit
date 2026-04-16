b8ecaaf502e7c6ec5a610f62224c3a32003fd5df
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 13:24:15 +0300

    Add "TryGetAuthored" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/GameObjectExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/GameObjectExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Shared.Utils;
+using Leopotam.EcsProto.Unity;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Shared.Extensions
@@ -15,6 +16,33 @@ namespace _Project.Scripts.Runtime.Shared.Extensions
             { 
                 LogUtils.LogError($"Component {typeof(T).Name} not found on {gameObject.name}");
             }
+            return false;
+        }
+
+        public static bool TryGetAuthored<T>(this GameObject gameObject, out T authored, bool logNull = true)
+            where T : struct
+        {
+            authored = default;
+            
+            if (!gameObject.TryGet(out ProtoUnityAuthoring authoring))
+            {
+                LogUtils.LogWarning($"Object {gameObject.name} is not authored");
+                return false;
+            }
+
+            foreach (var c in authoring.Components)
+            {
+                if (c is not T t) continue;
+                
+                authored = t;
+                return true;
+            }
+
+            if (logNull)
+            {
+                LogUtils.LogWarning($"Authored {typeof(T).Name} not found on {gameObject.name}");
+            }
+            
             return false;
         }
     }
