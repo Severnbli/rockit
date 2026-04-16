0f824bf4117ce3cb6b90a4f6da1c586a008e3f1b
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 02:48:11 +0300

    Add "_tService" field of type TimeService, initialize at constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashOnDashRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashOnDashRequestSystem.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
@@ -12,7 +13,13 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
         [DIRequests] private readonly CoreRequestsAspect _crAspect;
         [DI] private readonly MovingAspect _mAspect;
         private ProtoWorld _world;
-        
+        private TimeService _tService;
+
+        public ApplyDashOnDashRequestSystem(TimeService tService)
+        {
+            _tService = tService;
+        }
+
         public void Init(IProtoSystems systems)
         {
             _world = _mAspect.World();
