0d754750ca9eaa848f5cc2ff1f9d9c206599025c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 00:59:12 +0300

    Add "_mAspect" field of type MovingAspect with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SendJumpRequestByJumpBufferingSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SendJumpRequestByJumpBufferingSystem.cs
@@ -1,10 +1,12 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class SendJumpRequestByJumpBufferingSystem : IProtoRunSystem
     {
+        [DI] private readonly MovingAspect _mAspect;
         private readonly TimeService _tService;
 
         public SendJumpRequestByJumpBufferingSystem(TimeService tService)
