e1c06ff66d32f970b07d698ed4bf6985fc726f12
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 16:04:58 +0300

    Create BiDictionary generic class

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Tools/BiDictionary.cs
@@ -0,0 +1,9 @@
+﻿namespace _Project.Scripts.Runtime.Shared.Tools
+{
+    public class BiDictionary<TFirst, TSecond> 
+        where TFirst : notnull 
+        where TSecond : notnull
+    {
+        
+    }
+}
