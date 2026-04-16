7e4826f2e21d098dea786664749d2bf8fcf09052
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 21:46:39 +0300

    Define "FixedRun" method

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequestSystem.cs
@@ -3,6 +3,7 @@ using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using _Project.Scripts.Runtime.Core.Systems;
 using _Project.Scripts.Runtime.Features.Physics.Shared;
+using _Project.Scripts.Runtime.Shared.Extensions;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -29,7 +30,29 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
         
         public void FixedRun()
         {
-            
+            foreach (var requestEntity in _movingRequestsAspect.JumpRequests)
+            {
+                if (!_physicsSharedAspect.TryCompareRigidbody2DRequest(_coreRequestsAspect, requestEntity, _world,
+                        out var targetEntity, out var rigidbody))
+                {
+                    continue;
+                }
+
+                if (!_movingAspect.GroundCheckResultComponentPool.Has(targetEntity)) continue;
+
+                ref var groundCheckResult = ref _movingAspect.GroundCheckResultComponentPool.Get(targetEntity);
+                ref var jumpRequest = ref _movingRequestsAspect.JumpRequestPool.Get(requestEntity);
+                
+                if (groundCheckResult.Grounded)
+                {
+                    rigidbody.ApplyJump(jumpRequest.Factor);
+                    continue;
+                }
+                
+                ref var jumpBufferingComponent = ref _movingAspect.JumpBufferingComponentPool.GetOrAdd(targetEntity);
+                jumpBufferingComponent.Request = jumpRequest;
+                jumpBufferingComponent.LastJumpBufferingTiming = _timeService.UnscaledTime;
+            }
         }
     }
 }
