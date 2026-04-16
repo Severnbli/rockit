3beee0156be775d11e8a2c3df20c7a1e58eab092
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 00:26:52 +0300

    Add "_psAspect" field of type PhysicsSharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequest.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequest.cs
@@ -2,6 +2,7 @@
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using _Project.Scripts.Runtime.Core.Systems;
+using _Project.Scripts.Runtime.Features.Physics.Shared;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -12,6 +13,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
         [DIRequests] private readonly MovingRequestsAspect _mrAspect;
         [DIRequests] private readonly CoreRequestsAspect _crAspect;
         [DI] private readonly MovingAspect _mAspect;
+        [DI] private readonly PhysicsSharedAspect _psAspect;
         private ProtoWorld _world;
         private TimeService _timeService;
 
