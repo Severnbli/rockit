1e826553131a6ad921db738bbff9390c00c978f0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 20:37:06 +0300

    Create "FromExactWorld" extension method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/EcsExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/EcsExtensions.cs
@@ -1,7 +1,13 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Extensions
+﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions
 {
     public static class EcsExtensions
     {
-        
+        public static bool FromExactWorld(this ProtoPackedEntityWithWorld packed, ProtoWorld world)
+        {
+            return packed.World == world;
+        }
     }
 }
