ca5d6403887b398fa833b7181de4b4c798163aa3
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 15:59:47 +0300

    Add "PhysicsSharedAspect" field

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/DomainAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/DomainAspect.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Features.Input;
 using _Project.Scripts.Runtime.Features.Physics.Moving;
+using _Project.Scripts.Runtime.Features.Physics.Shared;
 using _Project.Scripts.Runtime.Shared;
 using Leopotam.EcsProto.QoL;
 using Leopotam.EcsProto.Unity.Physics2D;
@@ -12,6 +13,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain
         public UnityUguiAspect UguiAspect;
         public UnityPhysics2DAspect Physics2DAspect;
         public InputAspect InputAspect;
+        public PhysicsSharedAspect PhysicsSharedAspect;
         public MovingAspect MovingAspect;
         public SharedAspect SharedAspect;
     }
