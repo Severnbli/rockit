e348ddf984f71fe80664f39faca45d943bcaad77
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 13:08:11 +0300

    Change inheritation of GroundCheckVisualizer from MonoBehaviour to DebugBehaviour

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Monos/GroundCheckVisualizer.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Monos/GroundCheckVisualizer.cs
@@ -1,10 +1,10 @@
-﻿using _Project.Scripts.Runtime.Shared.Tools;
-using UnityEngine;
+﻿using _Project.Scripts.Runtime.Shared.Monos;
+using _Project.Scripts.Runtime.Shared.Tools;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Monos
 {
     [DebugOnlyBehaviour]
-    public class GroundCheckVisualizer : MonoBehaviour
+    public class GroundCheckVisualizer : DebugBehaviour
     {
         
     }
