343dc5b6b8771f0bafa55e54c2f11e35ca262250
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 15:29:54 +0300

    Add "_movingAspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/ApplyGroundVelocityToGroundedObjectSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/ApplyGroundVelocityToGroundedObjectSystem.cs
@@ -1,9 +1,12 @@
 ﻿using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Systems
 {
     public sealed class ApplyGroundVelocityToGroundedObjectSystem : IProtoFixedRunSystem
     {
+        [DI] private readonly MovingAspect _movingAspect;
+        
         public void FixedRun()
         {
             
