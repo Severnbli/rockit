63995a57a859b681ea51e8a2399e983338465145
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 02:22:18 +0300

    Bind DashAirQuantityResetSystem to MovingModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/Moving/MovingModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/Moving/MovingModule.cs
@@ -19,6 +19,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics.Movin
             BindSystem<SendJumpRequestByJumpBufferingSystem>();
             BindSystem<JumpBufferingExpireSystem>();
             BindSystem<DashTimeoutExpireSystem>();
+            BindSystem<DashAirQuantityResetSystem>();
         }
     }
 }
