88f967bae1f330e4c4d6174b31a3a780ae43ad6f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 01:48:52 +0300

    Create DashTimeoutExpireSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/DashTimeoutExpireSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
+{
+    public sealed class DashTimeoutExpireSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
