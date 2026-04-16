12ef873a8624646628940445e1284e42ffc70998
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 21:08:00 +0300

    Create "TryCompareRigidbody2DRequest" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PhysicsExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PhysicsExtensions.cs
@@ -1,7 +1,16 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Extensions
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Physics.Shared;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions
 {
     public static class PhysicsExtensions
     {
-        
+        public static bool TryCompareRigidbody2DRequest(this PhysicsSharedAspect physicsSharedAspect,
+            CoreRequestsAspect coreRequestsAspect, ProtoEntity entity, ProtoWorld world, out ProtoEntity targetEntity)
+        {
+            return coreRequestsAspect.TryCompareRequestWorld(entity, world, out targetEntity) 
+                   && physicsSharedAspect.Rigidbody2DComponentPool.Has(entity);
+        }
     }
 }
