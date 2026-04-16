65abc12be1df0c3aecbea49be383e8246ec1f1c0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 20:14:50 +0300

    Create ApplyJumpOnJumpRequestSystem that implements IProtoFixedRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyJumpOnJumpRequestSystem.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
+{
+    public sealed class ApplyJumpOnJumpRequestSystem : IProtoFixedRunSystem
+    {
+        public void FixedRun()
+        {
+            
+        }
+    }
+}
