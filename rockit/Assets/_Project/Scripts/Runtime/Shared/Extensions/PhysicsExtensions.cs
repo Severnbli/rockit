9f1492485a55d48a366caa1fd773ae94900b4624
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 21:28:05 +0300

    Redefine "TryCompareRigidbody2DRequest" method to return directly object of Rigidbody2D type

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PhysicsExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/PhysicsExtensions.cs
@@ -1,24 +1,25 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Features.Physics.Shared;
-using _Project.Scripts.Runtime.Features.Physics.Shared.Components;
 using Leopotam.EcsProto;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Shared.Extensions
 {
     public static class PhysicsExtensions
     {
         public static bool TryCompareRigidbody2DRequest(this PhysicsSharedAspect physicsSharedAspect,
-            CoreRequestsAspect coreRequestsAspect, ProtoEntity entity, ProtoWorld world, out ProtoEntity targetEntity, 
-            out Rigidbody2DComponent rigidbody2DComponent)
+            CoreRequestsAspect coreRequestsAspect, ProtoEntity requestEntity, ProtoWorld world, 
+            out ProtoEntity targetEntity, out Rigidbody2D rigidbody)
         {
-            rigidbody2DComponent = default;
+            rigidbody = null;
             
-            var compared = coreRequestsAspect.TryCompareRequestWorld(entity, world, out targetEntity) 
+            var compared = coreRequestsAspect.TryCompareRequestWorld(requestEntity, world, out targetEntity) 
                            && physicsSharedAspect.Rigidbody2DComponentPool.Has(targetEntity);
             
             if (compared)
             {
-                rigidbody2DComponent = physicsSharedAspect.Rigidbody2DComponentPool.Get(targetEntity);
+                ref var rigidbody2DComponent = ref physicsSharedAspect.Rigidbody2DComponentPool.Get(targetEntity);
+                rigidbody = rigidbody2DComponent.Rigidbody2D;
             }
             
             return compared;
