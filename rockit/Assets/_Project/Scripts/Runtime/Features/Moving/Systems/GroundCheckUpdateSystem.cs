c1d430a2dc7fc3f114771082b7190400eb13ba10
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 13:54:26 +0300

    Add "_aspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/GroundCheckUpdateSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/GroundCheckUpdateSystem.cs
@@ -1,9 +1,12 @@
 ﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Systems
 {
     public sealed class GroundCheckUpdateSystem : IProtoRunSystem
     {
+        [DI] private readonly MovingAspect _aspect;
+        
         public void Run()
         {
             
