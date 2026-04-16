feb367f12136c801b6436fcf16f6350e770d7231
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 02:01:36 +0300

    Add "_crAspect" field of type CoreRequestsAspect with "DIRequests" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashOnDashRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashOnDashRequestSystem.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Systems;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
@@ -6,6 +7,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
     public sealed class ApplyDashOnDashRequestSystem : IProtoFixedRunSystem
     {
         [DIRequests] private readonly MovingRequestsAspect _mrAspect;
+        [DIRequests] private readonly CoreRequestsAspect _crAspect;
         
         public void FixedRun()
         {
