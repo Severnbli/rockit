b034c8d01f862f35bc09ad8ccbe50202d6be7e8c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 02:15:54 +0300

    Add "_airDashes" field of type int and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Configs/MovingConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Configs/MovingConfig.cs
@@ -8,11 +8,13 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Configs
         [SerializeField] private float _walkSpeed = 5f;
         [SerializeField] private float _jumpPower = 4f;
         [SerializeField] private float _dashPower = 4f;
+        [SerializeField] private int _airDashes = 1;
         [SerializeField] private float _dashTimeout = 0.5f;
         
         public float WalkSpeed => _walkSpeed;
         public float JumpPower => _jumpPower;
         public float DashPower => _dashPower;
+        public int AirDashes => _airDashes;
         public float DashTimeout => _dashTimeout;
     }
 }
