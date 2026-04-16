18d21fc3c0a819998c73e813969693b87679bc14
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 02:17:16 +0300

    Create DashAirQuantityResetSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/DashAirQuantityResetSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
+{
+    public sealed class DashAirQuantityResetSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
