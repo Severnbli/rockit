e9fc2a9125782b631f17b9dbfef5d8563b0698c5
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 12:56:28 +0300

    Add "DebugOnlyBehaviour" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Monos/GroundCheckVisualizer.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Monos/GroundCheckVisualizer.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using _Project.Scripts.Runtime.Shared.Tools.Behaviour;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Moving.Monos
 {
+    [DebugOnlyBehaviour]
     public class GroundCheckVisualizer : MonoBehaviour
     {
         
