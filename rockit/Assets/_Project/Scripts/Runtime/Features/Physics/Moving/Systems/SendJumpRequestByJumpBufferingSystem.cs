4dfd42088ee816bc270eb97afdf96d33e734d432
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 00:57:17 +0300

    Add "_timeService" field, initialize from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SendJumpRequestByJumpBufferingSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SendJumpRequestByJumpBufferingSystem.cs
@@ -1,9 +1,17 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class SendJumpRequestByJumpBufferingSystem : IProtoRunSystem
     {
+        private readonly TimeService _timeService;
+
+        public SendJumpRequestByJumpBufferingSystem(TimeService timeService)
+        {
+            _timeService = timeService;
+        }
+
         public void Run()
         {
             
