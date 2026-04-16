0910bc2ad75ae98313d3f0e69f6951cf696ce4d1
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-16 14:43:54 +0300

    Add "_sharedAspect" field with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/GroundCheckUpdateSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Moving/Systems/GroundCheckUpdateSystem.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using _Project.Scripts.Runtime.Features.Moving.Configs;
+using _Project.Scripts.Runtime.Shared;
 using _Project.Scripts.Runtime.Shared.Configs;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
@@ -9,6 +10,7 @@ namespace _Project.Scripts.Runtime.Features.Moving.Systems
     public sealed class GroundCheckUpdateSystem : IProtoRunSystem
     {
         [DI] private readonly MovingAspect _movingAspect;
+        [DI] private readonly SharedAspect _sharedAspect;
         private readonly SharedMovingConfig _sharedMovingConfig;
         private readonly TimeService _timeService;
         private readonly LayersConfig _layersConfig;
