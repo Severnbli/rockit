0ae77e2d6d76c9c857552e5f2671397121e36c6d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 18:25:35 +0300

    Add "_mrAspect" field of type MovingRequestAspect with "DIRequests" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashTimeoutOnDashTimeoutRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashTimeoutOnDashTimeoutRequestSystem.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class ApplyDashTimeoutOnDashTimeoutRequestSystem : IProtoInitSystem, IProtoRunSystem
     {
+        [DIRequests] private readonly MovingRequestsAspect _mrAspect;
         private ProtoWorld _world;
         
         public void Init(IProtoSystems systems)
