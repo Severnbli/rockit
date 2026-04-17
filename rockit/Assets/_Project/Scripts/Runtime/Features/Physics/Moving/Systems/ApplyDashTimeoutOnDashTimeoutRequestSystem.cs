d0242ba1920ba202875334c5bd00a10f99d34998
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 18:26:49 +0300

    Add "_mAspect" field of type MovingAspect with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashTimeoutOnDashTimeoutRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashTimeoutOnDashTimeoutRequestSystem.cs
@@ -1,6 +1,7 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
@@ -8,6 +9,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
     {
         [DIRequests] private readonly MovingRequestsAspect _mrAspect;
         [DIRequests] private readonly CoreRequestsAspect _crAspect;
+        [DI] private readonly MovingAspect _mAspect;
         private ProtoWorld _world;
         
         public void Init(IProtoSystems systems)
