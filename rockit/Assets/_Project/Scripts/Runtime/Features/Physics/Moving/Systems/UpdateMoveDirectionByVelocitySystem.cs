38dff1dc36ec7e76f5b0d918ec3d29ec8a8087a0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 23:17:52 +0300

    Add "_psAspect" field of type PhysicsSharedAspect with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/UpdateMoveDirectionByVelocitySystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/UpdateMoveDirectionByVelocitySystem.cs
@@ -1,9 +1,13 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Features.Physics.Shared;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class UpdateMoveDirectionByVelocitySystem : IProtoRunSystem
     {
+        [DI] private readonly PhysicsSharedAspect _psAspect;
+        
         public void Run()
         {
             
