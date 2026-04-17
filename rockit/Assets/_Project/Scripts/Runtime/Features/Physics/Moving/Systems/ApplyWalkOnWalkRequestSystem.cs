8476ba8d85d57d08ef4d430c23f7b6914bb7c36e
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 17:33:48 +0300

    Add "_mAspect" field of type MovingAspect with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyWalkOnWalkRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyWalkOnWalkRequestSystem.cs
@@ -1,12 +1,14 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class ApplyWalkOnWalkRequestSystem : IProtoInitSystem, IProtoFixedRunSystem
     {
         [DIRequests] private readonly MovingRequestsAspect _mrAspect;
+        [DI] private readonly MovingAspect _mAspect;
         private ProtoWorld _world;
         
         public void Init(IProtoSystems systems)
