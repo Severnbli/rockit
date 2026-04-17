37ed83d0e9cb690728856ee179d7f357035a77ed
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 23:55:31 +0300

    Add "Serializable, ProtoUnityAuthoring" attribute to MovableTag

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Tags/MovableTag.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Tags/MovableTag.cs
@@ -1,5 +1,9 @@
-﻿namespace _Project.Scripts.Runtime.Features.Physics.Moving.Tags
+﻿using System;
+using Leopotam.EcsProto.Unity;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Tags
 {
+    [Serializable, ProtoUnityAuthoring]
     public struct MovableTag
     {
         
