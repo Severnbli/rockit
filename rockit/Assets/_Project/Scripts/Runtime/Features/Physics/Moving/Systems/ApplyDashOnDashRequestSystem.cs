0d7b90627042ec6dba6c64172a305b58d968d13a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 17:24:30 +0300

    Define ApplyDashOnDashRequestSystem main functionality with "FixedRun", "IncreaseAirQuantity", "CreateDashTimeout" and "ApplyDash" methods

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashOnDashRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashOnDashRequestSystem.cs
@@ -6,6 +6,7 @@ using _Project.Scripts.Runtime.Features.Physics.Moving.Components;
 using _Project.Scripts.Runtime.Features.Physics.Moving.Requests;
 using _Project.Scripts.Runtime.Features.Physics.Shared;
 using _Project.Scripts.Runtime.Shared.Extensions;
+using _Project.Scripts.Runtime.Shared.Utils;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -32,7 +33,40 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
         
         public void FixedRun()
         {
-            
+            foreach (var reqE in _mrAspect.DashRequests)
+            {
+                if (!_crAspect.TryCompareRequestWorld(reqE, _world, out var tarE)) continue;
+                if (!_mAspect.Dashables.Has(tarE)) continue;
+
+                ref var dRequest = ref _mrAspect.DashRequestPool.Get(reqE);
+                ref var dComponent = ref _mAspect.DashComponentPool.GetOrAdd(tarE);
+
+                if (dComponent.AirQuantity >= dRequest.AirQuantity) continue;
+                
+                IncreaseAirQuantity(ref dComponent, tarE);
+                CreateDashTimeout(dRequest, tarE);
+                ApplyDash(dRequest, tarE);
+            }
+        }
+
+        private void IncreaseAirQuantity(ref DashComponent dComponent, ProtoEntity tarE)
+        {
+            ref var gcResult = ref _mAspect.GroundCheckResultComponentPool.Get(tarE);
+            if (!gcResult.Grounded) dComponent.AirQuantity++;
+        }
+
+        private void CreateDashTimeout(DashRequest dRequest, ProtoEntity tarE)
+        {
+            ref var dtComponent = ref _mAspect.DashTimeoutComponentPool.Add(tarE);
+            dtComponent.CreationTime = _tService.UnscaledTime;
+            dtComponent.Timeout = dRequest.TimeOut;
+        }
+        
+        private void ApplyDash(DashRequest dRequest, ProtoEntity tarE)
+        {
+            ref var rbComponent = ref _psAspect.Rigidbody2DComponentPool.Get(tarE);
+            ref var mComponent = ref _mAspect.MoveComponentPool.GetOrAdd(tarE);
+            rbComponent.Rigidbody2D.ApplyDash(dRequest.Factor, MovingUtils.GetMoveXDirection(mComponent.Direction));
         }
     }
 }
