af3158ce83f54b182c70ac569681f3577126b3b4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 17:32:57 +0300

    Add "_mrAspect" field of type MovingRequestsAspect with "DIRequests" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyWalkOnWalkRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyWalkOnWalkRequestSystem.cs
@@ -1,10 +1,12 @@
-﻿using _Project.Scripts.Runtime.Core.Systems;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class ApplyWalkOnWalkRequestSystem : IProtoInitSystem, IProtoFixedRunSystem
     {
+        [DIRequests] private readonly MovingRequestsAspect _mrAspect;
         private ProtoWorld _world;
         
         public void Init(IProtoSystems systems)
