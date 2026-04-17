e905d0f2abfab16faebf4fe67bd7f9abe68758e8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 18:23:23 +0300

    Create ApplyDashTimeoutOnDashTimeoutRequestSystem that implements IProtoRunSystem and IProtoInitSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashTimeoutOnDashTimeoutRequestSystem.cs
@@ -0,0 +1,17 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
+{
+    public sealed class ApplyDashTimeoutOnDashTimeoutRequestSystem : IProtoInitSystem, IProtoRunSystem
+    {
+        public void Init(IProtoSystems systems)
+        {
+            
+        }
+
+        public void Run()
+        {
+            
+        }
+    }
+}
