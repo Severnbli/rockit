173dca87f6f26784d5be2e3e5973e83afedc702c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 18:36:04 +0300

    Merge MovingRequestsModule to MovingModule. Move MovingModule into Physics folder

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/SceneDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/SceneDomain.cs
@@ -1,5 +1,4 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics;
-using _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics.Moving;
 using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
