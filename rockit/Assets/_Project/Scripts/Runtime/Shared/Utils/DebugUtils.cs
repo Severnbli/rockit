e1ed7bef3b725b219d99ea0588c725fd1d7e3dd9
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 00:04:35 +0300

    Add "DrawGizmosWithColorBackup" method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/DebugUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/DebugUtils.cs
@@ -1,7 +1,16 @@
-﻿namespace _Project.Scripts.Runtime.Shared.Utils
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Shared.Utils
 {
     public static class DebugUtils
     {
-        
+        public static void DrawGizmosWithColorBackup(Color color, Action action)
+        {
+            var backupColor = Gizmos.color;
+            Gizmos.color = color;
+            action();
+            Gizmos.color = backupColor;
+        }
     }
 }
