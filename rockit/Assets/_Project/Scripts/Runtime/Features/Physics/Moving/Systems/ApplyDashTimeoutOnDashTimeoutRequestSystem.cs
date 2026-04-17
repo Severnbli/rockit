ecb9b3e21787efe4d07c30a86e93c34c9f9b493d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-17 18:24:56 +0300

    Add "_world" field, initialize at "Init" method

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashTimeoutOnDashTimeoutRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyDashTimeoutOnDashTimeoutRequestSystem.cs
@@ -4,9 +4,11 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class ApplyDashTimeoutOnDashTimeoutRequestSystem : IProtoInitSystem, IProtoRunSystem
     {
+        private ProtoWorld _world;
+        
         public void Init(IProtoSystems systems)
         {
-            
+            _world = systems.World();
         }
 
         public void Run()
