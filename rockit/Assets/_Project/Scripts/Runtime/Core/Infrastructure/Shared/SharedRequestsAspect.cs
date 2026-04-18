bc48ff9ae465382b5696dde2b2e2294aa36b39a9
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:09:59 +0300

    Add "InitializeRequestPool" field of type ProtoPool

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/SharedRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/SharedRequestsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared
 {
     public class SharedRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<InitializeRequest> InitializeRequestPool;
     }
 }
