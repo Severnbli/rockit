90b454d9bb0116349f9e2ebb24f2a0fc4305fa94
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 14:13:44 +0300

    Create UpdateMoveDecelerationOnWalkRequestSystem that implements IProtoInitSystem and IProtoFixedRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/UpdateMoveDecelerationOnWalkRequestSystem.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
+{
+    public class UpdateMoveDecelerationOnWalkRequestSystem : IProtoInitSystem, IProtoFixedRunSystem
+    {
+        public void Init(IProtoSystems systems)
+        {
+            
+        }
+
+        public void FixedRun()
+        {
+            
+        }
+    }
+}
