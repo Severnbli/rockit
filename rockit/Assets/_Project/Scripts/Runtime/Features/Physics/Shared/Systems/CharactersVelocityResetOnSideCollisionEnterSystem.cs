994cdbbb9a96bca3c19157fab66a92d62245b00a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 17:25:16 +0300

    Add "_pService" field of type PhysicsService, initialize from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/CharactersVelocityResetOnSideCollisionEnterSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/CharactersVelocityResetOnSideCollisionEnterSystem.cs
@@ -1,4 +1,5 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Features.Physics.Shared.Services;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
@@ -6,6 +7,12 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
     public sealed class CharactersVelocityResetOnSideCollisionEnterSystem : IProtoRunSystem
     {
         [DI] private readonly PhysicsSharedAspect _psAspect;
+        private readonly PhysicsService _pService;
+
+        public CharactersVelocityResetOnSideCollisionEnterSystem(PhysicsService pService)
+        {
+            _pService = pService;
+        }
         
         public void Run()
         {
