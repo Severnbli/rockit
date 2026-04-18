f8ee1900df19935023fdfb86ed43191d7b466fe8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:16:37 +0300

    Create SharedModule that inherited from BaseModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/SharedModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
+{
+    public sealed class SharedModule : BaseModule<SharedModule>
+    {
+        public SharedModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
