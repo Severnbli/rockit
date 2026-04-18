e9b1e6d3d22b494ed7172458f7acde6a59d3e05f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 14:51:31 +0300

    Rename LoadPlayersWalkDecelerationOnInitSystem to LoadPlayersWalkDecelerationSystem

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/LoadPlayersWalkDecelerationSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/LoadPlayersWalkDecelerationSystem.cs
@@ -1,13 +1,16 @@
 ﻿using _Project.Scripts.Runtime.Features.Physics.Moving.Configs;
+using _Project.Scripts.Runtime.Shared;
 using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
-    public class LoadPlayersWalkDecelerationOnInitSystem : IProtoInitSystem
+    public class LoadPlayersWalkDecelerationSystem : IProtoInitSystem
     {
+        [DI] private readonly SharedAspect _sAspect;
         private readonly PlayerMovingConfig _pmConfig;
 
-        public LoadPlayersWalkDecelerationOnInitSystem(PlayerMovingConfig pmConfig)
+        public LoadPlayersWalkDecelerationSystem(PlayerMovingConfig pmConfig)
         {
             _pmConfig = pmConfig;
         }
