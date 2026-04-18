452f4734339d6c9cbd03e52c187379f1d52cd164
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:15:05 +0300

    Create SendInitializeRequestsOnRunSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Systems/SendInitializeRequestsOnRunSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Systems
+{
+    public sealed class SendInitializeRequestsOnRunSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
