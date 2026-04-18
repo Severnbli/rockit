eaff7aafb3324f24389c1a960f7507b37b595d22
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 17:05:18 +0300

    Add "_psAspect" field of type PhysicsSharedAspect with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/CharactersVelocityResetOnSideCollisionEnterSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/CharactersVelocityResetOnSideCollisionEnterSystem.cs
@@ -1,9 +1,12 @@
 ﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
 {
     public sealed class CharactersVelocityResetOnSideCollisionEnterSystem : IProtoRunSystem
     {
+        [DI] private readonly PhysicsSharedAspect _psAspect;
+        
         public void Run()
         {
             
