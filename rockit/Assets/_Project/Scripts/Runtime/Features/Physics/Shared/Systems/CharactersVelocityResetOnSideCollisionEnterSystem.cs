7cb2e97f65639af0a66c800bee721c097da889a6
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 16:54:18 +0300

    Create CharactersVelocityResetOnSideCollisionEnterSystem that implements IProtoFixedRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/CharactersVelocityResetOnSideCollisionEnterSystem.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
+{
+    public sealed class CharactersVelocityResetOnSideCollisionEnterSystem : IProtoFixedRunSystem
+    {
+        public void FixedRun()
+        {
+            
+        }
+    }
+}
