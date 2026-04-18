a58048700330f8d5e751b0a387d45479f9eb751c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 17:01:21 +0300

    Add collisions and triggers enter and exit events pools

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/PhysicsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/PhysicsSharedAspect.cs
@@ -2,6 +2,7 @@
 using _Project.Scripts.Runtime.Features.Physics.Shared.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
+using Leopotam.EcsProto.Unity.Physics2D;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Shared
 {
@@ -9,6 +10,10 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared
     {
         public readonly ProtoPool<Collider2DComponent> Collider2DComponentPool;
         public readonly ProtoPool<Rigidbody2DComponent> Rigidbody2DComponentPool;
+        public readonly ProtoPool<UnityPhysics2DCollisionEnterEvent> CollisionEnterEvent;
+        public readonly ProtoPool<UnityPhysics2DCollisionExitEvent> CollisionExitEvent;
+        public readonly ProtoPool<UnityPhysics2DTriggerEnterEvent> TriggerEnterEvent;
+        public readonly ProtoPool<UnityPhysics2DTriggerExitEvent> TriggerExitEvent;
         public readonly ProtoIt Rigidbody2DPlayers = new (It.Inc<PlayerTag, Rigidbody2DComponent>());
         public readonly ProtoIt Rigidbody2DCharacters = new (It.Inc<CharacterTag, Rigidbody2DComponent>());
         public readonly ProtoIt Rigidbody2DColliders2D = new (It.Inc<Rigidbody2DComponent, Collider2DComponent>());
