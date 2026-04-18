28f90254143c9ead6a8f807bd818da33accd982d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 16:49:20 +0300

    Add "Rigidbody2DCharacters" field of type ProtoIt

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/PhysicsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/PhysicsSharedAspect.cs
@@ -10,6 +10,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared
         public readonly ProtoPool<Collider2DComponent> Collider2DComponentPool;
         public readonly ProtoPool<Rigidbody2DComponent> Rigidbody2DComponentPool;
         public readonly ProtoIt Rigidbody2DPlayers = new (It.Inc<PlayerTag, Rigidbody2DComponent>());
+        public readonly ProtoIt Rigidbody2DCharacters = new (It.Inc<CharacterTag, Rigidbody2DComponent>());
         public readonly ProtoIt Rigidbody2DColliders2D = new (It.Inc<Rigidbody2DComponent, Collider2DComponent>());
         public readonly ProtoIt Rigidbodies2D = new(It.Inc<Rigidbody2DComponent>());
     }
