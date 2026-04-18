bb5cc9900a58de6543dcab78c455722d1e04f40c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 14:01:25 +0300

    Add "_world" field, initialize from "Init" method

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/UpdateMoveDirectionOnWalkRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/UpdateMoveDirectionOnWalkRequestSystem.cs
@@ -5,9 +5,11 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public class UpdateMoveDirectionOnWalkRequestSystem : IProtoInitSystem, IProtoFixedRunSystem
     {
+        private ProtoWorld _world;
+        
         public void Init(IProtoSystems systems)
         {
-            
+            _world = systems.World();
         }
 
         public void FixedRun()
