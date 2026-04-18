9a60a4076139669c421c653171bf5151885b2cf5
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 14:49:26 +0300

    Add "_pmConfig" field, initialize from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/LoadPlayersWalkDecelerationOnInitSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/LoadPlayersWalkDecelerationOnInitSystem.cs
@@ -1,9 +1,17 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Features.Physics.Moving.Configs;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public class LoadPlayersWalkDecelerationOnInitSystem : IProtoInitSystem
     {
+        private readonly PlayerMovingConfig _pmConfig;
+
+        public LoadPlayersWalkDecelerationOnInitSystem(PlayerMovingConfig pmConfig)
+        {
+            _pmConfig = pmConfig;
+        }
+
         public void Init(IProtoSystems systems)
         {
             
