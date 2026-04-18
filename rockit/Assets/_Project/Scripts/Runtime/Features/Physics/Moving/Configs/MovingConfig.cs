ebea3325c42adbe1a92bf7e796719b62871ca4a1
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 13:52:06 +0300

    Add "_walkDeceleration" field of type float with default value "3f" and property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Configs/MovingConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Configs/MovingConfig.cs
@@ -6,12 +6,14 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Configs
     public class MovingConfig<T> : ScriptableObjectAutoInstaller<T> where T : MovingConfig<T>
     {
         [SerializeField] private float _walkSpeed = 5f;
+        [SerializeField] private float _walkDeceleration = 3f;
         [SerializeField] private float _jumpPower = 4f;
         [SerializeField] private float _dashPower = 4f;
         [SerializeField] private int _airDashes = 1;
         [SerializeField] private float _dashTimeout = 0.5f;
         
         public float WalkSpeed => _walkSpeed;
+        public float WalkDeceleration => _walkDeceleration;
         public float JumpPower => _jumpPower;
         public float DashPower => _dashPower;
         public int AirDashes => _airDashes;
