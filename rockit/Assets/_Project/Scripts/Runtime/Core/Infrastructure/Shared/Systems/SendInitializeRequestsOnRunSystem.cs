389cb6b39d58656021566413546a0f817a47e71c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:23:28 +0300

    Implement IProtoInitSystem to SendInitializeRequestsOnRunSystem

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Systems/SendInitializeRequestsOnRunSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Systems/SendInitializeRequestsOnRunSystem.cs
@@ -3,9 +3,14 @@ using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Systems
 {
-    public sealed class SendInitializeRequestsOnRunSystem : IProtoRunSystem
+    public sealed class SendInitializeRequestsOnRunSystem : IProtoInitSystem, IProtoRunSystem
     {
         [DI] private readonly SharedAspect _sAspect;
+        
+        public void Init(IProtoSystems systems)
+        {
+            
+        }
         
         public void Run()
         {
