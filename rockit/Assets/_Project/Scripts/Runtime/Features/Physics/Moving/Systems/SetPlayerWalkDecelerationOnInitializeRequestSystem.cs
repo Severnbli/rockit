a829626f7e721a3c5682921be06a889c40b6d802
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:34:28 +0300

    Add "_srAspect" field of type SharedRequestsAspect with "DIRequests" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SetPlayerWalkDecelerationOnInitializeRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SetPlayerWalkDecelerationOnInitializeRequestSystem.cs
@@ -1,9 +1,13 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class SetPlayerWalkDecelerationOnInitializeRequestSystem : IProtoRunSystem
     {
+        [DIRequests] private readonly SharedRequestsAspect _srAspect;
+        
         public void Run()
         {
             
