d513ba495532d13388fdac116a7ac33a2025018d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:09:04 +0300

    Add SharedRequestsAspect to RequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Requests/RequestsAspect.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Features.Input;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using _Project.Scripts.Runtime.Features.Input;
 using _Project.Scripts.Runtime.Features.Physics.Moving;
 using Leopotam.EcsProto.QoL;
 
@@ -9,5 +10,6 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Requests
         public readonly CoreRequestsAspect CoreRequestsAspect;
         public readonly InputRequestsAspect InputRequestsAspect;
         public readonly MovingRequestsAspect MovingRequestsAspect;
+        public readonly SharedRequestsAspect SharedRequestsAspect;
     }
 }
