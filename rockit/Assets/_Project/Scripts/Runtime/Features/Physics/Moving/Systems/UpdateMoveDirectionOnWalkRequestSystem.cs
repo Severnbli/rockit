6472fdef155579de74cdd39af36106fc072f1334
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 14:03:21 +0300

    Add "_crAspect" field of type CoreRequestsAspect with "DIRequests" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/UpdateMoveDirectionOnWalkRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/UpdateMoveDirectionOnWalkRequestSystem.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
 
@@ -7,6 +8,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
     public class UpdateMoveDirectionOnWalkRequestSystem : IProtoInitSystem, IProtoFixedRunSystem
     {
         [DIRequests] private readonly MovingRequestsAspect _mrAspect;
+        [DIRequests] private readonly CoreRequestsAspect _crAspect;
         private ProtoWorld _world;
         
         public void Init(IProtoSystems systems)
