6708971b37ead61fd25b732573590c046bf7ee87
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:48:56 +0300

    Add "_mAspect" field of type MovingAspect with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyWalkDecelerationOnFixedRunSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/ApplyWalkDecelerationOnFixedRunSystem.cs
@@ -1,9 +1,12 @@
 ﻿using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public sealed class ApplyWalkDecelerationOnFixedRunSystem : IProtoFixedRunSystem
     {
+        [DI] private readonly MovingAspect _mAspect;
+        
         public void FixedRun()
         {
             
