4fc1a15e9db9c857880f3924dd022635b058dcb0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 14:14:30 +0300

    Add "_mrAspect" field of type MovingRequestsAspect with "DIRequests" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/UpdateMoveDecelerationOnWalkRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/UpdateMoveDecelerationOnWalkRequestSystem.cs
@@ -1,10 +1,13 @@
-﻿using _Project.Scripts.Runtime.Core.Systems;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Systems;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
 {
     public class UpdateMoveDecelerationOnWalkRequestSystem : IProtoInitSystem, IProtoFixedRunSystem
     {
+        [DIRequests] private readonly MovingRequestsAspect _mrAspect;
+        
         public void Init(IProtoSystems systems)
         {
             
