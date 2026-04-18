b33c3725c79018025f7188ef27d0fde8b6268a2a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 14:48:51 +0300

    Create LoadPlayersWalkDecelerationOnInitSystem that implements IProtoInitSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/LoadPlayersWalkDecelerationOnInitSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
+{
+    public class LoadPlayersWalkDecelerationOnInitSystem : IProtoInitSystem
+    {
+        public void Init(IProtoSystems systems)
+        {
+            
+        }
+    }
+}
