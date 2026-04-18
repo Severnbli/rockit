fd05e39bb53035dd5a4912ca9b9afbc45a8265e4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 17:24:17 +0300

    Add "GameObjectRigidbody2DMatcher" update functionality

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/PhysicsServiceUpdateSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Systems/PhysicsServiceUpdateSystem.cs
@@ -12,29 +12,39 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared.Systems
         [DI] private readonly PhysicsSharedAspect _physicsSharedAspect;
         private readonly PhysicsService _service;
         private readonly HashSetPool<Collider2D> _collider2DSetPool;
+        private readonly HashSetPool<Rigidbody2D> _rigidbody2DSetPool;
 
         public PhysicsServiceUpdateSystem(PhysicsService service, IObjectDomain objectDomain)
         {
             _service = service;
             objectDomain.Get(out _collider2DSetPool);
+            objectDomain.Get(out _rigidbody2DSetPool);
         }
 
         public void Run()
         {
             var collider2DSet = _collider2DSetPool.Spawn();
+            var rigidbody2DSet = _rigidbody2DSetPool.Spawn();
 
             foreach (var e in _physicsSharedAspect.Rigidbody2DColliders2D)
             {
                 var collider = _physicsSharedAspect.Collider2DComponentPool.Get(e).Collider;
                 var rigidbody = _physicsSharedAspect.Rigidbody2DComponentPool.Get(e).Rigidbody2D;
                 collider2DSet.Add(collider);
+                rigidbody2DSet.Add(rigidbody);
+                
                 _service.PhysicsMatcher.TryAdd(collider, rigidbody);
                 _service.PhysicsMatcher.TrySet(collider, e);
+                
+                _service.GameObjectRigidbody2DMatcher.TryAdd(rigidbody.gameObject, rigidbody);
+                _service.GameObjectRigidbody2DMatcher.TrySet(rigidbody, e);
             }
             
             _service.PhysicsMatcher.KeepOnly(collider2DSet);
+            _service.GameObjectRigidbody2DMatcher.KeepOnly(rigidbody2DSet);
             
             _collider2DSetPool.Despawn(collider2DSet);
+            _rigidbody2DSetPool.Despawn(rigidbody2DSet);
         }
     }
 }
