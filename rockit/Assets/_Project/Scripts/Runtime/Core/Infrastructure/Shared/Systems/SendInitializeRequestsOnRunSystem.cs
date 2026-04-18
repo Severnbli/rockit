7397dd1c58f38ffade7a62c5261d597c889f0908
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:18:52 +0300

    Add "_sAspect" field of type SharedAspect with "DI" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Systems/SendInitializeRequestsOnRunSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Systems/SendInitializeRequestsOnRunSystem.cs
@@ -1,9 +1,12 @@
 ﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Systems
 {
     public sealed class SendInitializeRequestsOnRunSystem : IProtoRunSystem
     {
+        [DI] private readonly SharedAspect _sAspect;
+        
         public void Run()
         {
             
