83e7ec3c78f9d8452730b2a48a617f5cd70b0f23
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:24:13 +0300

    Add "_rAspect" field of type RequestsAspect with "DIRequests" attribute

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Systems/SendInitializeRequestsOnRunSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Systems/SendInitializeRequestsOnRunSystem.cs
@@ -1,10 +1,13 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Systems
 {
     public sealed class SendInitializeRequestsOnRunSystem : IProtoInitSystem, IProtoRunSystem
     {
+        [DIRequests] private readonly RequestsAspect _rAspect;
         [DI] private readonly SharedAspect _sAspect;
         
         public void Init(IProtoSystems systems)
