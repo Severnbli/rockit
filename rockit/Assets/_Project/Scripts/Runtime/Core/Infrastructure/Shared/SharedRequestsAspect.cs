c4671525c402e5c8afca7789198604c65608c957
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:08:29 +0300

    Create SharedRequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/SharedRequestsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared
+{
+    public class SharedRequestsAspect : ProtoAspectInject
+    {
+        
+    }
+}
