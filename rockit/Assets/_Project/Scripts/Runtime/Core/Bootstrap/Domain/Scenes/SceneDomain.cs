ad4376e2ba3b5ee6a940ff0ba4f62564d0f53ff2
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:17:25 +0300

    Register SharedModule to SceneDomain

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/SceneDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/SceneDomain.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics;
+using _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure;
 using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
@@ -16,6 +17,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
         {
             base.RegisterModules();
 
+            TryRegisterModule<SharedModule>();
             TryRegisterModule<PhysicsSharedModule>();
             TryRegisterModule<MovingModule>();
         }
