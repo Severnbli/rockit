60a7a9e6f552228bde9746560081b89b316d7bb8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:36:30 +0300

    Add "_world" field of type ProtoWorld

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SetPlayerWalkDecelerationOnInitializeRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SetPlayerWalkDecelerationOnInitializeRequestSystem.cs
@@ -11,10 +11,11 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
         [DIRequests] private readonly SharedRequestsAspect _srAspect;
         [DIRequests] private readonly CoreRequestsAspect _crAspect;
         [DI] private readonly MovingAspect _mAspect;
+        private ProtoWorld _world;
         
         public void Init(IProtoSystems systems)
         {
-            
+            _world = systems.World();
         }
         
         public void Run()
