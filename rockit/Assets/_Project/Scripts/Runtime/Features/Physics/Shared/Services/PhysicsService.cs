4faf9ba794e555bf77a42be2d5e1ce0d7121039f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 16:44:51 +0300

    Make "PhysicsMatcher" field readonly

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
@@ -6,6 +6,6 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared.Services
 {
     public sealed class PhysicsService
     {
-        public BiValueDictionary<Collider2D, Rigidbody2D, ProtoEntity> PhysicsMatcher = new ();
+        public readonly BiValueDictionary<Collider2D, Rigidbody2D, ProtoEntity> PhysicsMatcher = new ();
     }
 }
