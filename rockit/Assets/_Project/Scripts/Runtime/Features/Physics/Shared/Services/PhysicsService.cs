0387809d312eb272b3666100b325f1b9cba8b04f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 16:44:36 +0300

    Update "PhysicsMatcher" type to BiValueDictionary with ProtoEntity as value

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
@@ -1,10 +1,11 @@
 ﻿using _Project.Scripts.Runtime.Shared.Tools;
+using Leopotam.EcsProto;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Shared.Services
 {
     public sealed class PhysicsService
     {
-        public BiDictionary<Collider2D, Rigidbody2D> PhysicsMatcher = new ();
+        public BiValueDictionary<Collider2D, Rigidbody2D, ProtoEntity> PhysicsMatcher = new ();
     }
 }
