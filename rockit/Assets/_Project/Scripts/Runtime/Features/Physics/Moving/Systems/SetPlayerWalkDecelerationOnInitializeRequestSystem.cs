c8b8fb7f2d3be49cd915ac605b90078419d4e72d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 15:32:33 +0300

    Create SetPlayerWalkDecelerationOnInitializeRequestSystem that implements IProtoRunSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Systems/SetPlayerWalkDecelerationOnInitializeRequestSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Systems
+{
+    public sealed class SetPlayerWalkDecelerationOnInitializeRequestSystem : IProtoRunSystem
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
