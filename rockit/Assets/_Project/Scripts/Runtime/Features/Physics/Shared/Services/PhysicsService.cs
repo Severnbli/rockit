b5c22cc3a19adc92634cf5dcee2b82264f3480d2
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-18 17:21:35 +0300

    Add "GameObjectRigidbody2DMatcher" field of type BiValueDictionary

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
@@ -7,5 +7,6 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared.Services
     public sealed class PhysicsService
     {
         public readonly BiValueDictionary<Collider2D, Rigidbody2D, ProtoEntity> PhysicsMatcher = new ();
+        public readonly BiValueDictionary<GameObject, Rigidbody2D, ProtoEntity> GameObjectRigidbody2DMatcher = new ();
     }
 }
