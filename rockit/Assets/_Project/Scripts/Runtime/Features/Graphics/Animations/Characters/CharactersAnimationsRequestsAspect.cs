29f810013d6f2e0c3556dd70f1167b5e399a0e61
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 01:36:27 +0300

    Create KillCharacterRequest, add it pool and iterator to CharactersAnimationsRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Characters/CharactersAnimationsRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Characters/CharactersAnimationsRequestsAspect.cs
@@ -1,9 +1,14 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using _Project.Scripts.Runtime.Features.Graphics.Animations.Characters.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Characters
 {
     public class CharactersAnimationsRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<KillCharacterRequest> KillCharacterRequestPool;
+        public readonly ProtoIt KillCharacterRequests = new (It.Inc<RequestComponent, ActiveRequestTag, KillCharacterRequest>());
     }
 }
