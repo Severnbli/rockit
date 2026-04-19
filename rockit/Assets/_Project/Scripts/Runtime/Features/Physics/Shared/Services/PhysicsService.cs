89cd8d392f2e865521f030b5d7ba9e46f6807df3
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 15:50:00 +0300

    Remove "GameObjectRigidbody2DMatcher", change value type for "PhysicsMatcher" to "ProtoPackedEntityWithWorld"

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Services/PhysicsService.cs
@@ -1,12 +1,11 @@
 ﻿using _Project.Scripts.Runtime.Shared.Tools;
-using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Shared.Services
 {
     public sealed class PhysicsService
     {
-        public readonly BiValueDictionary<Collider2D, Rigidbody2D, ProtoEntity> PhysicsMatcher = new ();
-        public readonly BiValueDictionary<GameObject, Rigidbody2D, ProtoEntity> GameObjectRigidbody2DMatcher = new ();
+        public readonly BiValueDictionary<Collider2D, Rigidbody2D, ProtoPackedEntityWithWorld> PhysicsMatcher = new ();
     }
 }
