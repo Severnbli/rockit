97156fc824521fa8ad3bed5f579a01ebf9e71825
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 02:08:30 +0300

    Create FaceComponent with "Direction" field, add authoring attributes to it

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Shared/Components/FaceComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared.Enums;
+using Leopotam.EcsProto.Unity;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared.Components
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct FaceComponent
+    {
+        public FacingDirection Direction;
+    }
+}
