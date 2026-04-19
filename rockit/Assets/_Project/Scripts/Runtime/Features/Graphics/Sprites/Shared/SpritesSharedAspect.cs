a99336abd6ba8455d78d262051244df754cfa32c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 02:10:42 +0300

    Create SpritesSharedAspect that inherited from ProtoAspectInject. Add to it face component pool and iterator. Add SpritesSharedAspect to DomainAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Shared/SpritesSharedAspect.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared
+{
+    public class SpritesSharedAspect : ProtoAspectInject
+    {
+        public readonly ProtoPool<FaceComponent> FaceComponentPool;
+        public readonly ProtoIt Faces = new (It.Inc<FaceComponent>());
+    }
+}
