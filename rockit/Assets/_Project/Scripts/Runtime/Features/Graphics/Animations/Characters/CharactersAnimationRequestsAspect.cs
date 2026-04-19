0e080ea662990f51e8ba661680e3e70637bcf5d3
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 01:32:12 +0300

    Create CharactersAnimationRequestsAspect that inherited from ProtoAspectInject

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Characters/CharactersAnimationRequestsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Characters
+{
+    public class CharactersAnimationRequestsAspect : ProtoAspectInject
+    {
+        
+    }
+}
