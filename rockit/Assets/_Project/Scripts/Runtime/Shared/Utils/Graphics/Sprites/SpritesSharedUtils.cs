88267cb8dbb713d6a7c5b26da1a07884e58271e5
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 02:29:21 +0300

    Create SpritesSharedUtils with only "FacingDirectionMatchedWithMoveDirection" method

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Graphics/Sprites/SpritesSharedUtils.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared.Enums;
+using _Project.Scripts.Runtime.Features.Physics.Moving.Enums;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Graphics.Sprites
+{
+    public static class SpritesSharedUtils
+    {
+        public static bool FacingDirectionMatchedWithMoveDirection(FacingDirection facingDirection,
+            MoveDirection moveDirection)
+        {
+            return (facingDirection, moveDirection) switch
+            {
+                (FacingDirection.Left, MoveDirection.Left) or (FacingDirection.Right, MoveDirection.Right) => true,
+                _ => false
+            };
+        }
+    }
+}
