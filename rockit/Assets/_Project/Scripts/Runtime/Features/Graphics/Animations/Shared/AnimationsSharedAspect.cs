d4459309273258b11609ef8418fa9d8a9061e311
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 00:08:30 +0300

    Add "CharacterAnimators" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/AnimationsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/AnimationsSharedAspect.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Components;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Tags;
+using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -8,5 +9,6 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Shared
     {
         public readonly ProtoPool<AnimatorComponent> AnimatorComponentPool;
         public readonly ProtoIt Animators = new (It.Inc<AnimatorComponent>());
+        public readonly ProtoIt CharacterAnimators = new (It.Inc<AnimatorComponent, CharacterTag>());
     }
 }
