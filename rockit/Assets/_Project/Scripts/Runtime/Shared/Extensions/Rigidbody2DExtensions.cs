36c463b0bbb9227cd4091f3c2db72c4564d10b76
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 16:49:01 +0300

    Add "ResetVelocityY", update "ResetVelocityOnSideCollision" method name to "ResetVelocityYOnSideCollision" and call for "ResetVelocityY"

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Rigidbody2DExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Rigidbody2DExtensions.cs
@@ -46,14 +46,21 @@ namespace _Project.Scripts.Runtime.Shared.Extensions
             rigidbody2D.linearVelocity = velocity;
         }
 
-        public static void ResetVelocityOnSideCollision(this Rigidbody2D rigidbody2D, Vector2 normal, float tolerance)
+        public static void ResetVelocityY(this Rigidbody2D rigidbody2D)
+        {
+            var velocity = rigidbody2D.linearVelocity;
+            velocity.y = 0f;
+            rigidbody2D.linearVelocity = velocity;
+        }
+
+        public static void ResetVelocityYOnSideCollision(this Rigidbody2D rigidbody2D, Vector2 normal, float tolerance)
         {
             if (Mathf.Abs(normal.x) < tolerance) return;
             
             var dot = Vector2.Dot(rigidbody2D.linearVelocity, normal);
             if (dot >= 0f) return;
             
-            rigidbody2D.linearVelocity = Vector2.zero;
+            rigidbody2D.ResetVelocityY();
         }
     }
 }
