2340c1625437d0a4f3ec169dbd8c77ece9ac8ba9
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 17:44:33 +0300

    Add wrapper to obtain OnCollisionStay2D mono event. Form it as CallbackModule with CallbackAspect. In the future there is possible to easy add some more events

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Callback/CallbackModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using _Project.Scripts.Runtime.Shared.Callback.Events;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Shared.Callback
+{
+    public sealed class CallbackModule : IProtoModule
+    {
+        private readonly string _worldName;
+        private readonly int _clearPointWeight;
+        
+        public CallbackModule (string worldName = default, int clearPointWeight = default) {
+            _worldName = worldName;
+            _clearPointWeight = clearPointWeight;
+        }
+        
+        public void Init(IProtoSystems systems)
+        {
+            systems
+                .DelHere<UnityPhysics2DCollisionStayEvent>(_worldName, _clearPointWeight);
+        }
+
+        public IProtoAspect[] Aspects()
+        {
+            return _worldName != null 
+                ? null
+                : new IProtoAspect[] { new CallbackAspect() };
+        }
+
+        public Type[] Dependencies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
