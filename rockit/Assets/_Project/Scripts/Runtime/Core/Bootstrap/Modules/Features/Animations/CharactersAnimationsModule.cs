8bd10ed173ebfcf937e23039c177afe8fdd1e928
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 01:01:09 +0300

    Create UpdateFallBoolByVelocityOnFixedRunSystem and bind it to CharactersAnimationsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Animations/CharactersAnimationsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Animations/CharactersAnimationsModule.cs
@@ -15,6 +15,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Animations
             
             BindSystem<SendDashTriggerOnDashRequestSystem>();
             BindSystem<UpdateDashedBoolOnRunSystem>();
+            BindSystem<UpdateFallBoolByVelocityOnFixedRunSystem>();
         }
     }
 }
