6fcc369835e3e40d9643ad77b0d7b1309ad77b2b
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 15:58:56 +0300

    Create IndexableTag with authoring attributes, update SharedAspect with its pool and iterator

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/SharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/SharedAspect.cs
@@ -12,9 +12,11 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared
         public readonly ProtoPool<InitializableTag> InitializableTagPool;
         public readonly ProtoPool<CharacterTag> CharacterTagPool;
         public readonly ProtoPool<GameObjectComponent> GameObjectComponentPool;
+        public readonly ProtoPool<IndexableTag> IndexableTagPool;
         public readonly ProtoIt Players = new (It.Inc<PlayerTag>());
         public readonly ProtoIt Initializables = new (It.Inc<InitializableTag>());
         public readonly ProtoIt Characters = new (It.Inc<CharacterTag>());
         public readonly ProtoIt GameObjectComponents = new (It.Inc<GameObjectComponent>());
+        public readonly ProtoIt Indexables = new (It.Inc<IndexableTag>());
     }
 }
