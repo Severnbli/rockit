35ad925d2853b1597e065776e9dd3b5c46ad4182
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 23:49:53 +0300

    Create AnimatorComponent with "Animator" field and authoring attributes

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/Components/AnimatorComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Leopotam.EcsProto.Unity;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Components
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct AnimatorComponent
+    {
+        public Animator Animator;
+    }
+}
