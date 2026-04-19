78f3d723cf773534c28d0b41fa75905bfc0196d3
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 17:47:57 +0300

    Add collision stay event pool and iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/PhysicsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/PhysicsSharedAspect.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Tags;
 using _Project.Scripts.Runtime.Features.Physics.Shared.Components;
+using _Project.Scripts.Runtime.Shared.Callback.Events;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 using Leopotam.EcsProto.Unity.Physics2D;
@@ -11,6 +12,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared
         public readonly ProtoPool<Collider2DComponent> Collider2DComponentPool;
         public readonly ProtoPool<Rigidbody2DComponent> Rigidbody2DComponentPool;
         public readonly ProtoPool<UnityPhysics2DCollisionEnterEvent> CollisionEnterEventPool;
+        public readonly ProtoPool<UnityPhysics2DCollisionStayEvent> CollisionStayEventPool;
         public readonly ProtoPool<UnityPhysics2DCollisionExitEvent> CollisionExitEventPool;
         public readonly ProtoPool<UnityPhysics2DTriggerEnterEvent> TriggerEnterEventPool;
         public readonly ProtoPool<UnityPhysics2DTriggerExitEvent> TriggerExitEventPool;
@@ -19,6 +21,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Shared
         public readonly ProtoIt Rigidbody2DColliders2D = new (It.Inc<Rigidbody2DComponent, Collider2DComponent>());
         public readonly ProtoIt Rigidbodies2D = new(It.Inc<Rigidbody2DComponent>());
         public readonly ProtoIt CollisionEnterEvents = new (It.Inc<UnityPhysics2DCollisionEnterEvent>());
+        public readonly ProtoIt CollisionStayEvents = new (It.Inc<UnityPhysics2DCollisionStayEvent>());
         public readonly ProtoIt CollisionExitEvents = new (It.Inc<UnityPhysics2DCollisionExitEvent>());
         public readonly ProtoIt TriggerEnterEvents = new (It.Inc<UnityPhysics2DTriggerEnterEvent>());
         public readonly ProtoIt TriggerExitEvents = new (It.Inc<UnityPhysics2DTriggerExitEvent>());
