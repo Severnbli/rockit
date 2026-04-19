dedc999fb5c1d4b55627692dd366a63b1f85f7bd
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 17:44:33 +0300

    Add wrapper to obtain OnCollisionStay2D mono event. Form it as CallbackModule with CallbackAspect. In the future there is possible to easy add some more events

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Callback/CallbackAspect.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Shared.Callback.Events;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Shared.Callback
+{
+    public sealed class CallbackAspect : ProtoAspectInject
+    {
+        public readonly ProtoPool<UnityPhysics2DCollisionStayEvent> CollisionStayEvent;
+    }
+}
