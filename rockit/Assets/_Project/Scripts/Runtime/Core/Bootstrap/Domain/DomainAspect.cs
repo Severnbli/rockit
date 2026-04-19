a199367fd1f394d6e6601cc6a9b1054e2bb5a91b
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 23:53:23 +0300

    Place AnimationsSharedAspect to the root of the Animations.Shared folder

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/DomainAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/DomainAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared;
 using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Components;
 using _Project.Scripts.Runtime.Features.Input;
 using _Project.Scripts.Runtime.Features.Physics.Moving;
