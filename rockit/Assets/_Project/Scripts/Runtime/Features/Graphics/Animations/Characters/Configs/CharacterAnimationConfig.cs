2a09114625a986b60c5363036dc4457ad7372589
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 00:00:48 +0300

    Update CharacterAnimationConfig to new parameters at Character animation controller

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Characters/Configs/CharacterAnimationConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Characters/Configs/CharacterAnimationConfig.cs
@@ -5,15 +5,19 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Characters.Confi
 {
     public class CharacterAnimationConfig : ScriptableObjectAutoInstaller<CharacterAnimationConfig>
     {
+        [SerializeField] private string _deathTrigger = "Death";
         [SerializeField] private string _deadBool = "Dead";
         [SerializeField] private string _jumpTrigger = "Jump";
-        [SerializeField] private string _dashBool = "Dash";
+        [SerializeField] private string _dashTrigger = "Dash";
+        [SerializeField] private string _dashedBool = "Dash";
         [SerializeField] private string _fallBool = "Fall";
         [SerializeField] private string _runBool = "Run";
 
+        public int DeathTrigger => Animator.StringToHash(_deathTrigger);
         public int DeadBool => Animator.StringToHash(_deadBool);
         public int JumpTrigger => Animator.StringToHash(_jumpTrigger);
-        public int DashBool => Animator.StringToHash(_dashBool);
+        public int DashTrigger => Animator.StringToHash(_dashTrigger);
+        public int DashedBool => Animator.StringToHash(_dashedBool);
         public int FallBool => Animator.StringToHash(_fallBool);
         public int RunBool => Animator.StringToHash(_runBool);
     }
