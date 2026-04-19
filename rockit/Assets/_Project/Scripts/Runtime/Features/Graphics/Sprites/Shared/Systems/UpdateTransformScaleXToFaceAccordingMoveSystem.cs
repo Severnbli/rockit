b9da73133ec3b95785f4e78c0c152b5c4ab33382
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 02:33:41 +0300

    Create UpdateTransformScaleXToFaceAccordingMoveSystem and bind it to SpritesSharedModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Shared/Systems/UpdateTransformScaleXToFaceAccordingMoveSystem.cs
@@ -0,0 +1,37 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using _Project.Scripts.Runtime.Features.Physics.Moving;
+using _Project.Scripts.Runtime.Shared.Utils.Graphics.Sprites;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared.Systems
+{
+    public sealed class UpdateTransformScaleXToFaceAccordingMoveSystem : IProtoRunSystem
+    {
+        [DI] private readonly SpritesSharedAspect _ssAspect;
+        [DI] private readonly MovingAspect _mAspect;
+        [DI] private readonly SharedAspect _sAspect;
+        
+        public void Run()
+        {
+            foreach (var e in _ssAspect.MoveTransformFaces)
+            {
+                ref var fComponent = ref _ssAspect.FaceComponentPool.Get(e);
+                ref var mComponent = ref _mAspect.MoveComponentPool.Get(e);
+                ref var tComponent = ref _sAspect.TransformComponentPool.Get(e);
+
+                var scale = tComponent.Transform.localScale;
+                scale.x = Mathf.Abs(scale.x);
+
+                if (!SpritesSharedUtils.FacingDirectionMatchedWithMoveDirection(fComponent.Direction,
+                        mComponent.Direction))
+                {
+                    scale.x *= -1f;
+                }
+                
+                tComponent.Transform.localScale = scale;
+            }
+        }
+    }
+}
