85367612ea9096128f7b3bbe7e2e8e7151b44d1c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 23:52:02 +0300

    Create AnimationsSharedAspect with AnimatorComponent pool and iterator

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/Components/AnimationsSharedAspect.cs
@@ -0,0 +1,11 @@
+﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Components
+{
+    public class AnimationsSharedAspect : ProtoAspectInject
+    {
+        public readonly ProtoPool<AnimatorComponent> AnimatorComponentPool;
+        public readonly ProtoIt Animators = new (It.Inc<AnimatorComponent>());
+    }
+}
