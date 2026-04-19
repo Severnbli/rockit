b4de98d238be6df2c73bc972f0dd773bfd552a21
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 00:12:14 +0300

    Create CharactersAnimationsModule that inherited from BaseModule, bind there SendCharacterAnimatorDashTriggerOnDashRequestSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Animations/CharactersAnimationsModule.cs
@@ -0,0 +1,19 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Graphics.Animations.Characters.Systems;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Animations
+{
+    public sealed class CharactersAnimationsModule : BaseModule<CharactersAnimationsModule>
+    {
+        public CharactersAnimationsModule(IDomain domain) : base(domain)
+        {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<SendCharacterAnimatorDashTriggerOnDashRequestSystem>();
+        }
+    }
+}
