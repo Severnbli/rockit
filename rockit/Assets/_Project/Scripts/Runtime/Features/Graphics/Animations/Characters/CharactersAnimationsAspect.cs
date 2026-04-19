3a65d86d7e88f1f20e57da0ab441e6791077fcca
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 00:42:59 +0300

    Revert "Create CharactersAnimationsAspect"
    This reverts commit 9b10dc0343efb3dab2a9c227a039c037b318ab73.

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Characters/CharactersAnimationsAspect.cs
+++
@@ -1,9 +0,0 @@
-﻿using Leopotam.EcsProto.QoL;
-
-namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Characters
-{
-    public class CharactersAnimationsAspect : ProtoAspectInject
-    {
-        
-    }
-}
