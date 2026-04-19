530be189e37d4ef7606aa8d72df7a36699b22b16
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 23:47:23 +0300

    Create CharacterAnimationConfig with "_deadBool", "_jumpTrigger", "_dashBool", "_fallBool" and "_runBool" fields and they property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Characters/Configs/CharacterAnimationConfig.cs
@@ -0,0 +1,20 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Characters.Configs
+{
+    public class CharacterAnimationConfig : ScriptableObjectAutoInstaller<CharacterAnimationConfig>
+    {
+        [SerializeField] private string _deadBool = "Dead";
+        [SerializeField] private string _jumpTrigger = "Jump";
+        [SerializeField] private string _dashBool = "Dash";
+        [SerializeField] private string _fallBool = "Fall";
+        [SerializeField] private string _runBool = "Run";
+
+        public int DeadBool => Animator.StringToHash(_deadBool);
+        public int JumpTrigger => Animator.StringToHash(_jumpTrigger);
+        public int DashBool => Animator.StringToHash(_dashBool);
+        public int FallBool => Animator.StringToHash(_fallBool);
+        public int RunBool => Animator.StringToHash(_runBool);
+    }
+}
