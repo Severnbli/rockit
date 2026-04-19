da264dea97f2bf98dc891a929fccd49b608daca0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 02:13:59 +0300

    Create SpritesSharedModule that inherited from BaseModule. Register SpritesSharedModule into SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Sprites/SpritesSharedModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Sprites
+{
+    public sealed class SpritesSharedModule : BaseModule<SpritesSharedModule>
+    {
+        public SpritesSharedModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
