c5f7ed4a2ce7ed6b096b0532f2e0ba481384a81d
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-19 16:09:30 +0300

    Create SharedIndexService with "GameObjectIndex", bind its to SharedModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/SharedModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/SharedModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Services;
 using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
@@ -7,6 +8,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
     {
         public SharedModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindServices()
+        {
+            base.BindServices();
+            
+            BindService<SharedIndexService>();
         }
 
         protected override void BindSystems()
---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Services/SharedIndexService.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Shared.Tools;
+using Leopotam.EcsProto.QoL;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Services
+{
+    public sealed class SharedIndexService
+    {
+        public BiDictionary<GameObject, ProtoPackedEntityWithWorld> GameObjectIndex = new ();
+    }
+}
