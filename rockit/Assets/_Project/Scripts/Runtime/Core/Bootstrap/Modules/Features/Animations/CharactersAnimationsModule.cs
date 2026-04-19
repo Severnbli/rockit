01e7cffffbe8867e6ee7ce6ed6294b3e4345c62f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-20 00:46:41 +0300

    Create UpdateDashedBoolOnRunSystem and bind to CharactersAnimationsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Animations/CharactersAnimationsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Animations/CharactersAnimationsModule.cs
@@ -14,6 +14,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Animations
             base.BindSystems();
             
             BindSystem<SendDashTriggerOnDashRequestSystem>();
+            BindSystem<UpdateDashedBoolOnRunSystem>();
         }
     }
 }
