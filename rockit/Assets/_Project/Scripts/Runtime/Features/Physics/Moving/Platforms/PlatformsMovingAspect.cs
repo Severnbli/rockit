d07038a63df2aacb80dfd2b4c7658f3876dc6c2f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 14:41:46 +0300

    Add PlatformUpdatesBufferComponent pool and iterator to PlatformsMovingAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingAspect.cs
@@ -1,9 +1,12 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms
 {
     public class PlatformsMovingAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<PlatformUpdatesBufferComponent> PlatformUpdatesBufferComponentPool;
+        public readonly ProtoIt PlatformUpdatesBuffers = new (It.Inc<PlatformUpdatesBufferComponent>());
     }
 }
