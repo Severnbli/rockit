a33f662901ec54c15b92a14bf05dcc92262e33db
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 13:55:56 +0300

    Create PlatformsSharedModule, register it to GameSceneDomain

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/GameSceneDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/GameSceneDomain.cs
@@ -1,7 +1,14 @@
-﻿namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Platforms;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
 {
     public class GameSceneDomain : SceneDomain
     {
-        
+        protected override void RegisterModules()
+        {
+            base.RegisterModules();
+
+            TryRegisterModule<PlatformsSharedModule>();
+        }
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Platforms/PlatformsSharedModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Platforms
+{
+    public sealed class PlatformsSharedModule : BaseModule<PlatformsSharedModule>
+    {
+        public PlatformsSharedModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
