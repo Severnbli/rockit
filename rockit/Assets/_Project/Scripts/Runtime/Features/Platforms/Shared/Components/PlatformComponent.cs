646d81c9a6213ba0e4b0477da742cea83f0e4f73
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 10:44:42 +0300

    Create PlatformComponent with "Platform" field and authoring attribute

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Components/PlatformComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using _Project.Scripts.Runtime.Features.Platforms.Shared.Monos;
+using Leopotam.EcsProto.Unity;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Components
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct PlatformComponent
+    {
+        public Platform Platform;
+    }
+}
