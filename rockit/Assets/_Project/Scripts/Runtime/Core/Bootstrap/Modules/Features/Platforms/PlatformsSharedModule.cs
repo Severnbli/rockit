bc5f02b644618835af13678dace9b7d7e967cc5e
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 13:57:21 +0300

    Bind LoadPlatformStartStatesOnInitializeRequestSystem to PlatformsSharedModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Platforms/PlatformsSharedModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Platforms/PlatformsSharedModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Platforms.Shared.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Platforms
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Platforms
     {
         public PlatformsSharedModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<LoadPlatformStartStatesOnInitializeRequestSystem>();
         }
     }
 }
