a0c558a3e0b6cf724a491de81def38ecff5dfd4c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 10:46:31 +0300

    Create PlatformsSharedAspect with platforms pool and iterator

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedAspect.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Features.Platforms.Shared.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared
+{
+    public class PlatformsSharedAspect : ProtoAspectInject
+    {
+        public readonly ProtoPool<PlatformComponent> PlatformComponentPool;
+        public readonly ProtoIt Platforms = new (It.Inc<PlatformComponent>());
+    }
+}
