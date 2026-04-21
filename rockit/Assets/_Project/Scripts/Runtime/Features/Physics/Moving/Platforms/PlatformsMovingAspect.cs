c74751327b1368b27915bf7b89e0717f42287c24
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 16:23:07 +0300

    Create components PlatformPositionChangeComponent, PlatformRotationChangeComponent and PlatformScaleChangeComponent with only field "Target". Add their pools and iterators to PlatformsMovingAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingAspect.cs
@@ -7,6 +7,12 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms
     public class PlatformsMovingAspect : ProtoAspectInject
     {
         public readonly ProtoPool<PlatformUpdatesBufferComponent> PlatformUpdatesBufferComponentPool;
+        public readonly ProtoPool<PlatformPositionChangeComponent> PlatformPositionChangeComponentPool;
+        public readonly ProtoPool<PlatformRotationChangeComponent> PlatformRotationChangeComponentPool;
+        public readonly ProtoPool<PlatformScaleChangeComponent> PlatformScaleChangeComponentPool;
         public readonly ProtoIt PlatformUpdatesBuffers = new (It.Inc<PlatformUpdatesBufferComponent>());
+        public readonly ProtoIt PlatformPositionChanges = new (It.Inc<PlatformPositionChangeComponent>());
+        public readonly ProtoIt PlatformRotationChanges = new (It.Inc<PlatformRotationChangeComponent>());
+        public readonly ProtoIt PlatformScaleChanges = new (It.Inc<PlatformScaleChangeComponent>());
     }
 }
