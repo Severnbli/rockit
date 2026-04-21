69f5276fad71b9a24fb074d38e5dc2a664e65585
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 11:35:56 +0300

    Create SequenceElement generic class that allow to create linked sequence of elements

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Tools/SequenceElement.cs
@@ -0,0 +1,9 @@
+﻿namespace _Project.Scripts.Runtime.Shared.Tools
+{
+    public class SequenceElement<T>
+    {
+        public SequenceElement<T> Next;
+        public SequenceElement<T> Prev;
+        public T Value;
+    }
+}
