a10b0ecad5821a5bc84bd7194524f5b56743f061
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 16:29:17 +0300

    Add iterators "PositionChangeCreatables", "RotationChangeCreatables" and "ScaleChangeCreatables"

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Components;
+using _Project.Scripts.Runtime.Features.Platforms.Shared.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -14,5 +15,8 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms
         public readonly ProtoIt PlatformPositionChanges = new (It.Inc<PlatformPositionChangeComponent>());
         public readonly ProtoIt PlatformRotationChanges = new (It.Inc<PlatformRotationChangeComponent>());
         public readonly ProtoIt PlatformScaleChanges = new (It.Inc<PlatformScaleChangeComponent>());
+        public readonly ProtoItExc PositionChangeCreatables = new (It.Inc<PlatformComponent, PlatformUpdatesBufferComponent>(), It.Exc<PlatformPositionChangeComponent>());
+        public readonly ProtoItExc RotationChangeCreatables = new (It.Inc<PlatformComponent, PlatformUpdatesBufferComponent>(), It.Exc<PlatformRotationChangeComponent>());
+        public readonly ProtoItExc ScaleChangeCreatables = new (It.Inc<PlatformComponent, PlatformUpdatesBufferComponent>(), It.Exc<PlatformScaleChangeComponent>());
     }
 }
