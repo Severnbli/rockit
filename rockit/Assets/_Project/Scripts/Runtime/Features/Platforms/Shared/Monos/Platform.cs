57c6ad1a7bf8a6d9d2a15f491336e0fbd4c73311
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 10:43:07 +0300

    Create Platform mono with "_positionStates", "_rotationStates" and "_scaleStates" fields and its properties

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Monos/Platform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Runtime.Shared.Extensions;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Monos
+{
+    public class Platform : MonoBehaviour
+    {
+        [SerializeField] private GameObject _positionStates;
+        [SerializeField] private GameObject _rotationStates;
+        [SerializeField] private GameObject _scaleStates;
+
+        public List<Transform> PositionStates => _positionStates.GetChildrenComponents<Transform>();
+        public List<Transform> RotationStates => _rotationStates.GetChildrenComponents<Transform>();
+        public List<Transform> ScaleStates => _scaleStates.GetChildrenComponents<Transform>();
+    }
+}
