bd9e3285ecf09f3d4507149b2e4c58422e045d61
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 17:21:47 +0300

    Create MovePlatformByPlatformPositionChangeSystem, RotatePlatformByPlatformRotationChangeSystem and ScalePlatformByPlatformScaleChangeSystem, bind they to PlatformsMovingModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Systems/RotatePlatformByPlatformRotationChangeSystem.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Systems
+{
+    public sealed class RotatePlatformByPlatformRotationChangeSystem : IProtoFixedRunSystem
+    {
+        public void FixedRun()
+        {
+            
+        }
+    }
+}
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Systems/ScalePlatformByPlatformScaleChangeSystem.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Systems
+{
+    public class ScalePlatformByPlatformScaleChangeSystem : IProtoFixedRunSystem
+    {
+        public void FixedRun()
+        {
+            
+        }
+    }
+}
