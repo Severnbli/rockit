024c2197455895dd62a3fd5eeb5aec920197fbdb
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 14:13:35 +0300

    Create PlatformsMovingModule, register it to GameSceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/Moving/PlatformsMovingModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics.Moving
+{
+    public sealed class PlatformsMovingModule : BaseModule<PlatformsMovingModule>
+    {
+        public PlatformsMovingModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
