dd5a60b07fcc1c0a626de147a00623bb0d4fddba
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 10:54:42 +0300

    Add "_types" field to Platform mono

--- rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Monos/Platform.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Monos/Platform.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using _Project.Scripts.Runtime.Features.Platforms.Shared.Enums;
 using _Project.Scripts.Runtime.Shared.Extensions;
 using UnityEngine;
 
@@ -9,9 +10,11 @@ namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Monos
         [SerializeField] private GameObject _positionStates;
         [SerializeField] private GameObject _rotationStates;
         [SerializeField] private GameObject _scaleStates;
+        [SerializeField] private PlatformType[] _types;
 
         public List<Transform> PositionStates => _positionStates.GetChildrenComponents<Transform>();
         public List<Transform> RotationStates => _rotationStates.GetChildrenComponents<Transform>();
         public List<Transform> ScaleStates => _scaleStates.GetChildrenComponents<Transform>();
+        public PlatformType[] Types => _types;
     }
 }
