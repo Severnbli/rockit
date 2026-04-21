f1e74f7d7c820bb9719c74a28ca55aae20e27ec0
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 14:37:49 +0300

    Create PlatformUpdatesBufferComponent with "PositionUpdates", "RotationUpdates" and "ScaleUpdates" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Components/PlatformUpdatesBufferComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Components
+{
+    public struct PlatformUpdatesBufferComponent
+    {
+        public int PositionUpdates;
+        public int RotationUpdates;
+        public int ScaleUpdates;
+    }
+}
