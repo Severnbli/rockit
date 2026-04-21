feb72d27b6536d66610137e2de9a0895728f8090
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 11:28:35 +0300

    Create UpdatePlatformPositionRequest, add its pool and iterator to PlatformsMovingRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingRequestsAspect.cs
@@ -1,9 +1,12 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms
 {
     public class PlatformsMovingRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<UpdatePlatformPositionRequest> UpdatePlatformPositionRequestPool;
+        public readonly ProtoIt UpdatePlatformPositionRequests = new (It.Inc<UpdatePlatformPositionRequest>());
     }
 }
