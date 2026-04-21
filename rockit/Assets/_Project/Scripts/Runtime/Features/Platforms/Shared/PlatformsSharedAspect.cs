9a5e5e16ba3cea703dde536800d47c8ffcee4b40
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 11:20:44 +0300

    Add platform tags pools and iterators to PlatformsSharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Features.Platforms.Shared.Components;
+using _Project.Scripts.Runtime.Features.Platforms.Shared.Tags;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -7,6 +8,12 @@ namespace _Project.Scripts.Runtime.Features.Platforms.Shared
     public class PlatformsSharedAspect : ProtoAspectInject
     {
         public readonly ProtoPool<PlatformComponent> PlatformComponentPool;
+        public readonly ProtoPool<PositionPlatformTag> PositionPlatformTagPool;
+        public readonly ProtoPool<RotationPlatformTag> RotationPlatformTagPool;
+        public readonly ProtoPool<ScalePlatformTag> ScalePlatformTagPool;
         public readonly ProtoIt Platforms = new (It.Inc<PlatformComponent>());
+        public readonly ProtoIt PositionPlatforms = new (It.Inc<PlatformComponent, PositionPlatformTag>());
+        public readonly ProtoIt RotationPlatforms = new (It.Inc<PlatformComponent, RotationPlatformTag>());
+        public readonly ProtoIt ScalePlatforms = new (It.Inc<PlatformComponent, ScalePlatformTag>());
     }
 }
