71ec1a4a1d166f7854fd92e3c86cfb35c0198ec8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 16:55:45 +0300

    Create SpecifyPositionChangeOnRunSystem, bind it to PlatformsMovingModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Systems/SpecifyPositionChangeOnRunSystem.cs
@@ -0,0 +1,37 @@
+﻿using _Project.Scripts.Runtime.Features.Platforms.Shared;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Systems
+{
+    public sealed class SpecifyPositionChangeOnRunSystem : IProtoRunSystem
+    {
+        [DI] private readonly PlatformsMovingAspect _pmAspect;
+        [DI] private readonly PlatformsSharedAspect _psAspect;
+
+        public void Run()
+        {
+            foreach (var e in _pmAspect.PositionChangeCreatables)
+            {
+                ref var cbComponent = ref _pmAspect.ChangesBufferComponentPool.Get(e);
+                if (cbComponent.PositionUpdates <= 0) continue;
+                
+                ref var pComponent = ref _psAspect.PlatformComponentPool.Get(e);
+                var target = pComponent.CurrPosState == null 
+                        ? pComponent.StartPosState
+                        : pComponent.CurrPosState.Next;
+                
+                if (target == null)
+                {
+                    cbComponent.PositionUpdates = 0;
+                    continue;
+                }
+                
+                pComponent.CurrPosState = target;
+
+                ref var pcComponent = ref _pmAspect.PositionChangeComponentPool.Add(e);
+                pcComponent.Target = target.Value;
+            }
+        }
+    }
+}
