658faaa56f80444679b5739cbaee4c619ba5b795
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 13:34:25 +0300

    Update LoadPlatformPositionStatesOnInitializeRequestSystem logic to create sequence of positions

--- rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Systems/LoadPlatformPositionStatesOnInitializeRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Systems/LoadPlatformPositionStatesOnInitializeRequestSystem.cs
@@ -1,9 +1,13 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+﻿using System.Collections.Generic;
+using System.Linq;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
 using _Project.Scripts.Runtime.Shared.Extensions;
+using _Project.Scripts.Runtime.Shared.Tools;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Systems
 {
@@ -28,7 +32,31 @@ namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Systems
 
                 ref var pComponent = ref _psAspect.PlatformComponentPool.Get(tarE);
                 var pStates = pComponent.Platform.PositionStates;
+
+                if (!pStates.Any()) return;
+                
+                CreateSequence(pStates, out var first);
+                pComponent.CurPosState = first;
             }
+        }
+
+        private void CreateSequence(List<Transform> pStates, out SequenceElement<Vector3> first)
+        {
+            first = new SequenceElement<Vector3>(pStates[0].position);
+            var prev = first;
+
+            for (var i = 1; i < pStates.Count; i++)
+            {
+                var curr = new SequenceElement<Vector3>(pStates[i].position);
+                prev.Next = curr;
+                curr.Prev = prev;
+                prev = curr;
+            }
+
+            if (prev == first) return;
+            
+            prev.Next = first;
+            first.Prev = prev;
         }
     }
 }
