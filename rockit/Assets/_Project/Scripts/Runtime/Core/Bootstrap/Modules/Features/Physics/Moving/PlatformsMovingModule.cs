15f5d45aeac451428a2b57fcd934204c7b1754c5
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 14:59:33 +0300

    Replace separated position and rotation PlatformUpdatesBuffer updating with single UpdatePlatformUpdatesBufferOnUpdateStateRequestSystem

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/Moving/PlatformsMovingModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/Moving/PlatformsMovingModule.cs
@@ -14,8 +14,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics.Movin
             base.BindSystems();
             
             BindSystem<SendUpdatePlatformStatesRequestsOnInitializeRequestSystem>();
-            BindSystem<UpdateBufferOnUpdatePlatformPositionRequestSystem>();
-            BindSystem<UpdateBufferOnUpdatePlatformRotationRequestSystem>();
+            BindSystem<UpdatePlatformUpdatesBufferOnUpdateStateRequestSystem>();
         }
     }
 }
