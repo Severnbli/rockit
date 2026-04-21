1fb429f765131f552e41100b287e69bf67126789
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 11:29:28 +0300

    Create UpdatePlatformRotationRequest, add it pool and iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingRequestsAspect.cs
@@ -7,6 +7,8 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms
     public class PlatformsMovingRequestsAspect : ProtoAspectInject
     {
         public readonly ProtoPool<UpdatePlatformPositionRequest> UpdatePlatformPositionRequestPool;
+        public readonly ProtoPool<UpdatePlatformRotationRequest> UpdatePlatformRotationRequestPool;
         public readonly ProtoIt UpdatePlatformPositionRequests = new (It.Inc<UpdatePlatformPositionRequest>());
+        public readonly ProtoIt UpdatePlatformRotationRequests = new (It.Inc<UpdatePlatformRotationRequest>());
     }
 }
