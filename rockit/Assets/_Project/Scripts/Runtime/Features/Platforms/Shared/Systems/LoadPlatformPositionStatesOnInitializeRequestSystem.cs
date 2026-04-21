a327cb7146c14603a65fbb3b78fad4ac830967a1
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 13:15:38 +0300

    Create LoadPlatformPositionStatesOnInitializeRequestSystem

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Systems/LoadPlatformPositionStatesOnInitializeRequestSystem.cs
@@ -0,0 +1,34 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using _Project.Scripts.Runtime.Shared.Extensions;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Systems
+{
+    public sealed class LoadPlatformPositionStatesOnInitializeRequestSystem : IProtoInitSystem, IProtoRunSystem
+    {
+        [DIRequests] private readonly SharedRequestsAspect _srAspect;
+        [DIRequests] private readonly CoreRequestsAspect _crAspect;
+        [DI] private readonly PlatformsSharedAspect _psAspect;
+        private ProtoWorld _world;
+        
+        public void Init(IProtoSystems systems)
+        {
+            _world = systems.World();
+        }
+
+        public void Run()
+        {
+            foreach (var reqE in _srAspect.InitializeRunRequests)
+            {
+                if (!_crAspect.TryCompareRequestWorld(reqE, _world, out var tarE)) continue;
+                if (!_psAspect.Platforms.Has(tarE)) continue;
+
+                ref var pComponent = ref _psAspect.PlatformComponentPool.Get(tarE);
+                var pStates = pComponent.Platform.PositionStates;
+            }
+        }
+    }
+}
