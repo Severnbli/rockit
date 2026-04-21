dec959deaa128c4ce2ee0f8269ab31550a2f6ccb
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-21 11:16:58 +0300

    Remove PlatformType enum

--- rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Monos/Platform.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Monos/Platform.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using _Project.Scripts.Runtime.Features.Platforms.Shared.Enums;
 using _Project.Scripts.Runtime.Shared.Extensions;
 using UnityEngine;
 
@@ -10,11 +9,9 @@ namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Monos
         [SerializeField] private GameObject _positionStates;
         [SerializeField] private GameObject _rotationStates;
         [SerializeField] private GameObject _scaleStates;
-        [SerializeField] private PlatformType[] _types;
 
         public List<Transform> PositionStates => _positionStates.GetChildrenComponents<Transform>();
         public List<Transform> RotationStates => _rotationStates.GetChildrenComponents<Transform>();
         public List<Transform> ScaleStates => _scaleStates.GetChildrenComponents<Transform>();
-        public PlatformType[] Types => _types;
     }
 }
