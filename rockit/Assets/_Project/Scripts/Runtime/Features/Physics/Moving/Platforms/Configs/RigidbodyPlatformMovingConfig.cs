7918de81b0aa5e8569f80f93632a00e56f87f688
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-22 10:56:17 +0300

    Create RigidbodyPlatformMovingConfig that inherited from PlatformMovingConfig

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Configs/RigidbodyPlatformMovingConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Configs
+{
+    public class RigidbodyPlatformMovingConfig : PlatformMovingConfig<RigidbodyPlatformMovingConfig>
+    {
+        
+    }
+}
