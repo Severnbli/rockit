1d5736aa12341bcefd4b9b9e2a35ed463002b44c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-22 10:55:03 +0300

    Create generic PlatformMovingConfig that inherited from ScriptableObjectAutoInstaller with "_posChangeSpeed", "_rotChangeSpeed" and "_scaleChangeSpeed" fields ant its property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Configs/PlatformMovingConfig.cs
@@ -0,0 +1,17 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Configs
+{
+    public class PlatformMovingConfig<T> : ScriptableObjectAutoInstaller<T> 
+        where T : PlatformMovingConfig<T>
+    {
+        [SerializeField] private float _posChangeSpeed = 5f;
+        [SerializeField] private float _rotChangeSpeed = 3f;
+        [SerializeField] private float _scaleChangeSpeed = 2f;
+        
+        public float PosChangeSpeed => _posChangeSpeed;
+        public float RotChangeSpeed => _rotChangeSpeed;
+        public float ScaleChangeSpeed => _scaleChangeSpeed;
+    }
+}
