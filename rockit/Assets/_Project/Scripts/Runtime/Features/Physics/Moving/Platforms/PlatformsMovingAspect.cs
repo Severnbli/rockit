784cab1e5b58aafcb67cba7b2bea4b9706c66765
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-22 11:04:28 +0300

    Add "PositionChangableRigidbodyPlatforms", "RotationChangableRigidbodyPlatforms" and "ScaleChangableRigidbodyPlatforms" iterators to the PlatformsMovingAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Components;
+using _Project.Scripts.Runtime.Features.Physics.Shared.Components;
 using _Project.Scripts.Runtime.Features.Platforms.Shared.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
@@ -18,5 +19,8 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms
         public readonly ProtoItExc PlatformPositionChangeCreatables = new (It.Inc<PlatformComponent, PlatformStatesComponent, PlatformChangesBufferComponent>(), It.Exc<PlatformPositionChangeComponent>());
         public readonly ProtoItExc PlatformRotationChangeCreatables = new (It.Inc<PlatformComponent, PlatformStatesComponent, PlatformChangesBufferComponent>(), It.Exc<PlatformRotationChangeComponent>());
         public readonly ProtoItExc PlatformScaleChangeCreatables = new (It.Inc<PlatformComponent, PlatformStatesComponent, PlatformChangesBufferComponent>(), It.Exc<PlatformScaleChangeComponent>());
+        public readonly ProtoIt PositionChangableRigidbodyPlatforms = new (It.Inc<PlatformPositionChangeComponent, Rigidbody2DComponent>());
+        public readonly ProtoIt RotationChangableRigidbodyPlatforms = new (It.Inc<PlatformRotationChangeComponent, Rigidbody2DComponent>());
+        public readonly ProtoIt ScaleChangableRigidbodyPlatforms = new (It.Inc<PlatformScaleChangeComponent, Rigidbody2DComponent>());
     }
 }
