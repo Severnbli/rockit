651efdbcc104c3f3d2e04cc4317f9f00c0f6e689
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-22 21:26:22 +0300

    Create ApplySnapByMoveSnapSystem, bind it to MovingSharedModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/Moving/MovingSharedModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Physics/Moving/MovingSharedModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Physics.Moving.Shared.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics.Moving
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Physics.Movin
     {
         public MovingSharedModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<ApplySnapByMoveSnapSystem>();
         }
     }
 }
