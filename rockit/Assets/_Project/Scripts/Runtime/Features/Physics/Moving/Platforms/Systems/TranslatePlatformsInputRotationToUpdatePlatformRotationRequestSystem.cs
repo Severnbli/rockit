2edb62c598fd2b6d872a58b791a1f600d2484983
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-22 12:00:32 +0300

    Create TranslatePlatformsInputRotationToUpdatePlatformRotationRequestSystem, bind it to PlatformsMovingModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Systems/TranslatePlatformsInputRotationToUpdatePlatformRotationRequestSystem.cs
@@ -0,0 +1,34 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Input.Services;
+using _Project.Scripts.Runtime.Features.Platforms.Shared;
+using _Project.Scripts.Runtime.Shared.Utils.Moving;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Systems
+{
+    public sealed class TranslatePlatformsInputRotationToUpdatePlatformRotationRequestSystem : IProtoInitSystem, IProtoRunSystem
+    {
+        [DIRequests] private readonly RequestsAspect _rAspect;
+        [DI] private readonly PlatformsSharedAspect _psAspect;
+        private readonly PlatformsInputService _piService;
+        private ProtoWorld _world;
+        
+        public void Init(IProtoSystems systems)
+        {
+            _world = systems.World();
+        }
+
+        public void Run()
+        {
+            if (!_piService.RotationTriggered) return;
+
+            foreach (var e in _psAspect.Platforms)
+            {
+                var packed = _world.PackEntityWithWorld(e);
+                PlatformsMovingUtils.CreateUpdatePlatformRotationRequest(_rAspect, packed);
+            }
+        }
+    }
+}
