e8d9269639d72802bb826f2f9c269d4a77f443c4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-22 10:39:15 +0300

    Separate states fields from PlatformComponent into PlatformStatesComponent

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Components/PlatformStatesComponent.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts.Runtime.Shared.Tools;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Components
+{
+    public struct PlatformStatesComponent
+    {
+        public SequenceElement<Vector3> StartPosState;
+        public SequenceElement<Vector3> CurrPosState;
+        public SequenceElement<Quaternion> StartRotState;
+        public SequenceElement<Quaternion> CurrRotState;
+        public SequenceElement<Vector3> StartScaleState;
+        public SequenceElement<Vector3> CurrScaleState;
+    }
+}
