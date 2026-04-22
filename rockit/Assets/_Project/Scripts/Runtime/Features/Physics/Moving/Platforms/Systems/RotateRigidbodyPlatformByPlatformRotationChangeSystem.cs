b82dd860979a09b532d51635b2761e2de9cdecde
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-22 11:39:27 +0300

    Define RotateRigidbodyPlatformByPlatformRotationChangeSystem logic

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Systems/RotateRigidbodyPlatformByPlatformRotationChangeSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Systems/RotateRigidbodyPlatformByPlatformRotationChangeSystem.cs
@@ -1,12 +1,50 @@
-﻿using _Project.Scripts.Runtime.Core.Systems;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using _Project.Scripts.Runtime.Core.Systems;
+using _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Configs;
+using _Project.Scripts.Runtime.Features.Physics.Shared;
+using _Project.Scripts.Runtime.Shared.Extensions;
+using _Project.Scripts.Runtime.Shared.Utils;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Systems
 {
     public sealed class RotateRigidbodyPlatformByPlatformRotationChangeSystem : IProtoFixedRunSystem
     {
+        [DI] private readonly PlatformsMovingAspect _pmAspect;
+        [DI] private readonly PhysicsSharedAspect _psAspect;
+        [DI] private readonly SharedAspect _sAspect;
+        private readonly RigidbodyPlatformMovingConfig _rpmConfig;
+        private readonly SharedPlatformMovingConfig _spmConfig;
+        private readonly TimeService _tService;
+
+        public RotateRigidbodyPlatformByPlatformRotationChangeSystem(RigidbodyPlatformMovingConfig rpmConfig,
+            SharedPlatformMovingConfig spmConfig, TimeService tService)
+        {
+            _rpmConfig = rpmConfig;
+            _spmConfig = spmConfig;
+            _tService = tService;
+        }
+
         public void FixedRun()
         {
-            
+            foreach (var e in _pmAspect.RotationChangableRigidbodyPlatforms)
+            {
+                ref var prcComponent = ref _pmAspect.PlatformRotationChangeComponentPool.Get(e);
+                ref var rComponent = ref _psAspect.Rigidbody2DComponentPool.Get(e);
+                ref var tComponent = ref _sAspect.TransformComponentPool.Get(e);
+
+                if (!QuaternionUtils.AngleLessThanValue(prcComponent.Target, tComponent.Transform.rotation,
+                        _spmConfig.PosTolerance))
+                {
+                    rComponent.Rigidbody2D.RotateTo(prcComponent.Target, _rpmConfig.RotChangeSpeed,
+                        _tService.UnscaledFixedDeltaTime);
+                    continue;
+                }
+                
+                rComponent.Rigidbody2D.MoveRotation(prcComponent.Target);
+                _pmAspect.PlatformRotationChangeComponentPool.Del(e);
+            }
         }
     }
 }
