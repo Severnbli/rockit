88279e6c41955a2af87393435293420080482cd8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-22 19:16:17 +0300

    Fix using wrong updates counter at specify platform rotation and scale changes

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Systems/SpecifyPlatformScaleChangeOnRunSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/Systems/SpecifyPlatformScaleChangeOnRunSystem.cs
@@ -14,7 +14,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Systems
             foreach (var e in _pmAspect.PlatformScaleChangeCreatables)
             {
                 ref var pcbComponent = ref _pmAspect.PlatformChangesBufferComponentPool.Get(e);
-                if (pcbComponent.PositionUpdates <= 0) continue;
+                if (pcbComponent.ScaleUpdates <= 0) continue;
                 
                 ref var psComponent = ref _psAspect.PlatformStatesComponentPool.Get(e);
                 var target = psComponent.CurrScaleState == null 
