a21a45bbda87fb944e29735214b39dceea155405
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-22 12:44:39 +0300

    Fix requests iterators at PlatformsMovingRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingRequestsAspect.cs
@@ -1,4 +1,6 @@
-﻿using _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Requests;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using _Project.Scripts.Runtime.Features.Physics.Moving.Platforms.Requests;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -9,8 +11,8 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms
         public readonly ProtoPool<UpdatePlatformPositionRequest> UpdatePlatformPositionRequestPool;
         public readonly ProtoPool<UpdatePlatformRotationRequest> UpdatePlatformRotationRequestPool;
         public readonly ProtoPool<UpdatePlatformScaleRequest> UpdatePlatformScaleRequestPool;
-        public readonly ProtoIt UpdatePlatformPositionRequests = new (It.Inc<UpdatePlatformPositionRequest>());
-        public readonly ProtoIt UpdatePlatformRotationRequests = new (It.Inc<UpdatePlatformRotationRequest>());
-        public readonly ProtoIt UpdatePlatformScaleRequests = new (It.Inc<UpdatePlatformScaleRequest>());
+        public readonly ProtoIt UpdatePlatformPositionRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, UpdatePlatformPositionRequest>());
+        public readonly ProtoIt UpdatePlatformRotationRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, UpdatePlatformRotationRequest>());
+        public readonly ProtoIt UpdatePlatformScaleRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, UpdatePlatformScaleRequest>());
     }
 }
