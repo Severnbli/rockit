74b9671fd2f4d3c471a49e43e7f0535fe67d4164
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 14:41:58 +0300

    Create LocalizationAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/LocalizationAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization
+{
+    public sealed class LocalizationAspect : ProtoAspectInject
+    {
+        
+    }
+}
