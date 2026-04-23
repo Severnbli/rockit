a731987305490e4e248c20c3cba210c712fd4beb
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 14:20:13 +0300

    Expand IDataStorageKeyProvider with "GetKey" method with "type" parameter

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Core/IDataStorageKeyProvider.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Core/IDataStorageKeyProvider.cs
@@ -1,7 +1,10 @@
-﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Core
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Core
 {
     public interface IDataStorageKeyProvider
     {
         string GetKey<T>();
+        string GetKey(Type type);
     }
 }
