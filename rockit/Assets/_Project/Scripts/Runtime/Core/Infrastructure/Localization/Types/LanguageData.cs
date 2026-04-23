a3ba8cbd8ff8e2afe8f00407110628ed9ba8d58b
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 13:54:26 +0300

    Create LanguageData serializable class with "LanguageCode" and "Entries" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/Types/LanguageData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization.Types
+{
+    [Serializable]
+    public class LanguageData
+    {
+        public string LanguageCode = "en";
+        public List<LocalizationEntry> Entries = new();
+    }
+}
