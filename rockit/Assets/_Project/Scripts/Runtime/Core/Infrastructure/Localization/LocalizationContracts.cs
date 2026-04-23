a48292ef633022d3c4dce35e869fe4d5b15bcd29
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 23:18:08 +0300

    Move "LanguageDataFileName" field to LocalizationContracts and make it public

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/LocalizationContracts.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/LocalizationContracts.cs
@@ -3,5 +3,6 @@
     public static class LocalizationContracts
     {
         public const string DefaultLanguage = "en";
+        public const string LanguageDataFileName = "localizations";
     }
 }
