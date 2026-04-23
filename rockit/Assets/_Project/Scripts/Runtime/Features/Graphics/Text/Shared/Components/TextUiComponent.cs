9a39f9edac759efca51cbf12a7d278954f32bdfd
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 15:59:38 +0300

    Add authoring attributes to TextUiComponent

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Text/Shared/Components/TextUiComponent.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Text/Shared/Components/TextUiComponent.cs
@@ -1,7 +1,10 @@
-﻿using TMPro;
+﻿using System;
+using Leopotam.EcsProto.Unity;
+using TMPro;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Text.Shared.Components
 {
+    [Serializable, ProtoUnityAuthoring]
     public struct TextUiComponent
     {
         public TextMeshProUGUI Text;
