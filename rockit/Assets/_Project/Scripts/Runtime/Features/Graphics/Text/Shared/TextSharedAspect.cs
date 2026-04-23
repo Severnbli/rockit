dabcd8422858df17e0cb08a692dbf40b928c98d4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 15:45:06 +0300

    Create TextUIComponent, add it pool and iterator to TextSharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Text/Shared/TextSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Text/Shared/TextSharedAspect.cs
@@ -1,9 +1,12 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Text.Shared.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Text.Shared
 {
     public sealed class TextSharedAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<TextUiComponent> TextUiComponentPool;
+        public readonly ProtoIt TextUis = new (It.Inc<TextUiComponent>());
     }
 }
