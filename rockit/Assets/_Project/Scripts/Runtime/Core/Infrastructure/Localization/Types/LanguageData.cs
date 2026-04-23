6e9dc3b0fcd9ff072aed4a068beecc3e2bbc2615
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 14:34:42 +0300

    Make LocalizationEntry and LanguageData sealed

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/Types/LanguageData.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/Types/LanguageData.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization.Types
 {
     [Serializable]
-    public class LanguageData
+    public sealed class LanguageData
     {
         public string LanguageCode = "en";
         public List<LocalizationEntry> Entries = new();
