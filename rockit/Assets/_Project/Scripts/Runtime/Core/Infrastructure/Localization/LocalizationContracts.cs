aa57121d6ceb89c7912c6e220be6b19ae5dbe4f1
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 14:07:42 +0300

    Create LocalizationContracts with only field "DefaultLanguage"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/LocalizationContracts.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization
+{
+    public static class LocalizationContracts
+    {
+        public static string DefaultLanguage = "en";
+    }
+}
