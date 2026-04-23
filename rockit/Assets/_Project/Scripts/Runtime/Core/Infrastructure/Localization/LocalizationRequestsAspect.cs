add2e465385dd779f2334ae1d4c02f8565686a7f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 14:55:49 +0300

    Create ChangeLanguageRequest, add it pool and iterator to LocalizationRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/LocalizationRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/LocalizationRequestsAspect.cs
@@ -1,9 +1,14 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization
 {
     public sealed class LocalizationRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<ChangeLanguageRequest> ChangeLanguageRequestPool;
+        public readonly ProtoIt ChangeLanguageRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, ChangeLanguageRequest>());
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/Requests/ChangeLanguageRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization.Requests
+{
+    public struct ChangeLanguageRequest
+    {
+        public string LanguageCode;
+    }
+}
