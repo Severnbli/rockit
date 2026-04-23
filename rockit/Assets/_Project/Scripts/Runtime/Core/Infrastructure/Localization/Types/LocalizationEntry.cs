e6929f3de46a2e7b555b7ce4ae081836a18e4f75
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 13:53:54 +0300

    Create LocalizationEntry serializable class with "Key" and "Value" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/Types/LocalizationEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization.Types
+{
+    [Serializable]
+    public class LocalizationEntry
+    {
+        public string Key;
+        public string Value;
+    }
+}
