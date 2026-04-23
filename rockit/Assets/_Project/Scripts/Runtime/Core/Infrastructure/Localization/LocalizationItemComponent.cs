76d5262690663a5b12137214581cd8cc4838f0f3
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 14:44:23 +0300

    Create LocalizationItemComponent, add it pool and iterator to LocalizationAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/LocalizationItemComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Leopotam.EcsProto.Unity;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct LocalizationItemComponent
+    {
+        public string Key;
+    }
+}
