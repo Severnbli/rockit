21bbb024ec0b5c535b6907efe46e73f778190174
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 15:57:52 +0300

    Create UpdateTextUiOnUpdateLocalizationItemRequestSystem, bind it to TextSharedModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Text/TextSharedModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Text/TextSharedModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Graphics.Text.Shared.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Text
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Text
     {
         public TextSharedModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<UpdateTextUiOnUpdateLocalizationItemRequestSystem>();
         }
     }
 }
