fcf31a51af7e65db92eb686a86fef730b319ef67
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 15:45:06 +0300

    Create TextUIComponent, add it pool and iterator to TextSharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Text/Shared/Components/TextUiComponent.cs
@@ -0,0 +1,9 @@
+﻿using TMPro;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Text.Shared.Components
+{
+    public struct TextUiComponent
+    {
+        public TextMeshProUGUI Text;
+    }
+}
