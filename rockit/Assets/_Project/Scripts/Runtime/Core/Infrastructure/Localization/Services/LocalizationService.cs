f10f59eec51c057c6f3087cd4c86784c9d2346f7
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 13:59:48 +0300

    Create LocalizationService with "LangData" and "CurrLang" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/Services/LocalizationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Types;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization.Services
+{
+    public sealed class LocalizationService
+    {
+        public Dictionary<string, LanguageData> LangData;
+        public LanguageData CurrLang;
+    }
+}
