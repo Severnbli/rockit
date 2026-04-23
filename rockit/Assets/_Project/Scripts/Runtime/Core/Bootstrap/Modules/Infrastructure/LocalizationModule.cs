486507c72fdb57f86ace420a6a2d89737a50eb53
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 14:38:30 +0300

    Create LoadLocalizationServiceOnInitSystem and bind it to LocalizationModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/LocalizationModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/LocalizationModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure
     {
         public LocalizationModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<LoadLocalizationServiceOnInitSystem>();
         }
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/Systems/LoadLocalizationServiceOnInitSystem.cs
@@ -0,0 +1,31 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Services;
+using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Types;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Core;
+using _Project.Scripts.Runtime.Shared.Extensions;
+using _Project.Scripts.Runtime.Shared.Utils;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization.Systems
+{
+    public sealed class LoadLocalizationServiceOnInitSystem : IProtoInitSystem
+    {
+        private readonly LocalizationService _lService;
+        private readonly DataProvider _dProvider;
+
+        public LoadLocalizationServiceOnInitSystem(LocalizationService lService, DataProvider dProvider)
+        {
+            _lService = lService;
+            _dProvider = dProvider;
+        }
+
+        public void Init(IProtoSystems systems)
+        {
+            var lData = LocalizationUtils.GetLanguageDataDictionary();
+            _lService.LangData = lData;
+
+            if (lData.TryGetByKeyOrFirst(_dProvider.Language.Code, out _lService.CurrLang)) return;
+
+            _lService.CurrLang = new LanguageData();
+        }
+    }
+}
