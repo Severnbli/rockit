fc76540412a502a74d8fbe6247a86db3b60037b3
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 13:52:03 +0300

    Create LocalizationUtils with "GetLanguageDataPath", "WriteLanguageData" and "GetLanguageData" methods

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/LocalizationUtils.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Shared.Utils
+{
+    public static class LocalizationUtils
+    {
+        private const string LanguageDataFileName = "localizations";
+        
+        public static string GetLanguageDataPath()
+        {
+            return System.IO.Path.Combine(Application.streamingAssetsPath, $"{LanguageDataFileName}.json");
+        }
+
+        public static void WriteLanguageData(string json)
+        {
+            var path = GetLanguageDataPath();
+            System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(path) ?? "");
+            System.IO.File.WriteAllText(path, json);
+        }
+
+        public static string GetLanguageData()
+        {
+            var path = GetLanguageDataPath();
+            return !System.IO.File.Exists(path) ? "" : System.IO.File.ReadAllText(path);
+        }
+    }
+}
