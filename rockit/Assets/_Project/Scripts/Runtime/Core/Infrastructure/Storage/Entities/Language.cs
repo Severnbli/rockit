9e23465c71bf72122753cf4d0fe85e109870db49
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-23 14:14:23 +0300

    Create Language entity class

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/Language.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using _Project.Scripts.Runtime.Core.Infrastructure.Localization;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities
+{
+    [Serializable]
+    public class Language
+    {
+        public string Code = LocalizationContracts.DefaultLanguage;
+    }
+}
