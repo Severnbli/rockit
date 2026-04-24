8fb117652b3cdd460b470460d8d44af6c3a7d624
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-24 14:52:23 +0300

    Create GlitchEffectsModule that inherited from BaseModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Effects/GlitchEffectsModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Effects
+{
+    public sealed class GlitchEffectsModule : BaseModule<GlitchEffectsModule>
+    {
+        public GlitchEffectsModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
