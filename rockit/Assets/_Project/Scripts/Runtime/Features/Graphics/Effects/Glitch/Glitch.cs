405520d14b89496e981f0fb7ad0401d7487da555
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-24 13:59:06 +0300

    Create Glitch that inherited from ScriptableRenderFeature

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Glitch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch
+{
+    public class Glitch : ScriptableRendererFeature
+    {
+        [Serializable]
+        public class GlitchSettings
+        {
+            public Material BlitMaterial;
+            [Range(0, 1)] public float ChromaticGlitch;
+            [Range(0, 1)] public float FrameGlitch;
+            [Range(0, 1)] public float PixelGlitch;
+        }
+
+        public GlitchSettings settings = new ();
+
+        private GlitchPass m_Pass;
+
+        public override void Create()
+        {
+            m_Pass = new GlitchPass(
+                settings.BlitMaterial,
+                settings.ChromaticGlitch,
+                settings.FrameGlitch,
+                settings.PixelGlitch,
+                name
+            );
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (settings.BlitMaterial == null) return;
+
+            renderer.EnqueuePass(m_Pass);
+        }
+    }
+}
