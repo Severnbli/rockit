ac4ecc4d20644f033c8f293ff0caa75e75aba333
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-24 14:50:32 +0300

    Create GlitchService with "Glitch", "Chromatic01", "Frame01" and "Pixel01" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Services/GlitchService.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Renderer;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Services
+{
+    public sealed class GlitchService
+    {
+        public GlitchFeature Glitch;
+        public float Chromatic01;
+        public float Frame01;
+        public float Pixel01;
+    }
+}
