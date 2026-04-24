9beb0e657223f3ae01a46ca820ee2959f78cada4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-24 14:52:53 +0300

    Bind GlitchService to GlitchEffectsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Effects/GlitchEffectsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Effects/GlitchEffectsModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Services;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Effects
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Effe
     {
         public GlitchEffectsModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindServices()
+        {
+            base.BindServices();
+            
+            BindService<GlitchService>();
         }
     }
 }
