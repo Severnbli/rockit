157ded8e67bd5ba927fab21e9856a960abf36a0c
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-25 19:42:04 +0300

    Create ClampGlitchServiceFieldsOnRunSystem, add bind it before UpdateGlitchServiceFeatureOnRunSystem at GlitchEffectsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Effects/GlitchEffectsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Effects/GlitchEffectsModule.cs
@@ -22,6 +22,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Effe
             base.BindSystems();
             
             BindSystem<LoadGlitchServiceFeatureOnInitSystem>();
+            BindSystem<ClampGlitchServiceFieldsOnRunSystem>();
             BindSystem<UpdateGlitchServiceFeatureOnRunSystem>();
         }
     }
