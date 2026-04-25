e4c871d7541cdc6e70345743c26acb9ae12498bd
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-25 19:42:04 +0300

    Create ClampGlitchServiceFieldsOnRunSystem, add bind it before UpdateGlitchServiceFeatureOnRunSystem at GlitchEffectsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Systems/ClampGlitchServiceFieldsOnRunSystem.cs
@@ -0,0 +1,23 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Services;
+using Leopotam.EcsProto;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Systems
+{
+    public sealed class ClampGlitchServiceFieldsOnRunSystem : IProtoRunSystem
+    {
+        private readonly GlitchService _gService;
+
+        public ClampGlitchServiceFieldsOnRunSystem(GlitchService gService)
+        {
+            _gService = gService;
+        }
+
+        public void Run()
+        {
+            _gService.Chromatic01 = Mathf.Clamp01(_gService.Chromatic01);
+            _gService.Frame01 = Mathf.Clamp01(_gService.Frame01);
+            _gService.Pixel01 = Mathf.Clamp01(_gService.Pixel01);
+        }
+    }
+}
