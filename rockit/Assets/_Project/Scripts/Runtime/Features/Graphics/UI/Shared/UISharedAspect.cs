e3f8a4b3e161ce37bcfaadec0dee623b9ea863f6
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-26 13:56:17 +0300

    Create UISharedAspect, add it to DomainAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UISharedAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared
+{
+    public sealed class UISharedAspect : ProtoAspectInject
+    {
+        
+    }
+}
