929786e83fb7ea745b5e88b9852bcc8e1c9d8da1
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 12:45:38 +0300

    Create ButtonsSharedModule, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/Buttons/ButtonsSharedModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI.Buttons
+{
+    public sealed class ButtonsSharedModule : BaseModule<ButtonsSharedModule>
+    {
+        public ButtonsSharedModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
