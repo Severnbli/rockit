c86a6e7d426ad6ac917399d464cd4d31db6dbc62
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 11:37:27 +0300

    Create ButtonsSharedAspect, add it to UISharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UISharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UISharedAspect.cs
@@ -1,9 +1,10 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared
 {
     public sealed class UISharedAspect : ProtoAspectInject
     {
-        
+        public readonly ButtonsSharedAspect ButtonsSharedAspect;
     }
 }
