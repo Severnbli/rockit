397cf4ffc376035a107a8a6df8eb79b33612264b
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 11:54:40 +0300

    Rename CloseAppOnCloseAppRequestSystem to SwitchToProjectCloseStateOnCloseAppRequestSystem. Update logic to call for change state on this condition

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Systems/SwitchToProjectCloseStateOnCloseAppRequestSystem.cs
@@ -0,0 +1,26 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.States;
+using _Project.Scripts.Runtime.Core.Bootstrap.States.Project;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using Cysharp.Threading.Tasks;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Systems
+{
+    public sealed class SwitchToProjectCloseStateOnCloseAppRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly SharedRequestsAspect _srAspect;
+        private readonly IStateMachine _sMachine;
+
+        public SwitchToProjectCloseStateOnCloseAppRequestSystem(IStateMachine sMachine)
+        {
+            _sMachine = sMachine;
+        }
+
+        public void Run()
+        {
+            if (_srAspect.CloseAppRequests.IsEmptySlow()) return;
+
+            _sMachine.ChangeState<IProjectCloseState>().Forget();
+        }
+    }
+}
