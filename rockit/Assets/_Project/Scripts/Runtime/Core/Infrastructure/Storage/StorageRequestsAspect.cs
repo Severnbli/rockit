1283dc932f782876ed2ed0fdacbe9468561e254e
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 12:05:01 +0300

    Create SaveTrackedDataRequest, add it pool and iterator

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/StorageRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/StorageRequestsAspect.cs
@@ -1,9 +1,14 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage
 {
     public sealed class StorageRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<SaveTrackedDataRequest> SaveTrackedDataRequestPool;
+        public readonly ProtoIt SaveTrackedDataRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, SaveTrackedDataRequest>());
     }
 }
