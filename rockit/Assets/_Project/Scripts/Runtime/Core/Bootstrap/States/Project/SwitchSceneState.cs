f2dac99844dfa63d02f13b8460e4b74d33cd64bb
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 19:28:04 +0300

    Create SwitchSceneState

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Project/SwitchSceneState.cs
@@ -0,0 +1,17 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Project
+{
+    public class SwitchSceneState : IProjectState
+    {
+        public async UniTask OnEnter()
+        {
+            await UniTask.CompletedTask;
+        }
+
+        public async UniTask OnLeave()
+        {
+            await UniTask.CompletedTask;
+        }
+    }
+}
