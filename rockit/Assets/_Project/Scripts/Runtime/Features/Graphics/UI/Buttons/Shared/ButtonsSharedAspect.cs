bc79ed53a75ce2368dfcacdf93f3d227315398b9
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 12:49:09 +0300

    Create ButtonComponent with only "Button" field and authoring attribute, add it pool and iterator to ButtonsSharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Shared/ButtonsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Shared/ButtonsSharedAspect.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared.Tags;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared.Components;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared.Tags;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -7,6 +8,8 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared
     public sealed class ButtonsSharedAspect : ProtoAspectInject
     {
         public readonly ProtoPool<CloseAppButtonTag> CloseAppButtonTagPool;
+        public readonly ProtoPool<ButtonComponent> ButtonComponentPool;
         public readonly ProtoIt CloseAppButtonTags = new (It.Inc<CloseAppButtonTag>());
+        public readonly ProtoIt Buttons = new (It.Inc<ButtonComponent>());
     }
 }
