bc36a0e6af7db588ae678f05de8be03ae51cb924
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 17:06:12 +0300

    Create LevelButtonComponent, add it pool and iterator to ButtonsSharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Shared/Components/LevelButtonComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Leopotam.EcsProto.Unity;
+using TMPro;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared.Components
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct LevelButtonComponent
+    {
+        public TextMeshProUGUI IdText;
+    }
+}
