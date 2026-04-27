1638c069ff65da7c9360069419791e620cc6e42a
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 23:04:54 +0300

    Define "OnEnter" method at SwitchSceneState

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Project/SwitchSceneState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Project/SwitchSceneState.cs
@@ -1,11 +1,23 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Services;
+using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Switcher;
+using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Project
 {
     public class SwitchSceneState : IProjectState
     {
+        private readonly SceneSwitcherService _ssService;
+        private readonly ISceneSwitcher _sSwitcher;
+
+        public SwitchSceneState(SceneSwitcherService ssService, ISceneSwitcher sSwitcher)
+        {
+            _ssService = ssService;
+            _sSwitcher = sSwitcher;
+        }
+
         public async UniTask OnEnter()
         {
+            await _sSwitcher.SwitchScene(_ssService.Target);
             await UniTask.CompletedTask;
         }
 
