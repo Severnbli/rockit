9bbfc9ec0a61c1ef230307763f7e770f8463ddb4
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 22:57:02 +0300

    Add "Target" field of type string at SceneSwitcherService

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Services/SceneSwitcherService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Services/SceneSwitcherService.cs
@@ -4,6 +4,7 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Services
 {
     public sealed class SceneSwitcherService
     {
+        public string Target;
         public float Progress01;
         public float Progress0100;
         public SceneLoadStatus LoadStatus;
