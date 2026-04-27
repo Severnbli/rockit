97ac15b9da3eee7540263a00ba90965e51e40dcd
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 11:45:18 +0300

    Create CloseAppOnCloseAppRequestSystem, bind it to SharedModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Systems/CloseAppOnCloseAppRequestSystem.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using Leopotam.EcsProto;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Systems
+{
+    public sealed class CloseAppOnCloseAppRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly SharedRequestsAspect _srAspect;
+
+        public void Run()
+        {
+            if (_srAspect.CloseAppRequests.IsEmptySlow()) return;
+            
+            Application.Quit();
+        }
+    }
+}
