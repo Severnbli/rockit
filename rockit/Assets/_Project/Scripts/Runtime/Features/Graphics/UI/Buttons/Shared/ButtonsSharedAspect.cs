501602789aac086f9481dc32016fb00e5860ec73
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-27 11:39:17 +0300

    Create CloseAppButtonTag, add it pool and iterator to ButtonsSharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Shared/ButtonsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Shared/ButtonsSharedAspect.cs
@@ -1,9 +1,12 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared
 {
     public sealed class ButtonsSharedAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<CloseAppButtonTag> CloseAppButtonTagPool;
+        public readonly ProtoIt CloseAppButtonTags = new (It.Inc<CloseAppButtonTag>());
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Shared/Tags/CloseAppButtonTag.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared.Tags
+{
+    public struct CloseAppButtonTag
+    {
+        
+    }
+}
