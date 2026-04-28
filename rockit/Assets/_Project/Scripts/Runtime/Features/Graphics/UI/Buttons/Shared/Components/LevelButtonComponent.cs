01fc61d8983b99f4a9f743281b3d9f8c535eeb82
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-28 15:52:16 +0300

    Add "Id" field to LevelButtonComponent

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Shared/Components/LevelButtonComponent.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Shared/Components/LevelButtonComponent.cs
@@ -7,6 +7,7 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Shared.Component
     [Serializable, ProtoUnityAuthoring]
     public struct LevelButtonComponent
     {
+        public int Id;
         public LevelButtonView View;
     }
 }
