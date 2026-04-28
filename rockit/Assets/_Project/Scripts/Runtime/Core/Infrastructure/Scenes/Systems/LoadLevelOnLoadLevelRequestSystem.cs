b1f05e2712d4a1842a6881d0309587d235be8778
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-28 15:11:06 +0300

    Create LoadLevelOnLoadLevelRequestSystem, bind it to ScenesModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Systems/LoadLevelOnLoadLevelRequestSystem.cs
@@ -0,0 +1,36 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
+using _Project.Scripts.Runtime.Shared.Utils.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Systems
+{
+    public sealed class LoadLevelOnLoadLevelRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly ScenesRequestsAspect _srAspect;
+        [DIRequests] private readonly RequestsAspect _rAspect;
+        private readonly DataProvider _dProvider;
+
+        public LoadLevelOnLoadLevelRequestSystem(DataProvider dProvider)
+        {
+            _dProvider = dProvider;
+        }
+
+        public void Run()
+        {
+            var (e, ok) = ref _srAspect.LoadLevelRequests.FirstSlow();
+            if (!ok) return;
+            
+            ref var llRequest = ref _srAspect.LoadLevelRequestPool.Get(e);
+            _dProvider.GameSceneData.LevelIdToLoad = llRequest.Id;
+
+            var prepared = new SwitchSceneRequest
+            {
+                Target = ScenesContracts.GameScene
+            };
+            ScenesUtils.CreateSwitchSceneRequest(_rAspect, prepared);
+        }
+    }
+}
