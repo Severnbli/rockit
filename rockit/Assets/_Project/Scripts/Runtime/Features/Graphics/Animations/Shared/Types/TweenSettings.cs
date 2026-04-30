c3e42ddf9cdf9e62464674756b048a190d54cab5
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-30 13:18:50 +0300

    Create TweenSettings with "Relative", "Duration", "SpeedBased", "Delay", "Easing", "Loops" and "LoopType" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/Types/TweenSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DG.Tweening;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Types
+{
+    [Serializable]
+    public class TweenSettings
+    {
+        public bool Relative = false;
+        public float Duration = 0.5f;
+        public bool SpeedBased = false;
+        public float Delay = 0f;
+        public Ease Easing = Ease.Unset;
+        public int Loops = 0;
+        public LoopType LoopType = LoopType.Restart;
+    }
+}
