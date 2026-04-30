3217dae796d9a9ede5785e58a9b2fb9e6f40c1d8
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-30 14:18:47 +0300

    Create FloatTweenSettings that inherited from TweenSettings and add "To" and "From" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/Types/FloatTweenSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Types
+{
+    [Serializable]
+    public class FloatTweenSettings : TweenSettings
+    {
+        public float To;
+        public float From;
+    }
+}
