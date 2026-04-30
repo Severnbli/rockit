9702e7d0971b9e2a7ea9e183d39d6098bdec6050
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-30 21:14:44 +0300

    Create MonoWindow that inherited from MonoBehaviour with "OnOpen", "OnClose" events, "Opened" property and "Open" and "Close" methods

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Monos/MonoWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Monos
+{
+    public class MonoWindow : MonoBehaviour
+    {
+        public event Action OnOpen;
+        public event Action OnClose;
+
+        public bool Opened { get; private set; }
+
+        public async UniTask Open()
+        {
+            
+        }
+
+        public async UniTask Close()
+        {
+            
+        }
+    }
+}
