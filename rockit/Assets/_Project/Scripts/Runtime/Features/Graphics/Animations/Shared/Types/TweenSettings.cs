929cc3d3992cc9cbed6f7cdcb28746156c73f7fb
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-30 14:16:59 +0300

    Add "FromExact" field to TweenSettings

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/Types/TweenSettings.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/Types/TweenSettings.cs
@@ -6,6 +6,7 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Types
     [Serializable]
     public class TweenSettings
     {
+        public bool FromExact = false;
         public bool Relative = false;
         public float Duration = 0.5f;
         public bool SpeedBased = false;
