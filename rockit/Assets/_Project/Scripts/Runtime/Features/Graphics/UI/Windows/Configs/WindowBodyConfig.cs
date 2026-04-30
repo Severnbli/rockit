6a09d6dd1f2e0844310604ceef9d08f851288a98
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-30 22:38:11 +0300

    Rename WindowConfig to WindowBodyConfig, rename its fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Configs/WindowBodyConfig.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Types;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Configs
+{
+    public class WindowBodyConfig : ScriptableObject
+    {
+        [SerializeField] private FloatTweenSettings _openAnimation;
+        [SerializeField] private FloatTweenSettings _closeAnimation;
+        
+        public FloatTweenSettings OpenAnimation => _openAnimation;
+        public FloatTweenSettings CloseAnimation => _closeAnimation;
+    }
+}
