c79b8c97a1a052e8590c6f2159229aafa7ca1656
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-30 14:19:40 +0300

    Create Vector3TweenSettings that inherited from TweenSettings and add "To" and "From" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/Types/Vector3TweenSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Types
+{
+    [Serializable]
+    public class Vector3TweenSettings : TweenSettings
+    {
+        public Vector3 To;
+        public Vector3 From;
+    }
+}
