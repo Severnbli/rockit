a88abf68283a3185784d71495742a70e4979fa9f
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-30 22:36:29 +0300

    Create WindowConfig that inherited from ScriptableObject with "_openBodyAnimation" and "_closeBodyAnimation" fields and its property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Configs/WindowConfig.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Types;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Configs
+{
+    public class WindowConfig : ScriptableObject
+    {
+        [SerializeField] private FloatTweenSettings _openBodyAnimation;
+        [SerializeField] private FloatTweenSettings _closeBodyAnimation;
+        
+        public FloatTweenSettings OpenBodyAnimation => _openBodyAnimation;
+        public FloatTweenSettings CloseBodyAnimation => _closeBodyAnimation;
+    }
+}
