f7f577602cfe9b50c48820ca87d946da93b60a70
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-01 13:10:49 +0300

    Create MonoFadeWindow that inherited from MonoBaseWindow and add "_fade" field with its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Monos/MonoFadeWindow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Monos
+{
+    public class MonoFadeWindow : MonoBaseWindow
+    {
+        [SerializeField] private Image _fade;
+        
+        public Image Fade => _fade;
+    }
+}
