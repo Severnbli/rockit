d6b54ddb9c3023fe0de0d1c2416496d77243102a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-01 13:01:12 +0300

    Create generic FadeWindowConfig that inherited from BaseWindowConfig

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Configs/FadeWindowConfig.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Types;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Configs
+{
+    public class FadeWindowConfig<T> : BaseWindowConfig<T> where T : FadeWindowConfig<T>
+    {
+        [SerializeField] private FloatTweenSettings _fadeOpen;
+        [SerializeField] private FloatTweenSettings _fadeClose;
+        
+        public FloatTweenSettings FadeOpen => _fadeOpen;
+        public FloatTweenSettings FadeClose => _fadeClose;
+    }
+}
