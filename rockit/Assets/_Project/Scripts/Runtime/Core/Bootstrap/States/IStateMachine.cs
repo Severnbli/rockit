f04334bb290a2a2c16bf2d402932b432b12e2f07
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-01 19:58:41 +0300

    Add type defined methods "EnterModalState" and "LeaveModalState"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/IStateMachine.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/IStateMachine.cs
@@ -8,7 +8,9 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States
         UniTask ChangeState<T>() where T : IState;
         UniTask ChangeState(IState state);
         UniTask EnterModalState<T>() where T : IState;
+        UniTask EnterModalState(IState state);
         UniTask LeaveModalState<T>() where T : IState;
+        UniTask LeaveModalState(IState state);
         void BootstrapSceneStates(params ISceneState[] states);
     }
 }
