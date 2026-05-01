3fa76e9d650264b6262487f03340dcf2a635315b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-01 13:03:43 +0300

    Create generic BaseWindow with defined types limitation

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Types/BaseWindow.cs
@@ -0,0 +1,19 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Monos;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Types
+{
+    public class BaseWindow<TWindow, TConfig> 
+        where TWindow : MonoBaseWindow
+        where TConfig : BaseWindowConfig<TConfig>
+    {
+        private MonoBaseWindow _mbWindow;
+        private BaseWindowConfig<TConfig> _bwConfig;
+
+        public BaseWindow(TWindow window, BaseWindowConfig<TConfig> bwConfig)
+        {
+            _mbWindow = window;
+            _bwConfig = bwConfig;
+        }
+    }
+}
