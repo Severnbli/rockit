d7802535d9e8615bdec5603799a9494ee6cf2e5a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-01 13:14:45 +0300

    Create FadeWindow that inherited from BaseWindow and add "_mfWindow" and "_fwConfig" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Types/FadeWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Monos;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Types
+{
+    public class FadeWindow<TWindow, TConfig> : BaseWindow<TWindow, TConfig>
+        where TWindow : MonoFadeWindow 
+        where TConfig : FadeWindowConfig<TConfig>
+    {
+        private readonly MonoFadeWindow _mfWindow;
+        private readonly FadeWindowConfig<TConfig> _fwConfig;
+        
+        public FadeWindow(TWindow window, FadeWindowConfig<TConfig> fwConfig, CancellationToken ct) : 
+            base(window, fwConfig, ct)
+        {
+            _mfWindow = window;
+            _fwConfig = fwConfig;
+        }
+    }
+}
