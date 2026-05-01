00f2ba5834092d97167f9b2eb387177cb40641d8
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-01 13:08:19 +0300

    Move logic with events, "Opened" property and animations from MonobaseWindow to BaseWindow

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Types/BaseWindow.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Types/BaseWindow.cs
@@ -1,5 +1,9 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Configs;
+﻿using System;
+using System.Threading;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Configs;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Monos;
+using _Project.Scripts.Runtime.Shared.Extensions.Features.Graphics.Animations;
+using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Types
 {
@@ -10,10 +14,54 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Types
         private MonoBaseWindow _mbWindow;
         private BaseWindowConfig<TConfig> _bwConfig;
 
-        public BaseWindow(TWindow window, BaseWindowConfig<TConfig> bwConfig)
+        protected CancellationToken Ct;
+        
+        public event Action OnOpen;
+        public event Action OnClose;
+
+        public bool Opened { get; private set; }
+
+        public BaseWindow(TWindow window, BaseWindowConfig<TConfig> bwConfig, CancellationToken ct)
         {
             _mbWindow = window;
             _bwConfig = bwConfig;
+            Ct = ct;
+        }
+        
+        public async UniTask Open()
+        {
+            await PlayOpenAnimation();
+            OnOpen?.Invoke();
+            Opened = true;
+        }
+
+        public async UniTask Close()
+        {
+            await PlayCloseAnimation();
+            OnClose?.Invoke();
+            Opened = false;
+        }
+
+        protected virtual async UniTask PlayOpenAnimation()
+        {
+            await PlayOpenBodyAnimation();
+        }
+
+        protected virtual async UniTask PlayOpenBodyAnimation()
+        {
+            await _mbWindow.Body.transform.ScaleTween(_bwConfig.BodyOpen)
+                .ToUniTask(cancellationToken: Ct);
+        }
+
+        protected virtual async UniTask PlayCloseAnimation()
+        {
+            await PlayCloseBodyAnimation();
+        }
+
+        protected virtual async UniTask PlayCloseBodyAnimation()
+        {
+            await _mbWindow.Body.transform.ScaleTween(_bwConfig.BodyClose)
+                .ToUniTask(cancellationToken: Ct);
         }
     }
 }
