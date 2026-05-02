4ea927dea1f0635dd9615421c2e2edd42c46af3b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-02 22:20:48 +0300

    Create ITweenPipeline with only method "PlayWith"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/ITweenPipeline.cs
@@ -0,0 +1,10 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline
+{
+    public interface ITweenPipeline
+    {
+        UniTask PlayWith(GameObject go);
+    }
+}
