29ae57aa43ba69e706289cb12b8cc154efabaed0
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-02 22:31:00 +0300

    Change signature of "Run" method: add "tp" parameter of type TweenPipeline

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/ITweenPipelineRunner.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/ITweenPipelineRunner.cs
@@ -5,6 +5,6 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline
 {
     public interface ITweenPipelineRunner
     {
-        UniTask Run(GameObject go);
+        UniTask Run(TweenPipeline tp, GameObject go);
     }
 }
