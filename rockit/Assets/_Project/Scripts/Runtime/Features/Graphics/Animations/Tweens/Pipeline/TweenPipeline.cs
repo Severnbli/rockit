db62413234bdca23c7b2e9ce8392fff84cd96f0d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-02 22:23:12 +0300

    Create TweenPipeline that implements ITweenPipeline

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/TweenPipeline.cs
@@ -0,0 +1,13 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline
+{
+    public class TweenPipeline : ITweenPipeline
+    {
+        public async UniTask PlayWith(GameObject go)
+        {
+            await UniTask.CompletedTask;
+        }
+    }
+}
