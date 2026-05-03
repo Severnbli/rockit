3a52a51163bd906f5bafa596fa852bbdf9eea498
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-03 14:30:00 +0300

    Create TweenStepConnector enum with "Append" and "Join" elements

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/TweenStepConnector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core
+{
+    [Serializable]
+    public enum TweenStepConnector
+    {
+        Append = 0,
+        Join = 1
+    }
+}
