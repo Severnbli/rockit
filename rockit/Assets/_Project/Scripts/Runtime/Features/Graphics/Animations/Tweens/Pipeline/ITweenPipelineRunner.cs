df07e954def9e3d3a14157295fc8c525a2c2c98b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-03 14:02:44 +0300

    Update "Run" method signature: add "caching" flag parameter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/ITweenPipelineRunner.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/ITweenPipelineRunner.cs
@@ -5,6 +5,6 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline
 {
     public interface ITweenPipelineRunner
     {
-        UniTask Run(TweenPipeline tp, GameObject go);
+        UniTask Run(TweenPipeline tp, GameObject go, bool caching = false);
     }
 }
