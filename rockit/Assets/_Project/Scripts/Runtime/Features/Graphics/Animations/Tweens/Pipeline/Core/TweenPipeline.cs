0d77720dc2cdee2e6a43d9793e067dca6ed5e492
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-03 14:07:10 +0300

    Add "_steps" array field of type ITweenPipelineStep

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/TweenPipeline.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/TweenPipeline.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core
 {
     [Serializable]
     public class TweenPipeline
     {
+        [SerializeField] private ITweenPipelineStep[] _steps;
         
+        public ITweenPipelineStep[] Steps => _steps;
     }
 }
