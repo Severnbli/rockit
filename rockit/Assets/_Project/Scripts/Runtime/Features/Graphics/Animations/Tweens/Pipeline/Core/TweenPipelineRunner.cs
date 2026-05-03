74f388f75524d1e9f726c4f582378f3145c31f23
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-03 23:19:12 +0300

    Add "_cache" field of type BiValueDictionary

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/TweenPipelineRunner.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/TweenPipelineRunner.cs
@@ -1,10 +1,14 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using _Project.Scripts.Runtime.Shared.Tools;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core
 {
     public class TweenPipelineRunner : ITweenPipelineRunner
     {
+        protected readonly BiValueDictionary<GameObject, TweenPipeline, Sequence> _cache = new ();
+        
         public async UniTask Run(TweenPipeline tp, GameObject go, bool caching = false)
         {
             await UniTask.CompletedTask;
