f9f547e03659202b014c2495e3e7c6a06928d432
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-03 14:41:40 +0300

    Implement ISerializationCallbackReceiver into ScriptableObjectAutoInstaller and copy-paste logic from Sirenix.OdinInspector.SerializedScriptableObject

--- rockit/Assets/_Project/Scripts/Modules/Zenject/ScriptableObjectAutoInstaller.cs
+++ rockit/Assets/_Project/Scripts/Modules/Zenject/ScriptableObjectAutoInstaller.cs
@@ -1,10 +1,19 @@
-﻿using Zenject;
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using Sirenix.Utilities;
+using UnityEngine;
+using Zenject;
 
 namespace _Project.Scripts.Modules.Zenject
 {
-    public class ScriptableObjectAutoInstaller<T> : ScriptableObjectInstaller 
+    [ShowOdinSerializedPropertiesInInspector]
+    public class ScriptableObjectAutoInstaller<T> : ScriptableObjectInstaller, ISerializationCallbackReceiver
         where T : ScriptableObjectAutoInstaller<T>
     {
+        [SerializeField]
+        [HideInInspector]
+        private SerializationData serializationData;
+        
         public sealed override void InstallBindings()
         {
             RegisterBindings();
@@ -12,5 +21,35 @@ namespace _Project.Scripts.Modules.Zenject
         }
         
         protected virtual void RegisterBindings() {}
+        
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            if (this.SafeIsUnityNull()) return;
+            UnitySerializationUtility.DeserializeUnityObject(this, ref serializationData);
+            OnAfterDeserialize();
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+            if (this.SafeIsUnityNull()) return;
+            OnBeforeSerialize();
+            UnitySerializationUtility.SerializeUnityObject(this, ref serializationData);
+        }
+
+        protected virtual void OnAfterDeserialize()
+        {
+        }
+
+        protected virtual void OnBeforeSerialize()
+        {
+        }
+
+        [HideInTables]
+        [OnInspectorGUI]
+        [PropertyOrder(-2.1474836E+09f)]
+        private void InternalOnInspectorGUI()
+        {
+            EditorOnlyModeConfigUtility.InternalOnInspectorGUI(this);
+        }
     }
 }
