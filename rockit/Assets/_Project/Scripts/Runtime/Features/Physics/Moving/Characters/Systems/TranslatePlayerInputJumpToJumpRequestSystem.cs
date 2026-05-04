f201b2bc351b9ea030d67033475b13569b05b278
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-05 00:23:22 +0300

    Apply PlayerStats to TranslatePlayerInputJumpToJumpRequestSystem

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Systems/TranslatePlayerInputJumpToJumpRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Systems/TranslatePlayerInputJumpToJumpRequestSystem.cs
@@ -4,6 +4,7 @@ using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
 using _Project.Scripts.Runtime.Features.Input.Services;
 using _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Configs;
 using _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Requests;
+using _Project.Scripts.Runtime.Features.Stats.Player.Services;
 using _Project.Scripts.Runtime.Shared.Utils.Features.Physics.Moving;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
@@ -17,11 +18,14 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Systems
         private ProtoWorld _world;
         private readonly PlayerInputService _piService;
         private readonly PlayerMovingConfig _pmConfig;
+        private readonly PlayerStatsService _psService;
 
-        public TranslatePlayerInputJumpToJumpRequestSystem(PlayerInputService piService, PlayerMovingConfig pmConfig)
+        public TranslatePlayerInputJumpToJumpRequestSystem(PlayerInputService piService, PlayerMovingConfig pmConfig,
+            PlayerStatsService psService)
         {
             _piService = piService;
             _pmConfig = pmConfig;
+            _psService = psService;
         }
         
         public void Init(IProtoSystems systems)
@@ -35,7 +39,7 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Systems
 
             var prepared = new JumpRequest
             {
-                Factor = _pmConfig.JumpPower
+                Factor = _pmConfig.JumpPower * _psService.JumpModifier
             };
 
             foreach (var e in _sAspect.Players)
