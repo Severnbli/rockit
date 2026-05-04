4f1bd427d8f757ecb79bea16b25665b1f2c0e684
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 20:58:42 +0300

    Add "UpdatePlayerStatsService" extension method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Features/Stats/PlayerStatsExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Features/Stats/PlayerStatsExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities;
 using _Project.Scripts.Runtime.Features.Stats.Player.Configs;
+using _Project.Scripts.Runtime.Features.Stats.Player.Services;
+using _Project.Scripts.Runtime.Features.Stats.Shared;
 
 namespace _Project.Scripts.Runtime.Shared.Extensions.Features.Stats
 {
@@ -19,6 +22,25 @@ namespace _Project.Scripts.Runtime.Shared.Extensions.Features.Stats
             pStats.DashModifierIndex = psConfig.DashModifiers.Length == 0 || pStats.DashModifierIndex < 0
                 ? ProjectContracts.ArrayEmptyInt
                 : Math.Clamp(pStats.DashModifierIndex, 0, psConfig.DashModifiers.Length - 1);
+        }
+
+        public static void UpdatePlayerStatsService(this PlayerStatsService psService, PlayerStats pStats,
+            PlayerStatsConfig psConfig)
+        {
+            psService.WalkModifier =
+                psConfig.WalkModifiers.Any() && pStats.WalkModifierIndex != ProjectContracts.ArrayEmptyInt
+                    ? psConfig.WalkModifiers[pStats.WalkModifierIndex]
+                    : StatsSharedContracts.DefaultFloatModifier;
+            
+            psService.JumpModifier =
+                psConfig.JumpModifiers.Any() && pStats.JumpModifierIndex != ProjectContracts.ArrayEmptyInt
+                    ? psConfig.JumpModifiers[pStats.JumpModifierIndex]
+                    : StatsSharedContracts.DefaultFloatModifier;
+            
+            psService.DashModifier =
+                psConfig.DashModifiers.Any() && pStats.DashModifierIndex != ProjectContracts.ArrayEmptyInt
+                    ? psConfig.DashModifiers[pStats.DashModifierIndex]
+                    : StatsSharedContracts.DefaultFloatModifier;
         }
     }
 }
