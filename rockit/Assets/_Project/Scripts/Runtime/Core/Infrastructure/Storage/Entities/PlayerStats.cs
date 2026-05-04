ecdb0f5d07d07272c1bd2f130182448fa617cf59
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 17:53:29 +0300

    Define default values for player stats entity indexes as "-1"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/PlayerStats.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/PlayerStats.cs
@@ -5,8 +5,8 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities
     [Serializable]
     public sealed class PlayerStats
     {
-        public int WalkModifierIndex;
-        public int JumpModifierIndex;
-        public int DashModifierIndex;
+        public int WalkModifierIndex = -1;
+        public int JumpModifierIndex = -1;
+        public int DashModifierIndex = -1;
     }
 }
