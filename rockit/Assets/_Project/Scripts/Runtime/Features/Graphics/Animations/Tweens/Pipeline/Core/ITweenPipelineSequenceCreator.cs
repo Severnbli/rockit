a50bf459569a14aa97ea3367e2db81b0ad2336d5
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 13:56:16 +0300

    Create ITweenPipelineSequenceCreator with only method "CreateSequence"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/ITweenPipelineSequenceCreator.cs
@@ -0,0 +1,10 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core
+{
+    public interface ITweenPipelineSequenceCreator
+    {
+        Sequence CreateSequence(TweenPipeline tp, GameObject go);
+    }
+}
