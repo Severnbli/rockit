cdea15cef2e4a9d38ebfe90f5d87bd13dd8bd24d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 13:50:10 +0300

    Add "Cache" field of type dictionary

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/TweenPipelineCacheStorage.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/TweenPipelineCacheStorage.cs
@@ -1,10 +1,13 @@
-﻿using DG.Tweening;
+﻿using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core
 {
     public class TweenPipelineCacheStorage : ITweenPipelineCacheStorage
     {
+        protected readonly Dictionary<(TweenPipeline, GameObject), Sequence> Cache = new ();
+        
         public Sequence GetFromCache(TweenPipeline tp, GameObject go)
         {
             return null;
