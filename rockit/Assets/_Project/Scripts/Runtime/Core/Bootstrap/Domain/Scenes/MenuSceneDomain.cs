21378858e3a5baf1b1b0db590a953e8684659ec1
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-05 01:23:55 +0300

    Register MenuSceneSetupState into MenuSceneDomain

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/MenuSceneDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/MenuSceneDomain.cs
@@ -1,7 +1,14 @@
-﻿namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
 {
     public class MenuSceneDomain : SceneDomain
     {
-        
+        protected override void RegisterStates()
+        {
+            base.RegisterStates();
+            
+            RegisterState<MenuSceneSetupState>();
+        }
     }
 }
