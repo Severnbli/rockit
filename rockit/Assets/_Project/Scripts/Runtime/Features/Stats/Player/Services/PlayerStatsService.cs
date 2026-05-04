43eeef0b77b2f2de1b01357550859d93c8ec4222
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 18:05:03 +0300

    Rename "DefaultModifier" into "DefaultFloatModifier"

--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Player/Services/PlayerStatsService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Player/Services/PlayerStatsService.cs
@@ -4,8 +4,8 @@ namespace _Project.Scripts.Runtime.Features.Stats.Player.Services
 {
     public sealed class PlayerStatsService
     {
-        public float WalkModifier = StatsSharedContracts.DefaultModifier;
-        public float JumpModifier = StatsSharedContracts.DefaultModifier;
-        public float DashModifier = StatsSharedContracts.DefaultModifier;
+        public float WalkModifier = StatsSharedContracts.DefaultFloatModifier;
+        public float JumpModifier = StatsSharedContracts.DefaultFloatModifier;
+        public float DashModifier = StatsSharedContracts.DefaultFloatModifier;
     }
 }
