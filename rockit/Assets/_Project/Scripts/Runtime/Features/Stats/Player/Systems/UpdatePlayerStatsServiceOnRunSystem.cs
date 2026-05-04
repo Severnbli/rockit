375ca21eec9b10ba413b42ad1a12eed80fda920a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 21:00:22 +0300

    Create UpdatePlayerStatsServiceOnRunSystem, bind it to PlayerStatsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Player/Systems/UpdatePlayerStatsServiceOnRunSystem.cs
@@ -0,0 +1,29 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities;
+using _Project.Scripts.Runtime.Features.Stats.Player.Configs;
+using _Project.Scripts.Runtime.Features.Stats.Player.Services;
+using _Project.Scripts.Runtime.Shared.Extensions.Features.Stats;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Player.Systems
+{
+    public sealed class UpdatePlayerStatsServiceOnRunSystem : IProtoRunSystem
+    {
+        private readonly PlayerStatsService _psService;
+        private readonly PlayerStats _pStats;
+        private readonly PlayerStatsConfig _psConfig;
+
+        public UpdatePlayerStatsServiceOnRunSystem(PlayerStatsService psService, DataProvider dProvider,
+            PlayerStatsConfig psConfig)
+        {
+            _psService = psService;
+            _pStats = dProvider.PlayerStats;
+            _psConfig = psConfig;
+        }
+
+        public void Run()
+        {
+            _psService.UpdatePlayerStatsService(_pStats, _psConfig);
+        }
+    }
+}
