afda144a115ae9adce9a46a1b8a36ba7be723e0a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 17:36:16 +0300

    Create PlayerStatsService with "WalkModifier", "JumpModifier" and "DashModifier" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Player/Services/PlayerStatsService.cs
@@ -0,0 +1,9 @@
+﻿namespace _Project.Scripts.Runtime.Features.Stats.Player.Services
+{
+    public sealed class PlayerStatsService
+    {
+        public float WalkModifier = 1;
+        public float JumpModifier = 1;
+        public float DashModifier = 1;
+    }
+}
