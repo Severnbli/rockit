c7f7ab4648f4b9de1531df4cc73c9452415df1cf
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 14:36:39 +0300

    Update windows configs: replace tween settings fields with tween pipelines

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Configs/FadeWindowConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Configs/FadeWindowConfig.cs
@@ -1,14 +1,14 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Types;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Configs
 {
     public class FadeWindowConfig<T> : BaseWindowConfig<T> where T : FadeWindowConfig<T>
     {
-        [SerializeField] private FloatTweenSettings _fadeOpen;
-        [SerializeField] private FloatTweenSettings _fadeClose;
+        [SerializeField] private TweenPipeline _fadeOpen;
+        [SerializeField] private TweenPipeline _fadeClose;
         
-        public FloatTweenSettings FadeOpen => _fadeOpen;
-        public FloatTweenSettings FadeClose => _fadeClose;
+        public TweenPipeline FadeOpen => _fadeOpen;
+        public TweenPipeline FadeClose => _fadeClose;
     }
 }
