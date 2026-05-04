2f0d37bde19c5795c651351cc20a93bcc9d339ca
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 13:40:40 +0300

    Create TweenAnimationsModule that inherited from BaseModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Animations/TweensAnimationsModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Animations
+{
+    public sealed class TweensAnimationsModule : BaseModule<TweensAnimationsModule>
+    {
+        public TweensAnimationsModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
