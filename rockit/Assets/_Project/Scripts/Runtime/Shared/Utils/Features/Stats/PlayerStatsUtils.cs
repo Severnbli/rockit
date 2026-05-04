96b06caff50262a6c03a7c2eddab0b26fe92c84e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-05 00:13:29 +0300

    Create PlayerStatsUtils with only method "CreateUpdatePlayerStatsServiceRequest"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/PlayerStatsUtils.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.Stats
+{
+    public static class PlayerStatsUtils
+    {
+        public static ProtoEntity CreateUpdatePlayerStatsServiceRequest(RequestsAspect aspect)
+        {
+            return aspect.CreateRequest(aspect.PlayerStatsRequestsAspect.UpdatePlayerStatsServiceRequestPool);
+        }
+    }
+}
