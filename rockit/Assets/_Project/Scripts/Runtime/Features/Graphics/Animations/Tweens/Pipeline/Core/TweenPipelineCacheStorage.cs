f2a08e9be95a02320f9067acec2c03a343fb3c92
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 13:48:53 +0300

    Create TweenPipelineCacheStorage that implements ITweenPipelineCacheStorage

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/TweenPipelineCacheStorage.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core
+{
+    public class TweenPipelineCacheStorage : ITweenPipelineCacheStorage
+    {
+        public Sequence GetFromCache(TweenPipeline tp, GameObject go)
+        {
+            return null;
+        }
+
+        public void AddToCache(TweenPipeline tp, GameObject go)
+        {
+            
+        }
+    }
+}
