ea7fa51af405e828b35fc701a6a8cd49ad31abc3
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 18:09:00 +0300

    Create ClampPlayerStatsEntityIndexesOnInitSystem, bind it to PlayerStatsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Player/Systems/ClampPlayerStatsEntityIndexesOnInitSystem.cs
@@ -0,0 +1,25 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities;
+using _Project.Scripts.Runtime.Features.Stats.Player.Configs;
+using _Project.Scripts.Runtime.Shared.Extensions.Features.Stats;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Player.Systems
+{
+    public sealed class ClampPlayerStatsEntityIndexesOnInitSystem : IProtoInitSystem
+    {
+        private readonly PlayerStats _pStats;
+        private readonly PlayerStatsConfig _psConfig;
+
+        public ClampPlayerStatsEntityIndexesOnInitSystem(DataProvider dProvider, PlayerStatsConfig psConfig)
+        {
+            _psConfig = psConfig;
+            _pStats = dProvider.PlayerStats;
+        }
+
+        public void Init(IProtoSystems systems)
+        {
+            _pStats.ClampPlayerStatsEntityIndexes(_psConfig);
+        }
+    }
+}
