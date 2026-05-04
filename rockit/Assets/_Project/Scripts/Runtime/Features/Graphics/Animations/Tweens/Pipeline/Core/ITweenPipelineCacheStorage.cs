4a9eeafd08bda036739e59c3e54f0bbf450b1eb4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 13:48:00 +0300

    Create ITweenPipelineCacheStorage

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Core/ITweenPipelineCacheStorage.cs
@@ -0,0 +1,11 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core
+{
+    public interface ITweenPipelineCacheStorage
+    {
+        Sequence GetFromCache(TweenPipeline tp, GameObject go);
+        void AddToCache(TweenPipeline tp, GameObject go);
+    }
+}
