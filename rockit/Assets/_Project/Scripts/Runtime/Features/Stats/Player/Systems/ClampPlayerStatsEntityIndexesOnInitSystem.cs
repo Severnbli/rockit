30fe048f46af866e9319c1fa5c164a55ab980e0b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-05 01:58:23 +0300

    Replace player stats assignification at constructor with only data provider

--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Player/Systems/ClampPlayerStatsEntityIndexesOnInitSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Player/Systems/ClampPlayerStatsEntityIndexesOnInitSystem.cs
@@ -1,5 +1,4 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
-using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities;
 using _Project.Scripts.Runtime.Features.Stats.Player.Configs;
 using _Project.Scripts.Runtime.Shared.Extensions.Features.Stats;
 using Leopotam.EcsProto;
@@ -8,18 +7,18 @@ namespace _Project.Scripts.Runtime.Features.Stats.Player.Systems
 {
     public sealed class ClampPlayerStatsEntityIndexesOnInitSystem : IProtoInitSystem
     {
-        private readonly PlayerStats _pStats;
         private readonly PlayerStatsConfig _psConfig;
+        private readonly DataProvider _dProvider;
 
         public ClampPlayerStatsEntityIndexesOnInitSystem(DataProvider dProvider, PlayerStatsConfig psConfig)
         {
+            _dProvider = dProvider;
             _psConfig = psConfig;
-            _pStats = dProvider.PlayerStats;
         }
 
         public void Init(IProtoSystems systems)
         {
-            _pStats.ClampPlayerStatsEntityIndexes(_psConfig);
+            _dProvider.PlayerStats.ClampPlayerStatsEntityIndexes(_psConfig);
         }
     }
 }
