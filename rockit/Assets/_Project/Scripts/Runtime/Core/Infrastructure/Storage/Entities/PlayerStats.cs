438e1e05e01ff446de2fc0252b1a5ea02a02c26d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 18:00:48 +0300

    Replace magic "-1" default value at player stats with array empty int

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/PlayerStats.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/PlayerStats.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using _Project.Scripts.Runtime.Shared;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities
 {
     [Serializable]
     public sealed class PlayerStats
     {
-        public int WalkModifierIndex = -1;
-        public int JumpModifierIndex = -1;
-        public int DashModifierIndex = -1;
+        public int WalkModifierIndex = ProjectContracts.ArrayEmptyInt;
+        public int JumpModifierIndex = ProjectContracts.ArrayEmptyInt;
+        public int DashModifierIndex = ProjectContracts.ArrayEmptyInt;
     }
 }
