4d8fdf0dc8f1b4c566ac843336c2570476426a64
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-04 14:12:36 +0300

    Create RotateStep that inherited from TweenStep

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Tweens/Pipeline/Steps/RotateStep.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Types;
+using _Project.Scripts.Runtime.Shared.Extensions.Features.Graphics.Animations;
+using _Project.Scripts.Runtime.Shared.Extensions.Shared;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Steps
+{
+    [Serializable]
+    public class RotateStep : TweenStep
+    {
+        [SerializeField] private Vector3TweenSettings _settings;
+        
+        protected override bool TryDoTween(GameObject go, Dictionary<Type, Component> goCache, out Tween tween)
+        {
+            tween = null;
+
+            if (!go.TryGetComponentWithCache(goCache, out Transform tf)) return false;
+            
+            tween = tf.RotTween(_settings);
+            return true;
+        }
+    }
+}
