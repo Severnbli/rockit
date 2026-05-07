d7e2bc8f069d564c3b1cf5423ed245b63b32e09f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 00:18:59 +0300

    Create LifecycleAspect that inherited from ProtoAspectInject, add CreatedAtFactoryTag pool and iterator

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Objects/Lifecycle/LifecycleAspect.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle
+{
+    public sealed class LifecycleAspect : ProtoAspectInject
+    {
+        public ProtoPool<CreatedAtFactoryTag> CreatedAtFactoryTagPool;
+        public ProtoIt CreatedAtFactoryTags = new (It.Inc<CreatedAtFactoryTag>());
+    }
+}
