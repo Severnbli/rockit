7e646d594bc2a001fc8765c21c33ecf2d420550a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 00:54:32 +0300

    Add "VolumeDisabled" to AudioSharedContracts

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/AudioSharedContracts.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/AudioSharedContracts.cs
@@ -6,5 +6,6 @@
         public const string MusicVolume = "MusicVolume";
         public const string SfxVolume = "SfxVolume";
         public const float VolumeEnabled = 0f;
+        public const float VolumeDisabled = -80f;
     }
 }
