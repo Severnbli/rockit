e0c62304f45a052781715598d0f252ba4659a9a2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 22:30:22 +0300

    Create SfxAudioSourcePool that inherited from AudioSourcePool with defined "GetPrefab" overriding

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Sfx/Types/SfxAudioSourcePool.cs
@@ -0,0 +1,19 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Sfx.Configs;
+using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Monos;
+using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Types;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Sfx.Types
+{
+    public class SfxAudioSourcePool : AudioSourcePool
+    {
+        private readonly SfxConfig _sfxConfig;
+        
+        public SfxAudioSourcePool(SfxConfig sfxConfig, AudioSourceContainer asContainer) : base(asContainer)
+        {
+            _sfxConfig = sfxConfig;
+        }
+
+        protected override GameObject GetPrefab() => _sfxConfig.AudioSourcePrefab;
+    }
+}
