1d70f9d1174714b77246c4b29a503e8cf938c8a8
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 22:05:47 +0300

    Add "_asContainer" of type AudioSourceContainer to ProjectDomain, bind it to container

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Project/ProjectDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Project/ProjectDomain.cs
@@ -4,17 +4,22 @@ using _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats;
 using _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure;
 using _Project.Scripts.Runtime.Core.Bootstrap.States;
 using _Project.Scripts.Runtime.Core.Bootstrap.States.Project;
+using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Monos;
 using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Domain;
 using _Project.Scripts.Runtime.Core.Systems;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Project
 {
     public class ProjectDomain : BaseDomain
     {
+        [SerializeField] private AudioSourceContainer _asContainer;
+        
         protected override void RegisterBindings()
         {
             base.RegisterBindings();
 
+            Container.Bind<AudioSourceContainer>().FromInstance(_asContainer).AsSingle();
             Container.Bind<IStateMachine>().To<StateMachine>().AsSingle();
             Container.Bind<PausableSystemsSolver>().ToSelf().AsSingle();
             Container.BindInterfacesTo<DiContainerDomainObjectInstantiator>().AsSingle();
