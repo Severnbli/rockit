3a52294d37789308578ebe5a3c9be9dbe1c6da8b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 19:39:41 +0300

    Create AudioSharedContracts with "MusicVolume" and "SfxVolume" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/AudioSharedContracts.cs
@@ -0,0 +1,8 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared
+{
+    public static class AudioSharedContracts
+    {
+        public const string MusicVolume = "MusicVolume";
+        public const string SfxVolume = "SfxVolume";
+    }
+}
