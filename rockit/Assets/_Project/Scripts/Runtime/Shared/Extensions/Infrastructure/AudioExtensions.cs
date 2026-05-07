fbe85339c0942b0129c38f9ac7fc0fffe903a622
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 00:55:28 +0300

    Create AudioExtensions with only "SetVolumeEnable" method

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Infrastructure/AudioExtensions.cs
@@ -0,0 +1,16 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared;
+using UnityEngine.Audio;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions.Infrastructure
+{
+    public static class AudioExtensions
+    {
+        public static void SetVolumeEnabled(this AudioMixer mixer, string paramName, bool enabled)
+        {
+            mixer.SetFloat(paramName, enabled 
+                ? AudioSharedContracts.VolumeEnabled 
+                : AudioSharedContracts.VolumeDisabled
+            );
+        }
+    }
+}
