3991ed430e566bd43e491388b9025b1ae9046371
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 22:04:10 +0300

    Create AudioSourceContainer that inherited from Container

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/Monos/AudioSourceContainer.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Monos;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Monos
+{
+    public class AudioSourceContainer : Container
+    {
+        
+    }
+}
