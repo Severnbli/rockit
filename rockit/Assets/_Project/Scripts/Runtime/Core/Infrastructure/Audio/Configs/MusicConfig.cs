ee746cbb24e2db1b05c18ce4f2373a55f9187926
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 17:25:51 +0300

    Add "_transition" field of type TweenPipeline

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Configs/MusicConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Configs/MusicConfig.cs
@@ -1,13 +1,16 @@
 ﻿using _Project.Scripts.Modules.Zenject;
+using _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Configs
 {
     public class MusicConfig : ScriptableObjectAutoInstaller<MusicConfig>
     {
+        [SerializeField] private TweenPipeline _transition;
         [SerializeField] private AudioClip _menu;
         [SerializeField] private AudioClip _game;
-        
+
+        public TweenPipeline Transition => _transition;
         public AudioClip Menu => _menu;
         public AudioClip Game => _game;
     }
