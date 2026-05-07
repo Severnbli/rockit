a9a3667af5c585ac11a9e601c48b523a4951422b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 23:58:54 +0300

    Add "GetPool" ecs extension method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Shared/EcsExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Shared/EcsExtensions.cs
@@ -10,5 +10,10 @@ namespace _Project.Scripts.Runtime.Shared.Extensions.Shared
         {
             return packed.TryUnpack(out var packedWorld, out entity) && packedWorld.Equals(world);
         }
+
+        public static ProtoPool<T> GetPool<T>(this ProtoWorld world) where T : struct
+        {
+            return world.Pool<T>() as ProtoPool<T>;
+        }
     }
 }
