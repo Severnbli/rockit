1d57d12c69136a676a8ad964c68f33819865f86a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 22:28:04 +0300

    Create SfxConfig that inherited from ScriptableObjectAutoInstaller with "_audioSourcePrefab" field and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Sfx/Configs/SfxConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Sfx.Configs
+{
+    public class SfxConfig : ScriptableObjectAutoInstaller<SfxConfig>
+    {
+        [SerializeField] private GameObject _audioSourcePrefab;
+        
+        public GameObject AudioSourcePrefab => _audioSourcePrefab;
+    }
+}
