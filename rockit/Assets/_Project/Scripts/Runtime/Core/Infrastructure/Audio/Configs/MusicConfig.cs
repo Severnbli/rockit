d16e310be0307a5224903789138206352b38faaa
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 19:33:26 +0300

    Add "_loading" field to MusicConfig

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Configs/MusicConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Configs/MusicConfig.cs
@@ -10,10 +10,12 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Configs
         [SerializeField] private TweenPipeline _transitionTo;
         [SerializeField] private AudioClip _menu;
         [SerializeField] private AudioClip _game;
+        [SerializeField] private AudioClip _loading;
 
         public TweenPipeline TransitionFrom => _transitionFrom;
         public TweenPipeline TransitionTo => _transitionTo;
         public AudioClip Menu => _menu;
         public AudioClip Game => _game;
+        public AudioClip Loading => _loading;
     }
 }
