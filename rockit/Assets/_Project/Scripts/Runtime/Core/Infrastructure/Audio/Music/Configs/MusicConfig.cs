97301a6ce2a06cc3d978d0c2082c9deb749dbc86
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 00:49:29 +0300

    Create generic AudioConfig that inherited from ScriptableObjectAutoInstaller with only field "_audioSourcePrefab" and its property getter, update MusicConfig and SfxConfig to be inheritors from this

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Music/Configs/MusicConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Music/Configs/MusicConfig.cs
@@ -1,23 +1,22 @@
-﻿using _Project.Scripts.Modules.Zenject;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Sfx.Configs;
+using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Configs;
 using _Project.Scripts.Runtime.Features.Graphics.Animations.Tweens.Pipeline.Core;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Music.Configs
 {
-    public class MusicConfig : ScriptableObjectAutoInstaller<MusicConfig>
+    public class MusicConfig : AudioConfig<SfxConfig>
     {
         [SerializeField] private TweenPipeline _transitionFrom;
         [SerializeField] private TweenPipeline _transitionTo;
         [SerializeField] private AudioClip _menu;
         [SerializeField] private AudioClip _game;
         [SerializeField] private AudioClip _loading;
-        [SerializeField] private GameObject _audioSourcePrefab;
 
         public TweenPipeline TransitionFrom => _transitionFrom;
         public TweenPipeline TransitionTo => _transitionTo;
         public AudioClip Menu => _menu;
         public AudioClip Game => _game;
         public AudioClip Loading => _loading;
-        public GameObject AudioSourcePrefab => _audioSourcePrefab;
     }
 }
