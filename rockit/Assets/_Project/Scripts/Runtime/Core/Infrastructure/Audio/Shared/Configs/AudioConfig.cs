15ce429eaddda4ef7d4a983e7054ba89d1b36632
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 19:36:25 +0300

    Create AudioConfig that inherited from ScriptableObjectAutoInstaller with only field "_audioMixer" and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/Configs/AudioConfig.cs
@@ -0,0 +1,13 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Configs
+{
+    public class AudioConfig : ScriptableObjectAutoInstaller<AudioConfig>
+    {
+        [SerializeField] private AudioMixer _audioMixer;
+        
+        public AudioMixer AudioMixer => _audioMixer;
+    }
+}
