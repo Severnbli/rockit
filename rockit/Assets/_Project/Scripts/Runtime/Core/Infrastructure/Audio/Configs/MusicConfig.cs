ab60073fa8f14fe7d1c68c0394051f72e74c8a40
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 17:12:54 +0300

    Create MusicConfig with "_menu" and "_game" fields and its property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Configs/MusicConfig.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Configs
+{
+    public class MusicConfig : ScriptableObjectAutoInstaller<MusicConfig>
+    {
+        [SerializeField] private AudioClip _menu;
+        [SerializeField] private AudioClip _game;
+        
+        public AudioClip Menu => _menu;
+        public AudioClip Game => _game;
+    }
+}
