2c3e1d5dcefe8b0c4182daabbe4efb11b297b206
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 22:15:24 +0300

    Create abstract AudioSourcePool that inherited from BasePrefabPool with type AudioSource specification. Define "PostCreate" and "PostDespawn" methods overridings at it

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/Types/AudioSourcePool.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Types
+{
+    public abstract class AudioSourcePool : BasePrefabPool<AudioSource>
+    {
+        protected override void PostCreate(AudioSource instance)
+        {
+            base.PostCreate(instance);
+            
+            instance.playOnAwake = false;
+            instance.loop = false;
+        }
+
+        protected override void PostDespawn(AudioSource instance)
+        {
+            base.PostDespawn(instance);
+            
+            instance.Stop();
+            instance.clip = null;
+        }
+    }
+}
