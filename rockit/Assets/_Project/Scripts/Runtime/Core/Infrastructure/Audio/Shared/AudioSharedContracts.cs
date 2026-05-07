813719f1c060033b6f550f5d0573895b8ec44d83
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 19:40:59 +0300

    Add "MasterVolume" to AudioSharedContracts

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/AudioSharedContracts.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/AudioSharedContracts.cs
@@ -2,6 +2,7 @@
 {
     public static class AudioSharedContracts
     {
+        public const string MasterVolume = "MasterVolume";
         public const string MusicVolume = "MusicVolume";
         public const string SfxVolume = "SfxVolume";
     }
