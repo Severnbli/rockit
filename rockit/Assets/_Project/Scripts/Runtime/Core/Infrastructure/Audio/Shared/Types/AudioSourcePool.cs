7e3f21d74a59fff0b126c26a6427bd3096358d77
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-07 22:24:50 +0300

    Add "_asContainer" of type AudioSourceContainer into AudioSourcePool, define "FallbackContainer" overriding

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/Types/AudioSourcePool.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/Types/AudioSourcePool.cs
@@ -1,10 +1,18 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Monos;
+using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Types
 {
     public abstract class AudioSourcePool : BasePrefabPool<AudioSource>
     {
+        private readonly AudioSourceContainer _asContainer;
+
+        protected AudioSourcePool(AudioSourceContainer asContainer)
+        {
+            _asContainer = asContainer;
+        }
+
         protected override void PostCreate(AudioSource instance)
         {
             base.PostCreate(instance);
@@ -20,5 +28,7 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Types
             instance.Stop();
             instance.clip = null;
         }
+
+        protected override Transform FallbackContainer() => _asContainer.transform;
     }
 }
