8671e5c459498c58d83174f641b6ab1889a04cc3
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 11:49:33 +0300

    Create AudioRequestsAspect, add it pool and iterator to AudioRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/AudioRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/AudioRequestsAspect.cs
@@ -9,6 +9,8 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio
     public sealed class AudioRequestsAspect : ProtoAspectInject
     {
         public readonly ProtoPool<PlaySfxRequest> PlaySfxRequestPool;
+        public readonly ProtoPool<PlayMusicRequest> PlayMusicRequestPool;
         public readonly ProtoIt PlaySfxRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, PlaySfxRequest>());
+        public readonly ProtoIt PlayMusicRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, PlayMusicRequest>());
     }
 }
