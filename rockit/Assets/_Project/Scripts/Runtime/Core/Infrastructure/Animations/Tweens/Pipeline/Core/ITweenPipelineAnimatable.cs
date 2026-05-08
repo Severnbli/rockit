d19d537c331b5fae6023d71405b6b2a257e069fe
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 12:16:15 +0300

    Create ITweenPipelineAnimatable with only method "Animate"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tweens/Pipeline/Core/ITweenPipelineAnimatable.cs
@@ -0,0 +1,9 @@
+﻿using DG.Tweening;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tweens.Pipeline.Core
+{
+    public interface ITweenPipelineAnimatable
+    {
+        Sequence Animate(TweenPipeline tp);
+    }
+}
