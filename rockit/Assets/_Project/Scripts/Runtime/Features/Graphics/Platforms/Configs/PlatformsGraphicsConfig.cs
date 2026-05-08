95177e9d766bc4536e74276eb8180f8ea8638c33
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 01:47:32 +0300

    Create PlatformsGraphicsConfig that inherited from ScriptableObjectAutoInstaller with "_posColor", "_rotColor" and "_scaleColor" fields and its property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/Configs/PlatformsGraphicsConfig.cs
@@ -0,0 +1,16 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Platforms.Configs
+{
+    public sealed class PlatformsGraphicsConfig : ScriptableObjectAutoInstaller<PlatformsGraphicsConfig>
+    {
+        [SerializeField] private Color _posColor = Color.aquamarine;
+        [SerializeField] private Color _rotColor = Color.gold;
+        [SerializeField] private Color _scaleColor = Color.softRed;
+        
+        public Color PosColor => _posColor;
+        public Color RotColor => _rotColor;
+        public Color ScaleColor => _scaleColor;
+    }
+}
