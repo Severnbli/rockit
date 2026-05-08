3049bdcf8cf77ed10934ae71f1d12c9b40c6033b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 11:33:11 +0300

    Create ActiveAudioSourceTag, add it pool and iterators to AudioAspect

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/AudioAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/AudioAspect.cs
@@ -10,8 +10,12 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio
         public readonly ProtoPool<AudioSourceComponent> AudioSourceComponentPool;
         public readonly ProtoPool<SfxAudioSourceTag> SfxAudioSourceTagPool;
         public readonly ProtoPool<MusicAudioSourceTag> MusicAudioSourceTagPool;
+        public readonly ProtoPool<ActiveAudioSourceTag> ActiveAudioSourceTagPool;
         public readonly ProtoIt AudioSources = new (It.Inc<AudioSourceComponent>());
         public readonly ProtoIt SfxAudioSources = new (It.Inc<AudioSourceComponent, SfxAudioSourceTag>());
         public readonly ProtoIt MusicAudioSources = new (It.Inc<AudioSourceComponent, MusicAudioSourceTag>());
+        public readonly ProtoIt ActiveAudioSources = new (It.Inc<AudioSourceComponent, ActiveAudioSourceTag>());
+        public readonly ProtoIt ActiveMusicAudioSources = new (It.Inc<AudioSourceComponent, ActiveAudioSourceTag, MusicAudioSourceTag>());
+        public readonly ProtoIt ActiveSfxAudioSources = new (It.Inc<AudioSourceComponent, ActiveAudioSourceTag, SfxAudioSourceTag>());
     }
 }
