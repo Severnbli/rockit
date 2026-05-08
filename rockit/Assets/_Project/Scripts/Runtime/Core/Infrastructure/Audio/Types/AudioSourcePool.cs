7218ec61a2b7ba9e607941833b7fb6ce1061675d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 11:35:54 +0300

    Add "ConfigureEntityOnSpawn" and "ConfigureEntityOnDespawn" to AudioSourcePool to manage "ActiveAudioSourceTag"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Types/AudioSourcePool.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Types/AudioSourcePool.cs
@@ -1,6 +1,8 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Monos;
+using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Tags;
 using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
 using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Types
@@ -24,12 +26,26 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Types
             instance.loop = false;
         }
 
+        protected override void ConfigureEntityOnSpawn(AudioSource instance, ProtoEntity entity, TSpawnSettings settings = default)
+        {
+            base.ConfigureEntityOnSpawn(instance, entity, settings);
+
+            Pool<ActiveAudioSourceTag>().GetOrAdd(entity);
+        }
+
         protected override void PostDespawn(AudioSource instance, TDespawnSettings settings = default)
         {
             base.PostDespawn(instance, settings);
             
             instance.Stop();
             instance.clip = null;
+        }
+
+        protected override void ConfigureEntityOnDespawn(AudioSource instance, ProtoEntity entity, TDespawnSettings settings = default)
+        {
+            base.ConfigureEntityOnDespawn(instance, entity, settings);
+            
+            Pool<ActiveAudioSourceTag>().DelIfExists(entity);
         }
 
         protected override Transform FallbackContainer() => _asContainer.transform;
