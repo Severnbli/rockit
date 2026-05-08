0df93bceedecee1c227b7d8a345fc4f1d2e62ce4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 11:05:21 +0300

    Create AudioSourceComponent, add it pool and iterator

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/AudioSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Shared/AudioSharedAspect.cs
@@ -1,9 +1,12 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared
 {
     public sealed class AudioSharedAspect : ProtoAspectInject
     {
-        
+        public ProtoPool<AudioSourceComponent> AudioSourceComponentPool;
+        public ProtoIt AudioSources = new (It.Inc<AudioSourceComponent>());
     }
 }
