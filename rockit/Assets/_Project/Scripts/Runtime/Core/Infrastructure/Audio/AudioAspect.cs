9cc70f2cda49a7783f1c649edb518f5438d61024
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 11:15:00 +0300

    Make AudioSourceComponent pool and iterator readonly

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/AudioAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/AudioAspect.cs
@@ -7,9 +7,9 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio
 {
     public sealed class AudioAspect : ProtoAspectInject
     {
-        public ProtoPool<AudioSourceComponent> AudioSourceComponentPool;
+        public readonly ProtoPool<AudioSourceComponent> AudioSourceComponentPool;
         public readonly ProtoPool<SfxAudioSourceTag> SfxAudioSourceTagPool;
-        public ProtoIt AudioSources = new (It.Inc<AudioSourceComponent>());
+        public readonly ProtoIt AudioSources = new (It.Inc<AudioSourceComponent>());
         public readonly ProtoIt SfxAudioSources = new (It.Inc<AudioSourceComponent, SfxAudioSourceTag>());
     }
 }
