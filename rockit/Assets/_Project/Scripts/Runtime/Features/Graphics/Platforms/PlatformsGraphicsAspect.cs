a68c1ffc036e2e7c483cd0948893356072495a23
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 02:35:26 +0300

    Add "SpriteGlowPlatforms" iterator to PlatformsGraphicsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/PlatformsGraphicsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/PlatformsGraphicsAspect.cs
@@ -1,9 +1,12 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared.Components;
+using _Project.Scripts.Runtime.Features.Platforms.Shared.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Platforms
 {
     public sealed class PlatformsGraphicsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoIt SpriteGlowPlatforms = new (It.Inc<PlatformComponent, SpriteGlowComponent>());
     }
 }
