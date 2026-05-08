79a7c17014f0fdd3a70d797af7c4a5b8e3a27039
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 11:09:22 +0300

    Create SfxAudioSourceTag, addd it pool and iterator to SfxAudioAspect

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Sfx/SfxAudioAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Sfx/SfxAudioAspect.cs
@@ -1,9 +1,13 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Sfx.Tags;
+using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Shared.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Sfx
 {
     public sealed class SfxAudioAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<SfxAudioSourceTag> SfxAudioSourceTagPool;
+        public readonly ProtoIt SfxAudioSources = new (It.Inc<AudioSourceComponent, SfxAudioSourceTag>());
     }
 }
