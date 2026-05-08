22718a5b451b7dda84a04f0bb804fe49103227e3
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 01:49:27 +0300

    Add "_fallbackColor" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/Configs/PlatformsGraphicsConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/Configs/PlatformsGraphicsConfig.cs
@@ -5,10 +5,12 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Platforms.Configs
 {
     public sealed class PlatformsGraphicsConfig : ScriptableObjectAutoInstaller<PlatformsGraphicsConfig>
     {
+        [SerializeField] private Color _fallbackColor = Color.white;
         [SerializeField] private Color _posColor = Color.aquamarine;
         [SerializeField] private Color _rotColor = Color.gold;
         [SerializeField] private Color _scaleColor = Color.softRed;
         
+        public Color FallbackColor => _fallbackColor;
         public Color PosColor => _posColor;
         public Color RotColor => _rotColor;
         public Color ScaleColor => _scaleColor;
