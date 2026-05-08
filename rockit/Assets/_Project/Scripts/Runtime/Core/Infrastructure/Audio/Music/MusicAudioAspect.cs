9cbc85d90eacfc37ac4703540a275dffe1a133d9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 11:10:18 +0300

    Create MusicAudioAspect, add it to DomainAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Music/MusicAudioAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Music
+{
+    public sealed class MusicAudioAspect : ProtoAspectInject
+    {
+        
+    }
+}
