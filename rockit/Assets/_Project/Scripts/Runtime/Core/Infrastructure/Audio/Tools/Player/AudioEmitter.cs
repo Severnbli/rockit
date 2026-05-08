aa50fef851b2f68cd0850d277943a90747bfdf89
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 12:20:16 +0300

    Create AudioEmitter that implements IPlayable and ITweenPipelineAnimatable

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Tools/Player/AudioEmitter.cs
@@ -0,0 +1,35 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tweens.Pipeline.Core;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Tools.Player
+{
+    public class AudioEmitter : IPlayable, ITweenPipelineAnimatable
+    {
+        protected readonly AudioSource Source;
+        protected readonly ITweenPipelineSequenceCreator _tpsCreator;
+
+        public AudioEmitter(AudioSource source, ITweenPipelineSequenceCreator tpsCreator)
+        {
+            Source = source;
+            _tpsCreator = tpsCreator;
+        }
+
+        public void Play(AudioClip clip, bool looped)
+        {
+            Source.clip = clip;
+            Source.loop = looped;
+            Source.Play();
+        }
+
+        public void Stop()
+        {
+            Source.Stop();
+        }
+
+        public Sequence Animate(TweenPipeline tp)
+        {
+            return _tpsCreator.CreateSequence(tp, Source.gameObject);
+        }
+    }
+}
