e58bd5891de69969ef46a20f33f04eb6c9f2fe88
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 21:29:07 +0300

    Create PhysicsSharedConfig with only field "_initialGravityScale" and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Configs/PhysicsSharedConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Shared.Configs
+{
+    public sealed class PhysicsSharedConfig : ScriptableObjectAutoInstaller<PhysicsSharedConfig>
+    {
+        [SerializeField] private float _initialGravityScale;
+        
+        public float InitialGravityScale => _initialGravityScale;
+    }
+}
