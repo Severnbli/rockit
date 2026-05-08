2e7037ae626ea6b464b6d1749262204f4d00e003
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-08 11:48:07 +0300

    Create AudioUtils with only method "CreatePlaySfxRequest"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Infrastructure/AudioUtils.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Infrastructure
+{
+    public static class AudioUtils
+    {
+        public static ProtoEntity CreatePlaySfxRequest(RequestsAspect aspect, PlaySfxRequest prepared)
+        {
+            return aspect.CreateRequest(aspect.AudioRequestsAspect.PlaySfxRequestPool, prepared: prepared);
+        }
+    }
+}
