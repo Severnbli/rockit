0dffc3c3fbb9e4a145ce8c04989c42cf745f6ac4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 02:31:08 +0300

    Create SpriteGlowComponent with only "SpriteGlow" field and authoring attributes. Add pool and iterator of it to SpritesSharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Shared/Components/SpriteGlowComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Leopotam.EcsProto.Unity;
+using SpriteGlow;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared.Components
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct SpriteGlowComponent
+    {
+        public SpriteGlowEffect SpriteGlow;
+    }
+}
--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Shared/SpritesSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Shared/SpritesSharedAspect.cs
@@ -8,7 +8,9 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared
     public sealed class SpritesSharedAspect : ProtoAspectInject
     {
         public readonly ProtoPool<FaceComponent> FaceComponentPool;
+        public readonly ProtoPool<SpriteGlowComponent> SpriteGlowComponentPool;
         public readonly ProtoIt Faces = new (It.Inc<FaceComponent>());
         public readonly ProtoIt MoveTransformFaces = new (It.Inc<MoveComponent, FaceComponent>());
+        public readonly ProtoIt SpriteGlows = new (It.Inc<SpriteGlowComponent>());
     }
 }
