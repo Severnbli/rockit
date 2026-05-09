ac65d361842da4f79bfad595195b5b25b3988dec
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 13:31:39 +0300

    Create ColorUtils with only method "MoveTowards"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Graphics/Shared/ColorUtils.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.Graphics.Shared
+{
+    public static class ColorUtils
+    {
+        public static Color MoveTowards(Color a, Color b, float distance)
+        {
+            return Vector4.MoveTowards(a, b, distance);
+        }
+    }
+}
