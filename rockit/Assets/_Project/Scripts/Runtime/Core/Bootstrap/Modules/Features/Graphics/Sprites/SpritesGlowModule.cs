b8586b2ab78f38b40d67242a2b626c41812f20ea
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 13:03:28 +0300

    Create SpritesGlowModule, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Sprites/SpritesGlowModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Sprites
+{
+    public sealed class SpritesGlowModule : BaseModule<SpritesGlowModule>
+    {
+        public SpritesGlowModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
