a8cf95670d40b7508037844dfbeb607fb523a55c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 18:12:14 +0300

    Add "Velocity" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Components/CharacterVelocityComponent.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Components/CharacterVelocityComponent.cs
@@ -1,7 +1,9 @@
-﻿namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Components
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Components
 {
     public struct CharacterVelocityComponent
     {
-        
+        public Vector2 Velocity;
     }
 }
