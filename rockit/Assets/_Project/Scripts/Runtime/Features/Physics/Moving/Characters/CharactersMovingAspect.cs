6bac53e4ce3db29e5a4158ebd94708257ec82cd6
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 17:57:05 +0300

    Create CharacterVelocityComponent, add it pool and iterator to CharacterVelocityComponent

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/CharactersMovingAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/CharactersMovingAspect.cs
@@ -15,9 +15,11 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters
         public readonly ProtoPool<JumpBufferingComponent> JumpBufferingComponentPool;
         public readonly ProtoPool<DashTimeoutComponent> DashTimeoutComponentPool;
         public readonly ProtoPool<DashComponent> DashComponentPool;
+        public readonly ProtoPool<CharacterVelocityComponent> CharacterVelocityComponentPool;
         public readonly ProtoIt GroundCheckable = new (It.Inc<TransformComponent, GroundCheckComponent>());
         public readonly ProtoIt GroundCheckResults = new (It.Inc<GroundCheckResultComponent>());
         public readonly ProtoIt Rigidbody2DGroundCheckResults = new (It.Inc<Rigidbody2DComponent, GroundCheckResultComponent>());
+        public readonly ProtoIt Rigidbody2DCharactersVelocities = new (It.Inc<Rigidbody2DComponent, CharacterVelocityComponent>());
         public readonly ProtoIt JumpBufferingGroundCheckResults = new (It.Inc<JumpBufferingComponent, GroundCheckResultComponent>());
         public readonly ProtoIt JumpBuffers = new (It.Inc<JumpBufferingComponent>());
         public readonly ProtoIt DashTimeouts = new ProtoIt(It.Inc<DashTimeoutComponent>());
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Components/CharacterVelocityComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Components
+{
+    public struct CharacterVelocityComponent
+    {
+        
+    }
+}
