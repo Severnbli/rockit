04daa22da77b8b2932460a0f47a1903066a55a23
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 13:15:40 +0300

    Create SpriteGlowChangeComponent, add it pool and iterators to SpritesGlowAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Glow/SpritesGlowAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Glow/SpritesGlowAspect.cs
@@ -1,5 +1,4 @@
 ﻿using _Project.Scripts.Runtime.Features.Graphics.Sprites.Glow.Components;
-using _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -8,6 +7,9 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Sprites.Glow
     public sealed class SpritesGlowAspect : ProtoAspectInject
     {
         public readonly ProtoPool<SpriteGlowComponent> SpriteGlowComponentPool;
+        public readonly ProtoPool<SpriteGlowChangeComponent> SpriteGlowChangeComponentPool;
         public readonly ProtoIt SpriteGlows = new (It.Inc<SpriteGlowComponent>());
+        public readonly ProtoIt SpriteGlowChanges = new (It.Inc<SpriteGlowChangeComponent>());
+        public readonly ProtoIt SpriteGlowWithSpriteGlowChanges = new (It.Inc<SpriteGlowComponent, SpriteGlowChangeComponent>());
     }
 }
