727f7d60ac17920ec7080445d0ffad2350f7df1f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 13:26:35 +0300

    Create ColorConfig with only field "_colorChangeTolerance" and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Shared/Configs/ColorConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Shared.Configs
+{
+    public sealed class ColorConfig : ScriptableObjectAutoInstaller<ColorConfig>
+    {
+        [SerializeField] private float _colorChangeTolerance = 0.01f;
+        
+        public float ColorChangeTolerance => _colorChangeTolerance;
+    }
+}
