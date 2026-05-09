60f2efbf3a875ab31e92f8eb9228a7c1f0b0656d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 18:10:02 +0300

    Update "Walkables", "Dashables" and "Jumpables" iterators: replace Rigidbody2DComponent iType with CharacterTag

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/CharactersMovingAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/CharactersMovingAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Tags;
 using _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Components;
 using _Project.Scripts.Runtime.Features.Physics.Moving.Shared.Components;
 using _Project.Scripts.Runtime.Features.Physics.Moving.Shared.Tags;
@@ -24,10 +25,10 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters
         public readonly ProtoIt JumpBufferingGroundCheckResults = new (It.Inc<JumpBufferingComponent, GroundCheckResultComponent>());
         public readonly ProtoIt JumpBuffers = new (It.Inc<JumpBufferingComponent>());
         public readonly ProtoIt DashTimeouts = new ProtoIt(It.Inc<DashTimeoutComponent>());
-        public readonly ProtoItExc Walkables = new (It.Inc<Rigidbody2DComponent, MovableTag>(), It.Exc<DashTimeoutComponent>());
+        public readonly ProtoItExc Walkables = new (It.Inc<CharacterTag, MovableTag>(), It.Exc<DashTimeoutComponent>());
         public readonly ProtoIt DashGroundCheckResults = new (It.Inc<DashComponent, GroundCheckResultComponent>());
-        public readonly ProtoItExc Dashables = new (It.Inc<Rigidbody2DComponent, GroundCheckResultComponent, MovableTag>(), It.Exc<DashTimeoutComponent>());
-        public readonly ProtoIt Jumpables = new (It.Inc<Rigidbody2DComponent, GroundCheckResultComponent, MovableTag>());
+        public readonly ProtoItExc Dashables = new (It.Inc<CharacterTag, GroundCheckResultComponent, MovableTag>(), It.Exc<DashTimeoutComponent>());
+        public readonly ProtoIt Jumpables = new (It.Inc<CharacterTag, GroundCheckResultComponent, MovableTag>());
         public readonly ProtoItExc Deceleratables = new (It.Inc<Rigidbody2DComponent, CharacterMoveComponent>(), It.Exc<DashTimeoutComponent>());
         public readonly ProtoItExc MoveSnapCreatables = new (It.Inc<GroundCheckResultComponent, Rigidbody2DComponent>(), It.Exc<MoveSnapComponent>());
         public readonly ProtoIt MoveSnapRemovables = new (It.Inc<GroundCheckResultComponent, MoveSnapComponent>());
