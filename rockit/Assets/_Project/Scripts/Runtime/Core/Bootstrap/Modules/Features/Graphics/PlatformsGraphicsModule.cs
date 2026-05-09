48257e87a2beb2410c5f3cbb376afa0d94d68fe9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 14:50:42 +0300

    Create PlatformsGraphicsModule, register it to GameSceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/PlatformsGraphicsModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics
+{
+    public sealed class PlatformsGraphicsModule : BaseModule<PlatformsGraphicsModule>
+    {
+        public PlatformsGraphicsModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
