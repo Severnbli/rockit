a43aa888f3de695615d1aae0f6d0e323d871b983
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 13:53:06 +0300

    Create PlatformColorChangeComponent, add it pool to PlatformsGraphicsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/Components/PlatformColorChangeComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace _Project.Scripts.Runtime.Features.Graphics.Platforms.Components
+{
+    public struct PlatformColorChangeComponent
+    {
+        public bool PositionColorWas;
+        public bool RotationColorWas;
+        public bool ScaleColorWas;
+    }
+}
