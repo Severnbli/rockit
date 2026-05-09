0e753c5a388456887879e142d0d996985de76eaf
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 21:48:31 +0300

    Remove useless extension methods at rigidbody2d

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Shared/Rigidbody2DExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Shared/Rigidbody2DExtensions.cs
@@ -4,46 +4,9 @@ namespace _Project.Scripts.Runtime.Shared.Extensions.Shared
 {
     public static class Rigidbody2DExtensions
     {
-        public static void ResetVelocityY(this Rigidbody2D rigidbody2D)
-        {
-            var velocity = rigidbody2D.linearVelocity;
-            velocity.y = 0f;
-            rigidbody2D.linearVelocity = velocity;
-        }
-
-        public static void ResetVelocityX(this Rigidbody2D rigidbody2D)
-        {
-            var velocity = rigidbody2D.linearVelocity;
-            velocity.x = 0f;
-            rigidbody2D.linearVelocity = velocity;
-        }
-
-        public static void ResetPositiveVelocityY(this Rigidbody2D rigidbody2D)
-        {
-            if (rigidbody2D.linearVelocity.y < 0f) return;
-            rigidbody2D.ResetVelocityY();
-        }
-
-        public static void ResetPositiveVelocityYOnSideCollision(this Rigidbody2D rigidbody2D, Vector2 normal, float tolerance)
-        {
-            if (Mathf.Abs(normal.x) < tolerance) return;
-            rigidbody2D.ResetPositiveVelocityY();
-        }
-
-        public static void ResetVelocityXOnSideCollision(this Rigidbody2D rigidbody2D, Vector2 normal, float tolerance)
-        {
-            if (Mathf.Abs(normal.x) < tolerance) return;
-            rigidbody2D.ResetVelocityX();
-        }
-
         public static bool Falling(this Rigidbody2D rigidbody2D)
         {
             return rigidbody2D.linearVelocity.y < 0f;
-        }
-
-        public static bool MoveSideways(this Rigidbody2D rigidbody2D)
-        {
-            return !Mathf.Approximately(rigidbody2D.linearVelocity.x, 0f);
         }
 
         public static void MoveTo(this Rigidbody2D rigidbody2D, Vector2 pos, float speed, float deltaTime)
