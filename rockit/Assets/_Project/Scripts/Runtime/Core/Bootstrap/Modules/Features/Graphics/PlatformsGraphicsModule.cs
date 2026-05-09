d26983fbf44631a169976f44fd6f22873215574b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 15:48:44 +0300

    Create UpdateScalePlatformColorWithSpriteGlowSystem, bind it to PlatformsGraphicsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/PlatformsGraphicsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/PlatformsGraphicsModule.cs
@@ -17,6 +17,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics
             BindSystem<PlatformColorChangeTimeoutExpireSystem>();
             BindSystem<UpdatePositionPlatformColorWithSpriteGlowSystem>();
             BindSystem<UpdateRotationPlatformColorWithSpriteGlowSystem>();
+            BindSystem<UpdateScalePlatformColorWithSpriteGlowSystem>();
         }
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/Systems/UpdateScalePlatformColorWithSpriteGlowSystem.cs
@@ -0,0 +1,35 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Platforms.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.Sprites.Glow;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Platforms.Systems
+{
+    public class UpdateScalePlatformColorWithSpriteGlowSystem : IProtoRunSystem
+    {
+        [DI] private readonly PlatformsGraphicsAspect _pgAspect;
+        [DI] private readonly SpritesGlowAspect _sgAspect;
+        private readonly PlatformsGraphicsConfig _pgConfig;
+
+        public UpdateScalePlatformColorWithSpriteGlowSystem(PlatformsGraphicsConfig pgConfig)
+        {
+            _pgConfig = pgConfig;
+        }
+
+        public void Run()
+        {
+            foreach (var e in _pgAspect.SpriteGlowActivePositionPlatforms)
+            {
+                ref var pccComponent = ref _pgAspect.PlatformColorChangeComponentPool.GetOrAdd(e);
+                if (pccComponent.Blocked || pccComponent.RotationColorWas) continue;
+
+                pccComponent.Blocked = true;
+                pccComponent.ScaleColorWas = true;
+                
+                ref var sgcComponent = ref _sgAspect.SpriteGlowChangeComponentPool.GetOrAdd(e);
+                sgcComponent.Target = _pgConfig.ScaleColor;
+                sgcComponent.Speed = _pgConfig.TransitionSpeed;
+            }
+        }
+    }
+}
