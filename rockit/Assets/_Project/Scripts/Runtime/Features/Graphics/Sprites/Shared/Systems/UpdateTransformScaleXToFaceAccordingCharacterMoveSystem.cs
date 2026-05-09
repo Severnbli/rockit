a44c97679db387f2635c2a8b55b05e57c38a326a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 18:25:27 +0300

    Update UpdateTransformScaleXToFaceAccordingMoveSystem to new MoveComponent name (CharacterMoveComponent). Rename it into UpdateTransformScaleXToFaceAccordingCharacterMoveSystem according to its function. Also update iterator at SpritesSharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Shared/Systems/UpdateTransformScaleXToFaceAccordingCharacterMoveSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Shared/Systems/UpdateTransformScaleXToFaceAccordingCharacterMoveSystem.cs
@@ -1,5 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
-using _Project.Scripts.Runtime.Features.Physics.Moving.Shared;
+using _Project.Scripts.Runtime.Features.Physics.Moving.Characters;
 using _Project.Scripts.Runtime.Shared.Utils.Features.Graphics.Sprites;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
@@ -7,25 +7,25 @@ using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Sprites.Shared.Systems
 {
-    public sealed class UpdateTransformScaleXToFaceAccordingMoveSystem : IProtoRunSystem
+    public sealed class UpdateTransformScaleXToFaceAccordingCharacterMoveSystem : IProtoRunSystem
     {
         [DI] private readonly SpritesSharedAspect _ssAspect;
-        [DI] private readonly MovingSharedAspect _mSharedAspect;
+        [DI] private readonly CharactersMovingAspect _cmAspect;
         [DI] private readonly SharedAspect _sAspect;
         
         public void Run()
         {
-            foreach (var e in _ssAspect.MoveTransformFaces)
+            foreach (var e in _ssAspect.CharacterMoveTransformFaces)
             {
                 ref var fComponent = ref _ssAspect.FaceComponentPool.Get(e);
-                ref var mComponent = ref _mSharedAspect.MoveComponentPool.Get(e);
+                ref var cmComponent = ref _cmAspect.CharacterMoveComponentPool.Get(e);
                 ref var tComponent = ref _sAspect.TransformComponentPool.Get(e);
 
                 var scale = tComponent.Transform.localScale;
                 scale.x = Mathf.Abs(scale.x);
 
                 if (!SpritesSharedUtils.FacingDirectionMatchedWithMoveDirection(fComponent.Direction,
-                        mComponent.Direction))
+                        cmComponent.Direction))
                 {
                     scale.x *= -1f;
                 }
