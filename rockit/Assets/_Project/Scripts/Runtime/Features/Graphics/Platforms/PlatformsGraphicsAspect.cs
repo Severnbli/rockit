dccaaa70d452d6eb0bb2503396a1026238231ff2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 13:53:06 +0300

    Create PlatformColorChangeComponent, add it pool to PlatformsGraphicsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/PlatformsGraphicsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/PlatformsGraphicsAspect.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.Sprites.Glow.Components;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Platforms.Components;
+using _Project.Scripts.Runtime.Features.Graphics.Sprites.Glow.Components;
 using _Project.Scripts.Runtime.Features.Platforms.Shared.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
@@ -7,6 +8,7 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Platforms
 {
     public sealed class PlatformsGraphicsAspect : ProtoAspectInject
     {
+        public readonly ProtoPool<PlatformColorChangeComponent> PlatformColorChangeComponentPool;
         public readonly ProtoIt SpriteGlowPlatforms = new (It.Inc<PlatformComponent, SpriteGlowComponent>());
     }
 }
