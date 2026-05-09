b25a93b5c12b319e00825a5ac3739fb2254c03bd
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 14:39:12 +0300

    Create PlatformColorChangeTimeoutComponent, add it pool and iterator to PlatformsGraphicsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/PlatformsGraphicsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/PlatformsGraphicsAspect.cs
@@ -9,7 +9,9 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Platforms
     public sealed class PlatformsGraphicsAspect : ProtoAspectInject
     {
         public readonly ProtoPool<PlatformColorChangeComponent> PlatformColorChangeComponentPool;
+        public readonly ProtoPool<PlatformColorChangeTimeoutComponent> PlatformColorChangeTimeoutComponentPool;
         public readonly ProtoIt SpriteGlowPlatforms = new (It.Inc<PlatformComponent, SpriteGlowComponent>());
+        public readonly ProtoIt PlatformColorChangeTimeouts = new (It.Inc<PlatformColorChangeComponent>());
         public readonly ProtoItExc SpriteGlowPlatformWithoutSpriteGlowChanges = new (It.Inc<PlatformComponent, SpriteGlowComponent>(), It.Exc<SpriteGlowChangeComponent>());
     }
 }
