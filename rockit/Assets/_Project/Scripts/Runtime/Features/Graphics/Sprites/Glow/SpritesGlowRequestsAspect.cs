26368ce1cea5cac337fc10b7f3a843cbff05caf0
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 14:55:48 +0300

    Create SpriteGlowChangeCompletedRequest, add it to SpritesGlowRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Glow/SpritesGlowRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Glow/SpritesGlowRequestsAspect.cs
@@ -1,9 +1,14 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using _Project.Scripts.Runtime.Features.Graphics.Sprites.Glow.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Sprites.Glow
 {
     public sealed class SpritesGlowRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<SpriteGlowChangeCompletedRequest> SpriteGlowChangeCompletedRequestPool; 
+        public readonly ProtoIt SpriteGlowChangeCompletedRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, SpriteGlowChangeCompletedRequest>());
     }
 }
