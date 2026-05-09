bd6ff8ecb3bdf9417c27f068ca149b9a9d75dce9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 15:33:58 +0300

    Create ActivePlatformTag, add it pool and iterator to PlatformsSharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedAspect.cs
@@ -12,11 +12,13 @@ namespace _Project.Scripts.Runtime.Features.Platforms.Shared
         public readonly ProtoPool<PositionPlatformTag> PositionPlatformTagPool;
         public readonly ProtoPool<RotationPlatformTag> RotationPlatformTagPool;
         public readonly ProtoPool<ScalePlatformTag> ScalePlatformTagPool;
+        public readonly ProtoPool<ActivePlatformTag> ActivePlatformTagPool;
         public readonly ProtoIt Platforms = new (It.Inc<PlatformComponent>());
         public readonly ProtoIt PlatformStates = new (It.Inc<PlatformStatesComponent>());
         public readonly ProtoIt PlatformsWithPlatformStates = new (It.Inc<PlatformComponent, PlatformStatesComponent>());
         public readonly ProtoIt PositionPlatforms = new (It.Inc<PlatformComponent, PositionPlatformTag>());
         public readonly ProtoIt RotationPlatforms = new (It.Inc<PlatformComponent, RotationPlatformTag>());
         public readonly ProtoIt ScalePlatforms = new (It.Inc<PlatformComponent, ScalePlatformTag>());
+        public readonly ProtoIt ActivePlatforms = new (It.Inc<PlatformComponent, ActivePlatformTag>());
     }
 }
