a9bfb82f9fd46679e0c730feb9b5a7dd0ab34d87
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 15:45:42 +0300

    Create UpdatePositionPlatformColorWithSpriteGlowSystem, bind it to PlatformsGraphicsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/PlatformsGraphicsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/PlatformsGraphicsModule.cs
@@ -15,6 +15,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics
             
             BindSystem<CreatePlatformColorChangeTimeoutOnSpriteGlowChangeCompletedRequestSystem>();
             BindSystem<PlatformColorChangeTimeoutExpireSystem>();
+            BindSystem<UpdatePositionPlatformColorWithSpriteGlowSystem>();
         }
     }
 }
