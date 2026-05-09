997d5f657b087ba7c94ef6c4851ff8e95e990db0
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 20:33:10 +0300

    Add velocity y nullify on grounded status

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Systems/ApplyJumpDecelerationOnFixedRunSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Systems/ApplyJumpDecelerationOnFixedRunSystem.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using _Project.Scripts.Runtime.Core.Systems;
+using _Project.Scripts.Runtime.Features.Physics.Shared;
 using Leopotam.EcsProto.QoL;
 using UnityEngine;
 
@@ -21,14 +22,22 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Systems
             {
                 ref var cmComponent = ref _cmAspect.CharacterMoveComponentPool.Get(e);
                 ref var cvComponent = ref _cmAspect.CharacterVelocityComponentPool.GetOrAdd(e);
-                
+                ref var gcrComponent = ref _cmAspect.GroundCheckResultComponentPool.GetOrAdd(e);
+
                 var velocity = cvComponent.Velocity;
                 
-                velocity.y = Mathf.MoveTowards(
-                    velocity.y,
-                    -cmComponent.MaxFallingVelocity,
-                    cmComponent.JumpDeceleration * _tService.UnscaledFixedDeltaTime
-                );
+                if (gcrComponent.Grounded)
+                {
+                    velocity.y = PhysicsSharedContracts.ForceNotApplied;
+                }
+                else
+                {
+                    velocity.y = Mathf.MoveTowards(
+                        velocity.y,
+                        -cmComponent.MaxFallingVelocity,
+                        cmComponent.JumpDeceleration * _tService.UnscaledFixedDeltaTime
+                    );
+                }
                 
                 cvComponent.Velocity = velocity;
             }
