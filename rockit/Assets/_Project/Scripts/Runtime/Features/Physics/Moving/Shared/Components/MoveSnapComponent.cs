a2582d99cb36b9b39574070d8c36de6a02cc2392
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 17:44:52 +0300

    Remove "Tied" field and add "HostVelocity" field, update systems that use it

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Shared/Components/MoveSnapComponent.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Shared/Components/MoveSnapComponent.cs
@@ -6,6 +6,6 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Shared.Components
     {
         public Rigidbody2D Host;
         public Vector2 LastHostPos;
-        public Rigidbody2D Tied;
+        public Vector2 HostVelocity;
     }
 }
