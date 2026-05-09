f52b4efe4c2e08c09effadb4820b2793376ae822
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 18:39:55 +0300

    Remove useles "ApplyWalk", "ApplyJump" and "ApplyDash" rigidbody 2d extension methods

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Shared/Rigidbody2DExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Shared/Rigidbody2DExtensions.cs
@@ -4,28 +4,6 @@ namespace _Project.Scripts.Runtime.Shared.Extensions.Shared
 {
     public static class Rigidbody2DExtensions
     {
-        public static void ApplyWalk(this Rigidbody2D rigidbody2D, float factor)
-        {
-            var velocity = rigidbody2D.linearVelocity;
-            velocity.x = factor;
-            rigidbody2D.linearVelocity = velocity;
-        }
-        
-        public static void ApplyJump(this Rigidbody2D rigidbody2D, float factor)
-        {
-            var velocity = rigidbody2D.linearVelocity;
-            velocity.y = 0f;
-            rigidbody2D.linearVelocity = velocity;
-            rigidbody2D.AddForce(Vector2.up * factor, ForceMode2D.Impulse);
-        }
-
-        public static void ApplyDash(this Rigidbody2D rigidbody2D, float factor, int direction)
-        {
-            factor = Mathf.Abs(factor);
-            direction = Mathf.Clamp(direction, -1, 1);
-            rigidbody2D.linearVelocity = new Vector2(factor * direction, 0f);
-        }
-
         public static bool VelocityXZero(this Rigidbody2D rigidbody2D)
         {
             return Mathf.Approximately(rigidbody2D.linearVelocityX, 0f);
