9ab92d918abb3901ec65442c1428ab61b8cf641d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 14:59:23 +0300

    Create SpritesGlowUtils with only method "CreateSpriteGlowChangeCompletedRequest"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Graphics/Sprites/SpritesGlowUtils.cs
@@ -0,0 +1,21 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.Sprites.Glow.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.Graphics.Sprites
+{
+    public static class SpritesGlowUtils
+    {
+        public static ProtoEntity CreateSpriteGlowChangeCompletedRequest(RequestsAspect aspect, 
+            ProtoPackedEntityWithWorld targetEntity, SpriteGlowChangeCompletedRequest prepared)
+        {
+            return aspect.CreateRequest(
+                aspect.SpritesGlowRequestsAspect.SpriteGlowChangeCompletedRequestPool,
+                targetEntity: targetEntity,
+                prepared: prepared
+            );
+        }
+    }
+}
