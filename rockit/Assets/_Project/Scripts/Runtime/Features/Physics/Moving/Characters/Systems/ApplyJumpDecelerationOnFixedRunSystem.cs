7c4913d3a2844abf1804bf2ad20eace70112f84c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 18:57:32 +0300

    Split logic of walk and jump decelerations into 2 different systems

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Systems/ApplyJumpDecelerationOnFixedRunSystem.cs
@@ -0,0 +1,37 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using _Project.Scripts.Runtime.Core.Systems;
+using Leopotam.EcsProto.QoL;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Systems
+{
+    public sealed class ApplyJumpDecelerationOnFixedRunSystem : IProtoFixedRunSystem
+    {
+        [DI] private readonly CharactersMovingAspect _cmAspect;
+        private readonly TimeService _tService;
+
+        public ApplyJumpDecelerationOnFixedRunSystem(TimeService tService)
+        {
+            _tService = tService;
+        }
+
+        public void FixedRun()
+        {
+            foreach (var e in _cmAspect.JumpDeceleratables)
+            {
+                ref var cmComponent = ref _cmAspect.CharacterMoveComponentPool.Get(e);
+                ref var cvComponent = ref _cmAspect.CharacterVelocityComponentPool.Get(e);
+                
+                var velocity = cvComponent.Velocity;
+                
+                if (velocity.y <= 0f) return;
+
+                velocity.y = Mathf.MoveTowards(
+                    velocity.x,
+                    0f,
+                    cmComponent.JumpDeceleration * _tService.UnscaledFixedDeltaTime
+                );
+            }
+        }
+    }
+}
--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Systems/ApplyWalkDecelerationOnFixedRunSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Systems/ApplyWalkDecelerationOnFixedRunSystem.cs
@@ -1,25 +1,23 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
 using _Project.Scripts.Runtime.Core.Systems;
-using _Project.Scripts.Runtime.Features.Physics.Shared;
 using Leopotam.EcsProto.QoL;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Systems
 {
-    public sealed class ApplyDecelerationsOnFixedRunSystem : IProtoFixedRunSystem
+    public sealed class ApplyWalkDecelerationOnFixedRunSystem : IProtoFixedRunSystem
     {
         [DI] private readonly CharactersMovingAspect _cmAspect;
-        [DI] private readonly PhysicsSharedAspect _psAspect;
         private readonly TimeService _tService;
 
-        public ApplyDecelerationsOnFixedRunSystem(TimeService tService)
+        public ApplyWalkDecelerationOnFixedRunSystem(TimeService tService)
         {
             _tService = tService;
         }
 
         public void FixedRun()
         {
-            foreach (var e in _cmAspect.Deceleratables)
+            foreach (var e in _cmAspect.WalkDeceleratables)
             {
                 ref var cmComponent = ref _cmAspect.CharacterMoveComponentPool.Get(e);
                 ref var cvComponent = ref _cmAspect.CharacterVelocityComponentPool.Get(e);
@@ -30,14 +28,6 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Systems
                     velocity.x,
                     0f,
                     cmComponent.WalkDeceleration * _tService.UnscaledFixedDeltaTime
-                );
-                
-                if (velocity.y <= 0f) return;
-
-                velocity.y = Mathf.MoveTowards(
-                    velocity.x,
-                    0f,
-                    cmComponent.JumpDeceleration * _tService.UnscaledFixedDeltaTime
                 );
             }
         }
