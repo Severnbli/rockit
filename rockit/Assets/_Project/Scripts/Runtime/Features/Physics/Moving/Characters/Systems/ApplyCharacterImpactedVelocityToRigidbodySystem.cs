8867b3e28861cef9c22adf96876ba007a65e297a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 19:10:53 +0300

    Create ApplyCharacterImpactedVelocityToRigidbodySystem, bind it to CharactersMovingModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Systems/ApplyCharacterImpactedVelocityToRigidbodySystem.cs
@@ -0,0 +1,22 @@
+﻿using _Project.Scripts.Runtime.Core.Systems;
+using _Project.Scripts.Runtime.Features.Physics.Shared;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Systems
+{
+    public sealed class ApplyCharacterImpactedVelocityToRigidbodySystem : IProtoFixedRunSystem
+    {
+        [DI] private readonly CharactersMovingAspect _cmAspect;
+        [DI] private readonly PhysicsSharedAspect _psAspect;
+        
+        public void FixedRun()
+        {
+            foreach (var e in _cmAspect.Rigidbody2DCharactersImpactedVelocities)
+            {
+                ref var civComponent = ref _cmAspect.CharacterImpactedVelocityComponentPool.Get(e);
+                ref var rComponent = ref _psAspect.Rigidbody2DComponentPool.Get(e);
+                rComponent.Rigidbody2D.linearVelocity = civComponent.Velocity;
+            }
+        }
+    }
+}
