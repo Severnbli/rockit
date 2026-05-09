a3f34f17a4e616dce304b27ef7541d0930987de3
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 18:52:04 +0300

    Rename ApplyWalkDecelerationOnFixedRunSystem into ApplyDecelerationsOnFixedRunSystem. Also update logic to decelerate jump and operate with character velocity instead of rigidbody

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Characters/Systems/ApplyDecelerationsOnFixedRunSystem.cs
@@ -0,0 +1,45 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using _Project.Scripts.Runtime.Core.Systems;
+using _Project.Scripts.Runtime.Features.Physics.Shared;
+using Leopotam.EcsProto.QoL;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Moving.Characters.Systems
+{
+    public sealed class ApplyDecelerationsOnFixedRunSystem : IProtoFixedRunSystem
+    {
+        [DI] private readonly CharactersMovingAspect _cmAspect;
+        [DI] private readonly PhysicsSharedAspect _psAspect;
+        private readonly TimeService _tService;
+
+        public ApplyDecelerationsOnFixedRunSystem(TimeService tService)
+        {
+            _tService = tService;
+        }
+
+        public void FixedRun()
+        {
+            foreach (var e in _cmAspect.Deceleratables)
+            {
+                ref var cmComponent = ref _cmAspect.CharacterMoveComponentPool.Get(e);
+                ref var cvComponent = ref _cmAspect.CharacterVelocityComponentPool.Get(e);
+                
+                var velocity = cvComponent.Velocity;
+                
+                velocity.x = Mathf.MoveTowards(
+                    velocity.x,
+                    0f,
+                    cmComponent.WalkDeceleration * _tService.UnscaledFixedDeltaTime
+                );
+                
+                if (velocity.y <= 0f) return;
+
+                velocity.y = Mathf.MoveTowards(
+                    velocity.x,
+                    0f,
+                    cmComponent.JumpDeceleration * _tService.UnscaledFixedDeltaTime
+                );
+            }
+        }
+    }
+}
