86dc19bce3691d6976445d0dfe691d77b50636a8
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-09 13:00:59 +0300

    Create SpriteGlowChangeColorRequest, add it pool and iterator to SpritesGlowRequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Sprites/Glow/Requests/SpriteGlowChangeColorRequest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Sprites.Glow.Requests
+{
+    public struct SpriteGlowChangeColorRequest
+    {
+        public Color Target;
+        public float TransitionDuration;
+    }
+}
