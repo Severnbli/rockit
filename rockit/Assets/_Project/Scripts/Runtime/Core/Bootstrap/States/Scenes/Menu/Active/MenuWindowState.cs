56c627dd5c704a728a70e118cde25b3cf061a91a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-10 13:58:30 +0300

    Create MenuWindowState that implements ISceneState

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/MenuWindowState.cs
@@ -0,0 +1,25 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Defined.Menu;
+using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active
+{
+    public class MenuWindowState : ISceneState
+    {
+        private readonly MenuWindow _window;
+
+        public MenuWindowState(MenuWindow window)
+        {
+            _window = window;
+        }
+
+        public async UniTask OnEnter(IStateMachine stateMachine)
+        {
+            await _window.Open();
+        }
+
+        public async UniTask OnLeave(IStateMachine stateMachine)
+        {
+            await UniTask.NextFrame();
+        }
+    }
+}
