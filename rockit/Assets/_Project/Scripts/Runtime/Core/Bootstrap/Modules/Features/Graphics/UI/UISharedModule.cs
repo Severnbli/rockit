0ab21ecf3ccbfe7b56ff494c77c6ea6286211e08
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-10 16:34:38 +0300

    Create UISharedModule, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/UISharedModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI
+{
+    public sealed class UISharedModule : BaseModule<UISharedModule>
+    {
+        public UISharedModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
