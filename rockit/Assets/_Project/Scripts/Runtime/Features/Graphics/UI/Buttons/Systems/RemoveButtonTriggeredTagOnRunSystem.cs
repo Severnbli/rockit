f2499016c2ad8c792928e63277812ba5f068f8b2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-10 16:20:28 +0300

    Create RemoveButtonTriggeredTagOnRunSystem, bind it to ButtonsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Systems/RemoveButtonTriggeredTagOnRunSystem.cs
@@ -0,0 +1,18 @@
+﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Systems
+{
+    public sealed class RemoveButtonTriggeredTagOnRunSystem : IProtoRunSystem
+    {
+        [DI] private readonly ButtonsAspect _bAspect;
+        
+        public void Run()
+        {
+            foreach (var e in _bAspect.TriggeredButtons)
+            {
+                _bAspect.ButtonTriggeredTagPool.Del(e);
+            }
+        }
+    }
+}
