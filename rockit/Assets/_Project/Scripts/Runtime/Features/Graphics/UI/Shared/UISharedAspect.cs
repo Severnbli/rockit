30ae6628d512b7e0fbcb7d6b4ad08cddce1812e2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-10 17:05:10 +0300

    Create WindowsAspect, add it to UISharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UISharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UISharedAspect.cs
@@ -1,6 +1,7 @@
 ﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Tags;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Text.Shared;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -10,6 +11,7 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared
     {
         public readonly ButtonsAspect ButtonsAspect;
         public readonly TextSharedAspect TextSharedAspect;
+        public readonly WindowsAspect WindowsAspect;
         
         public readonly ProtoPool<ClickedTag> ClickedTagPool;
         public readonly ProtoIt ClickedTags = new (It.Inc<ClickedTag>());
