c39b77b746a3a1c4d1c6327b06e772004f27da8d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-10 13:51:34 +0300

    Add "_menuWindow" field, bind it to MenuSceneDomain

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/MenuSceneDomain.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Domain/Scenes/MenuSceneDomain.cs
@@ -1,9 +1,20 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Defined.Menu;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Domain.Scenes
 {
     public class MenuSceneDomain : SceneDomain
     {
+        [SerializeField] private MenuWindow _menuWindow;
+
+        protected override void RegisterBindings()
+        {
+            base.RegisterBindings();
+            
+            Container.Bind<MenuWindow>().FromInstance(_menuWindow).AsSingle();
+        }
+
         protected override void RegisterStates()
         {
             base.RegisterStates();
