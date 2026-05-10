39882283afafc82d2a9767191bd2fcae4718cfa2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-10 13:49:39 +0300

    Define MenuWindow

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Defined/Menu/MenuWindowConfig.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Configs;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Defined.Menu
+{
+    public sealed class MenuWindowConfig : FadeWindowConfig<MenuWindowConfig>
+    {
+        
+    }
+}
