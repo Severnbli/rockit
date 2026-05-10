3235b0eca3800d893814fc1db21e3eb331e983cf
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-10 16:20:28 +0300

    Create RemoveButtonTriggeredTagOnRunSystem, bind it to ButtonsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/ButtonsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/ButtonsModule.cs
@@ -15,6 +15,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI
             
             BindSystem<AddButtonTriggeredTagOnButtonTriggeredSystem>();
             BindSystem<SendCloseAppRequestOnCloseButtonClickedSystem>();
+            BindSystem<RemoveButtonTriggeredTagOnRunSystem>();
         }
     }
 }
