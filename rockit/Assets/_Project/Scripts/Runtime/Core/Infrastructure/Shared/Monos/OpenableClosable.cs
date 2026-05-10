dbeca9d9765ff40938dcc2d471643f28cfe0966f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-10 17:20:49 +0300

    Create OpenableClosable that inherited from MonoBehaviour and provide virtual "Open" and "Close" methods

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Monos/OpenableClosable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Monos
+{
+    public class OpenableClosable : MonoBehaviour
+    {
+        public virtual void Open() {}
+        public virtual void Close() {}
+    }
+}
