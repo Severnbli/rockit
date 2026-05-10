254fd6ba03823d2d4509fadb2d0ec6316fb60424
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-10 16:30:20 +0300

    Create ClickedTag, add it pool to UISharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UISharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UISharedAspect.cs
@@ -1,5 +1,7 @@
 ﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Tags;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Text.Shared;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared
@@ -8,5 +10,7 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared
     {
         public readonly ButtonsAspect ButtonsAspect;
         public readonly TextSharedAspect TextSharedAspect;
+        
+        public readonly ProtoPool<ClickedTag> ClickedTagPool;
     }
 }
