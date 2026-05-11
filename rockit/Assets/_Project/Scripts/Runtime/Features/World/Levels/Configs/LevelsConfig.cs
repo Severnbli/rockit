5f9034c086556e7b0d74aa72c35de46c8d3d84d4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 17:38:59 +0300

    Create LevelsConfig with only field "_levels" and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Configs/LevelsConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Modules.Zenject;
+using _Project.Scripts.Runtime.Features.World.Levels.Types;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels.Configs
+{
+    public sealed class LevelsConfig : ScriptableObjectAutoInstaller<LevelsConfig>
+    {
+        [SerializeField] private Dictionary<int, LevelDefinition> _levels = new ();
+        
+        public Dictionary<int, LevelDefinition> Levels => _levels;
+    }
+}
