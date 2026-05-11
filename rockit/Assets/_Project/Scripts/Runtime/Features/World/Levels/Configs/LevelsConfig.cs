e67532020175c3b1d1fc0fa9915de2140e4bcbae
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 23:58:42 +0300

    Change type of "_levels" to SortedDictionary

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Configs/LevelsConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Configs/LevelsConfig.cs
@@ -7,10 +7,10 @@ namespace _Project.Scripts.Runtime.Features.World.Levels.Configs
 {
     public sealed class LevelsConfig : ScriptableObjectAutoInstaller<LevelsConfig>
     {
-        [SerializeField] private Dictionary<int, LevelDefinition> _levels = new ();
+        [SerializeField] private SortedDictionary<int, LevelDefinition> _levels = new ();
         [SerializeField] private int _maxStarsPerLevel = 3;
         
-        public Dictionary<int, LevelDefinition> Levels => _levels;
+        public SortedDictionary<int, LevelDefinition> Levels => _levels;
         public int MaxStarsPerLevel => _maxStarsPerLevel;
     }
 }
