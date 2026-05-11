94d19290800ecb4b8d0e2983d72f7cb32408b5b6
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 16:50:29 +0300

    Remove "_image" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Monos/LevelButtonView.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Monos/LevelButtonView.cs
@@ -1,18 +1,15 @@
 ﻿using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Monos
 {
     public class LevelButtonView : ButtonView
     {
-        [SerializeField] private Image _image;
         [SerializeField] private TextMeshProUGUI _idText;
         [SerializeField] private GameObject _starsContainer;
         [SerializeField] private GameObject _unlockInfoArea;
         [SerializeField] private TextMeshProUGUI _starsToUnlockText;
         
-        public Image Image => _image;
         public TextMeshProUGUI IdText => _idText;
         public GameObject StarsContainer => _starsContainer;
         public GameObject UnlockInfoArea => _unlockInfoArea;
