ac501661479eec861d929d8a87d630cf91873849
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-12 00:15:09 +0300

    Create OpenableClosableSprite with "_opened" and "_closed" fields and they property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Monos/OpenableClosableSprite.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Monos
+{
+    public class OpenableClosableSprite : OpenableClosable
+    {
+        [SerializeField] private Sprite _opened;
+        [SerializeField] private Sprite _closed;
+        
+        public Sprite Opened => _opened;
+        public Sprite Closed => _closed;
+    }
+}
