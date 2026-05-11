4e6e1ef6d2bfbcad55774b45b85bb4cbcbd62ea5
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 23:17:38 +0300

    Create ButtonsUtils with "CreateCreateAllLevelButtonsRequest" and "CreateCreateLevelButtonRequest" methods

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Graphics/ButtonsUtils.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.Graphics
+{
+    public static class ButtonsUtils
+    {
+        public static ProtoEntity CreateCreateAllLevelButtonsRequest(RequestsAspect aspect,
+            CreateAllLevelButtonsRequest prepared)
+        {
+            return aspect.CreateRequest(aspect.UIRequestsAspect.ButtonsRequestsAspect.CreateAllLevelButtonsRequestPool,
+                prepared: prepared);
+        }
+
+        public static ProtoEntity CreateCreateLevelButtonRequest(RequestsAspect aspect,
+            CreateLevelButtonRequest prepared)
+        {
+            return aspect.CreateRequest(aspect.UIRequestsAspect.ButtonsRequestsAspect.CreateLevelButtonRequestPool,
+                prepared: prepared);
+        }
+    }
+}
