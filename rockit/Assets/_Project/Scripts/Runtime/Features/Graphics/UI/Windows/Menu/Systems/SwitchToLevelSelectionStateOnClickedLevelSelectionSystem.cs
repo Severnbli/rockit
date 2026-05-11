ed0cd81e711e8534c0f1a77b10e1ca374b90751c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 13:12:19 +0300

    Create SwitchToLevelSelectionStateOnClickedLevelSelectionSystem, bind it to MenuSceneWindowsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Menu/Systems/SwitchToLevelSelectionStateOnClickedLevelSelectionSystem.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.States;
+using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu.Systems
+{
+    public sealed class SwitchToLevelSelectionStateOnClickedLevelSelectionSystem : IProtoRunSystem
+    {
+        [DI] private readonly MenuSceneWindowsAspect _mswAspect;
+        private readonly IStateMachine _sMachine;
+
+        public SwitchToLevelSelectionStateOnClickedLevelSelectionSystem(IStateMachine sMachine)
+        {
+            _sMachine = sMachine;
+        }
+
+        public void Run()
+        {
+            if (_mswAspect.ClickedLevelSelections.IsEmptySlow()) return;
+            _sMachine.ChangeState<LevelSelectionState>();
+        }
+    }
+}
