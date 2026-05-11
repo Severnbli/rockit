6e466f1840734b4b31fc2c2e15904ea9f74a6b79
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 20:10:58 +0300

    Remove stars fields and add "_starIcon" field with its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/Configs/UIPrefabsStorageConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/Configs/UIPrefabsStorageConfig.cs
@@ -6,11 +6,9 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Configs
     public sealed class UIPrefabsStorageConfig : ScriptableObjectAutoInstaller<UIPrefabsStorageConfig>
     {
         [SerializeField] private GameObject _levelButton;
-        [SerializeField] private GameObject _filledStarImage;
-        [SerializeField] private GameObject _emptyStarImage;
+        [SerializeField] private GameObject _starIcon;
         
         public GameObject LevelButton => _levelButton;
-        public GameObject FilledStarImage => _filledStarImage;
-        public GameObject EmptyStarImage => _emptyStarImage;
+        public GameObject StarIcon => _starIcon;
     }
 }
