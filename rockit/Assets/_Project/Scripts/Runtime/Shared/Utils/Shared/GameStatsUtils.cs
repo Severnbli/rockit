21d12f741dc43d28ba2c1aefdb67578c234b074d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 23:10:48 +0300

    Create GameStatsUtils with only "GetTotalStars" method

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Shared/GameStatsUtils.cs
@@ -0,0 +1,19 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Shared
+{
+    public static class GameStatsUtils
+    {
+        public static int GetTotalStars(DataProvider dProvider)
+        {
+            var total = 0;
+
+            foreach (var kvp in dProvider.GameSceneData.CompletedLevels)
+            {
+                total += kvp.Value.Stars;
+            }
+            
+            return total;
+        }
+    }
+}
