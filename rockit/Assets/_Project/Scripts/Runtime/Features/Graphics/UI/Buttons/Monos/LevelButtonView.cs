a3494df87e264a3de0fd430a4ea0ec8497af068e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 16:25:41 +0300

    Add "_starsContainer" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Monos/LevelButtonView.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Monos/LevelButtonView.cs
@@ -6,7 +6,9 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Monos
     public class LevelButtonView : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _idText;
+        [SerializeField] private GameObject _starsContainer;
         
         public TextMeshProUGUI IdText => _idText;
+        public GameObject StarsContainer => _starsContainer;
     }
 }
