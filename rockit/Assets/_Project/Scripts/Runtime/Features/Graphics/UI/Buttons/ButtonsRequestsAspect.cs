56b657fd19b37d0afd28a161e1dc178006c09949
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 22:42:04 +0300

    Create ButtonsRequestsAspect, add it to UIRequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/ButtonsRequestsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons
+{
+    public sealed class ButtonsRequestsAspect : ProtoAspectInject
+    {
+        
+    }
+}
--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UIRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UIRequestsAspect.cs
@@ -1,9 +1,10 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared
 {
     public sealed class UIRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ButtonsRequestsAspect ButtonsRequestsAspect;
     }
 }
