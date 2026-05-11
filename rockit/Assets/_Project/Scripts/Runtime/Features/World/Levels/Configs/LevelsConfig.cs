a66c87f2f10ff0c0148caf47dbbf63caf6856ada
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 20:15:11 +0300

    Add "_maxStarsPerLevel" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Configs/LevelsConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Configs/LevelsConfig.cs
@@ -8,7 +8,9 @@ namespace _Project.Scripts.Runtime.Features.World.Levels.Configs
     public sealed class LevelsConfig : ScriptableObjectAutoInstaller<LevelsConfig>
     {
         [SerializeField] private Dictionary<int, LevelDefinition> _levels = new ();
+        [SerializeField] private int _maxStarsPerLevel = 3;
         
         public Dictionary<int, LevelDefinition> Levels => _levels;
+        public int MaxStarsPerLevel => _maxStarsPerLevel;
     }
 }
