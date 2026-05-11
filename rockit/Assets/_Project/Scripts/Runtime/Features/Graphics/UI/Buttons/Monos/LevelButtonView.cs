0729faf1385e4d6e1b3935068bad4c1b945041c2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 16:30:06 +0300

    Add "_unlockInfoArea" and "_starsToUnlockText" fields and they property getters

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Monos/LevelButtonView.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Monos/LevelButtonView.cs
@@ -7,8 +7,12 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Monos
     {
         [SerializeField] private TextMeshProUGUI _idText;
         [SerializeField] private GameObject _starsContainer;
+        [SerializeField] private GameObject _unlockInfoArea;
+        [SerializeField] private TextMeshProUGUI _starsToUnlockText;
         
         public TextMeshProUGUI IdText => _idText;
         public GameObject StarsContainer => _starsContainer;
+        public GameObject UnlockInfoArea => _unlockInfoArea;
+        public TextMeshProUGUI StarsToUnlockText => _starsToUnlockText;
     }
 }
