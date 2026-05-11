17d7cd903aef069e72e448d76096bc24118ac8a2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 17:13:11 +0300

    Create PrefabsStorageConfig that inherited from ScriptableObjectAutoInstaller with only field "_levelButton" and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Configs/PrefabsStorageConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Configs
+{
+    public sealed class PrefabsStorageConfig : ScriptableObjectAutoInstaller<PrefabsStorageConfig>
+    {
+        [SerializeField] private GameObject _levelButton;
+        
+        public GameObject LevelButton => _levelButton;
+    }
+}
