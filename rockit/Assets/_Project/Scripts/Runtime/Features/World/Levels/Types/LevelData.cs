98ca6e5c4ec4d99433ef4ad4b517b3ef37c00cd5
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 19:09:37 +0300

    Create serializable LevelData with only "Stars" field

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Types/LevelData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels.Types
+{
+    [Serializable]
+    public sealed class LevelData
+    {
+        public int Stars;
+    }
+}
