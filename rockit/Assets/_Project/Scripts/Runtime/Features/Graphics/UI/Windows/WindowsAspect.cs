be6c77bbcb639ecfea9c815147d79670ecb5ad32
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 12:46:11 +0300

    Create LevelSelectionTag, add it pool into WindowsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/WindowsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/WindowsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows
 {
     public sealed class WindowsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<LevelSelectionTag> LevelSelectionTagPool;
     }
 }
