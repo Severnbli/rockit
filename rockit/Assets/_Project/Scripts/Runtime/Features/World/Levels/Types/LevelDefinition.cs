218386c1d52da878142f8ce1dbd48d6fd7f2a1ae
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 17:31:58 +0300

    Remove Id field

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Types/LevelDefinition.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Types/LevelDefinition.cs
@@ -6,7 +6,6 @@ namespace _Project.Scripts.Runtime.Features.World.Levels.Types
     [Serializable]
     public sealed class LevelDefinition
     {
-        public int Id;
         public GameObject Prefab;
         public string NameKey;
         public int StarsToUnlock;
