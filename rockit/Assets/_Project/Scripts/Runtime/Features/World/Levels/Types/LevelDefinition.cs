d7dc14542f3b26cb6cf65d116e521247176d5045
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 17:30:37 +0300

    Rename "Number" field into "Id"

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Types/LevelDefinition.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Types/LevelDefinition.cs
@@ -6,7 +6,7 @@ namespace _Project.Scripts.Runtime.Features.World.Levels.Types
     [Serializable]
     public sealed class LevelDefinition
     {
-        public int Number;
+        public int Id;
         public GameObject Prefab;
         public string NameKey;
         public int StarsToUnlock;
