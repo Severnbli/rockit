6edb96f8ebb60a3f40de474979a1bc9c6391381e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 22:56:06 +0300

    Create SpawnLevelButtonRequest, add it pool and iterator

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Requests/SpawnLevelButtonRequest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Requests
+{
+    public struct SpawnLevelButtonRequest
+    {
+        public Transform At;
+        public int LevelId;
+    }
+}
