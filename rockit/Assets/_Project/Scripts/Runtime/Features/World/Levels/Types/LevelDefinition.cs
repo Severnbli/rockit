63735b413d6cce27a212c0d82004383a35c8eadb
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 17:29:07 +0300

    Create LevelDefinition with serializable attribute and "Number", "Prefab", "NameKey" and "StarsToUnlock" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Types/LevelDefinition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels.Types
+{
+    [Serializable]
+    public sealed class LevelDefinition
+    {
+        public int Number;
+        public GameObject Prefab;
+        public string NameKey;
+        public int StarsToUnlock;
+    }
+}
