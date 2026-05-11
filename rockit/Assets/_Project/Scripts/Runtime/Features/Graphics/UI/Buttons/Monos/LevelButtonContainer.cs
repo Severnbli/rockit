f6033f97290f80b844c5acfc94099b2766df5e44
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 17:07:37 +0300

    Create LevelButtonContainer that inherited from Container

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Monos/LevelButtonContainer.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Monos
+{
+    public class LevelButtonContainer : Container
+    {
+        
+    }
+}
