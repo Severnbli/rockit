bd524b90befb04401e5ed808b7045ab12aa4b5d0
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 19:21:45 +0300

    Add "_filledStarImage" and "_emptyStarImage" fields and they property getters into UIPrefabsStorageConfig

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/Configs/UIPrefabsStorageConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/Configs/UIPrefabsStorageConfig.cs
@@ -6,7 +6,11 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Configs
     public sealed class UIPrefabsStorageConfig : ScriptableObjectAutoInstaller<UIPrefabsStorageConfig>
     {
         [SerializeField] private GameObject _levelButton;
+        [SerializeField] private GameObject _filledStarImage;
+        [SerializeField] private GameObject _emptyStarImage;
         
         public GameObject LevelButton => _levelButton;
+        public GameObject FilledStarImage => _filledStarImage;
+        public GameObject EmptyStarImage => _emptyStarImage;
     }
 }
