4767bcdf125235dcc03903b887ae4196990363c8
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 21:53:38 +0300

    Create LevelItemComponent, add it pool to LevelsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Components/LevelItemComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.Runtime.Features.World.Levels.Components
+{
+    public struct LevelItemComponent
+    {
+        public int LevelId;
+    }
+}
--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.World.Levels.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.World.Levels
 {
     public sealed class LevelsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<LevelItemComponent> LevelItemComponentPool;
     }
 }
