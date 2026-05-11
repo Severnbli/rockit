05322579f947ecfff94109e1c658c7c559d61b1e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 22:14:21 +0300

    Add "SetStatus" method

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Monos/OpenableClosable.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Monos/OpenableClosable.cs
@@ -4,6 +4,18 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Monos
 {
     public class OpenableClosable : MonoBehaviour
     {
+        public virtual void SetStatus(bool opened)
+        {
+            if (opened)
+            {
+                Open();
+            }
+            else
+            {
+                Close();
+            }
+        }
+        
         public virtual void Open() {}
         public virtual void Close() {}
     }
