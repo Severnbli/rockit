b3d3a53cdcd8a4e945e2d72a8664ae23cb33bbfc
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 19:32:32 +0300

    Create StarIcon that inherited from AnimatableOpenableClosable

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Icons/Monos/StarIcon.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Monos;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Icons.Monos
+{
+    public class StarIcon : AnimatableOpenableClosable
+    {
+        
+    }
+}
