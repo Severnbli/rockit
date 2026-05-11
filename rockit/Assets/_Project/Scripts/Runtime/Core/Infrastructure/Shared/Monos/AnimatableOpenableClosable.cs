4d2b2bcc3215c320fd0707ef9bdfba798da96796
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-11 19:31:42 +0300

    Create AnimatableOpenableClosable that inherited from OpenableClosable

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Monos/AnimatableOpenableClosable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Monos
+{
+    public class AnimatableOpenableClosable : OpenableClosable
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _openTrigger = "Open";
+        [SerializeField] private string _closeTrigger = "Close";
+        
+        private int OpenTrigger => Animator.StringToHash(_openTrigger);
+        private int CloseTrigger => Animator.StringToHash(_closeTrigger);
+
+        public override void Open()
+        {
+            base.Open();
+            _animator.SetTrigger(OpenTrigger);
+        }
+
+        public override void Close()
+        {
+            base.Close();
+            _animator.SetTrigger(CloseTrigger);
+        }
+    }
+}
