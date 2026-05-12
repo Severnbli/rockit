4dbb30ba773514219a038db531c6f82f423353ea
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-12 19:18:52 +0300

    Add "LevelItemOpeners" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsAspect.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Features.World.Levels.Components;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Tags;
+using _Project.Scripts.Runtime.Features.World.Levels.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -7,5 +8,6 @@ namespace _Project.Scripts.Runtime.Features.World.Levels
     public sealed class LevelsAspect : ProtoAspectInject
     {
         public readonly ProtoPool<LevelItemComponent> LevelItemComponentPool;
+        public readonly ProtoIt LevelItemOpeners = new (It.Inc<LevelItemComponent, OpenerTag>());
     }
 }
