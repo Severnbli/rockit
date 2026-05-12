3d234ec9f4e3033bb75577db0448cdf578aade3f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 00:53:52 +0300

    Create SetActiveUIElementStatusByButtonInteractableStatusSystem, bind it to ButtonsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Buttons/Systems/SetActiveUIElementStatusByButtonInteractableStatusSystem.cs
@@ -0,0 +1,22 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Shared;
+using _Project.Scripts.Runtime.Shared.Extensions.Shared;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Systems
+{
+    public sealed class SetActiveUIElementStatusByButtonInteractableStatusSystem : IProtoRunSystem
+    {
+        [DI] private readonly ButtonsAspect _bAspect;
+        [DI] private readonly UISharedAspect _usAspect;
+        
+        public void Run()
+        {
+            foreach (var e in _bAspect.Buttons)
+            {
+                ref var bComponent = ref _bAspect.ButtonComponentPool.Get(e);
+                _usAspect.ActiveUIElementTagPool.AddOrDelOnCondition(e, bComponent.Button.interactable);
+            }
+        }
+    }
+}
