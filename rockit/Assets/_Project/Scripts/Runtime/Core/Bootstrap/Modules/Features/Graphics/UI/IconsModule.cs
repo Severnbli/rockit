93e9965a65583d198ca8516bc400307599f38afe
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 00:59:42 +0300

    Move StarIconFactory binding from UISharedModule into IconsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/IconsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/IconsModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Icons.Types;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI
     {
         public IconsModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void RegisterBindings()
+        {
+            base.RegisterBindings();
+            
+            Container.Bind<StarIconFactory>().ToSelf().AsSingle();
         }
     }
 }
