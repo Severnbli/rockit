1d8b7a2d87de75fdbc2fc40c68c736afba280240
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 00:51:52 +0300

    Create 2 "AddOrDelOnCondition" ecs extensions methods

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Shared/EcsExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Shared/EcsExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Leopotam.EcsProto;
+﻿using System;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Shared.Extensions.Shared
@@ -14,6 +15,25 @@ namespace _Project.Scripts.Runtime.Shared.Extensions.Shared
         public static ProtoPool<T> GetPool<T>(this ProtoWorld world) where T : struct
         {
             return world.Pool<T>() as ProtoPool<T>;
+        }
+
+        public static void AddOrDelOnCondition<T>(this ProtoPool<T> pool, ProtoEntity entity, bool add) 
+            where T : struct
+        {
+            if (add)
+            {
+                pool.GetOrAdd(entity);
+            }
+            else
+            {
+                pool.DelIfExists(entity);
+            }
+        }
+
+        public static void AddOrDelOnCondition<T>(this ProtoPool<T> pool, ProtoEntity entity, Func<bool> add)
+            where T : struct
+        {
+            AddOrDelOnCondition(pool, entity, add());
         }
     }
 }
