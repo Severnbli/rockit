f7998b167df73c6b10a4dfe3186c6d585a659402
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-12 19:31:22 +0300

    Move "ClickedLevelItemOpeners" iterator from LevelsAspect into UISharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsAspect.cs
@@ -1,6 +1,4 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Tags;
-using _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Tags;
-using _Project.Scripts.Runtime.Features.World.Levels.Components;
+﻿using _Project.Scripts.Runtime.Features.World.Levels.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -9,6 +7,5 @@ namespace _Project.Scripts.Runtime.Features.World.Levels
     public sealed class LevelsAspect : ProtoAspectInject
     {
         public readonly ProtoPool<LevelItemComponent> LevelItemComponentPool;
-        public readonly ProtoIt ClickedLevelItemOpeners = new (It.Inc<ClickedTag, LevelItemComponent, OpenerTag>());
     }
 }
