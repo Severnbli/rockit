5cad688288bcdea2b76d9f18998aa42e8f59e9c9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 00:53:52 +0300

    Create SetActiveUIElementStatusByButtonInteractableStatusSystem, bind it to ButtonsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/ButtonsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/ButtonsModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI
     {
         public ButtonsModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<SetActiveUIElementStatusByButtonInteractableStatusSystem>();
         }
     }
 }
