1cfc7f0bd8b822a28485555ee67dd43e7975d0b5
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-12 19:48:10 +0300

    Create SendSwitchSceneRequestOnClickedGameSceneOpenerSystem, bind it to UISharedModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/Systems/SendSwitchSceneRequestOnClickedGameSceneOpenerSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Scenes;
+using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Requests;
+using _Project.Scripts.Runtime.Shared.Utils.Infrastructure;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Systems
+{
+    public sealed class SendSwitchSceneRequestOnClickedGameSceneOpenerSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly RequestsAspect _rAspect;
+        [DI] private readonly UISharedAspect _usAspect;
+        
+        public void Run()
+        {
+            if (_usAspect.ClickedLevelItemOpeners.IsEmptySlow()) return;
+
+            var prepared = new SwitchSceneRequest
+            {
+                Target = ScenesContracts.GameScene
+            };
+
+            ScenesUtils.CreateSwitchSceneRequest(_rAspect, prepared: prepared);
+        }
+    }
+}
