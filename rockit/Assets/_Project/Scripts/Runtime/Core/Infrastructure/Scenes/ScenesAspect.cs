b047c57114a7386255b32819e0a85a9ae893e4a6
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-12 19:39:40 +0300

    Create MenuSceneTag with authoring attributes, add it pool and iterator

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/ScenesAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/ScenesAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes
 {
     public sealed class ScenesAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<MenuSceneTag> MenuSceneTagPool;
     }
 }
