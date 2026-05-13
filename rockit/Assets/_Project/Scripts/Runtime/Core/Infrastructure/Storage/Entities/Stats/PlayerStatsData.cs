9526bc40ee1856e424d4043e91bacddd9ea4583c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 01:57:06 +0300

    Replace "Modifier" phrase at fields with "FactorUpdate"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/Stats/PlayerStatsData.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/Stats/PlayerStatsData.cs
@@ -6,8 +6,8 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities.Stats
     [Serializable]
     public sealed class PlayerStatsData
     {
-        public int WalkModifierIndex = ProjectContracts.ArrayEmptyInt;
-        public int JumpModifierIndex = ProjectContracts.ArrayEmptyInt;
-        public int DashModifierIndex = ProjectContracts.ArrayEmptyInt;
+        public int WalkFactorUpdateIndex = ProjectContracts.ArrayEmptyInt;
+        public int JumpFactorUpdateIndex = ProjectContracts.ArrayEmptyInt;
+        public int DashFactorUpdateIndex = ProjectContracts.ArrayEmptyInt;
     }
 }
