9f26535fb567f289ecd71e215e69f272064ced09
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 02:45:38 +0300

    Create EnableRequest, add it pool and iterator to SharedRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/SharedRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/SharedRequestsAspect.cs
@@ -11,9 +11,11 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared
         public readonly ProtoPool<InitializeRequest> InitializeRequestPool;
         public readonly ProtoPool<CloseAppRequest> CloseAppRequestPool;
         public readonly ProtoPool<DisableRequest> DisableRequestPool;
+        public readonly ProtoPool<EnableRequest> EnableRequestPool;
         public readonly ProtoIt InitializeRunRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, InitializeRequest>());
         public readonly ProtoIt InitializeFixedRunRequests = new (It.Inc<RequestComponent, ActiveRequestTag, FixedRunRequestTag, InitializeRequest>());
         public readonly ProtoIt CloseAppRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, CloseAppRequest>());
         public readonly ProtoIt DisableRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, DisableRequest>());
+        public readonly ProtoIt EnableRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, EnableRequest>());
     }
 }
