a4cec90fed306a6dbbb16f96bb73b0f00642d593
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 19:41:37 +0300

    Create GlitchPhaseExpireSystem, bind it to GlitchEffectsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Systems/GlitchPhaseExpireSystem.cs
@@ -0,0 +1,26 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
+using _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Services;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Systems
+{
+    public sealed class GlitchPhaseExpireSystem : IProtoRunSystem
+    {
+        private readonly GlitchPhaseService _gpService;
+        private readonly TimeService _tService;
+
+        public GlitchPhaseExpireSystem(GlitchPhaseService gpService, TimeService tService)
+        {
+            _gpService = gpService;
+            _tService = tService;
+        }
+
+        public void Run()
+        {
+            if (!_gpService.Applied || !_tService.Expired(_gpService.StartTime, _gpService.Timeout)) return;
+            _gpService.Applied = false;
+            _gpService.LastApplyTime = _tService.UnscaledTime;
+        }
+    }
+}
