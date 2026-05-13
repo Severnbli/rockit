894337c75d1c705f50ab9a3d10cc6aa5f3e2cb3b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 02:25:32 +0300

    Add "ClampPlayerStatsDataIndexesToConfig" player stats util method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/PlayerStatsUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/PlayerStatsUtils.cs
@@ -1,11 +1,37 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
-using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
-using Leopotam.EcsProto;
+﻿using System;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities.Stats;
+using _Project.Scripts.Runtime.Features.Stats.Player.Configs;
+using _Project.Scripts.Runtime.Features.Stats.Shared;
 
 namespace _Project.Scripts.Runtime.Shared.Utils.Features.Stats
 {
     public static class PlayerStatsUtils
     {
-        
+        public static void ClampPlayerStatsDataIndexesToConfig(PlayerStatsData psData, PlayerStatsConfig psConfig)
+        {
+            psData.WalkFactorUpdateIndex = Math.Clamp(
+                psData.WalkFactorUpdateIndex,
+                StatsSharedContracts.NullStatUpdateIndex,
+                psConfig.WalkFactorUpdates.Length
+            );
+
+            psData.JumpFactorUpdateIndex = Math.Clamp(
+                psData.JumpFactorUpdateIndex,
+                StatsSharedContracts.NullStatUpdateIndex,
+                psConfig.JumpFactorUpdates.Length
+            );
+
+            psData.JumpFactorUpdateIndex = Math.Clamp(
+                psData.JumpFactorUpdateIndex,
+                StatsSharedContracts.NullStatUpdateIndex,
+                psConfig.JumpFactorUpdates.Length
+            );
+
+            psData.DashQuantityUpdateIndex = Math.Clamp(
+                psData.DashQuantityUpdateIndex,
+                StatsSharedContracts.NullStatUpdateIndex,
+                psConfig.DashQuantityUpdates.Length
+            );
+        }
     }
 }
