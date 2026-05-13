4f565d59f6c692e563c713e498d27b67d8b0b456
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 19:52:42 +0300

    Create StatsSharedModule, register it to ProjectDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/StatsSharedModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats
+{
+    public sealed class StatsSharedModule : BaseModule<StatsSharedModule>
+    {
+        public StatsSharedModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
