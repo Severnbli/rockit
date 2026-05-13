3a812d4eff636443ed508e367a651c0b0b57bb11
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 20:27:16 +0300

    Create ResetGlitchServiceFeatureOnDestroySystem, bind it to GlitchEffectsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Systems/ResetGlitchServiceFeatureOnDestroySystem.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Services;
+using _Project.Scripts.Runtime.Shared.Extensions.Features.Graphics.Effects;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Systems
+{
+    public sealed class ResetGlitchServiceFeatureOnDestroySystem : IProtoDestroySystem
+    {
+        private readonly GlitchService _gService;
+        private readonly GlitchConfig _gConfig;
+
+        public ResetGlitchServiceFeatureOnDestroySystem(GlitchService gService, GlitchConfig gConfig)
+        {
+            _gService = gService;
+            _gConfig = gConfig;
+        }
+
+        public void Destroy()
+        {
+            _gService.SetValuesFromSettingsToFeature(_gConfig.DefaultSettings);
+        }
+    }
+}
