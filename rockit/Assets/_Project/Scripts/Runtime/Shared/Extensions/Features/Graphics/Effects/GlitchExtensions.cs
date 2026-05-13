f9290b0c33e9c9c71b0c86dda1a3510434095471
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 18:53:31 +0300

    Create GlitchExtensions with only method "SetValuesFromSettings"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Features/Graphics/Effects/GlitchExtensions.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Services;
+using _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Types;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions.Features.Graphics.Effects
+{
+    public static class GlitchExtensions
+    {
+        public static void SetValuesFromSettings(this GlitchService service, GlitchSettings settings)
+        {
+            service.Chromatic01 = settings.ChromaticGlitch;
+            service.Frame01 = settings.FrameGlitch;
+            service.Pixel01 = settings.PixelGlitch;
+        }
+    }
+}
