eb724dbdb54e0e92449aef63cfca7e8b1af68e78
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 19:51:02 +0300

    Create GameStatsService with "ProgressOfPassage01" and "ProgressOfPassage0100" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Shared/Services/GameStatsService.cs
@@ -0,0 +1,8 @@
+﻿namespace _Project.Scripts.Runtime.Features.Stats.Shared.Services
+{
+    public sealed class GameStatsService
+    {
+        public float ProgressOfPassage01;
+        public float ProgressOfPassage0100;
+    }
+}
