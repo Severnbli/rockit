4801557574255df99acc5c322bcea2eaf360107e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 02:31:51 +0300

    Create CoinsRequestsAspect, add it to RequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/CoinsRequestsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Economy.Coins
+{
+    public sealed class CoinsRequestsAspect : ProtoAspectInject
+    {
+        
+    }
+}
