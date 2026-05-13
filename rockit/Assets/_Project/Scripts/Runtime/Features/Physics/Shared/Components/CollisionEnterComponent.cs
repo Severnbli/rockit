24c7e97e7306b0a3967bdab970075bfcf4bdbad7
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 18:29:07 +0300

    Create CollisionEnterComponent, add it pool and iterator to PhysicsSharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Components/CollisionEnterComponent.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.Unity.Physics2D;
+
+namespace _Project.Scripts.Runtime.Features.Physics.Shared.Components
+{
+    public struct CollisionEnterComponent
+    {
+        public UnityPhysics2DCollisionEnterEvent Data;
+    }
+}
