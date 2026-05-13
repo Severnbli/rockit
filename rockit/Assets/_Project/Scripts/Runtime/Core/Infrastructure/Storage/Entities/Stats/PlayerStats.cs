0770ef0303f18a805e8a9d43c2febefce367dae5
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 01:54:01 +0300

    Move PlayerStats into Stats folder

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/Stats/PlayerStats.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/Stats/PlayerStats.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using _Project.Scripts.Runtime.Shared;
 
-namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities.Stats
 {
     [Serializable]
     public sealed class PlayerStats
