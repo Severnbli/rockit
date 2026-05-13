29876c1af3593cd111635574d0d830fec219ea8f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 20:01:26 +0300

    Create UpdateGameStatsServiceTotalStarsOnRunSystem, bind it to StatsSharedModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Shared/Systems/UpdateGameStatsServiceTotalStarsOnRunSystem.cs
@@ -0,0 +1,23 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
+using _Project.Scripts.Runtime.Features.Stats.Shared.Services;
+using _Project.Scripts.Runtime.Shared.Utils.Features.Stats;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Shared.Systems
+{
+    public sealed class UpdateGameStatsServiceTotalStarsOnRunSystem : IProtoRunSystem
+    {
+        private readonly GameStatsService _gsService;
+        private readonly DataProvider _dProvider;
+
+        public UpdateGameStatsServiceTotalStarsOnRunSystem(GameStatsService gsService)
+        {
+            _gsService = gsService;
+        }
+
+        public void Run()
+        {
+            _gsService.TotalStars = GameStatsUtils.GetTotalStars(_dProvider);
+        }
+    }
+}
