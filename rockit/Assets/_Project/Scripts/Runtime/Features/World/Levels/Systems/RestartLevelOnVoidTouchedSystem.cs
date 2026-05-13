98f27c5cf6ce47139eb8bd7085f5aeb38894aa4b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 17:35:16 +0300

    Change to delete in the future

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Systems/RestartLevelOnVoidTouchedSystem.cs
@@ -0,0 +1,44 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Scenes;
+using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Configs;
+using _Project.Scripts.Runtime.Features.Physics.Shared;
+using _Project.Scripts.Runtime.Shared.Utils.Infrastructure;
+using _Project.Scripts.Runtime.Shared.Utils.Shared;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels.Systems
+{
+    public sealed class RestartLevelOnVoidTouchedSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly RequestsAspect _rAspect;
+        [DI] private readonly PhysicsEventsAspect _peAspect;
+        private readonly LayersConfig _lConfig;
+
+        public RestartLevelOnVoidTouchedSystem(LayersConfig lConfig)
+        {
+            _lConfig = lConfig;
+        }
+
+        public void Run()
+        {
+            LogUtils.Log("A");
+            foreach (var e in _peAspect.TriggerEnterEvents)
+            {
+                LogUtils.Log("TRIGGER");
+                
+                var prepared = new SwitchSceneRequest
+                {
+                    Target = ScenesContracts.GameScene
+                };
+
+                ScenesUtils.CreateSwitchSceneRequest(_rAspect, prepared);
+                
+                ref var data = ref _peAspect.TriggerEnterEventPool.Get(e);
+                if (data.Collider.gameObject.layer != _lConfig.VoidLayer) continue;
+            }
+        }
+    }
+}
