1a09f706360680716dc93e006d994baae07d034d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 17:01:16 +0300

    Fix initializing of "_lFactory" field at contructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/SpawnLevelState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/SpawnLevelState.cs
@@ -6,7 +6,12 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
     public class SpawnLevelState : ISceneState
     {
         private readonly LevelFactory _lFactory;
-        
+
+        public SpawnLevelState(LevelFactory lFactory)
+        {
+            _lFactory = lFactory;
+        }
+
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
             _lFactory.Create();
