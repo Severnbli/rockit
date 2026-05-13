c7a623bb38788b243321734fe8bf7e55272d6ddb
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 17:03:49 +0300

    Add "_voidLayer" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Configs/LayersConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Configs/LayersConfig.cs
@@ -6,7 +6,9 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Configs
     public sealed class LayersConfig : ScriptableObjectAutoInstaller<LayersConfig>
     {
         [SerializeField] private LayerMask _groundLayer;
+        [SerializeField] private LayerMask _voidLayer;
         
         public LayerMask GroundLayer => _groundLayer;
+        public LayerMask VoidLayer => _voidLayer;
     }
 }
