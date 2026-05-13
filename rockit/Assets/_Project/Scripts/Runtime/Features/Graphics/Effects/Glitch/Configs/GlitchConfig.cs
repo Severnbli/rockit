820356db5bdaa3503a519f85789cba96991d028d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 18:49:18 +0300

    Create GlitchConfig with "_fallbackSettings" and "_steps" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Configs/GlitchConfig.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Types;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Configs
+{
+    public sealed class GlitchConfig : ScriptableObjectAutoInstaller<GlitchConfig>
+    {
+        [SerializeField] private GlitchSettings _fallbackSettings;
+        [SerializeField] private GlitchStep[] _steps;
+        
+        public GlitchSettings FallbackSettings => _fallbackSettings;
+        public GlitchStep[] Steps => _steps;
+    }
+}
