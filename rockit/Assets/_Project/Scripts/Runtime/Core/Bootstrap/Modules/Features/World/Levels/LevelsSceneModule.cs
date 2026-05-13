f59b30a8260955eb228000c272ac77c010b7cd07
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 16:54:42 +0300

    Create LevelSceneModule , register it to GameSceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Levels/LevelsSceneModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World.Levels
+{
+    public sealed class LevelsSceneModule : BaseModule<LevelsSceneModule>
+    {
+        public LevelsSceneModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
