64f91f539ba562fa88bea022a9102e609aaa69a9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 18:48:49 +0300

    Create GlitchStep with "Serializable" attribute and "GameProgress", "Duration", "Frequency" and "GlitchSettings" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Types/GlitchStep.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Types
+{
+    [Serializable]
+    public sealed class GlitchStep
+    {
+        public float GameProgress;
+        public float Duration;
+        public float Frequency;
+        public GlitchSettings GlitchSettings;
+    }
+}
