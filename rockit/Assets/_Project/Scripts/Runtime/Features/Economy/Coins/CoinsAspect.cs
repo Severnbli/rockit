d158de13c49d1726462b27b3c5500df4b192fe6b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 01:01:56 +0300

    Create CoinComponent with authoring attributes and "Id" field, add it pool to CoinsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/CoinsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/CoinsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Economy.Coins.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Economy.Coins
 {
     public sealed class CoinsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<CoinComponent> CoinComponentPool;
     }
 }
