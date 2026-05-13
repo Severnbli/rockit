3e58a92f82a4cbb6e6c17ed7742eb7ec2eb16f6e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 02:41:15 +0300

    Create CoinsProjectModule, register it to ProjectDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Economy/Coins/CoinsProjectModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Economy.Coins
+{
+    public sealed class CoinsProjectModule : BaseModule<CoinsProjectModule>
+    {
+        public CoinsProjectModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
