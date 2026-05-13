9ac7395cb872e709fe324e41863b23eeb764ff02
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 16:55:34 +0300

    Bind LevelFactory to LevelsSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Levels/LevelsSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Levels/LevelsSceneModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.World.Levels.Types;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World.Levels
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World.Levels
     {
         public LevelsSceneModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void RegisterBindings()
+        {
+            base.RegisterBindings();
+
+            Container.Bind<LevelFactory>().AsSingle();
         }
     }
 }
