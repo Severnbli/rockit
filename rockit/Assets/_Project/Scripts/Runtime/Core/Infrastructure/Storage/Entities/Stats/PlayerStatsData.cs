8573a4c61f5621748f5ef05c4f7046f27680a362
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 02:22:36 +0300

    Replace "ProjectContract.ArrayEmptyInt" with "StatsSharedContracts.NullStatUpdateIndex"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/Stats/PlayerStatsData.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/Stats/PlayerStatsData.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using _Project.Scripts.Runtime.Shared;
+using _Project.Scripts.Runtime.Features.Stats.Shared;
 
 namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities.Stats
 {
     [Serializable]
     public sealed class PlayerStatsData
     {
-        public int WalkFactorUpdateIndex = ProjectContracts.ArrayEmptyInt;
-        public int JumpFactorUpdateIndex = ProjectContracts.ArrayEmptyInt;
-        public int DashFactorUpdateIndex = ProjectContracts.ArrayEmptyInt;
-        public int DashQuantityUpdateIndex = ProjectContracts.ArrayEmptyInt;
+        public int WalkFactorUpdateIndex = StatsSharedContracts.NullStatUpdateIndex;
+        public int JumpFactorUpdateIndex = StatsSharedContracts.NullStatUpdateIndex;
+        public int DashFactorUpdateIndex = StatsSharedContracts.NullStatUpdateIndex;
+        public int DashQuantityUpdateIndex = StatsSharedContracts.NullStatUpdateIndex;
     }
 }
