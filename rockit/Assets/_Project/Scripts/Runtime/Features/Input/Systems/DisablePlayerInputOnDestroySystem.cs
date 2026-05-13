01dab3b22dc0286e383a057872ee4b0ecc23a7cc
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 23:25:55 +0300

    Manage enable/disable status of inputs directly on init and on destroy methods instead of calling for requests

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/DisablePlayerInputOnDestroySystem.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Features.Input.Configs;
+using _Project.Scripts.Runtime.Features.Input.Services;
+using _Project.Scripts.Runtime.Shared.Utils.Features.Input;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Input.Systems
+{
+    public sealed class DisablePlayerInputOnDestroySystem : IProtoDestroySystem
+    {
+        private readonly PlayerInputService _service;
+        private readonly PlayerInputConfig _config;
+
+        public DisablePlayerInputOnDestroySystem(PlayerInputService service, PlayerInputConfig config)
+        {
+            _service = service;
+            _config = config;
+        }
+        
+        public void Destroy()
+        {
+            PlayerInputUtils.DisableInput(_service, _config);
+        }
+    }
+}
