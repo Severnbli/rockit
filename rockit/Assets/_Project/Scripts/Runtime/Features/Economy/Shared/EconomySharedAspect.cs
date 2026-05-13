15d9252e0f8ed77b0ed827dd15098fa4c276fa0d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 00:59:42 +0300

    Create EconomySharedAspect, add it to DomainAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Shared/EconomySharedAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Economy.Shared
+{
+    public sealed class EconomySharedAspect : ProtoAspectInject
+    {
+        
+    }
+}
