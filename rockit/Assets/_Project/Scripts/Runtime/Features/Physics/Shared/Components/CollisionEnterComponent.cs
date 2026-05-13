0bb14046c3cab19b56e6988ef02201f2d5d97a9c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 18:35:04 +0300

    Remove CollisionEnterComponent

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Shared/Components/CollisionEnterComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Leopotam.EcsProto.Unity.Physics2D;
-
-namespace _Project.Scripts.Runtime.Features.Physics.Shared.Components
-{
-    public struct CollisionEnterComponent
-    {
-        public UnityPhysics2DCollisionEnterEvent Data;
-    }
-}
