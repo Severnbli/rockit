90e1d23a88218a430aa74df5a2d6887b59916898
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 19:01:46 +0300

    Rename "GameProcess" into "ProgressOfPassage"

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Types/GlitchStep.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Types/GlitchStep.cs
@@ -5,7 +5,7 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Types
     [Serializable]
     public sealed class GlitchStep
     {
-        public float GameProgress;
+        public float ProgressOfPassage;
         public float Duration;
         public float Frequency;
         public GlitchSettings GlitchSettings;
