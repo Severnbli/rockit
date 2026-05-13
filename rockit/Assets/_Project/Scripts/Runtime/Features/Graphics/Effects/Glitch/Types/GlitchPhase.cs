0de743239b23d684dd2536b942e805bfee357d86
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-13 19:25:27 +0300

    Add "Range" attribute to GlitchPhase

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Types/GlitchPhase.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Effects/Glitch/Types/GlitchPhase.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Effects.Glitch.Types
 {
     [Serializable]
     public sealed class GlitchPhase
     {
-        public float ProgressOfPassage;
+        [Range(0,1)] public float ProgressOfPassage;
         public float Duration;
         public float Delay;
         public GlitchSettings GlitchSettings;
