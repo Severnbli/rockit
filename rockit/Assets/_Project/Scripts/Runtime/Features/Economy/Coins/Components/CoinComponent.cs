abc90395296a3a26d3706196482c52dbb54c5441
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 01:01:56 +0300

    Create CoinComponent with authoring attributes and "Id" field, add it pool to CoinsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Components/CoinComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Leopotam.EcsProto.Unity;
+
+namespace _Project.Scripts.Runtime.Features.Economy.Coins.Components
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct CoinComponent
+    {
+        public int Id;
+    }
+}
