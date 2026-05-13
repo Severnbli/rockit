ccd83e15d9382180b2005d59eda71ad1ba1d27a6
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 00:55:50 +0300

    Create EconomyData with "Serializable" attribute and "CoinsQuantity" and "CollectedCoins" fields. Add it to tracked at DataProvider

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/EconomyData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities
+{
+    [Serializable]
+    public sealed class EconomyData
+    {
+        public int CoinsQuantity;
+        public SortedDictionary<int, CoinData> CollectedCoins = new ();
+    }
+}
