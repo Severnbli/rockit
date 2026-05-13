cbfe9c12be7873ab74d5af1e0ce114f564137b53
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 01:24:34 +0300

    Remove unnecessary "SerializeField" from "_factor" field. Remove "Factor" property. Make "_factor" public and rename into "Factor"

--- rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Types/FactorPaidWithCoins.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Types/FactorPaidWithCoins.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Economy.Coins.Types
 {
     [Serializable]
     public class FactorPaidWithCoins : PaidWithCoins
     {
-        [SerializeField] private float _factor;
-        
-        public float Factor => _factor;
+        public float Factor;
     }
 }
