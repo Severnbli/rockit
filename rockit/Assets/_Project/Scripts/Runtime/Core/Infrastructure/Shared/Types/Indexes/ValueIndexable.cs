f1010245a29bb5b07a7d1718b213b982f617effc
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 12:54:12 +0300

    Move Indexable and ValueIndexable into newly create Indexes folder

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Types/Indexes/ValueIndexable.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Types/Indexes/ValueIndexable.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Types
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Types.Indexes
 {
     [Serializable]
     public class ValueIndexable<T> : Indexable
