5ce2da980530428fc5d3c040a7597f09912bd860
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 13:52:25 +0300

    Create StatsSharedUtils with "TryCreateModifierSequence" methods for FactorPaidWithCoins and QuantityPaidWithCoins

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/StatsSharedUtils.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Runtime.Features.Economy.Coins.Types;
+using _Project.Scripts.Runtime.Features.Stats.Shared;
+using _Project.Scripts.Runtime.Shared.Tools;
+using _Project.Scripts.Runtime.Shared.Utils.Shared;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.Stats
+{
+    public static class StatsSharedUtils
+    {
+        public static bool TryCreateModifierSequence(IReadOnlyList<FactorPaidWithCoins> data,
+            out SequenceElement<FactorPaidWithCoinsElement> first)
+        {
+            return SequenceElementUtils.TryCreateMappedSequenceWithNull(
+                data,
+                x => new FactorPaidWithCoinsElement
+                {
+                    Value = x
+                },
+                () => new FactorPaidWithCoinsElement
+                {
+                    Index = StatsSharedContracts.NullStatUpdateIndex,
+                    Value = new FactorPaidWithCoins
+                    {
+                        Factor = StatsSharedContracts.DefaultFloatFactorModifier
+                    }
+                },
+                out first
+            );
+        }
+        
+        public static bool TryCreateModifierSequence(IReadOnlyList<QuantityPaidWithCoins> data,
+            out SequenceElement<QuantityPaidWithCoinsElement> first)
+        {
+            return SequenceElementUtils.TryCreateMappedSequenceWithNull(
+                data,
+                x => new QuantityPaidWithCoinsElement
+                {
+                    Value = x
+                },
+                () => new QuantityPaidWithCoinsElement
+                {
+                    Index = StatsSharedContracts.NullStatUpdateIndex,
+                    Value = new QuantityPaidWithCoins
+                    {
+                        Quantity = StatsSharedContracts.DefaultIntQuantityModifier
+                    }
+                },
+                out first
+            );
+        }
+    }
+}
