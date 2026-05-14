f5a640351f8acae285a77355925269547fb9ed3f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 20:46:16 +0300

    Create UpdateConstantDisplayAnimatorActiveBoolByConstantDisplayActiveTagExistenceSystem, bind it to ConstantsAnimationsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Constants/Systems/UpdateConstantDisplayAnimatorActiveBoolByConstantDisplayActiveTagExistenceSystem.cs
@@ -0,0 +1,30 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Animations.Constants.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared;
+using _Project.Scripts.Runtime.Features.Stats.Constants;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Constants.Systems
+{
+    public sealed class UpdateConstantDisplayAnimatorActiveBoolByConstantDisplayActiveTagExistenceSystem : IProtoRunSystem
+    {
+        [DI] private readonly AnimationsSharedAspect _asAspect;
+        [DI] private readonly ConstantsAspect _cAspect;
+        private readonly ConstantsDisplaysAnimationsConfig _cdaConfig;
+
+        public UpdateConstantDisplayAnimatorActiveBoolByConstantDisplayActiveTagExistenceSystem(ConstantsDisplaysAnimationsConfig cdaConfig)
+        {
+            _cdaConfig = cdaConfig;
+        }
+
+        public void Run()
+        {
+            foreach (var e in _asAspect.ConstantDisplayAnimators)
+            {
+                ref var aComponent = ref _asAspect.AnimatorComponentPool.Get(e);
+                var existence = _cAspect.ConstantActiveDisplays.Has(e);
+                aComponent.Animator.SetBool(_cdaConfig.ActiveBool, existence);
+            }
+        }
+    }
+}
