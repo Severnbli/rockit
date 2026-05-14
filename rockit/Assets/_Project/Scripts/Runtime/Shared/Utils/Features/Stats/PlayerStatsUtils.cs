fbfe50c5d5bb3c824130438a5bf75cc880ed4a67
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 14:47:50 +0300

    Create "CreateSyncPlayerStatsServiceToStorageRequest" player stats util method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/PlayerStatsUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/PlayerStatsUtils.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities.Stats;
 using _Project.Scripts.Runtime.Features.Stats.Player.Configs;
 using _Project.Scripts.Runtime.Features.Stats.Shared;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Shared.Utils.Features.Stats
 {
@@ -32,6 +35,11 @@ namespace _Project.Scripts.Runtime.Shared.Utils.Features.Stats
                 StatsSharedContracts.NullStatUpdateIndex,
                 psConfig.DashQuantityUpdates.Length
             );
+        }
+
+        public static ProtoEntity CreateSyncPlayerStatsServiceToStorageRequest(RequestsAspect aspect)
+        {
+            return aspect.CreateRequest(aspect.PlayerStatsRequestsAspect.SyncPlayerStatsServiceToStorageRequestPool);
         }
     }
 }
