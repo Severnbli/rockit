b841c9305efe22bb84472701afe31fbc5748cd5a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 22:37:07 +0300

    Set default value for "_activeDistance" to "0.5f"

--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Configs/ConstantsDisplaysConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Configs/ConstantsDisplaysConfig.cs
@@ -5,7 +5,7 @@ namespace _Project.Scripts.Runtime.Features.Stats.Constants.Configs
 {
     public sealed class ConstantsDisplaysConfig : ScriptableObjectAutoInstaller<ConstantsDisplaysConfig>
     {
-        [SerializeField] private float _activateDistance = 0.1f;
+        [SerializeField] private float _activateDistance = 0.5f;
         
         public float ActivateDistance => _activateDistance;
     }
