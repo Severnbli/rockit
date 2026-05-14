f5afea8d2dfcdc41ffd997ae06842e486459943d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 17:29:31 +0300

    Create sealed DashesConstantDefinition, FlightsConstantDefinition, MultiConstantDefinition and SpeedConstantDefinition that inherited from ConstantDefinition

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Types/DashesConstantDefinition.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Localization;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Constants.Types
+{
+    public sealed class DashesConstantDefinition : ConstantDefinition
+    {
+        public override string Name() => LocalizationEntriesContracts.Const.Dashes.Name;
+        public override string Discoverer() => LocalizationEntriesContracts.Const.Dashes.Discoverer;
+        public override string Info() => LocalizationEntriesContracts.Const.Dashes.Info;
+    }
+}
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Types/SpeedConstantDefinition.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Localization;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Constants.Types
+{
+    public sealed class SpeedConstantDefinition : ConstantDefinition
+    {
+        public override string Name() => LocalizationEntriesContracts.Const.Speed.Name;
+        public override string Discoverer() => LocalizationEntriesContracts.Const.Speed.Discoverer;
+        public override string Info() => LocalizationEntriesContracts.Const.Speed.Info;
+    }
+}
