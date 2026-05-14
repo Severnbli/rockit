0d43043fe122379be69a63a187d6f977dce985ee
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 22:12:41 +0300

    Create ConstantsDisplaysConfig with only field "_activateDistance" and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Configs/ConstantsDisplaysConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Constants.Configs
+{
+    public sealed class ConstantsDisplaysConfig : ScriptableObjectAutoInstaller<ConstantsDisplaysConfig>
+    {
+        [SerializeField] private float _activateDistance = 0.1f;
+        
+        public float ActivateDistance => _activateDistance;
+    }
+}
