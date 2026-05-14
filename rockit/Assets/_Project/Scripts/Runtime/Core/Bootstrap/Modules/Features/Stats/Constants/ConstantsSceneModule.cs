bac995b66aee147d5fdb2e987aaecb4136a72618
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 21:08:36 +0300

    Create ManageInvestigatedConstantTagExistenceByStorageOnRunSystem, bind it to ConstantsSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsSceneModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Stats.Constants.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Constants
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Constan
     {
         public ConstantsSceneModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<ManageInvestigatedConstantTagExistenceByStorageOnRunSystem>();
         }
     }
 }
