20285db934281ed1c16ea17057178970fa202d1a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 20:50:27 +0300

    Craete ConstantAnimationsAspect, add it to AnimationsSharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Constants/ConstantsAnimationsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Constants
+{
+    public sealed class ConstantsAnimationsAspect : ProtoAspectInject 
+    {
+        
+    }
+}
