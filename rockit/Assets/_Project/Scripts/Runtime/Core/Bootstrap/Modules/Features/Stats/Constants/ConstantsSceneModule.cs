9ae4e234a3701acde1cdf2ceaf8d9b75a3ac8a7b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 21:07:44 +0300

    Create ConstantsSceneModulem register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsSceneModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Constants
+{
+    public sealed class ConstantsSceneModule : BaseModule<ConstantsSceneModule>
+    {
+        public ConstantsSceneModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
