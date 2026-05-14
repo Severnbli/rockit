f8c86e5a0b66f80029c710d7a5b1653c837cb44f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 19:45:46 +0300

    Add "IconPrefab" and "ItemPrefab" fields

--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Types/ConstantDefinition.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Types/ConstantDefinition.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Stats.Constants.Types
 {
     [Serializable]
     public abstract class ConstantDefinition
     {
+        public GameObject IconPrefab;
+        public GameObject ItemPrefab;
+        
         public abstract string Name();
         public abstract string Discoverer();
         public abstract string Info();
