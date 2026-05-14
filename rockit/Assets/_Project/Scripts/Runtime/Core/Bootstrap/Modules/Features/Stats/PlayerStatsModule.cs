42c0cf8ead691502ada4ae56927dda34c9d5f82f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 14:02:55 +0300

    Create CreatePlayerStatsServiceModifiersSequencesOnInitSystem, bind it to PlayerStatsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/PlayerStatsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/PlayerStatsModule.cs
@@ -22,6 +22,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats
             base.BindSystems();
             
             BindSystem<ClampPlayerStatsDataIndexesOnInitSystem>();
+            BindSystem<CreatePlayerStatsServiceModifiersSequencesOnInitSystem>();
         }
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Player/Systems/CreatePlayerStatsServiceModifiersSequencesOnInitSystem.cs
@@ -0,0 +1,32 @@
+﻿using _Project.Scripts.Runtime.Features.Stats.Player.Configs;
+using _Project.Scripts.Runtime.Features.Stats.Player.Services;
+using _Project.Scripts.Runtime.Shared.Utils.Features.Stats;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Player.Systems
+{
+    public sealed class CreatePlayerStatsServiceModifiersSequencesOnInitSystem : IProtoInitSystem
+    {
+        private readonly PlayerStatsService _psService;
+        private readonly PlayerStatsConfig _psConfig;
+
+        public CreatePlayerStatsServiceModifiersSequencesOnInitSystem(PlayerStatsService psService,
+            PlayerStatsConfig psConfig)
+        {
+            _psService = psService;
+            _psConfig = psConfig;
+        }
+
+        public void Init(IProtoSystems systems)
+        {
+            StatsSharedUtils.CreateModifierSequence(_psConfig.WalkFactorUpdates,
+                out _psService.WalkFactorModifierElement);
+            StatsSharedUtils.CreateModifierSequence(_psConfig.JumpFactorUpdates,
+                out _psService.JumpFactorModifierElement);
+            StatsSharedUtils.CreateModifierSequence(_psConfig.DashFactorUpdates,
+                out _psService.DashFactorModifierElement);
+            StatsSharedUtils.CreateModifierSequence(_psConfig.DashQuantityUpdates,
+                out _psService.DashQuantityModifierElement);
+        }
+    }
+}
