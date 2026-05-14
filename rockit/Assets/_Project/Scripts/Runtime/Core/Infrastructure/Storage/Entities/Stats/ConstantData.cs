ee9520d6b4c148b86f161615a3dc361fd92cb567
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 19:38:30 +0300

    Create ConstantData with "Serializable" attribute

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/Stats/ConstantData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities.Stats
+{
+    [Serializable]
+    public sealed class ConstantData
+    {
+        
+    }
+}
