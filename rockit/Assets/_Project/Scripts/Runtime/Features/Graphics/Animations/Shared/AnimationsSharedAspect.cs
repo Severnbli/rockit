2eb463ca1542e0d40d9e047aa0f0fc78ee9924bc
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 20:40:12 +0300

    Add "ConstantDisplayAnimators" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/AnimationsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Shared/AnimationsSharedAspect.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Tags;
 using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Components;
+using _Project.Scripts.Runtime.Features.Stats.Constants.Tags;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -10,5 +11,6 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Shared
         public readonly ProtoPool<AnimatorComponent> AnimatorComponentPool;
         public readonly ProtoIt Animators = new (It.Inc<AnimatorComponent>());
         public readonly ProtoIt CharacterAnimators = new (It.Inc<AnimatorComponent, CharacterTag>());
+        public readonly ProtoIt ConstantDisplayAnimators = new (It.Inc<AnimatorComponent, ConstantDisplayTag>());
     }
 }
