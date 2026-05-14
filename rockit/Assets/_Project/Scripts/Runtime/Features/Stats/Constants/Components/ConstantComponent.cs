1b3a19d403e926185c1e19f68ade72a1904082d7
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 20:00:09 +0300

    Create ConstantComponent, add it pool to ConstantsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Components/ConstantComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.Runtime.Features.Stats.Constants.Components
+{
+    public struct ConstantComponent
+    {
+        public int Id;
+    }
+}
--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/ConstantsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/ConstantsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Stats.Constants.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Stats.Constants
 {
     public sealed class ConstantsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<ConstantComponent> ConstantComponentPool;
     }
 }
