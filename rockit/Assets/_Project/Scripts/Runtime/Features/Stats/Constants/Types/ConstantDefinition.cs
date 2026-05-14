998d3098a10c4edc53ea529334d4f7e16ba05325
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 17:23:52 +0300

    Create abstract ConstantDefinition with "Name", "Dicoverer" and "Info" abstract methods

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Types/ConstantDefinition.cs
@@ -0,0 +1,9 @@
+﻿namespace _Project.Scripts.Runtime.Features.Stats.Constants.Types
+{
+    public abstract class ConstantDefinition
+    {
+        public abstract string Name();
+        public abstract string Discoverer();
+        public abstract string Info();
+    }
+}
