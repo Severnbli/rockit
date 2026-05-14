a3d8ca8b3cc9fb050bfd9db316740a9898d4ccd2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 12:53:30 +0300

    Create ValueIndexable that inherited from Indexable with "Serializable" attribute and generic "Value" field

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Types/ValueIndexable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Types
+{
+    [Serializable]
+    public class ValueIndexable<T> : Indexable
+    {
+        public T Value;
+    }
+}
