9b2a2e67d53e54de8d6ef7b7ef4ec7427ccec821
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 20:55:50 +0300

    Remove "ConstantDisplayAnimatorsWithConstants" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Constants/ConstantsAnimationsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Constants/ConstantsAnimationsAspect.cs
@@ -1,5 +1,4 @@
 ﻿using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Components;
-using _Project.Scripts.Runtime.Features.Stats.Constants.Components;
 using _Project.Scripts.Runtime.Features.Stats.Constants.Tags;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
@@ -9,6 +8,5 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Constants
     public sealed class ConstantsAnimationsAspect : ProtoAspectInject 
     {
         public readonly ProtoIt ConstantDisplayAnimators = new (It.Inc<AnimatorComponent, ConstantDisplayTag>());
-        public readonly ProtoIt ConstantDisplayAnimatorsWithConstants = new (It.Inc<AnimatorComponent, ConstantComponent, ConstantDisplayTag>());
     }
 }
