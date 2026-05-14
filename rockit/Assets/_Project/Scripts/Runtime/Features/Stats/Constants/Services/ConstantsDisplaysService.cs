c47a772256ef87d54adc16aa8a07819d7a68b13f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 22:34:49 +0300

    Add "NearestConstantId" field with "ProjectContracts.NullIntId" default value

--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Services/ConstantsDisplaysService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Services/ConstantsDisplaysService.cs
@@ -1,7 +1,9 @@
-﻿namespace _Project.Scripts.Runtime.Features.Stats.Constants.Services
+﻿using _Project.Scripts.Runtime.Shared;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Constants.Services
 {
     public sealed class ConstantsDisplaysService
     {
-        
+        public int NearestConstantId = ProjectContracts.NullIntId;
     }
 }
