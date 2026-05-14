82e52aa4a920292db9c5ce3f508c1239ccebfe85
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 21:17:15 +0300

    Create ConstantWindow that inherited from BaseWindow with "_nameText" and "_infoText" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Monos/ConstantWindow.cs
@@ -0,0 +1,13 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Monos;
+using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Constants.Monos
+{
+    public class ConstantWindow : MonoBaseWindow<BaseWindowConfig>
+    {
+        [SerializeField] private TextMeshProUGUI _nameText;
+        [SerializeField] private TextMeshProUGUI _infoText;
+    }
+}
