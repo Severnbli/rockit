e8b360d0b12eb1f47d88497a5b25defdc0a2810f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 22:21:33 +0300

    Create ManageConstantDisplayActiveStatusByPlayerLocatorOnRunSystem, bind it to ConstantsSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsSceneModule.cs
@@ -14,6 +14,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Constan
             base.BindSystems();
             
             BindSystem<ManageConstantInvestigatedStatusByStorageOnRunSystem>();
+            BindSystem<ManageConstantDisplayActiveStatusByPlayerLocatorOnRunSystem>();
         }
     }
 }
