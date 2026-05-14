179ae378034809f6545f87ca8032db89f6bd2422
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 20:32:39 +0300

    Create ConstantsDisplaysAnimationsConfig with "_enableBool" and "_investigatedBool" fields and they property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Constants/Configs/ConstantsDisplaysAnimationsConfig.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Constants.Configs
+{
+    public sealed class ConstantsDisplaysAnimationsConfig : ScriptableObjectAutoInstaller<ConstantsDisplaysAnimationsConfig>
+    {
+        [SerializeField] private string _enableBool = "Enable";
+        [SerializeField] private string _investigatedBool = "Investigated";
+        
+        public int EnableBool => Animator.StringToHash(_enableBool);
+        public int InvestigatedBool => Animator.StringToHash(_investigatedBool);
+    }
+}
