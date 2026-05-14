fc9524c9e7d6549d6af929543dd2b8033120857d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 22:47:49 +0300

    Create UpdateConstantDisplaysServiceNearestConstantIdOnRunSystem, bind it to ConstantsSceneModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Systems/UpdateConstantDisplaysServiceNearestConstantIdOnRunSystem.cs
@@ -0,0 +1,39 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using _Project.Scripts.Runtime.Features.Stats.Constants.Services;
+using _Project.Scripts.Runtime.Shared;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Constants.Systems
+{
+    public sealed class UpdateConstantDisplaysServiceNearestConstantIdOnRunSystem : IProtoRunSystem
+    {
+        [DI] private readonly ConstantsAspect _cAspect;
+        [DI] private readonly SharedAspect _sAspect;
+        private readonly ConstantsDisplaysService _cdService;
+
+        public UpdateConstantDisplaysServiceNearestConstantIdOnRunSystem(ConstantsDisplaysService cdService)
+        {
+            _cdService = cdService;
+        }
+
+        public void Run()
+        {
+            var minDistance = Mathf.Infinity;
+            var constantId = ProjectContracts.NullIntId;
+            
+            foreach (var e in _cAspect.ConstantActiveDisplayConstantPlayerLocators)
+            {
+                ref var plComponent = ref _sAspect.PlayerLocatorComponentPool.Get(e);
+                if (plComponent.Distance > minDistance) continue;
+                
+                minDistance = plComponent.Distance;
+                ref var cComponent = ref _cAspect.ConstantComponentPool.Get(e);
+                constantId = cComponent.Id;
+            }
+            
+            _cdService.NearestConstantId = constantId;
+        }
+    }
+}
