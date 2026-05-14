9b551a8bd7ccdd1dce5a9940230622ad0e0c4b1b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 23:49:28 +0300

    Create SequenceElementObserver with "Element" field and "GoToNext" and "GoToPrev" methods

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Tools/SequenceElementObserver.cs
@@ -0,0 +1,19 @@
+﻿namespace _Project.Scripts.Runtime.Shared.Tools
+{
+    public class SequenceElementObserver<TElement>
+    {
+        public SequenceElement<TElement> Element;
+
+        public void GoToNext()
+        {
+            if (Element?.Next == null) return;
+            Element = Element.Next;
+        }
+
+        public void GoToPrev()
+        {
+            if (Element?.Prev == null) return;
+            Element = Element.Prev;
+        }
+    }
+}
