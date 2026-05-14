4fab4cd7b434abfb29c10602d3031cc4ec478e96
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 19:37:55 +0300

    Create ConstantsConfig with only field "_constants" of type dictionary and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Configs/ConstantsConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Modules.Zenject;
+using _Project.Scripts.Runtime.Features.Stats.Constants.Types;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Constants.Configs
+{
+    public sealed class ConstantsConfig : ScriptableObjectAutoInstaller<ConstantsConfig>
+    {
+        [SerializeField] private Dictionary<int, ConstantDefinition> _constants = new ();
+        
+        public Dictionary<int, ConstantDefinition> Constants => _constants;
+    }
+}
