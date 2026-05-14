3e65526cb621a2c83f8738377b2c773b1d1f5e2f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-14 16:35:07 +0300

    Create LocalizationEntriesContracts

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/LocalizationEntriesContracts.cs
@@ -0,0 +1,54 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization
+{
+    public static class LocalizationEntriesContracts
+    {
+        public const string Play = "play";
+        public const string LanguageNameS = "language_name_s";
+        public const string LanguageNameV = "language_name_v";
+        public const string LoadingS = "loading_s";
+        public const string Collection = "collection";
+    
+        public static class Const
+        {
+            public const string S = "const_s";
+            public const string V = "const_v";
+            public const string UndefinedInfo = "const_undefined_info";
+            public const string InvestigatedInfo = "const_investigated_info";
+            public const string WithNameS = "const_with_name_s";
+            public const string WithNameV = "const_with_name_v";
+            public const string DiscovererInfo = "const_discoverer_info";
+            public const string CurrValue = "const_curr_value";
+            public const string CurrValueIsMax = "const_curr_value_is_max";
+            public const string ImproveInfo = "const_improve_info";
+            public const string Improve = "const_improve";
+        
+            public static class Speed
+            {
+                public const string Name = "const_speed_name";
+                public const string Discoverer = "const_speed_discoverer";
+                public const string Info = "const_speed_info";
+            }
+        
+            public static class Flights
+            {
+                public const string Name = "const_flights_name";
+                public const string Discoverer = "const_flights_discoverer";
+                public const string Info = "const_flights_info";
+            }
+        
+            public static class Dashes
+            {
+                public const string Name = "const_dashes_name";
+                public const string Discoverer = "const_dashes_discoverer";
+                public const string Info = "const_dashes_info";
+            }
+        
+            public static class Multi
+            {
+                public const string Name = "const_multi_name";
+                public const string Discoverer = "const_multi_discoverer";
+                public const string Info = "const_multi_info";
+            }
+        }
+    }
+}
