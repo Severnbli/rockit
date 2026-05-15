7bab00e47197eb1f0b0275148d10195594e9ad3d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 00:27:06 +0300

    Add "Sequence" field, initialize from contructor

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tools/Pipeline/Core/TweenPipelineRunObserver.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tools/Pipeline/Core/TweenPipelineRunObserver.cs
@@ -1,8 +1,16 @@
-﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline.Core
+﻿using DG.Tweening;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline.Core
 {
     public abstract class TweenPipelineRunObserver : ITweenPipelineRunObserver
     {
+        protected Sequence Sequence;
         public bool Canceled { get; protected set; }
+
+        protected TweenPipelineRunObserver(Sequence sequence)
+        {
+            Sequence = sequence;
+        }
         
         public abstract void CancelRun();
     }
