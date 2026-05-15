91fc9e534579af91ee372f46f0043550e0a33ddf
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 16:30:39 +0300

    Create UpdateConstantImproversCoinsAmountOnRunSystem, bind it to ConstantsSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsSceneModule.cs
@@ -19,6 +19,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Constan
             BindSystem<SendConstantDisplayWindowRequestsByConstantsDisplaysServiceSystem>();
             BindSystem<PrepareConstantDisplayWindowServiceOnRebuildConstantDisplayWindowRequestSystem>();
             BindSystem<UpdateConstantDisplayWindowOnRebuildConstantDisplayWindowRequestSystem>();
+            BindSystem<UpdateConstantImproversCoinsAmountOnRunSystem>();
         }
     }
 }
