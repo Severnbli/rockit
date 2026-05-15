eae5c0bc5f23c0b56292124741db8d88a352e6ca
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 00:32:50 +0300

    Update sequence initialization: add "Initialize" method

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tools/Pipeline/Core/ITweenPipelineRunObserver.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tools/Pipeline/Core/ITweenPipelineRunObserver.cs
@@ -1,8 +1,11 @@
-﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline.Core
+﻿using DG.Tweening;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline.Core
 {
     public interface ITweenPipelineRunObserver
     {
         bool Canceled { get; }
+        void Initialize(Sequence sequence);
         void CancelRun();
         void Reset();
     }
