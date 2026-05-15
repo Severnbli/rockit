dc26c3fd6f46de71088f37d8a9f3a0d3134a7ea2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 03:40:32 +0300

    Create BuildConstantDisplayWindowRequest with only field "ConstantId", add it pool and iterator to ConstantsRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/ConstantsRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/ConstantsRequestsAspect.cs
@@ -1,9 +1,14 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using _Project.Scripts.Runtime.Features.Stats.Constants.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Stats.Constants
 {
     public sealed class ConstantsRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<BuildConstantDisplayWindowRequest> BuildConstantDisplayWindowRequestPool;
+        public readonly ProtoIt BuildConstantDisplayWindowRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, BuildConstantDisplayWindowRequest>());
     }
 }
