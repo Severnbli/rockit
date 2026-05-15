fa80befc1b396691afb5d979dd804a117eaa8384
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 14:24:42 +0300

    Add "GetInvestigatedStatus" constants util method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/ConstantsUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/ConstantsUtils.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
 using _Project.Scripts.Runtime.Features.Stats.Constants.Requests;
 using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
 using Leopotam.EcsProto;
@@ -25,6 +26,11 @@ namespace _Project.Scripts.Runtime.Shared.Utils.Features.Stats
         {
             return aspect.CreateRequest(aspect.StatsSharedRequestsAspect.ConstantsRequestsAspect
                 .HideConstantDisplayWindowRequestPool);
+        }
+
+        public static bool GetInvestigatedStatus(DataProvider dProvider, int constantId)
+        {
+            return dProvider.StatsData.InvestigatedConstants.ContainsKey(constantId);
         }
     }
 }
