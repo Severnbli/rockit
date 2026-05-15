bf9e88de8a9b263497011a17dc4da8007ff3f1f8
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 15:57:24 +0300

    Create PaidWithCoinsComponent, add it pool to CoinsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/CoinsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/CoinsAspect.cs
@@ -8,6 +8,7 @@ namespace _Project.Scripts.Runtime.Features.Economy.Coins
     {
         public readonly ProtoPool<CoinComponent> CoinComponentPool;
         public readonly ProtoPool<CoinsAmountComponent> CoinsAmountComponentPool;
+        public readonly ProtoPool<PaidWithCoinsComponent> PaidWithCoinsComponentPool;
         public readonly ProtoIt Coins = new (It.Inc<CoinComponent>());
     }
 }
