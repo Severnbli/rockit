ab17252f1ea7cda388a8b6c9055e945e8342ba6f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 16:05:03 +0300

    Create UpdateCoinsTextUIOutputWithCoinsAmountSystem, bind it to CoinsSceneModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Systems/UpdateCoinsTextUIOutputWithCoinsAmountSystem.cs
@@ -0,0 +1,20 @@
+﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Economy.Coins.Systems
+{
+    public sealed class UpdateCoinsTextUIOutputWithCoinsAmountSystem : IProtoRunSystem
+    {
+        [DI] private readonly CoinsAspect _cAspect;
+        
+        public void Run()
+        {
+            foreach (var e in _cAspect.CoinsAmountTextUIOutputs)
+            {
+                ref var ctuoComponent = ref _cAspect.CoinsTextUIOutputComponentPool.Get(e);
+                ref var caComponent = ref _cAspect.CoinsAmountComponentPool.Get(e);
+                ctuoComponent.Text.text = caComponent.Amount.ToString();
+            }
+        }
+    }
+}
