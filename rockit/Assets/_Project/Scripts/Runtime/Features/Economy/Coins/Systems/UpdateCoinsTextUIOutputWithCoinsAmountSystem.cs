9f5d87e626a172afb0764ef9e6cf7e8581b905a9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 16:13:56 +0300

    Remove CoinsTextUIOutputComponent, use TextUiComponent instead

--- rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Systems/UpdateCoinsTextUIOutputWithCoinsAmountSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Systems/UpdateCoinsTextUIOutputWithCoinsAmountSystem.cs
@@ -1,4 +1,5 @@
-﻿using Leopotam.EcsProto;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Text.Shared;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Economy.Coins.Systems
@@ -6,12 +7,13 @@ namespace _Project.Scripts.Runtime.Features.Economy.Coins.Systems
     public sealed class UpdateCoinsTextUIOutputWithCoinsAmountSystem : IProtoRunSystem
     {
         [DI] private readonly CoinsAspect _cAspect;
+        [DI] private readonly TextSharedAspect _tsAspect;
         
         public void Run()
         {
-            foreach (var e in _cAspect.CoinsAmountTextUIOutputs)
+            foreach (var e in _cAspect.CoinsAmountTextUis)
             {
-                ref var ctuoComponent = ref _cAspect.CoinsTextUIOutputComponentPool.Get(e);
+                ref var ctuoComponent = ref _tsAspect.TextUiComponentPool.Get(e);
                 ref var caComponent = ref _cAspect.CoinsAmountComponentPool.Get(e);
                 ctuoComponent.Text.text = caComponent.Amount.ToString();
             }
