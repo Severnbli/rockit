c6edd4193178d2191e01ae981023cbb5a0980ead
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 14:08:33 +0300

    Bind ConstatnsDisplaysService to ConstantsProjectModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsProjectModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsProjectModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Stats.Constants.Services;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Constants
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Constan
     {
         public ConstantsProjectModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindServices()
+        {
+            base.BindServices();
+            
+            BindService<ConstantsDisplaysService>();
         }
     }
 }
