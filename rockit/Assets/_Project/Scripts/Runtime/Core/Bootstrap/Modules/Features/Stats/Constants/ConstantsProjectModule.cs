d0801fc72183b2e509d9357278dd1cad763f4abe
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 14:07:41 +0300

    Create ConstantsProjectModule, register it to ProjectDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsProjectModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Constants
+{
+    public sealed class ConstantsProjectModule : BaseModule<ConstantsProjectModule>
+    {
+        public ConstantsProjectModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
