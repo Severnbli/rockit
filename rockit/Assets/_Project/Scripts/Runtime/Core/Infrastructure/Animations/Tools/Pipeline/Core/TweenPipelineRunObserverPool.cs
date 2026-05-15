c274f0bdf60de1e890df9c18d3e3b44dbe67273b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 00:42:42 +0300

    Create TweenPipelineRunObserverPool that inherited from BasePool and specify "PostDespawn" overriding

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tools/Pipeline/Core/TweenPipelineRunObserverPool.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline.Core
+{
+    public class TweenPipelineRunObserverPool<T> : BasePool<T> where T : ITweenPipelineRunObserver, new ()
+    {
+        protected override void PostDespawn(T instance)
+        {
+            base.PostDespawn(instance);
+            
+            instance.Reset();
+        }
+    }
+}
