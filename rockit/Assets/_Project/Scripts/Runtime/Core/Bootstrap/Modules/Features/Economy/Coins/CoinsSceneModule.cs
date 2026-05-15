794ddc53a190e227a7a757f3fa2c341869b960b9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 16:11:43 +0300

    Create ManageButtonsInteractableStatusByCoinsAmountSystem, bind it to CoinsSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Economy/Coins/CoinsSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Economy/Coins/CoinsSceneModule.cs
@@ -15,6 +15,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Economy.Coins
             
             BindSystem<DisableCollectedCoinOnInitializeRequestSystem>();
             BindSystem<UpdateCoinsTextUIOutputWithCoinsAmountSystem>();
+            BindSystem<ManageButtonsInteractableStatusByCoinsAmountSystem>();
         }
     }
 }
