ee282988fe1f2a1fc39ef107414fb69197734717
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 16:05:58 +0300

    Add "CoinsAmountButtons" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/CoinsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/CoinsAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Features.Economy.Coins.Components;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -11,5 +12,6 @@ namespace _Project.Scripts.Runtime.Features.Economy.Coins
         public readonly ProtoPool<CoinsTextUIOutputComponent> CoinsTextUIOutputComponentPool;
         public readonly ProtoIt Coins = new (It.Inc<CoinComponent>());
         public readonly ProtoIt CoinsAmountTextUIOutputs = new (It.Inc<CoinsAmountComponent, CoinsTextUIOutputComponent>());
+        public readonly ProtoIt CoinsAmountButtons = new (It.Inc<CoinsAmountComponent, ButtonComponent>());
     }
 }
