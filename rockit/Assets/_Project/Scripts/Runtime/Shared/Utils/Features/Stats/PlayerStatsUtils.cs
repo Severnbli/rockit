52425ab2d97c935c2b90b68d172889cf98dbf411
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 03:05:37 +0300

    Create "TryGetObserverByConstantDefinition" player stat util method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/PlayerStatsUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Stats/PlayerStatsUtils.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities.Stats;
+using _Project.Scripts.Runtime.Features.Economy.Coins.Types;
+using _Project.Scripts.Runtime.Features.Stats.Constants.Types;
 using _Project.Scripts.Runtime.Features.Stats.Player.Configs;
+using _Project.Scripts.Runtime.Features.Stats.Player.Services;
 using _Project.Scripts.Runtime.Features.Stats.Shared;
 using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using _Project.Scripts.Runtime.Shared.Tools;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Shared.Utils.Features.Stats
@@ -40,6 +44,42 @@ namespace _Project.Scripts.Runtime.Shared.Utils.Features.Stats
         public static ProtoEntity CreateSyncPlayerStatsServiceToStorageRequest(RequestsAspect aspect)
         {
             return aspect.CreateRequest(aspect.PlayerStatsRequestsAspect.SyncPlayerStatsServiceToStorageRequestPool);
+        }
+        
+        public static bool TryGetObserverByConstantDefinition(ConstantDefinition def, PlayerStatsService psService, 
+            out SequenceElementObserver<IndexableFloatPaidWithCoins> observer)
+        {
+            observer = null;
+            
+            switch (def)
+            {
+                case SpeedConstantDefinition:
+                {
+                    observer = psService.WalkFactorModifierObserver;
+                    break;
+                }
+                case FlightsConstantDefinition:
+                {
+                    observer = psService.JumpFactorModifierObserver;
+                    break;
+                }
+                case DashesConstantDefinition:
+                {
+                    observer = psService.DashFactorModifierObserver;
+                    break;
+                }
+                case MultiConstantDefinition:
+                {
+                    observer = psService.DashQuantityModifierObserver;
+                    break;
+                }
+                default:
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
