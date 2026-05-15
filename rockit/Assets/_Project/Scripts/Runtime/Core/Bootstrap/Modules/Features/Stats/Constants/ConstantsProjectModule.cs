4ca6ceb29a76575c0317fd37af57198ae4bdf00b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 14:09:14 +0300

    Bind ConstantDisplayWindowService into ConstantsProjectModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsProjectModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Constants/ConstantsProjectModule.cs
@@ -14,6 +14,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Constan
             base.BindServices();
             
             BindService<ConstantsDisplaysService>();
+            BindService<ConstantDisplayWindowService>();
         }
     }
 }
