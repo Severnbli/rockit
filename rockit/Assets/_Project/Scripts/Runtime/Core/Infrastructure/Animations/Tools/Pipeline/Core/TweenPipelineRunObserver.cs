71128690323ad58f42d0f0167553baf9809789a7
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 00:23:35 +0300

    Create abstract TweenPipelineRunObserver that implements ITweenPipelineRunObserver and add "_canceled" field

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tools/Pipeline/Core/TweenPipelineRunObserver.cs
@@ -0,0 +1,11 @@
+﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline.Core
+{
+    public abstract class TweenPipelineRunObserver : ITweenPipelineRunObserver
+    {
+        protected bool _canceled;
+        
+        public bool Canceled() => _canceled;
+        
+        public abstract void CancelRun();
+    }
+}
