2d4508d0aaa5c7f375749612b23652e7325346e3
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 16:11:43 +0300

    Create ManageButtonsInteractableStatusByCoinsAmountSystem, bind it to CoinsSceneModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Systems/ManageButtonsInteractableStatusByCoinsAmountSystem.cs
@@ -0,0 +1,30 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons;
+using _Project.Scripts.Runtime.Shared.Utils.Features.Economy;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Economy.Coins.Systems
+{
+    public sealed class ManageButtonsInteractableStatusByCoinsAmountSystem : IProtoRunSystem
+    {
+        [DI] private readonly CoinsAspect _cAspect;
+        [DI] private readonly ButtonsAspect _bAspect;
+        private readonly DataProvider _dProvider;
+
+        public ManageButtonsInteractableStatusByCoinsAmountSystem(DataProvider dProvider)
+        {
+            _dProvider = dProvider;
+        }
+
+        public void Run()
+        {
+            foreach (var e in _cAspect.CoinsAmountButtons)
+            {
+                ref var caComponent = ref _cAspect.CoinsAmountComponentPool.Get(e);
+                ref var bComponent = ref _bAspect.ButtonComponentPool.Get(e);
+                bComponent.Button.interactable = CoinsUtils.CoinsEnoughToBuy(_dProvider, caComponent.Amount);
+            }
+        }
+    }
+}
