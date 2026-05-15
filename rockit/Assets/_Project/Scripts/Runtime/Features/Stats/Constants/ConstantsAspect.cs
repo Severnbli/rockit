4ed640f4c75898490b07e87bc3428078344e5e0e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 16:32:39 +0300

    Add "ClickedConstantsImprovers" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/ConstantsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/ConstantsAspect.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Components;
 using _Project.Scripts.Runtime.Features.Economy.Coins.Components;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Tags;
 using _Project.Scripts.Runtime.Features.Stats.Constants.Components;
 using _Project.Scripts.Runtime.Features.Stats.Constants.Tags;
 using Leopotam.EcsProto;
@@ -20,5 +21,6 @@ namespace _Project.Scripts.Runtime.Features.Stats.Constants
         public readonly ProtoIt PlayerLocatorConstantDisplays = new (It.Inc<PlayerLocatorComponent, ConstantDisplayTag>());
         public readonly ProtoIt ConstantActiveDisplayConstantPlayerLocators = new (It.Inc<ConstantActiveDisplayTag, ConstantComponent, PlayerLocatorComponent>());
         public readonly ProtoIt CoinsAmountConstantsImprovers = new (It.Inc<CoinsAmountComponent, ConstantImproverTag>());
+        public readonly ProtoIt ClickedConstantsImprovers = new (It.Inc<ClickedTag, ConstantImproverTag>());
     }
 }
