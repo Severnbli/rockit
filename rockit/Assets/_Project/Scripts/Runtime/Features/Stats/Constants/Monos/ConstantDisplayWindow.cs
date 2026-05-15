33db23db31a11b2a64704c7aa7aae5bf6169a1fa
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-15 15:31:08 +0300

    Add "_costText" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Monos/ConstantDisplayWindow.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Constants/Monos/ConstantDisplayWindow.cs
@@ -9,8 +9,10 @@ namespace _Project.Scripts.Runtime.Features.Stats.Constants.Monos
     {
         [SerializeField] private TextMeshProUGUI _nameText;
         [SerializeField] private TextMeshProUGUI _infoText;
+        [SerializeField] private TextMeshProUGUI _costText;
         
         public TextMeshProUGUI NameText => _nameText;
         public TextMeshProUGUI InfoText => _infoText;
+        public TextMeshProUGUI CostText => _costText;
     }
 }
