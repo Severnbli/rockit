f86185c46aaf724d0e18d5e02ac1635fd1d5c851
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 00:32:50 +0300

    Update sequence initialization: add "Initialize" method

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tools/Pipeline/Core/TweenPipelineRunObserver.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tools/Pipeline/Core/TweenPipelineRunObserver.cs
@@ -5,13 +5,14 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline
     public abstract class TweenPipelineRunObserver : ITweenPipelineRunObserver
     {
         protected Sequence Sequence;
+
         public bool Canceled { get; protected set; }
 
-        protected TweenPipelineRunObserver(Sequence sequence)
+        public void Initialize(Sequence sequence)
         {
             Sequence = sequence;
         }
-        
+
         public abstract void CancelRun();
 
         public void Reset()
