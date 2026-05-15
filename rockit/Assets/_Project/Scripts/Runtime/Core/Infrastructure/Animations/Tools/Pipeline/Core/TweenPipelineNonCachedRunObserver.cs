917fe8b75d6f0e855eccc7cd6856a9695c3a1ef1
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 00:37:42 +0300

    Create TweenPipelineNonCachedRunObserver

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Animations/Tools/Pipeline/Core/TweenPipelineNonCachedRunObserver.cs
@@ -0,0 +1,12 @@
+﻿using DG.Tweening;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline.Core
+{
+    public class TweenPipelineNonCachedRunObserver : TweenPipelineRunObserver
+    {
+        protected override void DoCancel()
+        {
+            Sequence?.Kill();
+        }
+    }
+}
