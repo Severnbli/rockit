ede27076036291b511e7826ace61129a2bd613f1
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 17:33:31 +0300

    Create CamerasExtensions with only method "Reset" for CamerasSwitchService

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Features/Graphics/CamerasExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Services;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions.Features.Graphics
+{
+    public static class CamerasExtensions
+    {
+        public static void Reset(this CamerasSwitchService csService)
+        {
+            csService.SwitchCts?.Cancel();
+            csService.SwitchCts?.Dispose();
+            csService.SwitchCts = new CancellationTokenSource();
+            csService.SwitchStarted = false;
+        }
+    }
+}
