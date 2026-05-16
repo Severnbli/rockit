cf4816d94e7328c771552ba97212e301683c3597
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 22:24:20 +0300

    Create AudioData with "Serializable" attribute and "MasterAudioEnabled", "MusicEnabled" and "SfxEnabled" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/AudioData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities
+{
+    [Serializable]
+    public sealed class AudioData
+    {
+        public bool MasterAudioEnabled = true;
+        public bool MusicEnabled = true;
+        public bool SfxEnabled = true;
+    }
+}
