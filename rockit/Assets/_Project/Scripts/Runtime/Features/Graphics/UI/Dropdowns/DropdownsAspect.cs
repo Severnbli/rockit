2f750c9c863ea6d030ed473a5b68a36277850654
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 02:04:29 +0300

    Create DropdownComponent with authoring attributes and "Dropdown" field, add it to DropdownsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Dropdowns/DropdownsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Dropdowns/DropdownsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns
 {
     public sealed class DropdownsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<DropdownComponent> DropdownComponentPool;
     }
 }
