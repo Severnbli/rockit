8ff98e5c217ef72755527701b3dd0d033b0e4464
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 00:30:15 +0300

    Create SettingsWindow that inherited from MonoFadeWindow

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Shared/Monos/SettingsWindow.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Monos;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Shared.Monos
+{
+    public class SettingsWindow : MonoFadeWindow<FadeWindowConfig>
+    {
+        
+    }
+}
