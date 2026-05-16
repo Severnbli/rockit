344fe662f8585ab168dbdc3046cef9deaebe7540
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 04:23:29 +0300

    Add switching to menus camera at "OnEnter" method

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/MenuState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/MenuState.cs
@@ -1,4 +1,8 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu.Monos;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu.Monos;
+using _Project.Scripts.Runtime.Shared.Utils.Features.Graphics;
 using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active
@@ -6,15 +10,29 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active
     public class MenuState : ISceneState
     {
         private readonly MenuWindow _mWindow;
+        private readonly MenusCamera _mCamera;
+        private readonly RequestsAspect _rAspect;
 
-        public MenuState(MenuWindow mWindow)
+        public MenuState(MenuWindow mWindow, MenusCamera mCamera, RequestsAspect rAspect)
         {
             _mWindow = mWindow;
+            _mCamera = mCamera;
+            _rAspect = rAspect;
         }
 
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
+            SwitchToMenuCamera();
             await _mWindow.OpenAwait();
+        }
+
+        private void SwitchToMenuCamera()
+        {
+            var prepared = new SwitchCameraRequest
+            {
+                Target = _mCamera.Camera
+            };
+            CamerasUtils.CreateSwitchCameraRequest(_rAspect, prepared);
         }
 
         public async UniTask OnLeave(IStateMachine stateMachine)
