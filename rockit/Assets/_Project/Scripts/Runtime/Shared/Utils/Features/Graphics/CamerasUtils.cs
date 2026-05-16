1e309ae40749abb7c99a5587768717bdb09b7479
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 03:52:52 +0300

    Create CameraUtils with only method "CreateSwitchCameraRequest"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Graphics/CamerasUtils.cs
@@ -0,0 +1,16 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.Graphics
+{
+    public static class CamerasUtils
+    {
+        public static ProtoEntity CreateSwitchCameraRequest(RequestsAspect aspect, SwitchCameraRequest prepared)
+        {
+            return aspect.CreateRequest(aspect.GraphicsSharedRequestsAspect.CamerasRequestsAspect.SwitchCameraRequestsPool, 
+                prepared: prepared);
+        }
+    }
+}
