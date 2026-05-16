6c7c38ee0f0fd4bd26e732fe77cd488be26d4bb9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 03:51:09 +0300

    Create SwitchCameraRequest, add it pool and iterator to CamerasRequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Requests/SwitchCameraRequest.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Cinemachine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests
+{
+    public struct SwitchCameraRequest
+    {
+        public CinemachineCamera Target;
+    }
+}
