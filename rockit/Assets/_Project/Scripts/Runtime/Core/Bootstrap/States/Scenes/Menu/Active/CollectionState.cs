e232913d3fdc28e138b4c674e2bf96c815044d5a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 19:18:35 +0300

    Add usings of camera switch awaiters on camera setup installation

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/CollectionState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/CollectionState.cs
@@ -1,6 +1,8 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+﻿using System.Threading;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
 using _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Types;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu.Monos;
 using _Project.Scripts.Runtime.Shared.Utils.Features.Graphics;
 using _Project.Scripts.Runtime.Shared.Utils.Features.Stats;
@@ -13,18 +15,24 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active
         private readonly RequestsAspect _rAspect;
         private readonly PlayerCamera _pCamera;
         private readonly CollectionWindow _cWindow;
+        private readonly ICameraSwitchAwaiter _csAwaiter;
+        private readonly CancellationToken _ct;
 
-        public CollectionState(RequestsAspect rAspect, PlayerCamera pCamera, CollectionWindow cWindow)
+        public CollectionState(RequestsAspect rAspect, PlayerCamera pCamera, CollectionWindow cWindow, 
+            ICameraSwitchAwaiter csAwaiter, CancellationToken ct)
         {
             _rAspect = rAspect;
             _pCamera = pCamera;
             _cWindow = cWindow;
+            _csAwaiter = csAwaiter;
+            _ct = ct;
         }
 
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
             ActivateConstantDisplayWindow();
             SwitchToPlayerCamera();
+            await _csAwaiter.AwaitSwitch(_ct);
             await _cWindow.OpenAwait();
         }
 
--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/MenuState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/MenuState.cs
@@ -1,6 +1,8 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+﻿using System.Threading;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
 using _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Types;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu.Monos;
 using _Project.Scripts.Runtime.Shared.Utils.Features.Graphics;
 using Cysharp.Threading.Tasks;
@@ -12,17 +14,23 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active
         private readonly MenuWindow _mWindow;
         private readonly MenusCamera _mCamera;
         private readonly RequestsAspect _rAspect;
+        private readonly ICameraSwitchAwaiter _csAwaiter;
+        private readonly CancellationToken _ct;
 
-        public MenuState(MenuWindow mWindow, MenusCamera mCamera, RequestsAspect rAspect)
+        public MenuState(MenuWindow mWindow, MenusCamera mCamera, RequestsAspect rAspect,
+            ICameraSwitchAwaiter csAwaiter, CancellationToken ct)
         {
             _mWindow = mWindow;
             _mCamera = mCamera;
             _rAspect = rAspect;
+            _csAwaiter = csAwaiter;
+            _ct = ct;
         }
 
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
             SwitchToMenuCamera();
+            await _csAwaiter.AwaitSwitch(_ct);
             await _mWindow.OpenAwait();
         }
 
