2675c72f021a106e7425631d462133f8cf2033cd
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 00:17:53 +0300

    Create SharedSceneWindowsAspect, add it to WindowsSharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Shared/SharedSceneWindowsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Shared
+{
+    public sealed class SharedSceneWindowsAspect : ProtoAspectInject
+    {
+        
+    }
+}
