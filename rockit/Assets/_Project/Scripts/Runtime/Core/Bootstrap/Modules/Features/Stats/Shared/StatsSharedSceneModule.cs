c105cec155985e5abc8e7c45e28ef749866ca4f8
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 21:58:25 +0300

    Create UpdateStarsTotalizersTextUisOnRunSystem, bind it to StatsSharedSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Shared/StatsSharedSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Shared/StatsSharedSceneModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Stats.Shared.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Shared
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Shared
     {
         public StatsSharedSceneModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<UpdateStarsTotalizersTextUisOnRunSystem>();
         }
     }
 }
