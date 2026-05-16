4d7556cb6fa5f338589f32ea0c30954226e3054d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 17:18:17 +0300

    Create CamerasServiceSwitchCtsOnRunObserverSystem, bind it to CamerasModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/CamerasModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/CamerasModule.cs
@@ -22,6 +22,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics
             base.BindSystems();
             
             BindSystem<SwitchCameraOnSwitchCameraRequestSystem>();
+            BindSystem<CamerasServiceSwitchCtsOnRunObserverSystem>();
         }
     }
 }
