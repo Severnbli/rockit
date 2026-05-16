a850a83e98e3574edb411ebec26709a02ebc4788
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 03:48:48 +0300

    Create CamerasRequestsAspect, add it to GraphicsSharedRequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/CamerasRequestsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Cameras
+{
+    public sealed class CamerasRequestsAspect : ProtoAspectInject
+    {
+        
+    }
+}
--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Shared/GraphicsSharedRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Shared/GraphicsSharedRequestsAspect.cs
@@ -1,9 +1,10 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Cameras;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Shared
 {
     public sealed class GraphicsSharedRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly CamerasRequestsAspect CamerasRequestsAspect;
     }
 }
