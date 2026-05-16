5e2ad832a207846af6cf29b4363534a11eed328b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 18:41:49 +0300

    Update camera switch await logic to use gen, no cancellation token source

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Systems/CamerasSwitchServiceOnRunObserverSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Systems/CamerasSwitchServiceOnRunObserverSystem.cs
@@ -1,8 +1,5 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Time.Services;
-using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
 using _Project.Scripts.Runtime.Features.Graphics.Cameras.Services;
-using _Project.Scripts.Runtime.Shared.Extensions.Features.Graphics;
-using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Systems
@@ -10,43 +7,33 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Systems
     public sealed class CamerasSwitchServiceOnRunObserverSystem : IProtoRunSystem
     {
         private readonly CamerasSwitchService _csService;
-        private readonly CamerasService _cService;
         private readonly CameraBrain _cBrain;
-        private readonly TimeService _tService;
 
-        public CamerasSwitchServiceOnRunObserverSystem(CamerasSwitchService csService, CamerasService cService, 
-            CameraBrain cBrain, TimeService tService)
+        public CamerasSwitchServiceOnRunObserverSystem(CamerasSwitchService csService, CameraBrain cBrain)
         {
             _csService = csService;
-            _cService = cService;
             _cBrain = cBrain;
-            _tService = tService;
         }
 
         public void Run()
         {
             var brain = _cBrain.Brain;
-            
+
             if (!brain.IsBlending)
             {
-                if (_csService.SwitchStarted) _csService.Reset();
+                if (_csService.SwitchStarted) CompleteSwitching();
                 return;
             }
 
-            if (!_csService.SwitchStarted)
-            {
-                _csService.EscortCamera = _cService.LastCamera;
-                _csService.SwitchStartTime = _tService.UnscaledTime;
-                _csService.SwitchDuration = brain.ActiveBlend.Duration + CamerasContracts.BrainBlendDurationLag;
-                _csService.SwitchStarted = true;
-                return;
-            }
+            if (_csService.SwitchStarted) return;
+            
+            _csService.SwitchStarted = true;
+        }
 
-            if (_tService.Expired(_csService.SwitchStartTime, _csService.SwitchDuration) 
-                || _csService.EscortCamera != _cService.LastCamera)
-            {
-                _csService.Reset();
-            }
+        private void CompleteSwitching()
+        {
+            _csService.SwitchGen++;
+            _csService.SwitchStarted = false;
         }
     }
 }
