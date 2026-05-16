bff89d5f91c61ec00c6097a98cfcb3b600ab526a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 17:18:17 +0300

    Create CamerasServiceSwitchCtsOnRunObserverSystem, bind it to CamerasModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Systems/CamerasServiceSwitchCtsOnRunObserverSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Services;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Systems
+{
+    public sealed class CamerasServiceSwitchCtsOnRunObserverSystem : IProtoRunSystem
+    {
+        private readonly CamerasService _cService;
+        private readonly CameraBrain _cBrain;
+
+        public CamerasServiceSwitchCtsOnRunObserverSystem(CamerasService cService, CameraBrain cBrain)
+        {
+            _cService = cService;
+            _cBrain = cBrain;
+        }
+
+        public void Run()
+        {
+            switch (_cBrain.Brain.IsBlending, _cService.SwitchStarted)
+            {
+                case (false, false):
+                case (true, true):
+                {
+                    return;
+                }
+                case (true, false):
+                {
+                    _cService.SwitchStarted = true;
+                    return;
+                }
+            }
+
+            _cService.SwitchCts?.Cancel();
+            _cService.SwitchCts?.Dispose();
+            _cService.SwitchCts = new CancellationTokenSource();
+        }
+    }
+}
