6843b93dd1269a6f7af975113982380cc189ba69
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 15:35:44 +0300

    Move SettingsTag into Windows.Shared.Tags

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Shared/WindowsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Shared/WindowsSharedAspect.cs
@@ -1,4 +1,7 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Tags;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu.Tags;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared
@@ -6,5 +9,8 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared
     public sealed class WindowsSharedAspect : ProtoAspectInject
     {
         public readonly MenuSceneWindowsAspect MenuSceneWindowsAspect;
+        
+        public readonly ProtoPool<SettingsTag> SettingsTagPool;
+        public readonly ProtoIt ClickedSettings = new (It.Inc<ClickedTag, SettingsTag>());
     }
 }
