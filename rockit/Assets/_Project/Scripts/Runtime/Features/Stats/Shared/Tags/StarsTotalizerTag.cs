936249f34a4742c2d6f4ac56cd2a14c3cac06bb6
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 21:49:50 +0300

    Create StarsTotalizerTag, add it pool and iterator into StatsSharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Shared/Tags/StarsTotalizerTag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Leopotam.EcsProto.Unity;
+
+namespace _Project.Scripts.Runtime.Features.Stats.Shared.Tags
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct StarsTotalizerTag
+    {
+        
+    }
+}
