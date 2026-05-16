fa971177ad03523eb531d7d08bfab41c2790781d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 15:37:12 +0300

    Fix namespacing

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Shared/Tags/SettingsTag.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Shared/Tags/SettingsTag.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Leopotam.EcsProto.Unity;
 
-namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu.Tags
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Tags
 {
     [Serializable, ProtoUnityAuthoring]
     public struct SettingsTag
