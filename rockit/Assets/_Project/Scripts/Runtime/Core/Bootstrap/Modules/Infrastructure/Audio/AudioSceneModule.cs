5e8be876e355925d3ad2bcaa310027a9b11b79d2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 00:02:15 +0300

    Create AudioSceneModule, register to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/Audio/AudioSceneModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure.Audio
+{
+    public sealed class AudioSceneModule : BaseModule<AudioSceneModule>
+    {
+        public AudioSceneModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
