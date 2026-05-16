f07183c26db5a6c69a9148b2b135bda5c39552ec
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 20:24:35 +0300

    Create UpdateCoinsTotalizersUiTextsOnRunSystem, bind it to CoinsSceneModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Systems/UpdateCoinsTotalizersUiTextsOnRunSystem.cs
@@ -0,0 +1,30 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Text.Shared;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Economy.Coins.Systems
+{
+    public sealed class UpdateCoinsTotalizersUiTextsOnRunSystem : IProtoRunSystem
+    {
+        [DI] private readonly CoinsAspect _cAspect;
+        [DI] private readonly TextSharedAspect _tsAspect;
+        private readonly DataProvider _dProvider;
+
+        public UpdateCoinsTotalizersUiTextsOnRunSystem(DataProvider dProvider)
+        {
+            _dProvider = dProvider;
+        }
+
+        public void Run()
+        {
+            var eData = _dProvider.EconomyData;
+            
+            foreach (var e in _cAspect.CoinsTotalizerTextUis)
+            {
+                ref var tuComponent = ref _tsAspect.TextUiComponentPool.Get(e);
+                tuComponent.Text.text = eData.CoinsQuantity.ToString();
+            }
+        }
+    }
+}
