3b2b5cf55d3e4cebf5185176b2f056b2e48023aa
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 23:48:27 +0300

    Create AudioVolumeTag, add it pool to AudioAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Tags/AudioVolumeTag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Leopotam.EcsProto.Unity;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Tags
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct AudioVolumeTag
+    {
+        
+    }
+}
