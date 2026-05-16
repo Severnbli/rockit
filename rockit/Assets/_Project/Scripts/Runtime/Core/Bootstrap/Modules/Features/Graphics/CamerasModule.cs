ca37ac36d6a27864c35f4a01cd65f18e59e7f3d4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 03:41:51 +0300

    Create CamerasModule, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/CamerasModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics
+{
+    public sealed class CamerasModule : BaseModule<CamerasModule>
+    {
+        public CamerasModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
