bb6932cbea1b7c35aa8bc121fca2973e50621d5d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 22:37:24 +0300

    Rename "MasterAudioEnabled" into "MasterVolumeEnabled"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/AudioData.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Storage/Entities/AudioData.cs
@@ -5,7 +5,7 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities
     [Serializable]
     public sealed class AudioData
     {
-        public bool MasterAudioEnabled = true;
+        public bool MasterVolumeEnabled = true;
         public bool MusicEnabled = true;
         public bool SfxEnabled = true;
     }
