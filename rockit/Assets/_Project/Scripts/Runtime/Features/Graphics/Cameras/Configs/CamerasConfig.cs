eadbe5ad88b11c93345608c0cdfcc8ed348d209b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 03:38:29 +0300

    Create CamerasConfig with "_primaryCameraPriority" and "_secondaryCameraPriority" fields and they property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Configs/CamerasConfig.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Configs
+{
+    public sealed class CamerasConfig : ScriptableObjectAutoInstaller<CamerasConfig>
+    {
+        [SerializeField] private int _primaryCameraPriority = 10;
+        [SerializeField] private int _secondaryCameraPriority = 0;
+        
+        public int PrimaryCameraPriority => _primaryCameraPriority;
+        public int SecondaryCameraPriority => _secondaryCameraPriority;
+    }
+}
