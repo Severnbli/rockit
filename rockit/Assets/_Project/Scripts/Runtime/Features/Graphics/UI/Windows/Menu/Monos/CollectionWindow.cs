15ea9c170b3a1e0a2a8368d20dd0bdbaa57e074c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 16:28:05 +0300

    Create CollectionWindow that inherited from MonoBaseWindow

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Menu/Monos/CollectionWindow.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Monos;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Menu.Monos
+{
+    public class CollectionWindow : MonoBaseWindow<BaseWindowConfig>
+    {
+        
+    }
+}
