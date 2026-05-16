14ee3f766278e0bfe8a93156f25b3bc248eaea0b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 23:33:24 +0300

    Create AudioGroup enum with "Master, "Music" and "Sfx" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Types/AudioGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Types
+{
+    [Serializable]
+    public enum AudioGroup
+    {
+        Master = 0,
+        Music = 1,
+        Sfx = 2
+    }
+}
