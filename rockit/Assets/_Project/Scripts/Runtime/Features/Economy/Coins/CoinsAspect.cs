3f4473b5b2f2ad23a5be90ff16bc5cd91151d261
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 20:20:56 +0300

    Create CoinsTotalizerTag, add it pool and iterator to CoinsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/CoinsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/CoinsAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Features.Economy.Coins.Components;
+using _Project.Scripts.Runtime.Features.Economy.Coins.Tags;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons.Components;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Tags;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Text.Shared.Components;
@@ -11,9 +12,11 @@ namespace _Project.Scripts.Runtime.Features.Economy.Coins
     {
         public readonly ProtoPool<CoinComponent> CoinComponentPool;
         public readonly ProtoPool<CoinsAmountComponent> CoinsAmountComponentPool;
+        public readonly ProtoPool<CoinsTotalizerTag> CoinsTotalizerTagPool;
         public readonly ProtoIt Coins = new (It.Inc<CoinComponent>());
         public readonly ProtoIt CoinsAmountTextUis = new (It.Inc<CoinsAmountComponent, TextUiComponent>());
         public readonly ProtoIt CoinsAmountButtons = new (It.Inc<CoinsAmountComponent, ButtonComponent>());
         public readonly ProtoIt ClickedCoinsAmounts = new (It.Inc<ClickedTag, CoinsAmountComponent>());
+        public readonly ProtoIt CoinsTotalizerTextUis = new (It.Inc<CoinsTotalizerTag, TextUiComponent>());
     }
 }
