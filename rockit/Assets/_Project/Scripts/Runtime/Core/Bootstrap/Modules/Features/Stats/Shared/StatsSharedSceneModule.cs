cb6c3cb6b8c086614f0eb188945728b7583040fe
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 21:57:45 +0300

    Create StatsSharedSceneModule, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Stats/Shared/StatsSharedSceneModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Stats.Shared
+{
+    public sealed class StatsSharedSceneModule : BaseModule<StatsSharedSceneModule>
+    {
+        public StatsSharedSceneModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
