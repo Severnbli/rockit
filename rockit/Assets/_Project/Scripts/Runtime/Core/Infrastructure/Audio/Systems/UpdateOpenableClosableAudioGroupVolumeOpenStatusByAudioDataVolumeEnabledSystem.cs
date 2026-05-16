b75a67cb24cb8e6822a17645815dc69c257bd586
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 00:09:21 +0300

    Create UpdateOpenableClosableAudioGroupVolumeOpenStatusByAudioDataVolumeEnabledSystem, bind it to AudioSceneModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Systems/UpdateOpenableClosableAudioGroupVolumeOpenStatusByAudioDataVolumeEnabledSystem.cs
@@ -0,0 +1,32 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
+using _Project.Scripts.Runtime.Shared.Utils.Infrastructure;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Systems
+{
+    public sealed class UpdateOpenableClosableAudioGroupVolumeOpenStatusByAudioDataVolumeEnabledSystem : IProtoRunSystem
+    {
+        [DI] private readonly AudioAspect _aAspect;
+        [DI] private readonly SharedAspect _sAspect;
+        private readonly DataProvider _dProvider;
+
+        public UpdateOpenableClosableAudioGroupVolumeOpenStatusByAudioDataVolumeEnabledSystem(DataProvider dProvider)
+        {
+            _dProvider = dProvider;
+        }
+
+        public void Run()
+        {
+            foreach (var e in _aAspect.OpenableClosableAudioGroupVolumes)
+            {
+                ref var agComponent = ref _aAspect.AudioGroupComponentPool.Get(e);
+                ref var ocComponent = ref _sAspect.OpenableClosableComponentPool.Get(e);
+
+                var enabled = AudioUtils.GetAudioDataVolumeEnabledByAudioGroup(_dProvider, agComponent.Group);
+                ocComponent.OpenableClosable.SetStatus(enabled);
+            }
+        }
+    }
+}
