83da1c2f3afc9ee8d5cb0355a8652535e752f329
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 00:22:04 +0300

    Create SharedSceneWindowsAspect, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/Windows/SharedSceneWindowsModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI.Windows
+{
+    public sealed class SharedSceneWindowsModule : BaseModule<SharedSceneWindowsModule>
+    {
+        public SharedSceneWindowsModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
