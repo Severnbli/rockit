34b7b876487ba2b09f085a9939a33fd06ae58ab3
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 04:25:51 +0300

    Add switching to player camera at "OnEnter" method

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/CollectionState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/CollectionState.cs
@@ -1,5 +1,8 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests;
 using _Project.Scripts.Runtime.Features.Stats.Constants.Services;
+using _Project.Scripts.Runtime.Shared.Utils.Features.Graphics;
 using _Project.Scripts.Runtime.Shared.Utils.Features.Stats;
 using Cysharp.Threading.Tasks;
 
@@ -9,18 +12,35 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active
     {
         private readonly ConstantDisplayWindowService _cdwService;
         private readonly RequestsAspect _rAspect;
+        private readonly PlayerCamera _pCamera;
 
-        public CollectionState(ConstantDisplayWindowService cdwService, RequestsAspect rAspect)
+        public CollectionState(ConstantDisplayWindowService cdwService, RequestsAspect rAspect, PlayerCamera pCamera)
         {
             _cdwService = cdwService;
             _rAspect = rAspect;
+            _pCamera = pCamera;
         }
 
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
+            ActivateConstantDisplayWindow();
+            SwitchToPlayerCamera();
+            await UniTask.NextFrame();
+        }
+
+        private void ActivateConstantDisplayWindow()
+        {
             _cdwService.Active = true;
             ConstantsUtils.CreateShowConstantDisplayWindowRequest(_rAspect);
-            await UniTask.NextFrame();
+        }
+
+        private void SwitchToPlayerCamera()
+        {
+            var prepared = new SwitchCameraRequest
+            {
+                Target = _pCamera.Camera
+            };
+            CamerasUtils.CreateSwitchCameraRequest(_rAspect, prepared);
         }
 
         public async UniTask OnLeave(IStateMachine stateMachine)
