1b2552a09efc467c62e96b52602df253aa951615
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 23:58:44 +0300

    Add "SwitchAudioDataVolumeEnabledByAudioGroup" audio util method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Infrastructure/AudioUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Infrastructure/AudioUtils.cs
@@ -1,5 +1,8 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Requests;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Configs;
+using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Types;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
 using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
 using Leopotam.EcsProto;
 
@@ -15,6 +18,30 @@ namespace _Project.Scripts.Runtime.Shared.Utils.Infrastructure
         public static ProtoEntity CreatePlayMusicRequest(RequestsAspect aspect, PlayMusicRequest prepared)
         {
             return aspect.CreateRequest(aspect.AudioRequestsAspect.PlayMusicRequestPool, prepared: prepared);
+        }
+
+        public static void SwitchAudioDataVolumeEnabledByAudioGroup(DataProvider dProvider, AudioGroup aGroup)
+        {
+            var aData = dProvider.AudioData;
+
+            switch (aGroup)
+            {
+                case AudioGroup.Master:
+                {
+                    aData.MasterVolumeEnabled = !aData.MasterVolumeEnabled;
+                    break;
+                }
+                case AudioGroup.Music:
+                {
+                    aData.MusicVolumeEnabled = !aData.MusicVolumeEnabled;
+                    break;
+                }
+                case AudioGroup.Sfx:
+                {
+                    aData.SfxVolumeEnabled = !aData.SfxVolumeEnabled;
+                    break;
+                }
+            }
         }
     }
 }
