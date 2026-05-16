80dc95bb5cec12fd3711bcdf05db896518d22a11
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 17:25:34 +0300

    Move fields of switch logic into separated newly created CamerasSwitchService

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Services/CamerasService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Services/CamerasService.cs
@@ -1,12 +1,9 @@
-﻿using System.Threading;
-using Unity.Cinemachine;
+﻿using Unity.Cinemachine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Services
 {
     public sealed class CamerasService
     {
         public CinemachineCamera LastCamera = null;
-        public CancellationTokenSource SwitchCts = new ();
-        public bool SwitchStarted = false; 
     }
 }
