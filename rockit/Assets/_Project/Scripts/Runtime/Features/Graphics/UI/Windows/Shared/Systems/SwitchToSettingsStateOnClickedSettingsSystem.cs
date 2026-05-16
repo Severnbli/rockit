fe5ad29cb3e917e07c7407e6bb0a0af281b316c2
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 15:41:29 +0300

    Create SwitchToSettingsStateOnClickedSettingsSystem, bind it to WindowsSharedModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Shared/Systems/SwitchToSettingsStateOnClickedSettingsSystem.cs
@@ -0,0 +1,25 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.States;
+using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes;
+using Cysharp.Threading.Tasks;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Systems
+{
+    public sealed class SwitchToSettingsStateOnClickedSettingsSystem : IProtoRunSystem
+    {
+        [DI] private readonly WindowsSharedAspect _wsAspect;
+        private readonly IStateMachine _sMachine;
+
+        public SwitchToSettingsStateOnClickedSettingsSystem(IStateMachine sMachine)
+        {
+            _sMachine = sMachine;
+        }
+
+        public void Run()
+        {
+            if (_wsAspect.ClickedSettings.IsEmptySlow()) return;
+            _sMachine.ChangeState<ISettingsState>().Forget();
+        }
+    }
+}
