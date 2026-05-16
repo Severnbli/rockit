365a26249f796da5ff907890150a6812d02b262b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 03:46:44 +0300

    Create CamerasService with only field "LastCamera", bind it tot CamerasModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Services/CamerasService.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Cinemachine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Services
+{
+    public class CamerasService
+    {
+        public CinemachineCamera LastCamera = null;
+    }
+}
