4c3c5e460153699c48ce93e69281d4aee2c36048
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 02:04:29 +0300

    Create DropdownComponent with authoring attributes and "Dropdown" field, add it to DropdownsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Dropdowns/Components/DropdownComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Leopotam.EcsProto.Unity;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns.Components
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct DropdownComponent
+    {
+        public Dropdown Dropdown;
+    }
+}
