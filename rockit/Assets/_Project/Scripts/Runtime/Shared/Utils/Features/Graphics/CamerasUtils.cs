e25e538b61cf4d1a0c9696eeb26cd1d97793783b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 17:54:48 +0300

    Add "AwaitCamerasSwitching" cameras util method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Graphics/CamerasUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Graphics/CamerasUtils.cs
@@ -1,6 +1,11 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+﻿using System;
+using System.Threading;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras;
 using _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Services;
 using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Cysharp.Threading.Tasks;
 using Leopotam.EcsProto;
 
 namespace _Project.Scripts.Runtime.Shared.Utils.Features.Graphics
@@ -11,6 +16,14 @@ namespace _Project.Scripts.Runtime.Shared.Utils.Features.Graphics
         {
             return aspect.CreateRequest(aspect.GraphicsSharedRequestsAspect.CamerasRequestsAspect.SwitchCameraRequestsPool, 
                 prepared: prepared);
+        }
+
+        public static async UniTask AwaitCamerasSwitching(CamerasSwitchService csService, CancellationToken ct = default)
+        {
+            using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(ct, csService.SwitchCts.Token);
+            
+            await UniTask.Delay(TimeSpan.FromSeconds(CamerasContracts.MaxBrainBlendDuration), 
+                cancellationToken: linkedCts.Token);
         }
     }
 }
