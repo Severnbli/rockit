9bf555bb4e49d6d0d511a6637e0414ac8bebaeea
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 21:49:50 +0300

    Create StarsTotalizerTag, add it pool and iterator into StatsSharedAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Shared/StatsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Stats/Shared/StatsSharedAspect.cs
@@ -1,4 +1,7 @@
-﻿using _Project.Scripts.Runtime.Features.Stats.Constants;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Text.Shared.Components;
+using _Project.Scripts.Runtime.Features.Stats.Constants;
+using _Project.Scripts.Runtime.Features.Stats.Shared.Tags;
+using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Stats.Shared
@@ -6,5 +9,8 @@ namespace _Project.Scripts.Runtime.Features.Stats.Shared
     public sealed class StatsSharedAspect : ProtoAspectInject
     {
         public readonly ConstantsAspect ConstantsAspect;
+        
+        public readonly ProtoPool<StarsTotalizerTag> StarsTotalizerTagPool;
+        public readonly ProtoIt StarsTotalizerTextUis = new (It.Inc<StarsTotalizerTag, TextUiComponent>());
     }
 }
