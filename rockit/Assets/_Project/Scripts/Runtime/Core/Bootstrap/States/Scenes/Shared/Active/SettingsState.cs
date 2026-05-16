2d54ed278887598c57e66e7321374fdb9d7f170f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 00:33:27 +0300

    Create SettingsState that implements ISettingsState

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Shared/Active/SettingsState.cs
@@ -0,0 +1,20 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Shared.Monos;
+using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Shared.Active
+{
+    public class SettingsState : ISettingsState
+    {
+        private readonly SettingsWindow _sWindow;
+        
+        public async UniTask OnEnter(IStateMachine stateMachine)
+        {
+            await _sWindow.OpenAwait();
+        }
+
+        public async UniTask OnLeave(IStateMachine stateMachine)
+        {
+            await _sWindow.CloseAwait();
+        }
+    }
+}
