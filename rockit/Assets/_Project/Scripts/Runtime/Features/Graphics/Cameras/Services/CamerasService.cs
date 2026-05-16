658e76ef55800ff05e72d4f63413311973f885c0
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-16 17:06:44 +0300

    Add "ChangeCts" field with default value as "new ()"

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Services/CamerasService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Services/CamerasService.cs
@@ -1,9 +1,11 @@
-﻿using Unity.Cinemachine;
+﻿using System.Threading;
+using Unity.Cinemachine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Services
 {
     public class CamerasService
     {
         public CinemachineCamera LastCamera = null;
+        public CancellationTokenSource ChangeCts = new ();
     }
 }
