7d09a35a231d6da85d037aa8a066093ecbb4eccd
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 02:01:39 +0300

    Create DropdownsAspect, add it to UISharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Dropdowns/DropdownsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns
+{
+    public sealed class DropdownsAspect : ProtoAspectInject
+    {
+        
+    }
+}
--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UISharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UISharedAspect.cs
@@ -2,6 +2,7 @@
 using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Components;
 using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Tags;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Tags;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Text.Shared;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared;
@@ -16,6 +17,7 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared
         public readonly ButtonsAspect ButtonsAspect;
         public readonly TextSharedAspect TextSharedAspect;
         public readonly WindowsSharedAspect WindowsSharedAspect;
+        public readonly DropdownsAspect DropdownsAspect;
         
         public readonly ProtoPool<ClickedTag> ClickedTagPool;
         public readonly ProtoPool<ActiveUIElementTag> ActiveUIElementTagPool;
