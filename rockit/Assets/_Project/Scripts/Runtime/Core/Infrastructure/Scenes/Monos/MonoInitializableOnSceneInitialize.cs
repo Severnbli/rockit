87338f270e9d3658fa1d262c84b1f1d287566454
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 15:19:20 +0300

    Create abstract MonoInitializableOnSceneInitialize, inherit AuthoringOnSceneInitialize from it

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Monos/MonoInitializableOnSceneInitialize.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Types;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Monos
+{
+    public abstract class MonoInitializableOnSceneInitialize : MonoBehaviour, IInitializableOnSceneInitialize
+    {
+        public abstract void Initialize();
+    }
+}
