9e906b7363e28f8332c760154c5c4c17dc04ec69
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 04:06:22 +0300

    Create RemoveDropdownChangeOnRunSystem, bind it to UISharedModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/Systems/RemoveDropdownChangeOnRunSystem.cs
@@ -0,0 +1,18 @@
+﻿using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Systems
+{
+    public sealed class RemoveDropdownChangeOnRunSystem : IProtoRunSystem
+    {
+        [DI] private readonly UISharedAspect _usAspect;
+        
+        public void Run()
+        {
+            foreach (var e in _usAspect.DropdownChanges)
+            {
+                _usAspect.DropdownChangeComponentPool.Del(e);
+            }
+        }
+    }
+}
