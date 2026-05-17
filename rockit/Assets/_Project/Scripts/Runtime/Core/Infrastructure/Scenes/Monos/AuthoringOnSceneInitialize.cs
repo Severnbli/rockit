f53e4e6f101d22baf956f05aae0c7a5061fcb76a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 15:16:48 +0300

    Create AuthoringOnSceneInitialize that inherited from MonoBehaviour and implements IInitializableOnSceneInitialize

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Scenes/Monos/AuthoringOnSceneInitialize.cs
@@ -0,0 +1,16 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Types;
+using Leopotam.EcsProto.Unity;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Scenes.Monos
+{
+    public class AuthoringOnSceneInitialize : MonoBehaviour, IInitializableOnSceneInitialize
+    {
+        [SerializeField] private ProtoUnityAuthoring _authoring;
+        
+        public void Initialize()
+        {
+            _authoring.ProcessAuthoring();
+        }
+    }
+}
