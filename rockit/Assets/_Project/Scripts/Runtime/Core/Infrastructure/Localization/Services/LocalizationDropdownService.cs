88c9a47a8e02e4f41717d93621f9b6e54edfce84
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 12:36:57 +0300

    Create LocalizationDropdownService with "OptionDataList", "IdLangCodeMapper" and "CurrentId" fields

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Localization/Services/LocalizationDropdownService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TMPro;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Localization.Services
+{
+    public sealed class LocalizationDropdownService
+    {
+        public List<TMP_Dropdown.OptionData> OptionDataList = new ();
+        public SortedDictionary<int, string> IdLangCodeMapper = new ();
+        public int CurrentId;
+    }
+}
