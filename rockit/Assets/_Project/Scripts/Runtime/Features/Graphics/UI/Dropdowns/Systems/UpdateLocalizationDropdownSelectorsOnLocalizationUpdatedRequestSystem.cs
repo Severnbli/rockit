586b53ace249231dd49ec588d348d5598911f991
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 13:00:46 +0300

    Update UpdateLocalizationDropdownSelectorsOnLocalizationUpdatedRequestSystem to use "LocalizaionDropdownService.UpdateDropdown" instead of directly LocalizationService fields processing

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Dropdowns/Systems/UpdateLocalizationDropdownSelectorsOnLocalizationUpdatedRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Dropdowns/Systems/UpdateLocalizationDropdownSelectorsOnLocalizationUpdatedRequestSystem.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections.Generic;
-using _Project.Scripts.Runtime.Core.Infrastructure.Localization;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Localization;
 using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Services;
 using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
-using TMPro;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns.Systems
 {
@@ -13,48 +11,21 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns.Systems
     {
         [DIRequests] private readonly LocalizationRequestsAspect _lrAspect;
         [DI] private readonly DropdownsAspect _dAspect;
-        private readonly LocalizationService _lService;
+        private readonly LocalizationDropdownService _ldService;
 
-        public UpdateLocalizationDropdownSelectorsOnLocalizationUpdatedRequestSystem(LocalizationService lService)
+        public UpdateLocalizationDropdownSelectorsOnLocalizationUpdatedRequestSystem(LocalizationDropdownService ldService)
         {
-            _lService = lService;
+            _ldService = ldService;
         }
 
         public void Run()
         {
-            if (_lrAspect.LocalizationUpdatedRequests.IsEmptySlow() 
-                || _dAspect.DropdownLocalizationSelectors.IsEmptySlow()) return;
-
-            var list = new List<TMP_Dropdown.OptionData>();
-
-            var currId = 0;
-            var selectedId = 0;
-            
-            foreach (var mapKvp in _lService.IdLanguageCodeMapper)
-            {
-                var langCode = mapKvp.Value;
-
-                if (_lService.CurrLang.LanguageCode == langCode)
-                {
-                    selectedId = currId;
-                }
-                
-                list.Add(new TMP_Dropdown.OptionData
-                {
-                    text = _lService.GetString(langCode, LocalizationEntriesContracts.LanguageNameS),
-                });
-                
-                currId++;
-            }
+            if (_lrAspect.LocalizationUpdatedRequests.IsEmptySlow()) return;
 
             foreach (var e in _dAspect.DropdownLocalizationSelectors)
             {
                 ref var dComponent = ref _dAspect.DropdownComponentPool.Get(e);
-                var dropdown = dComponent.Dropdown;
-
-                dropdown.options = list;
-                dropdown.SetValueWithoutNotify(selectedId);
-                dropdown.RefreshShownValue();
+                _ldService.UpdateDropdown(dComponent.Dropdown);
             }
         }
     }
