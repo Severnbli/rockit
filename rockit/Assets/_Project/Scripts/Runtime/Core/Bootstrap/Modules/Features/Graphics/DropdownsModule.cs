8b53b1f90569240e25789d203038d0786bb98f54
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 04:16:58 +0300

    Create SendChangeLanguageRequestOnDropdownChangeLocalizationSelectorsSystem, bind it to DropdownsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/DropdownsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/DropdownsModule.cs
@@ -14,6 +14,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics
             base.BindSystems();
             
             BindSystem<UpdateLocalizationDropdownSelectorsOnLocalizationUpdatedRequestSystem>();
+            BindSystem<SendChangeLanguageRequestOnDropdownChangeLocalizationSelectorsSystem>();
         }
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Dropdowns/Systems/SendChangeLanguageRequestOnDropdownChangeLocalizationSelectorsSystem.cs
@@ -0,0 +1,41 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Services;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Shared;
+using _Project.Scripts.Runtime.Shared.Utils.Infrastructure;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns.Systems
+{
+    public sealed class SendChangeLanguageRequestOnDropdownChangeLocalizationSelectorsSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly RequestsAspect _rAspect;
+        [DI] private readonly DropdownsAspect _dAspect;
+        [DI] private readonly UISharedAspect _usAspect;
+
+        private readonly LocalizationService _lService;
+
+        public SendChangeLanguageRequestOnDropdownChangeLocalizationSelectorsSystem(LocalizationService lService)
+        {
+            _lService = lService;
+        }
+
+        public void Run()
+        {
+            var (e, ok) = _dAspect.DropdownChangeLocalizationSelectors.FirstSlow();
+            if (!ok) return;
+            
+            ref var dcComponent = ref _usAspect.DropdownChangeComponentPool.Get(e);
+
+            if (!_lService.IdLanguageCodeMapper.TryGetValue(dcComponent.Value, out var langCode)) return;
+
+            var prepared = new ChangeLanguageRequest
+            {
+                LanguageCode = langCode
+            };
+            LocalizationUtils.CreateChangeLanguageRequest(_rAspect, prepared);
+        }
+    }
+}
