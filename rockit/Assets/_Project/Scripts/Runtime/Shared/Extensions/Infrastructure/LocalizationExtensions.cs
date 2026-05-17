d9d1a609a136c7c89615958e571ad127416ff631
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 12:46:30 +0300

    Add "Reload" LocalizationDropdownService method

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Infrastructure/LocalizationExtensions.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Infrastructure/LocalizationExtensions.cs
@@ -1,4 +1,6 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Services;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Localization;
+using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Services;
+using TMPro;
 
 namespace _Project.Scripts.Runtime.Shared.Extensions.Infrastructure
 {
@@ -14,6 +16,29 @@ namespace _Project.Scripts.Runtime.Shared.Extensions.Infrastructure
         {
             if (!service.LangData.TryGetValue(langCode, out var lData) || !lData.Entries.TryGetValue(key, out var entry)) return "";
             return entry.String;
+        }
+
+        public static void Reload(this LocalizationDropdownService ldService, LocalizationService lService)
+        {
+            var id = 0;
+            var selectedId = 0;
+            
+            ldService.IdLangCodeMapper.Clear();
+            ldService.OptionDataList.Clear();
+            
+            foreach (var langCode in lService.LangData.Keys)
+            {
+                if (langCode == lService.CurrLang.LanguageCode) selectedId = id;
+                
+                ldService.IdLangCodeMapper[id++] = langCode;
+                
+                ldService.OptionDataList.Add(new TMP_Dropdown.OptionData
+                {
+                    text = lService.GetString(langCode, LocalizationEntriesContracts.LanguageNameS),
+                });
+            }
+            
+            ldService.CurrentId = selectedId;
         }
     }
 }
