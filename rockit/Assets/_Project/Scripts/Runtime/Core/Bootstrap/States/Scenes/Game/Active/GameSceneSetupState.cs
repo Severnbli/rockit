878b2e82a069aa92f9e81a8f8321c999e419c480
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 15:00:34 +0300

    Inherit MenuSceneSetupState and GameSceneSetupState from SceneSetupState. Update their constructors and "OnEnter" and "OnLeave" methods to define overriding

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/GameSceneSetupState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/GameSceneSetupState.cs
@@ -1,30 +1,33 @@
-﻿using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Configs;
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Shared.Active;
+using _Project.Scripts.Runtime.Core.Engine;
+using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Configs;
 using _Project.Scripts.Runtime.Core.Infrastructure.Audio.Tools.Player;
 using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
 {
-    public class GameSceneSetupState : ISceneSetupState
+    public class GameSceneSetupState : SceneSetupState
     {
         private readonly IMusicPlayer _mPlayer;
         private readonly MusicConfig _mConfig;
 
-        public GameSceneSetupState(IMusicPlayer mPlayer, MusicConfig mConfig)
+        public GameSceneSetupState(IEngine engine, IMusicPlayer mPlayer, MusicConfig mConfig) : base(engine)
         {
             _mPlayer = mPlayer;
             _mConfig = mConfig;
         }
 
-        public async UniTask OnEnter(IStateMachine stateMachine)
+        public override async UniTask OnEnter(IStateMachine stateMachine)
         {
+            await base.OnEnter(stateMachine);
+            
             _mPlayer.Play(_mConfig.Game, true);
             stateMachine.ChangeState<SpawnLevelState>().Forget();
-            await UniTask.NextFrame();
         }
 
-        public async UniTask OnLeave(IStateMachine stateMachine)
+        public override async UniTask OnLeave(IStateMachine stateMachine)
         {
-            await UniTask.NextFrame();
+            await base.OnLeave(stateMachine);
         }
     }
 }
