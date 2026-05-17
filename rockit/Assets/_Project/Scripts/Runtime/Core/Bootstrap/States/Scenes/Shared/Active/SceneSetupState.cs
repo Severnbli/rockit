f24e6a0bff03754d925776c4377e12a36c9e8901
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 14:56:44 +0300

    Create SceneSetupState that implements ISceneSetupState with "Engine" field. Call for "Engine.Init()" at "OnEnter" method

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Shared/Active/SceneSetupState.cs
@@ -0,0 +1,26 @@
+﻿using _Project.Scripts.Runtime.Core.Engine;
+using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Shared.Active
+{
+    public abstract class SceneSetupState : ISceneSetupState
+    {
+        protected readonly IEngine Engine;
+
+        protected SceneSetupState(IEngine engine)
+        {
+            Engine = engine;
+        }
+
+        public virtual UniTask OnEnter(IStateMachine stateMachine)
+        {
+            Engine.Init();
+            return UniTask.CompletedTask;
+        }
+
+        public virtual UniTask OnLeave(IStateMachine stateMachine)
+        {
+            return UniTask.CompletedTask;
+        }
+    }
+}
