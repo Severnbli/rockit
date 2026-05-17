be801ee8a395a981882d3e99bf437c76d7b61d3d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 04:16:40 +0300

    Add "DropdownChangeLocalizationSelectors" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Dropdowns/DropdownsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Dropdowns/DropdownsAspect.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Tags;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns.Components;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Components;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -9,5 +10,6 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Dropdowns
     {
         public readonly ProtoPool<DropdownComponent> DropdownComponentPool;
         public readonly ProtoIt DropdownLocalizationSelectors = new (It.Inc<DropdownComponent, LocalizationSelectorTag>());
+        public readonly ProtoIt DropdownChangeLocalizationSelectors = new (It.Inc<DropdownChangeComponent, LocalizationSelectorTag>());
     }
 }
