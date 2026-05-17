9df2f60c166d6d96c27e2ae48b389e3957073e1f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 03:02:36 +0300

    Create LocalizationSceneModule, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Infrastructure/Localization/LocalizationSceneModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Infrastructure.Localization
+{
+    public sealed class LocalizationSceneModule : BaseModule<LocalizationSceneModule>
+    {
+        public LocalizationSceneModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
