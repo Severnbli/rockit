ded107a4b1d402d42eae17c5b746a030657f643e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 14:46:59 +0300

    Move load lang data to ProjectSetupState

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Project/ProjectSetupState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Project/ProjectSetupState.cs
@@ -1,12 +1,22 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes;
+using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Services;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
 using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Project
 {
     public class ProjectSetupState : IProjectSetupState
     {
+        private readonly LocalizationService _lService;
+
+        public ProjectSetupState(LocalizationService lService)
+        {
+            _lService = lService;
+        }
+
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
+            await _lService.LoadLangData();
             stateMachine.ChangeState<ISceneSetupState>().Forget();
             await UniTask.NextFrame();
         }
