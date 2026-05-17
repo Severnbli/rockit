eb1a4e8e180dc924b143c0b2d67bb8fa0858f636
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-17 14:24:31 +0300

    Create FilesUtils, add there "Write" and "Read" methods with "path" as parameter. Update StreamingAssetsUtils to use this

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Shared/FilesUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Shared
+{
+    public static class FilesUtils
+    {
+        public static void Write(string path, string data)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path) ?? "");
+            File.WriteAllText(path, data);
+        }
+
+        public static string Read(string path)
+        {
+            return !File.Exists(path) ? "" : File.ReadAllText(path);
+        }
+    }
+}
