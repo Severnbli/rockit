b313a7ae5f66b914caa4b807177bbf3403a3ebb3
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 00:28:43 +0300

    Revert "Remove InputSystemSelectableInvoker"
    This reverts commit 21f98660ff67629290b5ba0ff8ac87af0a3e34fd.

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/Monos/InputSystemSelectableInvoker.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Monos
+{
+    public class InputSystemSelectableInvoker : OnScreenControl, IPointerUpHandler, IPointerDownHandler, 
+        IPointerExitHandler, ICancelHandler
+    {
+        [SerializeField] private Selectable _selectable;
+        
+        [InputControl(layout = "Button")]
+        [SerializeField]
+        private string _controlPath;
+
+        protected override string controlPathInternal
+        {
+            get => _controlPath;
+            set => _controlPath = value;
+        }
+        
+        private bool CanInteract => enabled && _selectable != null && _selectable.interactable;
+
+        private void Enable()
+        {
+            if (!CanInteract) return;
+            
+            SendValueToControl(1.0f);
+        }
+
+        private void Disable()
+        {
+            if (!CanInteract) return;
+            
+            SendValueToControl(0.0f);
+        }
+        
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Disable();
+        }
+        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Enable();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Disable();
+        }
+
+        public void OnCancel(BaseEventData eventData)
+        {
+            Disable();
+        }
+    }
+}
