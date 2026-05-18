517cacccd0bf999eaa21c716dda767eaf371e8f9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-18 20:32:06 +0300

    Create InputSystemSelectableInvoker

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/Monos/InputSystemSelectableInvoker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Monos
+{
+    public class InputSystemSelectableInvoker : OnScreenControl, IPointerUpHandler, IPointerDownHandler
+    {
+        [SerializeField] private Selectable _selectable;
+        
+        [InputControl(layout = "Button")]
+        [SerializeField]
+        private string _controlPath;
+
+        protected override string controlPathInternal
+        {
+            get => _controlPath;
+            set => _controlPath = value;
+        }
+        
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (_selectable.interactable) SendValueToControl(0.0f);
+        }
+        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (_selectable.interactable) SendValueToControl(1.0f);
+        }
+    }
+}
