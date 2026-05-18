ef377ba286b8e81f52b319ffb6c9ab2fba2dcefa
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-18 19:25:24 +0300

    Create OpenClosePlatformsInputWindowOnPlatformsInputEnabledDisabledSystem, bind it to InputModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Systems/OpenClosePlatformsInputWindowOnPlatformsInputEnabledDisabledSystem.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Input.Monos;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Input.Systems
+{
+    public sealed class OpenClosePlatformsInputWindowOnPlatformsInputEnabledDisabledSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly InputRequestsAspect _irAspect;
+        private readonly PlayerInputWindow _piWindow;
+
+        public OpenClosePlatformsInputWindowOnPlatformsInputEnabledDisabledSystem(PlayerInputWindow piWindow)
+        {
+            _piWindow = piWindow;
+        }
+
+        public void Run()
+        {
+            if (!_irAspect.PlatformsInputEnabledRequests.IsEmptySlow()) _piWindow.Open();
+            
+            if (!_irAspect.PlatformsInputDisabledRequests.IsEmptySlow()) _piWindow.Close();
+        }
+    }
+}
