66b45f32b189b1e9a7eda8427564ce81aad59e00
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-18 17:00:25 +0300

    Add "_positionButton" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Input/Monos/PlatformsInputWindow.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Input/Monos/PlatformsInputWindow.cs
@@ -1,10 +1,14 @@
 ﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Configs;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Monos;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Project.Scripts.Runtime.Features.Input.Monos
 {
     public class PlatformsInputWindow : MonoBaseWindow<BaseWindowConfig>
     {
+        [SerializeField] private Button _positionButton;
         
+        public Button PositionButton => _positionButton;
     }
 }
