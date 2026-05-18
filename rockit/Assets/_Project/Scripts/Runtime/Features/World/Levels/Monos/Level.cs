593798c6d1d7745dd0ed7cee2dc3782894154d29
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-18 17:15:04 +0300

    Add "_piProfile" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Monos/Level.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Monos/Level.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using _Project.Scripts.Runtime.Features.Input.Types;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.World.Levels.Monos
 {
     public class Level : MonoBehaviour
     {
         [SerializeField] private GameObject _playerSpawnPosition;
+        [SerializeField] private PlatformsInputProfile _piProfile;
+        
+        public PlatformsInputProfile PiProfile => _piProfile;
     }
 }
