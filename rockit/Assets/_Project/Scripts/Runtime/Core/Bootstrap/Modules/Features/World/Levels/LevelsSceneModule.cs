5954f9f2c5019b0a880c257ff9c47be5b98660d8
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 21:24:13 +0300

    Bind LevelsService to LevelsSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Levels/LevelsSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Levels/LevelsSceneModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.World.Levels.Services;
 using _Project.Scripts.Runtime.Features.World.Levels.Types;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World.Levels
@@ -14,6 +15,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World.Levels
             base.RegisterBindings();
 
             Container.Bind<LevelFactory>().AsSingle();
+        }
+        
+        protected override void BindServices()
+        {
+            base.BindServices();
+            
+            BindService<LevelsService>();
         }
     }
 }
