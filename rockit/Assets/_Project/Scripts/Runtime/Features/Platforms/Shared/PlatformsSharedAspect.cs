d2dd3d693e38c123bf61a449fbcd4b9303eff13f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 00:29:06 +0300

    Add "ActivePositionPlatforms" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedAspect.cs
@@ -20,6 +20,7 @@ namespace _Project.Scripts.Runtime.Features.Platforms.Shared
         public readonly ProtoIt RotationPlatforms = new (It.Inc<PlatformComponent, RotationPlatformTag>());
         public readonly ProtoIt ScalePlatforms = new (It.Inc<PlatformComponent, ScalePlatformTag>());
         public readonly ProtoIt ActivePlatforms = new (It.Inc<PlatformComponent, ActivePlatformTag>());
+        public readonly ProtoIt ActivePositionPlatforms = new (It.Inc<PlatformComponent, ActivePlatformTag, PositionPlatformTag>());
         public readonly ProtoItExc InactivePlatforms = new (It.Inc<PlatformComponent>(), It.Exc<ActivePlatformTag>());
     }
 }
