ae3c0b6f51eb539420a5f3ea35af768b9c4109ab
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 15:48:00 +0300

    Create ControlsState that inherited from ISceneState

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Shared/Active/ControlsState.cs
@@ -0,0 +1,25 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Project.Monos;
+using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Shared.Active
+{
+    public class ControlsState : ISceneState
+    {
+        private readonly ControlsWindow _cWindow;
+
+        public ControlsState(ControlsWindow cWindow)
+        {
+            _cWindow = cWindow;
+        }
+
+        public async UniTask OnEnter(IStateMachine stateMachine)
+        {
+            await _cWindow.OpenAwait();
+        }
+
+        public async UniTask OnLeave(IStateMachine stateMachine)
+        {
+            await _cWindow.CloseAwait();
+        }
+    }
+}
