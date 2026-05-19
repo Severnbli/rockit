cc173b1b9eee55280bccbfe78b37a28d7dfc2685
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 21:35:44 +0300

    Add virtual "GetContainer" method

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Monos/Container.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Shared/Monos/Container.cs
@@ -4,6 +4,6 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Shared.Monos
 {
     public class Container : MonoBehaviour
     {
-        
+        public virtual Transform GetContainer() => transform;
     }
 }
