27a6eb08bf219808fabc9944e68f811c7427eb59
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 21:30:22 +0300

    Rename LevelsRequestAspect into LevelsRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsRequestsAspect.cs
@@ -6,7 +6,7 @@ using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.World.Levels
 {
-    public sealed class LevelsRequestAspect : ProtoAspectInject
+    public sealed class LevelsRequestsAspect : ProtoAspectInject
     {
         public readonly ProtoPool<SpawnLevelRequest> SpawnLevelRequestPool;
         public readonly ProtoIt SpawnLevelRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, SpawnLevelRequest>());
