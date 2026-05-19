7ab6d74b83d8570ea90cc157b529c6795d587f83
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 22:04:50 +0300

    Add "LevelIdToSpawn" field

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Services/LevelsService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Services/LevelsService.cs
@@ -4,6 +4,7 @@ namespace _Project.Scripts.Runtime.Features.World.Levels.Services
 {
     public sealed class LevelsService
     {
+        public int LevelIdToSpawn;
         public int CurrLevelId;
         public Level CurrLevel;
     }
