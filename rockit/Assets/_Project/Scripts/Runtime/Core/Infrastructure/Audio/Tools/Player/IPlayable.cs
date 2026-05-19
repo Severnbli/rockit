291c667f6b0ef3940befec805ae3fe09458670fa
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 23:04:19 +0300

    Expand IPlayable with "Pause" and "Resume" methods. Update AudioEmitter and MusicPlayer by providing definition for this methods

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Tools/Player/IPlayable.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Tools/Player/IPlayable.cs
@@ -5,6 +5,8 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Tools.Player
     public interface IPlayable
     {
         void Play(AudioClip clip, bool looped = true);
+        void Pause();
+        void Resume();
         void Stop();
     }
 }
