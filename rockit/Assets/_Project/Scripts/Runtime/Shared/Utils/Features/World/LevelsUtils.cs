215d994f9bc26864c4c1d178613342451c1d7f5b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 21:31:53 +0300

    Create LevelsUtils with only method "CreateSpawnLevelRequest"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/World/LevelsUtils.cs
@@ -0,0 +1,16 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.World.Levels.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.World
+{
+    public static class LevelsUtils
+    {
+        public static ProtoEntity CreateSpawnLevelRequest(RequestsAspect aspect, SpawnLevelRequest prepared)
+        {
+            return aspect.CreateRequest(aspect.WorldSharedRequestsAspect.LevelsRequestsAspect.SpawnLevelRequestPool, 
+                prepared: prepared);
+        }
+    }
+}
