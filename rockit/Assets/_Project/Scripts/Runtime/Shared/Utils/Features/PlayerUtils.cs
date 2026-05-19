7930ab5e296b47e081a50e8aa09a05e0fa2fc068
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 22:26:32 +0300

    Create PlayerUtils with only method "CreatePlacePlayerRequest"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/PlayerUtils.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Player.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features
+{
+    public static class PlayerUtils
+    {
+        public static ProtoEntity CreatePlacePlayerRequest(RequestsAspect aspect, PlacePlayerRequest prepared)
+        {
+            return aspect.CreateRequest(aspect.PlayerRequestsAspect.PlacePlayerRequestPool, prepared: prepared);
+        }
+    }
+}
