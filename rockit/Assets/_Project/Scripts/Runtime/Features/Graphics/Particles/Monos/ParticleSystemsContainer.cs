7085e62252414826755cce2413f492913f0d9095
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 02:51:22 +0300

    Create ParticleSystemsContainer that inherited from Container

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Monos/ParticleSystemsContainer.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Monos;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Monos
+{
+    public class ParticleSystemsContainer : Container
+    {
+        
+    }
+}
