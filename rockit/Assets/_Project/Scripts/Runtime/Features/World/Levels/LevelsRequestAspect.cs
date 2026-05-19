d718501d3f8ae932982d74645c313fc969ff61f1
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 21:26:16 +0300

    Create LevelsRequestAspect, add it to WorldSharedRequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsRequestAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels
+{
+    public sealed class LevelsRequestAspect : ProtoAspectInject
+    {
+        
+    }
+}
