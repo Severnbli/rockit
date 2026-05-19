279a970cb5e2af4984011c185e90f82d61821d45
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 00:36:34 +0300

    Create PlatformsSharedRequestsAspect, add it to RequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedRequestsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared
+{
+    public sealed class PlatformsSharedRequestsAspect : ProtoAspectInject
+    {
+        
+    }
+}
