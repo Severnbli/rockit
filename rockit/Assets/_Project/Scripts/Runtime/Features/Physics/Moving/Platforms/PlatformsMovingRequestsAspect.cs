790d5d268a3c37690eee4f52bd92990b49ca098f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 00:32:59 +0300

    Create PositionPlatformsTriggeredRequest, add it pool and iterator to PlatformsMovingRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Physics/Moving/Platforms/PlatformsMovingRequestsAspect.cs
@@ -11,8 +11,10 @@ namespace _Project.Scripts.Runtime.Features.Physics.Moving.Platforms
         public readonly ProtoPool<UpdatePlatformPositionRequest> UpdatePlatformPositionRequestPool;
         public readonly ProtoPool<UpdatePlatformRotationRequest> UpdatePlatformRotationRequestPool;
         public readonly ProtoPool<UpdatePlatformScaleRequest> UpdatePlatformScaleRequestPool;
+        public readonly ProtoPool<PositionPlatformsTriggeredRequest> PositionPlatformsTriggeredRequestPool;
         public readonly ProtoIt UpdatePlatformPositionRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, UpdatePlatformPositionRequest>());
         public readonly ProtoIt UpdatePlatformRotationRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, UpdatePlatformRotationRequest>());
         public readonly ProtoIt UpdatePlatformScaleRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, UpdatePlatformScaleRequest>());
+        public readonly ProtoIt PositionPlatformsTriggeredRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, PositionPlatformsTriggeredRequest>());
     }
 }
