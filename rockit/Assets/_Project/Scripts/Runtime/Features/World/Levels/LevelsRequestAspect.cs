a2c0cdde69ec5a6165e93a93c7d0ae4afe1815e4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 21:30:02 +0300

    Create SpawnLevelRequest, add it pool and iterator to LevelsRequestAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsRequestAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsRequestAspect.cs
@@ -1,9 +1,14 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using _Project.Scripts.Runtime.Features.World.Levels.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.World.Levels
 {
     public sealed class LevelsRequestAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<SpawnLevelRequest> SpawnLevelRequestPool;
+        public readonly ProtoIt SpawnLevelRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, SpawnLevelRequest>());
     }
 }
