6fa069173162a20bcd1d077bc729156d9b50328f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 22:19:14 +0300

    Create DestroyPrevLevelOnLevelSpawnedRequestSystem, bind it to LevelsSceneModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Systems/DestroyPrevLevelOnLevelSpawnedRequestSystem.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.World.Levels.Services;
+using Leopotam.EcsProto;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels.Systems
+{
+    public sealed class DestroyPrevLevelOnLevelSpawnedRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly LevelsRequestsAspect _lrAspect;
+        private readonly LevelsService _lService;
+
+        public DestroyPrevLevelOnLevelSpawnedRequestSystem(LevelsService lService)
+        {
+            _lService = lService;
+        }
+
+        public void Run()
+        {
+            if (_lrAspect.LevelSpawnedRequests.IsEmptySlow()) return;
+            Object.Destroy(_lService.CurrLevel.gameObject);
+        }
+    }
+}
