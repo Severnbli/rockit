72c2653343a1d02ccdbfa3a8993fca36f69c9745
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 00:41:49 +0300

    Create PlatformsUtils with "CreatePositionPlatformTriggeredRequest", "CreateRotationPlatformTriggeredRequest" and "CreateScalePlatformTriggeredRequest" methods

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/PlatformsUtils.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features
+{
+    public static class PlatformsUtils
+    {
+        public static ProtoEntity CreatePositionPlatformTriggeredRequest(RequestsAspect aspect)
+        {
+            return aspect.CreateRequest(aspect.PlatformsSharedRequestsAspect.PositionPlatformsTriggeredRequestPool);
+        }
+
+        public static ProtoEntity CreateRotationPlatformTriggeredRequest(RequestsAspect aspect)
+        {
+            return aspect.CreateRequest(aspect.PlatformsSharedRequestsAspect.RotationPlatformsTriggeredRequestPool);
+        }
+
+        public static ProtoEntity CreateScalePlatformTriggeredRequest(RequestsAspect aspect)
+        {
+            return aspect.CreateRequest(aspect.PlatformsSharedRequestsAspect.ScalePlatformsTriggeredRequestPool);
+        }
+    }
+}
