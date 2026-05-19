295d734fe92bf8b48720612efd7a335614a32fe4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 00:19:05 +0300

    Create ParticleSystemComponent, add it pool to ParticlesAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/ParticlesAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/ParticlesAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Particles.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Particles
 {
     public sealed class ParticlesAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<ParticleSystemComponent> ParticleSystemComponentPool;
     }
 }
