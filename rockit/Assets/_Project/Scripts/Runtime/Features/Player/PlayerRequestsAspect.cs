52990dc55aca0818f48b8a7a357880bde1948ff1
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 22:25:10 +0300

    Create PlacePlayerRequest, add it pool and iterator to PlayerRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Player/PlayerRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Player/PlayerRequestsAspect.cs
@@ -1,9 +1,14 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using _Project.Scripts.Runtime.Features.Player.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Player
 {
     public sealed class PlayerRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<PlacePlayerRequest> PlacePlayerRequestPool;
+        public readonly ProtoIt PlacePlayerRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, PlacePlayerRequest>());
     }
 }
