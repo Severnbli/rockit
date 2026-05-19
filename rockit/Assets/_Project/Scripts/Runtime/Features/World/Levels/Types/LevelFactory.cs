0504678e6b840c9f6231de5e34760e47c4c5d937
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 21:49:32 +0300

    Use LevelService id provider instead of parse level id to load from data provider

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Types/LevelFactory.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Types/LevelFactory.cs
@@ -2,6 +2,7 @@
 using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
 using _Project.Scripts.Runtime.Features.World.Levels.Configs;
 using _Project.Scripts.Runtime.Features.World.Levels.Monos;
+using _Project.Scripts.Runtime.Features.World.Levels.Services;
 using Leopotam.EcsProto;
 using UnityEngine;
 
@@ -9,21 +10,21 @@ namespace _Project.Scripts.Runtime.Features.World.Levels.Types
 {
     public class LevelFactory : BasePrefabFactory<Level, LevelFactoryCreateSettings>
     {
-        private readonly DataProvider _dProvider;
-        private readonly LevelsConfig _lConfig;
         private readonly LevelsContainer _lContainer;
+        private readonly LevelsService _lService;
+        private readonly LevelsConfig _lConfig;
 
-        public LevelFactory(ProtoWorld world, DataProvider dProvider, LevelsConfig lConfig, 
-            LevelsContainer lContainer) : base(world)
+        public LevelFactory(ProtoWorld world, LevelsContainer lContainer, LevelsService lService, 
+            LevelsConfig lConfig) : base(world)
         {
-            _dProvider = dProvider;
-            _lConfig = lConfig;
             _lContainer = lContainer;
+            _lService = lService;
+            _lConfig = lConfig;
         }
 
         protected override GameObject GetPrefab()
         {
-            return !_lConfig.Levels.TryGetValue(_dProvider.GameSceneData.LevelIdToLoad, out var lDefinition) 
+            return !_lConfig.Levels.TryGetValue(_lService.CurrLevelId, out var lDefinition) 
                 ? null 
                 : lDefinition.Prefab;
         }
