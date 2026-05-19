75da8908a9fb2fc95f3669b572a7642efc9dcc5a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 22:35:39 +0300

    Create PlayerModule, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/PlayerModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features
+{
+    public sealed class PlayerModule : BaseModule<PlayerModule>
+    {
+        public PlayerModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
