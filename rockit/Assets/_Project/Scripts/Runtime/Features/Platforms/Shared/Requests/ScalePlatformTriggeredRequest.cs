eae351b7d6a8d65abec8dc533fe99b97429c8c9a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 00:45:57 +0300

    Rename ScalePlatformsTriggeredRequest into ScalePlatformTriggeredRequest

--- rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Requests/ScalePlatformTriggeredRequest.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Requests/ScalePlatformTriggeredRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Requests
 {
-    public struct ScalePlatformsTriggeredRequest
+    public struct ScalePlatformTriggeredRequest
     {
         
     }
