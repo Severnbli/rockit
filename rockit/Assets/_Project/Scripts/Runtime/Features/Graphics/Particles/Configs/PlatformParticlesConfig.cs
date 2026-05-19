2c7d077de555dc4caf63c276aa61ebc4dbb64359
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 02:58:14 +0300

    Create PlatformParticlesConfig with "_positionPrefab", "_rotationPrefab" and "_scalePrefab" fields and they property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Configs/PlatformParticlesConfig.cs
@@ -0,0 +1,16 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Configs
+{
+    public sealed class PlatformParticlesConfig : ScriptableObjectAutoInstaller<PlatformParticlesConfig>
+    {
+        [SerializeField] private GameObject _positionPrefab;
+        [SerializeField] private GameObject _rotationPrefab;
+        [SerializeField] private GameObject _scalePrefab;
+        
+        public GameObject PositionPrefab => _positionPrefab;
+        public GameObject RotationPrefab => _rotationPrefab;
+        public GameObject ScalePrefab => _scalePrefab;
+    }
+}
