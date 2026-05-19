b500aa0424b697ba1d2e086b5af8f4a09902e018
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 00:19:05 +0300

    Create ParticleSystemComponent, add it pool to ParticlesAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Components/ParticleSystemComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Leopotam.EcsProto.Unity;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Components
+{
+    [Serializable, ProtoUnityAuthoring]
+    public struct ParticleSystemComponent
+    {
+        public ParticleSystem ParticleSystem;
+    }
+}
