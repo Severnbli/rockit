18f46b8119132e0cb47a75ef5cb324170493c49c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 17:22:57 +0300

    Create LoadingWindow that inherited from MonoFadeWindow with "_loadingText" field and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Project/Monos/LoadingWindow.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Monos;
+using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Project.Monos
+{
+    public class LoadingWindow : MonoFadeWindow<FadeWindowConfig>
+    {
+        [SerializeField] private TextMeshProUGUI _loadingText;
+        
+        public TextMeshProUGUI LoadingText => _loadingText;
+    }
+}
