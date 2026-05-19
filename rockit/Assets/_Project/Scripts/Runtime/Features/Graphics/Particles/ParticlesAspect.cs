b6351b40fa0f22fa5226c9f09689cd2a453bcdec
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 01:02:24 +0300

    Add "PositionPlatformParticleSystems" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/ParticlesAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/ParticlesAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Features.Graphics.Particles.Components;
+using _Project.Scripts.Runtime.Features.Platforms.Shared.Tags;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -7,5 +8,6 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Particles
     public sealed class ParticlesAspect : ProtoAspectInject
     {
         public readonly ProtoPool<ParticleSystemComponent> ParticleSystemComponentPool;
+        public readonly ProtoIt PositionPlatformParticleSystems = new (It.Inc<PositionPlatformTag, ParticleSystemComponent>());
     }
 }
