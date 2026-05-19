bc43f3d7f295b37056f839651630a6efaf0789a4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 15:41:00 +0300

    Create ControlsWindowTag, add it pool to ProjectWindowsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Project/ProjectWindowsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Project/ProjectWindowsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Project.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Project
 {
     public sealed class ProjectWindowsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<ControlsWindowTag> ControlsWindowTagPool;
     }
 }
