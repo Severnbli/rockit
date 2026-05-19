5db31953a87fdafb891595869ca35a3180b34ddd
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 22:28:28 +0300

    Add "TransformPlayers" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Player/PlayerAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Player/PlayerAspect.cs
@@ -1,9 +1,12 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Shared.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Player
 {
     public sealed class PlayerAspect : ProtoAspectInject
     {
-        
+        public ProtoIt TransformPlayers = new (It.Inc<PlayerTag, TransformComponent>());
     }
 }
