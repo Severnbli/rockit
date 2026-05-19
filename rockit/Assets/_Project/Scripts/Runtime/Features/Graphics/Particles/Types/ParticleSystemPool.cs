bf02043a0cdf7ede29a787d01b43048d4b661b74
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 02:47:57 +0300

    Create ParticleSystemPool that inherited from BasePrefabPool. Define settings: ParticleSystemPoolSpawnSettings with "At" field of type Vecto2 and ParticleSystemPoolDespawnSettings

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Types/ParticleSystemPool.cs
@@ -0,0 +1,23 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
+using Leopotam.EcsProto;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Types
+{
+    public abstract class ParticleSystemPool : BasePrefabPool<ParticleSystem, ParticleSystemPoolSpawnSettings, ParticleSystemPoolDespawnSettings>
+    {
+        protected ParticleSystemPool(ProtoWorld world) : base(world)
+        {
+        }
+    }
+
+    public struct ParticleSystemPoolSpawnSettings
+    {
+        public Vector2 At;
+    }
+
+    public struct ParticleSystemPoolDespawnSettings
+    {
+        
+    }
+}
