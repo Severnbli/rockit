7a0b06b4fb6c6a4fad3345fb2b3b2efbb28bbd62
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 15:37:56 +0300

    Create ProjectWindowsExtensions with only method "UpdateInfo" for ControlsWindow

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Features/Graphics/UI/Windows/ProjectWindowsExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using _Project.Scripts.Runtime.Core.Infrastructure.Localization;
+using _Project.Scripts.Runtime.Core.Infrastructure.Localization.Services;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Project.Monos;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions.Features.Graphics.UI.Windows
+{
+    public static class ProjectWindowsExtensions
+    {
+        public static void UpdateInfo(this ControlsWindow cWindow, LocalizationService lService)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(lService.GetString(LocalizationEntriesContracts.Controls.Run.Control));
+            sb.Append(LocalizationEntriesContracts.Controls.Separator);
+            sb.Append(lService.GetString(LocalizationEntriesContracts.Controls.Run.Info));
+            sb.AppendLine();
+            
+            sb.AppendLine(lService.GetString(LocalizationEntriesContracts.Controls.Jump.Control));
+            sb.Append(LocalizationEntriesContracts.Controls.Separator);
+            sb.Append(lService.GetString(LocalizationEntriesContracts.Controls.Jump.Info));
+            sb.AppendLine();
+            
+            sb.AppendLine(lService.GetString(LocalizationEntriesContracts.Controls.Dash.Control));
+            sb.Append(LocalizationEntriesContracts.Controls.Separator);
+            sb.Append(lService.GetString(LocalizationEntriesContracts.Controls.Dash.Info));
+            sb.AppendLine();
+            
+            sb.AppendLine(lService.GetString(LocalizationEntriesContracts.Controls.Platforms.Position.Control));
+            sb.Append(LocalizationEntriesContracts.Controls.Separator);
+            sb.Append(lService.GetString(LocalizationEntriesContracts.Controls.Platforms.Position.Info));
+            sb.AppendLine();
+            
+            sb.AppendLine(lService.GetString(LocalizationEntriesContracts.Controls.Platforms.Rotation.Control));
+            sb.Append(LocalizationEntriesContracts.Controls.Separator);
+            sb.Append(lService.GetString(LocalizationEntriesContracts.Controls.Platforms.Rotation.Info));
+            sb.AppendLine();
+            
+            sb.AppendLine(lService.GetString(LocalizationEntriesContracts.Controls.Platforms.Scale.Control));
+            sb.Append(LocalizationEntriesContracts.Controls.Separator);
+            sb.Append(lService.GetString(LocalizationEntriesContracts.Controls.Platforms.Scale.Info));
+            sb.AppendLine();
+            
+            sb.AppendLine(lService.GetString(LocalizationEntriesContracts.ClickToContinue));
+            
+            cWindow.InfoText.text = sb.ToString();
+        }
+    }
+}
