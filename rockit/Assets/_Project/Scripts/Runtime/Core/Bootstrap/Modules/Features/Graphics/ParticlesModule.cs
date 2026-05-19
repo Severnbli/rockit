2565ebb4139dd21c3f210b134d00371f17f09572
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 01:07:45 +0300

    Create PlayRotationPlatformParticleSystemOnRotationPlatformTriggeredRequestSystem, bind it to ParticlesModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/ParticlesModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/ParticlesModule.cs
@@ -14,6 +14,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics
             base.BindSystems();
             
             BindSystem<PlayPositionPlatformParticleSystemOnPositionPlatformTriggeredRequestSystem>();
+            BindSystem<PlayRotationPlatformParticleSystemOnRotationPlatformTriggeredRequestSystem>();
         }
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Systems/PlayRotationPlatformParticleSystemOnRotationPlatformTriggeredRequestSystem.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Platforms.Shared;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Systems
+{
+    public sealed class PlayRotationPlatformParticleSystemOnRotationPlatformTriggeredRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly PlatformsSharedRequestsAspect _psrAspect;
+        [DI] private readonly ParticlesAspect _pAspect;
+        
+        public void Run()
+        {
+            if (_psrAspect.RotationPlatformTriggeredRequests.IsEmptySlow()) return;
+
+            foreach (var e in _pAspect.RotationPlatformParticleSystems)
+            {
+                ref var psComponent = ref _pAspect.ParticleSystemComponentPool.Get(e);
+                psComponent.ParticleSystem.Play();
+            }
+        }
+    }
+}
