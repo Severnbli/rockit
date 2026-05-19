200ce9486cbbb4197dc48210528c428634b14df7
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 04:01:40 +0300

    Add try-catch block to "WebUtils.GetText"

--- rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Shared/WebUtils.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Shared/WebUtils.cs
@@ -1,4 +1,5 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine.Networking;
 
 namespace _Project.Scripts.Runtime.Shared.Utils.Shared
@@ -8,9 +9,17 @@ namespace _Project.Scripts.Runtime.Shared.Utils.Shared
         public static async UniTask<string> GetText(string url)
         {
             using var request = UnityWebRequest.Get(url);
-            
-            await request.SendWebRequest();
-            if (request.result == UnityWebRequest.Result.Success) return request.downloadHandler.text;
+
+            try
+            {
+                await request.SendWebRequest();
+                if (request.result == UnityWebRequest.Result.Success) return request.downloadHandler.text;
+            }
+            catch (Exception e)
+            {
+                LogUtils.LogError($"Error: {e}");
+                throw;
+            }
 
             LogUtils.LogError($"Failed to get text. Url: {url}. Error: {request.error}");
             
