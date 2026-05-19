7fbb575db323251d1f5274c04410fd75e1f8901f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-19 16:18:24 +0300

    Update GameState to enabled/disable inputs at "OnEnter" and "OnLeave"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/GameState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/GameState.cs
@@ -1,16 +1,29 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Shared.Utils.Features.Input;
+using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
 {
     public class GameState : ISceneState
     {
+        private readonly RequestsAspect _rAspect;
+
+        public GameState(RequestsAspect rAspect)
+        {
+            _rAspect = rAspect;
+        }
+
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
+            PlayerInputUtils.CreateEnableRequest(_rAspect);
+            PlatformsInputUtils.CreateEnableRequest(_rAspect);
             await UniTask.NextFrame();
         }
 
         public async UniTask OnLeave(IStateMachine stateMachine)
         {
+            PlayerInputUtils.CreateDisableRequest(_rAspect);
+            PlatformsInputUtils.CreateDisableRequest(_rAspect);
             await UniTask.NextFrame();
         }
     }
