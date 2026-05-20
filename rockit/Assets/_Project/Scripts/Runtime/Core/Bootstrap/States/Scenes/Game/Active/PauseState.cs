57e9e968ba64417711becd66ab2eadd673a2423c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 22:07:32 +0300

    Create PauseState that implements ISceneState and define logic to open/close pause window

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/PauseState.cs
@@ -0,0 +1,25 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Monos;
+using Cysharp.Threading.Tasks;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
+{
+    public class PauseState : ISceneState
+    {
+        private readonly PauseWindow _pWindow;
+
+        public PauseState(PauseWindow pWindow)
+        {
+            _pWindow = pWindow;
+        }
+
+        public async UniTask OnEnter(IStateMachine stateMachine)
+        {
+            await _pWindow.OpenAwait();
+        }
+
+        public async UniTask OnLeave(IStateMachine stateMachine)
+        {
+            await _pWindow.CloseAwait();
+        }
+    }
+}
