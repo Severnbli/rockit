f3619ab2b82d658053fdb1ee2275c9d3a543a08e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 16:43:02 +0300

    Create CheckpointsUtils with only method "CreateActivateCheckpointRequest" method

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/World/CheckpointsUtils.cs
@@ -0,0 +1,17 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.World
+{
+    public static class CheckpointsUtils
+    {
+        public static ProtoEntity CreateActivateCheckpointRequest(RequestsAspect aspect,
+            ProtoPackedEntityWithWorld packed)
+        {
+            return aspect.CreateRequest(
+                aspect.WorldSharedRequestsAspect.CheckpointsRequestsAspect.ActivateCheckpointRequestPool, packed);
+        }
+    }
+}
