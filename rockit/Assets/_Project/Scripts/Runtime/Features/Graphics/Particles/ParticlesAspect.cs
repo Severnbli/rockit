909e6b25320d6015468f55cadcd7346b06135db9
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 03:04:10 +0300

    Create ActiveParticleSystemTag, add it pool to ParticlesAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/ParticlesAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/ParticlesAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Features.Graphics.Particles.Components;
+using _Project.Scripts.Runtime.Features.Graphics.Particles.Tags;
 using _Project.Scripts.Runtime.Features.Platforms.Shared.Tags;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
@@ -8,6 +9,7 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Particles
     public sealed class ParticlesAspect : ProtoAspectInject
     {
         public readonly ProtoPool<ParticleSystemComponent> ParticleSystemComponentPool;
+        public readonly ProtoPool<ActiveParticleSystemTag> ActiveParticleSystemTagPool;
         public readonly ProtoIt PositionPlatformParticleSystems = new (It.Inc<PositionPlatformTag, ParticleSystemComponent>());
         public readonly ProtoIt RotationPlatformParticleSystems = new (It.Inc<RotationPlatformTag, ParticleSystemComponent>());
         public readonly ProtoIt ScalePlatformParticleSystems = new (It.Inc<ScalePlatformTag, ParticleSystemComponent>());
