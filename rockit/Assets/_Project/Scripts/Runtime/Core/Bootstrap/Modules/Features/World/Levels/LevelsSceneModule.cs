9eb5e0010e30c60ef9aeffdd203969494194be45
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-21 01:56:10 +0300

    Create UpdateLevelsServiceUsedTransformsOnAnyPlatformsTriggeredRequestSystem, bind it to LevelsSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Levels/LevelsSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Levels/LevelsSceneModule.cs
@@ -34,6 +34,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World.Levels
             BindSystem<ResetLevelsServiceOnLevelSpawnedRequestSystem>();
             BindSystem<InstallLevelToLevelsServiceOnLevelSpawnedRequestSystem>();
             BindSystem<SendPlacePlayerRequestOnLevelSpawnedRequestSystem>();
+            BindSystem<UpdateLevelsServiceUsedTransformsOnAnyPlatformsTriggeredRequestSystem>();
         }
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Systems/UpdateLevelsServiceUsedTransformsOnAnyPlatformsTriggeredRequestSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Platforms.Shared;
+using _Project.Scripts.Runtime.Features.World.Levels.Services;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels.Systems
+{
+    public sealed class UpdateLevelsServiceUsedTransformsOnAnyPlatformsTriggeredRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly PlatformsSharedRequestsAspect _psrAspect;
+        private readonly LevelsService _lService;
+
+        public UpdateLevelsServiceUsedTransformsOnAnyPlatformsTriggeredRequestSystem(LevelsService lService)
+        {
+            _lService = lService;
+        }
+
+        public void Run()
+        {
+            if (_psrAspect.AnyPlatformTriggeredRequests.IsEmptySlow()) return;
+
+            var useRate = _lService.CurrLevelDefinition?.TransformsUseRate ?? LevelsContracts.DefaultTransformsUseRate;
+            var quantity = _psrAspect.AnyPlatformTriggeredRequests.LenSlow();
+            
+            _lService.UsedTransforms += quantity * useRate;
+        }
+    }
+}
