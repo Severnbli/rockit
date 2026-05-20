fb93f66f94b9d1b683786e5ecfa97474cfe2e1fa
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 23:57:35 +0300

    Add "_confiner" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Monos/PlayerCamera.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Monos/PlayerCamera.cs
@@ -1,7 +1,12 @@
-﻿namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos
 {
     public class PlayerCamera : MonoCamera
     {
+        [SerializeField] private CinemachineConfiner2D _confiner;
         
+        public CinemachineConfiner2D Confiner => _confiner;
     }
 }
