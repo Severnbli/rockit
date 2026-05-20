42760a5859f373e5028ad8f85a6741c991b07374
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 03:25:54 +0300

    Update PlayScalePlatformParticleSystemOnScalePlatformTriggeredRequestSystem to pool usage

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Systems/PlayScalePlatformParticleSystemOnScalePlatformTriggeredRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Systems/PlayScalePlatformParticleSystemOnScalePlatformTriggeredRequestSystem.cs
@@ -1,23 +1,40 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using _Project.Scripts.Runtime.Features.Graphics.Particles.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.Particles.Types;
 using _Project.Scripts.Runtime.Features.Platforms.Shared;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Systems
 {
     public sealed class PlayScalePlatformParticleSystemOnScalePlatformTriggeredRequestSystem : IProtoRunSystem
     {
         [DIRequests] private readonly PlatformsSharedRequestsAspect _psrAspect;
-        [DI] private readonly ParticlesAspect _pAspect;
-        
+        [DI] private readonly SharedAspect _sAspect;
+        private readonly ScalePlatformParticleSystemPool _sppsPool;
+        private readonly PlatformParticlesConfig _ppConfig;
+
+        public PlayScalePlatformParticleSystemOnScalePlatformTriggeredRequestSystem(ScalePlatformParticleSystemPool sppsPool, PlatformParticlesConfig ppConfig)
+        {
+            _sppsPool = sppsPool;
+            _ppConfig = ppConfig;
+        }
+
         public void Run()
         {
             if (_psrAspect.ScalePlatformTriggeredRequests.IsEmptySlow()) return;
 
-            foreach (var e in _pAspect.ScalePlatformParticleSystems)
+            foreach (var e in _sAspect.TransformPlayers)
             {
-                ref var psComponent = ref _pAspect.ParticleSystemComponentPool.Get(e);
-                psComponent.ParticleSystem.Play();
+                ref var tComponent = ref _sAspect.TransformComponentPool.Get(e);
+
+                var settings = new ParticleSystemPoolSpawnSettings
+                {
+                    At = (Vector2) tComponent.Transform.position + _ppConfig.PlayerOffset
+                };
+                _sppsPool.Spawn(settings: settings);
             }
         }
     }
