b65c8c52694fbf874af2d0866eb1bc2ab05f679a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 03:22:16 +0300

    Add "_playerOffset" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Configs/PlatformParticlesConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Configs/PlatformParticlesConfig.cs
@@ -8,9 +8,11 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Configs
         [SerializeField] private GameObject _positionPrefab;
         [SerializeField] private GameObject _rotationPrefab;
         [SerializeField] private GameObject _scalePrefab;
+        [SerializeField] private Vector2 _playerOffset = new (0, 2);
         
         public GameObject PositionPrefab => _positionPrefab;
         public GameObject RotationPrefab => _rotationPrefab;
         public GameObject ScalePrefab => _scalePrefab;
+        public Vector2 PlayerOffset => _playerOffset;
     }
 }
