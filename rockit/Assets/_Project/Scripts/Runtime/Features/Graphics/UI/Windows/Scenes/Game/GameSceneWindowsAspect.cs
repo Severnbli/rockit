6506af308c007769fb7f7fbabd33ef3b3312c062
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 22:10:14 +0300

    Create PauseTag with authoring attributes, add it pool to GameSceneWindowsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Game/GameSceneWindowsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Game/GameSceneWindowsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game
 {
     public sealed class GameSceneWindowsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<PauseTag> PauseTagPool;
     }
 }
