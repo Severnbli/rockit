989d2364ab4dbd4ed36753641ec7506ef2939765
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 16:37:44 +0300

    Create ActivateCheckpointRequest, add it pool and iterator to CheckpointsRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Checkpoints/CheckpointsRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Checkpoints/CheckpointsRequestsAspect.cs
@@ -1,9 +1,14 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using _Project.Scripts.Runtime.Features.World.Checkpoints.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.World.Checkpoints
 {
     public sealed class CheckpointsRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<ActivateCheckpointRequest> ActivateCheckpointRequestPool;
+        public readonly ProtoIt ActivateCheckpointRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, ActivateCheckpointRequest>());
     }
 }
