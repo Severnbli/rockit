d2da675580570ffa289f06500c6bfceeee8f6420
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 22:57:09 +0300

    Fix all places that directly uses camera switch awaiter: use camera switcher instead

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/MenuState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Menu/Active/MenuState.cs
@@ -1,10 +1,6 @@
-﻿using System.Threading;
-using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
-using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
-using _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests;
-using _Project.Scripts.Runtime.Features.Graphics.Cameras.Types;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Tools.Switcher;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Menu.Monos;
-using _Project.Scripts.Runtime.Shared.Utils.Features.Graphics;
 using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active
@@ -13,34 +9,19 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Menu.Active
     {
         private readonly MenuWindow _mWindow;
         private readonly MenusCamera _mCamera;
-        private readonly RequestsAspect _rAspect;
-        private readonly ICameraSwitchAwaiter _csAwaiter;
-        private readonly CancellationToken _ct;
+        private readonly ICameraSwitcher _cSwitcher;
 
-        public MenuState(MenuWindow mWindow, MenusCamera mCamera, RequestsAspect rAspect,
-            ICameraSwitchAwaiter csAwaiter, CancellationToken ct)
+        public MenuState(MenuWindow mWindow, MenusCamera mCamera, ICameraSwitcher cSwitcher)
         {
             _mWindow = mWindow;
             _mCamera = mCamera;
-            _rAspect = rAspect;
-            _csAwaiter = csAwaiter;
-            _ct = ct;
+            _cSwitcher = cSwitcher;
         }
 
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
-            SwitchToMenuCamera();
-            await _csAwaiter.AwaitSwitch(_ct);
+            await _cSwitcher.SwitchTo(_mCamera.Camera);
             await _mWindow.OpenAwait();
-        }
-
-        private void SwitchToMenuCamera()
-        {
-            var prepared = new SwitchCameraRequest
-            {
-                Target = _mCamera.Camera
-            };
-            CamerasUtils.CreateSwitchCameraRequest(_rAspect, prepared);
         }
 
         public async UniTask OnLeave(IStateMachine stateMachine)
