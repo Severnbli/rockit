c206b1be5a86dcba88502fab085da9637806658b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 16:39:40 +0300

    Create CheckpointsSceneModule, register it to GameSceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Checkpoints/CheckpointsSceneModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World.Checkpoints
+{
+    public sealed class CheckpointsSceneModule : BaseModule<CheckpointsSceneModule>
+    {
+        public CheckpointsSceneModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
