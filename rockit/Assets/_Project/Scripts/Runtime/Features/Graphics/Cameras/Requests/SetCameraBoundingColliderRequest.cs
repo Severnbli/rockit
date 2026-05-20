63642080442d1007b72ab025743e7b5ea9f05640
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 23:51:15 +0300

    Create SetCameraBoundingColliderRequest, add it pool and iterator to CamerasRequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Requests/SetCameraBoundingColliderRequest.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests
+{
+    public struct SetCameraBoundingColliderRequest
+    {
+        public CinemachineCamera Camera;
+        public Collider2D Collider;
+    }
+}
