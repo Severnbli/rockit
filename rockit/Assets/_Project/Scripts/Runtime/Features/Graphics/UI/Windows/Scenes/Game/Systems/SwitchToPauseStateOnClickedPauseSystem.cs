3e1b24fff9ce73895f990d41085233ac5c116127
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 22:17:52 +0300

    Create SwitchToPauseStateOnClickedPauseSystem, bind it to GameSceneWindowsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Game/Systems/SwitchToPauseStateOnClickedPauseSystem.cs
@@ -0,0 +1,25 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.States;
+using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active;
+using Cysharp.Threading.Tasks;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Systems
+{
+    public sealed class SwitchToPauseStateOnClickedPauseSystem : IProtoRunSystem
+    {
+        [DI] private readonly GameSceneWindowsAspect _gswAspect;
+        private readonly IStateMachine _sMachine;
+
+        public SwitchToPauseStateOnClickedPauseSystem(IStateMachine sMachine)
+        {
+            _sMachine = sMachine;
+        }
+
+        public void Run()
+        {
+            if (_gswAspect.ClickedPauses.IsEmptySlow()) return;
+            _sMachine.ChangeState<PauseState>().Forget();
+        }
+    }
+}
