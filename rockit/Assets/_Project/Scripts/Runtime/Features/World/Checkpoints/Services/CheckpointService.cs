18632e6c14963f0ad8909cde8cdf75be5cb41a43
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 16:32:49 +0300

    Create CheckpointService with only field "LastCheckpoint"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Checkpoints/Services/CheckpointService.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Runtime.Features.World.Checkpoints.Monos;
+
+namespace _Project.Scripts.Runtime.Features.World.Checkpoints.Services
+{
+    public sealed class CheckpointService
+    {
+        public Checkpoint LastCheckpoint;
+    }
+}
