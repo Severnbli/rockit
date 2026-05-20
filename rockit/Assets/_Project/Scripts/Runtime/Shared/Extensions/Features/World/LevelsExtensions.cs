1aff4b95fb88757898f47fabafb5c65698413700
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-21 01:37:46 +0300

    Create LevelsExtensions with only method "Reset"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Extensions/Features/World/LevelsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts.Runtime.Features.World.Levels;
+using _Project.Scripts.Runtime.Features.World.Levels.Services;
+
+namespace _Project.Scripts.Runtime.Shared.Extensions.Features.World
+{
+    public static class LevelsExtensions
+    {
+        public static void Reset(this LevelsService lService)
+        {
+            lService.CurrLevel = null;
+            lService.CurrLevelId = ProjectContracts.NullIntId;
+            lService.UsedTransforms = LevelsContracts.MinUsedTransforms;
+        }
+    }
+}
