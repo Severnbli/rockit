6cbaeb0294621965944002d1982e6ef261d261a5
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 16:25:22 +0300

    Create CheckpointComponent with authoring attributes and only field "Checkpoint", add it pool to CheckpointsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Checkpoints/CheckpointsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Checkpoints/CheckpointsAspect.cs
@@ -1,9 +1,11 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.World.Checkpoints.Components;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.World.Checkpoints
 {
     public sealed class CheckpointsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<CheckpointComponent> CheckpointComponentPool;
     }
 }
