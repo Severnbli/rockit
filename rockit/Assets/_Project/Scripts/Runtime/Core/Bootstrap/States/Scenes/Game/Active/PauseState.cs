e3ad9265b18ef946ac874cd6d33506b77d284dfc
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 22:58:38 +0300

    Implement logic to switch to menus camera at pause state "OnEnter"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/PauseState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/PauseState.cs
@@ -1,4 +1,6 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Monos;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Tools.Switcher;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Monos;
 using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
@@ -6,17 +8,22 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
     public class PauseState : ISceneState
     {
         private readonly PauseWindow _pWindow;
+        private readonly MenusCamera _mCamera;
+        private readonly ICameraSwitcher _cSwitcher;
 
-        public PauseState(PauseWindow pWindow)
+        public PauseState(PauseWindow pWindow, MenusCamera mCamera, ICameraSwitcher cSwitcher)
         {
             _pWindow = pWindow;
+            _mCamera = mCamera;
+            _cSwitcher = cSwitcher;
         }
 
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
+            await _cSwitcher.SwitchTo(_mCamera.Camera);
             await _pWindow.OpenAwait();
         }
-
+        
         public async UniTask OnLeave(IStateMachine stateMachine)
         {
             await _pWindow.CloseAwait();
