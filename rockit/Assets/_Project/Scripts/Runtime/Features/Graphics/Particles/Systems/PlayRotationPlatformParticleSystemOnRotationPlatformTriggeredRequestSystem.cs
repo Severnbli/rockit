e2598210aa606694a8349f744a9781ca0096b9a8
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 03:24:50 +0300

    Update PlayRotationPlatformParticleSystemOnRotationPlatformTriggeredRequestSystem to pool usage

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Systems/PlayRotationPlatformParticleSystemOnRotationPlatformTriggeredRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Systems/PlayRotationPlatformParticleSystemOnRotationPlatformTriggeredRequestSystem.cs
@@ -1,23 +1,34 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Shared;
+using _Project.Scripts.Runtime.Features.Graphics.Particles.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.Particles.Types;
 using _Project.Scripts.Runtime.Features.Platforms.Shared;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
+using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Systems
 {
     public sealed class PlayRotationPlatformParticleSystemOnRotationPlatformTriggeredRequestSystem : IProtoRunSystem
     {
         [DIRequests] private readonly PlatformsSharedRequestsAspect _psrAspect;
-        [DI] private readonly ParticlesAspect _pAspect;
+        [DI] private readonly SharedAspect _sAspect;
+        private readonly RotationPlatformParticleSystemPool _rppsPool;
+        private readonly PlatformParticlesConfig _ppConfig;
         
         public void Run()
         {
             if (_psrAspect.RotationPlatformTriggeredRequests.IsEmptySlow()) return;
 
-            foreach (var e in _pAspect.RotationPlatformParticleSystems)
+            foreach (var e in _sAspect.TransformPlayers)
             {
-                ref var psComponent = ref _pAspect.ParticleSystemComponentPool.Get(e);
-                psComponent.ParticleSystem.Play();
+                ref var tComponent = ref _sAspect.TransformComponentPool.Get(e);
+
+                var settings = new ParticleSystemPoolSpawnSettings
+                {
+                    At = (Vector2) tComponent.Transform.position + _ppConfig.PlayerOffset
+                };
+                _rppsPool.Spawn(settings: settings);
             }
         }
     }
