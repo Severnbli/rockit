fef05f81b5b86cff2f12c5c5c3a549fa3a258a63
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 18:51:04 +0300

    Add "_rAspect" field, initialize it from contructor, create place player to last checkpoint request at "OnEnter"

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/VoidState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/VoidState.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Threading;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Configs;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Monos;
+using _Project.Scripts.Runtime.Shared.Utils.Features;
 using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
@@ -11,17 +13,22 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
         private readonly VoidWindow _vWindow;
         private readonly VoidWindowConfig _vwConfig;
         private readonly CancellationToken _ct;
+        private readonly RequestsAspect _rAspect;
 
-        public VoidState(VoidWindow vWindow, VoidWindowConfig vwConfig, CancellationToken ct)
+        public VoidState(VoidWindow vWindow, VoidWindowConfig vwConfig, CancellationToken ct, RequestsAspect rAspect)
         {
             _vWindow = vWindow;
             _vwConfig = vwConfig;
             _ct = ct;
+            _rAspect = rAspect;
         }
 
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
             await _vWindow.OpenAwait();
+            
+            PlayerUtils.CreatePlacePlayerToLastCheckpointRequest(_rAspect);
+            
             await UniTask.Delay(TimeSpan.FromSeconds(_vwConfig.Duration), cancellationToken: _ct);
         }
 
