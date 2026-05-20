745e930bd20414d73d0c2ad6382b8f97748bfca1
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 23:21:53 +0300

    Move game scene dependable systems binding into PlayerGameSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Player/PlayerGameSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Player/PlayerGameSceneModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Player.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Player
 {
@@ -6,6 +7,15 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Player
     {
         public PlayerGameSceneModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            
+            BindSystem<SendPlayerTriggeredVoidOnTriggerEnterEventSystem>();
+            BindSystem<SendPlacePlayerRequestOnPlacePlayerToLastCheckpointRequestSystem>();
         }
     }
 }
