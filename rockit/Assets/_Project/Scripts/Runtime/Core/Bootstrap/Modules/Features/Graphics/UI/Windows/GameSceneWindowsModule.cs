039fe386f20bfac44f61f4cec9450d669b733a91
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 22:12:21 +0300

    Create GameSceneWindowsModule, register it to GameSceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/Windows/GameSceneWindowsModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI.Windows
+{
+    public sealed class GameSceneWindowsModule : BaseModule<GameSceneWindowsModule>
+    {
+        public GameSceneWindowsModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
