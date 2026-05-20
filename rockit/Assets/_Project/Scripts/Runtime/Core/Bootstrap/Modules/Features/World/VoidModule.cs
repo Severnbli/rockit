1c7d45a770315dd9992850338a7640c78c1aff0d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 18:28:58 +0300

    Create VoidModule, register it to GameSceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/VoidModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World
+{
+    public sealed class VoidModule : BaseModule<VoidModule>
+    {
+        public VoidModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
