8c8babe3991dd4295bdb3c054b88a994fa3ad92b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 22:10:53 +0300

    Add "ClickedPauses" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Game/GameSceneWindowsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Game/GameSceneWindowsAspect.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Tags;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Shared.Tags;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Tags;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -7,5 +8,6 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game
     public sealed class GameSceneWindowsAspect : ProtoAspectInject
     {
         public readonly ProtoPool<PauseTag> PauseTagPool;
+        public readonly ProtoIt ClickedPauses = new (It.Inc<ClickedTag, PauseTag>());
     }
 }
