e463399a60cb64f983a245c08bb9a785cf2a4231
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 16:13:57 +0300

    Create CheckpointsAnimationsConfig with "_activatedBool" field and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Checkpoints/Configs/CheckpointsAnimationsConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Checkpoints.Configs
+{
+    public sealed class CheckpointsAnimationsConfig : ScriptableObjectAutoInstaller<CheckpointsAnimationsConfig>
+    {
+        [SerializeField] private string _activatedBool = "Activated";
+        
+        public int ActivatedBool => Animator.StringToHash(_activatedBool);
+    }
+}
