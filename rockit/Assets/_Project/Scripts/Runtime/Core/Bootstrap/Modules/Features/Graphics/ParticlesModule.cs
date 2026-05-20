0c0092878d21e791e351a5690dd3e564b8d3ee8d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 03:17:08 +0300

    Create DespawnRotationPlatformParticleSystemOnNotAliveSystem, bind it to ParticlesModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/ParticlesModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/ParticlesModule.cs
@@ -28,6 +28,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics
             BindSystem<PlayScalePlatformParticleSystemOnScalePlatformTriggeredRequestSystem>();
             
             BindSystem<DespawnPositionPlatformParticleSystemOnNotAliveSystem>();
+            BindSystem<DespawnRotationPlatformParticleSystemOnNotAliveSystem>();
         }
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Systems/DespawnRotationPlatformParticleSystemOnNotAliveSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Particles.Types;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Systems
+{
+    public sealed class DespawnRotationPlatformParticleSystemOnNotAliveSystem : IProtoRunSystem
+    {
+        [DI] private readonly ParticlesAspect _pAspect;
+        private readonly RotationPlatformParticleSystemPool _rppsPool;
+
+        public DespawnRotationPlatformParticleSystemOnNotAliveSystem(RotationPlatformParticleSystemPool rppsPool)
+        {
+            _rppsPool = rppsPool;
+        }
+
+        public void Run()
+        {
+            foreach (var e in _pAspect.ActiveRotationPlatformParticleSystems)
+            {
+                ref var psComponent = ref _pAspect.ParticleSystemComponentPool.Get(e);
+                if (psComponent.ParticleSystem.IsAlive()) continue;
+                
+                _rppsPool.Despawn(psComponent.ParticleSystem);
+            }
+        }
+    }
+}
