06cbe7b5de262fd189400cf7e898a597d2191e7a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 16:55:01 +0300

    Bind CheckpointsService to CheckpointsSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Checkpoints/CheckpointsSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/Checkpoints/CheckpointsSceneModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.World.Checkpoints.Services;
 using _Project.Scripts.Runtime.Features.World.Checkpoints.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World.Checkpoints
@@ -7,6 +8,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World.Checkpo
     {
         public CheckpointsSceneModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindServices()
+        {
+            base.BindServices();
+            
+            BindService<CheckpointsService>();
         }
 
         protected override void BindSystems()
