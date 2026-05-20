295a1115623519326291e71c8498071c79e56ecb
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 17:22:20 +0300

    Create CheckpointsAnimationsModule, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Animations/CheckpointsAnimationsModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Animations
+{
+    public sealed class CheckpointsAnimationsModule : BaseModule<CheckpointsAnimationsModule>
+    {
+        public CheckpointsAnimationsModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
