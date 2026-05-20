8e880d3e3be54c3245d38c0142967a7fa0e14cfa
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 22:09:25 +0300

    Create GameSceneWindowsAspect, add it to WindowsSharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Game/GameSceneWindowsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game
+{
+    public sealed class GameSceneWindowsAspect : ProtoAspectInject
+    {
+        
+    }
+}
