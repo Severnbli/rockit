74dc54129d7867042f94af0e9b613c4d2ac62967
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-21 01:14:49 +0300

    Move platfrom input profile specification from Level to LevelDefinition

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Monos/Level.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Monos/Level.cs
@@ -1,16 +1,13 @@
-﻿using _Project.Scripts.Runtime.Features.Input.Types;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.World.Levels.Monos
 {
     public class Level : MonoBehaviour
     {
         [SerializeField] private GameObject _psPosition;
-        [SerializeField] private PlatformsInputProfile _piProfile;
         [SerializeField] private Collider2D _cameraBounds;
         
         public GameObject PsPosition => _psPosition;
-        public PlatformsInputProfile PiProfile => _piProfile;
         public Collider2D CameraBounds => _cameraBounds;
     }
 }
