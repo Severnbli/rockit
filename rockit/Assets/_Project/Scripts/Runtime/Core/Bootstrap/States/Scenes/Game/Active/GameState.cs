2b094f0547584aeac7342e7e4ee1cbb0fd88925a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 22:57:09 +0300

    Fix all places that directly uses camera switch awaiter: use camera switcher instead

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/GameState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/GameState.cs
@@ -1,10 +1,7 @@
-﻿using System.Threading;
-using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
 using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
-using _Project.Scripts.Runtime.Features.Graphics.Cameras.Requests;
-using _Project.Scripts.Runtime.Features.Graphics.Cameras.Types;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Tools.Switcher;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Monos;
-using _Project.Scripts.Runtime.Shared.Utils.Features.Graphics;
 using _Project.Scripts.Runtime.Shared.Utils.Features.Input;
 using Cysharp.Threading.Tasks;
 
@@ -14,39 +11,25 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
     {
         private readonly RequestsAspect _rAspect;
         private readonly PlayerCamera _pCamera;
-        private readonly ICameraSwitchAwaiter _csAwaiter;
-        private readonly CancellationToken _ct;
         private readonly GameWindow _gWindow;
+        private readonly ICameraSwitcher _cSwitcher;
 
-        public GameState(RequestsAspect rAspect, PlayerCamera pCamera, ICameraSwitchAwaiter csAwaiter, 
-            CancellationToken ct, GameWindow gWindow)
+        public GameState(RequestsAspect rAspect, PlayerCamera pCamera, GameWindow gWindow, ICameraSwitcher cSwitcher)
         {
             _rAspect = rAspect;
             _pCamera = pCamera;
-            _csAwaiter = csAwaiter;
-            _ct = ct;
             _gWindow = gWindow;
+            _cSwitcher = cSwitcher;
         }
 
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
-            SwitchToPlayerCamera();
-            await _csAwaiter.AwaitSwitch(_ct);
+            await _cSwitcher.SwitchTo(_pCamera.Camera);
             
             PlayerInputUtils.CreateEnableRequest(_rAspect);
             PlatformsInputUtils.CreateEnableRequest(_rAspect);
             
             await _gWindow.OpenAwait();
-        }
-
-        private void SwitchToPlayerCamera()
-        {
-            var prepared = new SwitchCameraRequest
-            {
-                Target = _pCamera.Camera
-            };
-            
-            CamerasUtils.CreateSwitchCameraRequest(_rAspect, prepared);
         }
 
         public async UniTask OnLeave(IStateMachine stateMachine)
