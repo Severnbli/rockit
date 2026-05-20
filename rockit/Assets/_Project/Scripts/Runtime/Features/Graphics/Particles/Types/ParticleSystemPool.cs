1b4f1971ae0acc61203c9eda7056867f95d9c45a
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 03:06:02 +0300

    Define "ConfigureEntityOnSpawn" and "ConfigureEntityOnDespawn" with managing "ActiveParticleSystemTag"

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Types/ParticleSystemPool.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Particles/Types/ParticleSystemPool.cs
@@ -1,7 +1,9 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
 using _Project.Scripts.Runtime.Features.Graphics.Particles.Monos;
+using _Project.Scripts.Runtime.Features.Graphics.Particles.Tags;
 using _Project.Scripts.Runtime.Shared.Extensions.Shared;
 using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 using UnityEngine;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Types
@@ -21,6 +23,22 @@ namespace _Project.Scripts.Runtime.Features.Graphics.Particles.Types
             
             instance.transform.PlaceTo(settings.At);
             instance.Play();
+        }
+
+        protected override void ConfigureEntityOnSpawn(ParticleSystem instance, ProtoEntity entity,
+            ParticleSystemPoolSpawnSettings settings = default)
+        {
+            base.ConfigureEntityOnSpawn(instance, entity, settings);
+
+            Pool<ActiveParticleSystemTag>().GetOrAdd(entity);
+        }
+
+        protected override void ConfigureEntityOnDespawn(ParticleSystem instance, ProtoEntity entity,
+            ParticleSystemPoolDespawnSettings settings = default)
+        {
+            base.ConfigureEntityOnDespawn(instance, entity, settings);
+            
+            Pool<ActiveParticleSystemTag>().DelIfExists(entity);
         }
 
         protected override Transform FallbackContainer() => PsContainer.GetContainer();
