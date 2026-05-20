3e5e7af34e84569dc1d3d7c60b5a587190fdb12f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-21 00:06:47 +0300

    Create SetPlayerCameraConfinerBoundingShapeOnLevelSpawnedRequestSystem, bind it to CamerasModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Cameras/Systems/SetPlayerCameraConfinerBoundingShapeOnLevelSpawnedRequestSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
+using _Project.Scripts.Runtime.Features.World.Levels;
+using _Project.Scripts.Runtime.Shared.Extensions.Features.Graphics;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Cameras.Systems
+{
+    public sealed class SetPlayerCameraConfinerBoundingShapeOnLevelSpawnedRequestSystem : IProtoRunSystem
+    {
+        [DI] private readonly LevelsRequestsAspect _lrAspect;
+        private readonly PlayerCamera _pCamera;
+
+        public SetPlayerCameraConfinerBoundingShapeOnLevelSpawnedRequestSystem(PlayerCamera pCamera)
+        {
+            _pCamera = pCamera;
+        }
+
+        public void Run()
+        {
+            var (e, ok) = _lrAspect.LevelSpawnedRequests.FirstSlow();
+            if (!ok) return;
+
+            ref var lsRequest = ref _lrAspect.LevelSpawnedRequestPool.Get(e);
+            _pCamera.Confiner.SetBoundingShape(lsRequest.Level.CameraBounds);
+        }
+    }
+}
