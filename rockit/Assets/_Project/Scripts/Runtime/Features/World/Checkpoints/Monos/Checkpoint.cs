5cc493c056a7e40d5c06a0e2cfd6cdd369e4b877
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-20 16:18:40 +0300

    Create Checkpoint that inherited from MonoBehaviour with "_checkLocation" field and "GetCheckLocation" method

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Checkpoints/Monos/Checkpoint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.World.Checkpoints.Monos
+{
+    public class Checkpoint : MonoBehaviour
+    {
+        [SerializeField] private GameObject _checkLocation;
+        
+        public Vector2 GetCheckLocation() => _checkLocation.transform.position;
+    }
+}
