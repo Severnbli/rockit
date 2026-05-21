22525772e3eb97722c597ffafbbe79b82613e266
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:22:35 +0300

    Create SendTeleporterTriggeredRequestOnPlayerTriggerEnterRequestSystem, bind it to TeleportersModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/TeleportersModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/TeleportersModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.World.Teleporters.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World
     {
         public TeleportersModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<SendTeleporterTriggeredRequestOnPlayerTriggerEnterRequestSystem>();
         }
     }
 }
