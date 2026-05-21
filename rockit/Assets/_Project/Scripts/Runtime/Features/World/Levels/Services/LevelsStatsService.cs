63489b2ca1ffb0beec8ce712aeafe989478b9e36
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-21 15:21:46 +0300

    Add "CachedCollectedCoins" field, specify its default value as "new ()". Update levels extension "Reset" method to clear it

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Services/LevelsStatsService.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Services/LevelsStatsService.cs
@@ -1,9 +1,13 @@
-﻿namespace _Project.Scripts.Runtime.Features.World.Levels.Services
+﻿using System.Collections.Generic;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities.Economy;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels.Services
 {
     public sealed class LevelsStatsService
     {
         public int UsedTransforms;
         public int RemainTransforms;
         public int StarsScore;
+        public Dictionary<int, CoinData> CachedCollectedCoins = new ();
     }
 }
