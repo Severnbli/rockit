0154b4d1258308a273d6893bdb657881525686df
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:51:41 +0300

    Create SendEnablePlayerRequestOnLevelSpawnedRequestSystem, bind it to PlayerGameSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Player/PlayerGameSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Player/PlayerGameSceneModule.cs
@@ -16,6 +16,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Player
             
             BindSystem<SendPlayerTriggeredVoidOnTriggerEnterEventSystem>();
             BindSystem<SendPlacePlayerRequestOnPlacePlayerToLastCheckpointRequestSystem>();
+            BindSystem<SendEnablePlayerRequestOnLevelSpawnedRequestSystem>();
         }
     }
 }
