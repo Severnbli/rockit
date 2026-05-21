9636376d418d0a2d4a79d5b5360c24933dcb5632
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:43:35 +0300

    Create SendTeleporterActivateTriggerOnTeleporterTriggeredRequestSystem, bind it to TeleportersAnimationsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Animations/TeleportersAnimationsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Animations/TeleportersAnimationsModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Graphics.Animations.Teleporters.Systems;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Animations
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Anim
     {
         public TeleportersAnimationsModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<SendTeleporterActivateTriggerOnTeleporterTriggeredRequestSystem>();
         }
     }
 }
