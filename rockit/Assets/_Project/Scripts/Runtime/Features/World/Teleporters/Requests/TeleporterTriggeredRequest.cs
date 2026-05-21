8bda258e9fe84811de0c581e4ab3c1d78a45e4e4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:12:03 +0300

    Create TeleporterTriggeredRequest, add it pool and iterator to TeleportersRequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Teleporters/Requests/TeleporterTriggeredRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.Runtime.Features.World.Teleporters.Requests
+{
+    public struct TeleporterTriggeredRequest
+    {
+        
+    }
+}
--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Teleporters/TeleportersRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Teleporters/TeleportersRequestsAspect.cs
@@ -1,9 +1,14 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Components;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.Tags;
+using _Project.Scripts.Runtime.Features.World.Teleporters.Requests;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.World.Teleporters
 {
     public sealed class TeleportersRequestsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<TeleporterTriggeredRequest> TeleporterTriggeredRequestPool;
+        public readonly ProtoIt TeleporterTriggeredRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, TeleporterTriggeredRequest>());
     }
 }
