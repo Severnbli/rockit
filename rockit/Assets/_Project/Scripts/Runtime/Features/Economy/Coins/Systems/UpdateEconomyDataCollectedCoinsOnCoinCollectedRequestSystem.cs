33a3200b043f50fdb8081795a2cc94b3698102ba
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-21 14:46:14 +0300

    Create UpdateEconomyDataCollectedCoinsOnCoinCollectedRequestSystem, bind it to CoinsProjectModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Systems/UpdateEconomyDataCollectedCoinsOnCoinCollectedRequestSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage;
+using _Project.Scripts.Runtime.Core.Infrastructure.Storage.Entities.Economy;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Economy.Coins.Systems
+{
+    public sealed class UpdateEconomyDataCollectedCoinsOnCoinCollectedRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly CoinsRequestsAspect _crAspect;
+        private readonly DataProvider _dProvider;
+
+        public UpdateEconomyDataCollectedCoinsOnCoinCollectedRequestSystem(DataProvider dProvider)
+        {
+            _dProvider = dProvider;
+        }
+
+        public void Run()
+        {
+            if (_crAspect.CoinCollectedRequests.IsEmptySlow()) return;
+
+            var dict = _dProvider.EconomyData.CollectedCoins;
+            foreach (var e in _crAspect.CoinCollectedRequests)
+            {
+                ref var ccRequest = ref _crAspect.CoinCollectedRequestPool.Get(e);
+                dict.TryAdd(ccRequest.CoinId, new CoinData());
+            }
+            _dProvider.SaveTracked();
+        }
+    }
+}
