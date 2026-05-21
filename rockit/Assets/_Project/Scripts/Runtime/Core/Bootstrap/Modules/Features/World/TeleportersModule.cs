c0665e92c333f2ddece3e5210a80c956bfb35922
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:16:16 +0300

    Create TeleportersModule, register it to GameSceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/World/TeleportersModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.World
+{
+    public sealed class TeleportersModule : BaseModule<TeleportersModule>
+    {
+        public TeleportersModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
