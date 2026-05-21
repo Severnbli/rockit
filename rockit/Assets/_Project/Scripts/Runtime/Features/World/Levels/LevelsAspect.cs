38749ae908e794f67f448988e1c0f310df9f04a4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-21 03:22:35 +0300

    Create TransformsStatsTag with authoring attributes, add it pool and iterator to LevelsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/LevelsAspect.cs
@@ -1,4 +1,6 @@
-﻿using _Project.Scripts.Runtime.Features.World.Levels.Components;
+﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Text.Shared.Components;
+using _Project.Scripts.Runtime.Features.World.Levels.Components;
+using _Project.Scripts.Runtime.Features.World.Levels.Tags;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
@@ -7,5 +9,8 @@ namespace _Project.Scripts.Runtime.Features.World.Levels
     public sealed class LevelsAspect : ProtoAspectInject
     {
         public readonly ProtoPool<LevelItemComponent> LevelItemComponentPool;
+        
+        public readonly ProtoPool<TransformsStatsTag> TransformsStatsTagPool;
+        public readonly ProtoIt TransformsStatsTextUis = new (It.Inc<TransformsStatsTag, TextUiComponent>());
     }
 }
