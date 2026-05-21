719d9852cfc726e312adbcb018b643d9b1db85cb
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:32:39 +0300

    Create TeleportersAnimationsModule, register it to SceneDomain

---
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/Animations/TeleportersAnimationsModule.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+
+namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.Animations
+{
+    public sealed class TeleportersAnimationsModule : BaseModule<TeleportersAnimationsModule>
+    {
+        public TeleportersAnimationsModule(IDomain domain) : base(domain)
+        {
+        }
+    }
+}
