ecc98730624b8cb5ab499a9fffca89f8b56c7b4b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:14:27 +0300

    Create TeleportersUtils with only method "CreateTeleporterTriggeredRequest"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/World/TeleportersUtils.cs
@@ -0,0 +1,17 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.World
+{
+    public static class TeleportersUtils
+    {
+        public static ProtoEntity CreateTeleporterTriggeredRequest(RequestsAspect aspect,
+            ProtoPackedEntityWithWorld packed)
+        {
+            return aspect.CreateRequest(
+                aspect.WorldSharedRequestsAspect.TeleportersRequestsAspect.TeleporterTriggeredRequestPool, packed);
+        }
+    }
+}
