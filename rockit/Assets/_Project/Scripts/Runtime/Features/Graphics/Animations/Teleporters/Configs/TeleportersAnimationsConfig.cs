a5353013672fcbecc4cfc0dc70fab9bf7a8f54d0
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:25:46 +0300

    Create TeleportersAnimationsConfig with only field "_activateTrigger" with its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Teleporters/Configs/TeleportersAnimationsConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Teleporters.Configs
+{
+    public sealed class TeleportersAnimationsConfig : ScriptableObjectAutoInstaller<TeleportersAnimationsConfig>
+    {
+        [SerializeField] private string _activateTrigger = "Activate";
+        
+        public int ActivateTrigger => Animator.StringToHash(_activateTrigger);
+    }
+}
