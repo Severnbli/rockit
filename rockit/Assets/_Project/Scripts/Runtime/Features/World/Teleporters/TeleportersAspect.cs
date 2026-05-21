9d1cd1559a110717529801d6a873191f8b20deb0
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:08:56 +0300

    Create TeleporterTag, add it pool and iterator to TeleportersAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/World/Teleporters/TeleportersAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Teleporters/TeleportersAspect.cs
@@ -1,9 +1,12 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.World.Teleporters.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.World.Teleporters
 {
     public sealed class TeleportersAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoPool<TeleporterTag> TeleporterTagPool;
+        public readonly ProtoIt Teleportes = new (It.Inc<TeleporterTag>());
     }
 }
