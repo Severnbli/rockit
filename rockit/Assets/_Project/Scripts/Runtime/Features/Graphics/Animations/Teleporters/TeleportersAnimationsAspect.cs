0b1f34c2c49adadc4a24b4202ff1bf93b0422b77
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:23:43 +0300

    Add "TeleporterAnimators" iterator

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Teleporters/TeleportersAnimationsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Animations/Teleporters/TeleportersAnimationsAspect.cs
@@ -1,9 +1,12 @@
-﻿using Leopotam.EcsProto.QoL;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Animations.Shared.Components;
+using _Project.Scripts.Runtime.Features.World.Teleporters.Tags;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.Animations.Teleporters
 {
     public sealed class TeleportersAnimationsAspect : ProtoAspectInject
     {
-        
+        public readonly ProtoIt TeleporterAnimators = new (It.Inc<TeleporterTag, AnimatorComponent>());
     }
 }
