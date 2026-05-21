e7ececb32b281ba8d1b65f6d087eb41913ab3fa6
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:53:32 +0300

    Create SendDisablePlayerRequestOnLevelCompletedRequestSystem, bind it to PlayerGameSceneModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Player/PlayerGameSceneModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Player/PlayerGameSceneModule.cs
@@ -17,6 +17,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Player
             BindSystem<SendPlayerTriggeredVoidOnTriggerEnterEventSystem>();
             BindSystem<SendPlacePlayerRequestOnPlacePlayerToLastCheckpointRequestSystem>();
             BindSystem<SendEnablePlayerRequestOnLevelSpawnedRequestSystem>();
+            BindSystem<SendDisablePlayerRequestOnLevelCompletedRequestSystem>();
         }
     }
 }
