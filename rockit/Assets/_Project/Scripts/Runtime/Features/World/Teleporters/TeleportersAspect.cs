8d06e0af765ab6dff08d3c530526b97eda70928e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:07:18 +0300

    Create TeleportersAspect, add it to WorldSharedAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Teleporters/TeleportersAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.World.Teleporters
+{
+    public sealed class TeleportersAspect : ProtoAspectInject
+    {
+        
+    }
+}
