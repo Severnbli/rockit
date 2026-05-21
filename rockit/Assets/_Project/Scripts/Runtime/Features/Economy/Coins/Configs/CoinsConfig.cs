10b95312fdf07d3a6181e82cd48b716bd63fdfbe
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-21 14:16:54 +0300

    Create CoinsConfig with only field "_coinsPerCollectedCoin" with default value "1" and property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Economy/Coins/Configs/CoinsConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Economy.Coins.Configs
+{
+    public sealed class CoinsConfig : ScriptableObjectAutoInstaller<CoinsConfig>
+    {
+        [SerializeField] private int _coinsPerCollectedCoin = 1;
+        
+        public int CoinsPerCollectedCoin => _coinsPerCollectedCoin;
+    }
+}
