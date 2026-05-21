2b3f5829eb1ddee79f0b8541a3b623f5b7c1af1d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 01:47:07 +0300

    Create SendLevelCompletedRequestOnTeleporterTriggeredRequestSystem, bind it to LevelsSceneModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Systems/SendLevelCompletedRequestOnTeleporterTriggeredRequestSystem.cs
@@ -0,0 +1,21 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.World.Teleporters;
+using _Project.Scripts.Runtime.Shared.Utils.Features.World;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels.Systems
+{
+    public sealed class SendLevelCompletedRequestOnTeleporterTriggeredRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly RequestsAspect _rAspect;
+        [DIRequests] private readonly TeleportersRequestsAspect _trAspect;
+        
+        public void Run()
+        {
+            if (_trAspect.TeleporterTriggeredRequests.IsEmptySlow()) return;
+
+            LevelsUtils.CreateLevelCompletedRequest(_rAspect);
+        }
+    }
+}
