f2e4af2387f73046d3e2a1d77d20c9c24a866271
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 03:21:14 +0300

    Create LevelCompletedWindowConfig with "_starsAppear", "_coinsAppear", "_controlsAppear" and "_controlsDisappear" fields and they property getters

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Game/Configs/LevelCompletedWindowConfig.cs
@@ -0,0 +1,19 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline.Core;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Configs;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Configs
+{
+    public class LevelCompletedWindowConfig : FadeWindowConfig
+    {
+        [SerializeField] private TweenPipeline _starsAppear;
+        [SerializeField] private TweenPipeline _coinsAppear;
+        [SerializeField] private TweenPipeline _controlsAppear;
+        [SerializeField] private TweenPipeline _controlsDisappear;
+        
+        public TweenPipeline StarsAppear => _starsAppear;
+        public TweenPipeline CoinsAppear => _coinsAppear;
+        public TweenPipeline ControlsAppear => _controlsAppear;
+        public TweenPipeline ControlsDisappear => _controlsDisappear;
+    }
+}
