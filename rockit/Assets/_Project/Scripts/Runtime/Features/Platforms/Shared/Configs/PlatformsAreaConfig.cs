ef3cf5cdfec7d32d715f43d3a821f8dbf586ca7c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 22:51:03 +0300

    Create PlatformsAreaConfig with only field "_radius" with default value "4f" and property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Configs/PlatformsAreaConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Configs
+{
+    public sealed class PlatformsAreaConfig : ScriptableObjectAutoInstaller<PlatformsAreaConfig>
+    {
+        [SerializeField] private float _radius = 4f;
+        
+        public float Radius => _radius;
+    }
+}
