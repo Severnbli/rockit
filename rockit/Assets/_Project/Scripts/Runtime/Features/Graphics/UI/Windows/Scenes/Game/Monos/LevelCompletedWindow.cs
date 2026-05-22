01ad0e956676866297aed584e92242d8d80c0104
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 03:27:27 +0300

    Create LevelCompletedWindow that inherited from MonoFadeWindow with self defined config type as LevelCompletedWindowConfig

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Windows/Scenes/Game/Monos/LevelCompletedWindow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Icons.Monos;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Shared.Monos;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Monos
+{
+    public class LevelCompletedWindow : MonoFadeWindow<LevelCompletedWindowConfig>
+    {
+        [SerializeField] private GameObject _starsContainer;
+        [SerializeField] private GameObject _coinsContainer;
+        [SerializeField] private GameObject _buttons;
+        
+        public GameObject StarsContainer => _starsContainer;
+        public GameObject CoinsContainer => _coinsContainer;
+        public GameObject Buttons => _buttons;
+     
+        public HashSet<AnimatableStarIcon> EarnedStars { get; protected set; } = new ();
+        public HashSet<CoinIcon> CollectedCoins { get; protected set; } = new ();
+
+        protected override async UniTask PlayOpenAnimation()
+        {
+            await base.PlayOpenAnimation();
+            await PlayStarsAnimation();
+            await PlayCoinsAnimation();
+            await TpRunner.Run(Config.ButtonsOpen, _buttons);
+        }
+
+        private async UniTask PlayStarsAnimation()
+        {
+            foreach (var star in EarnedStars)
+            {
+                star.Open();
+                await TpRunner.Run(Config.StarsAppear, star.gameObject);
+            }
+        }
+
+        private async UniTask PlayCoinsAnimation()
+        {
+            foreach (var coin in CollectedCoins)
+            {
+                await TpRunner.Run(Config.CoinsAppear, coin.gameObject);
+            }
+        }
+
+        protected override async UniTask PlayCloseAnimation()
+        {
+            await UniTask.WhenAll(
+                base.PlayCloseAnimation(),
+                TpRunner.Run(Config.ButtonsClose, _buttons)
+            );
+        }
+    }
+}
