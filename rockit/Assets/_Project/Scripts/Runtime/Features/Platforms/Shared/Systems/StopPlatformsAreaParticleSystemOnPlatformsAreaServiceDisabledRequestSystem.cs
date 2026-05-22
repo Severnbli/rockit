e8435fd6deaf438b6f6936d61f30ddd4036c7ddf
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-23 00:01:43 +0300

    Update logic of platforms area particle system stop status installation: move from level completed request observer to platforms area service disabeled request observer. Also update system name

--- rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Systems/StopPlatformsAreaParticleSystemOnPlatformsAreaServiceDisabledRequestSystem.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Systems/StopPlatformsAreaParticleSystemOnPlatformsAreaServiceDisabledRequestSystem.cs
@@ -1,20 +1,19 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
 using _Project.Scripts.Runtime.Features.Graphics.Particles;
-using _Project.Scripts.Runtime.Features.World.Levels;
 using Leopotam.EcsProto;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Systems
 {
-    public sealed class StopPlatformsAreaParticleSystemOnLevelCompletedRequestSystem : IProtoRunSystem
+    public sealed class StopPlatformsAreaParticleSystemOnPlatformsAreaServiceDisabledRequestSystem : IProtoRunSystem
     {
-        [DIRequests] private readonly LevelsRequestsAspect _lrAspect;
+        [DIRequests] private readonly PlatformsSharedRequestsAspect _psrAspect;
         [DI] private readonly PlatformsSharedAspect _psAspect;
         [DI] private readonly ParticlesAspect _pAspect;
         
         public void Run()
         {
-            if (_lrAspect.LevelCompletedRequests.IsEmptySlow()) return;
+            if (_psrAspect.PlatformsAreaServiceDisabledRequests.IsEmptySlow()) return;
 
             foreach (var e in _psAspect.PlatformsAreaParticleSystems)
             {
