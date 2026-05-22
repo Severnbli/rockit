cdb0387bc7e51a68f2d59eb2801ae214c5ebfd95
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 23:40:10 +0300

    Create UpdatePlatformsAreaParticleSystemRadiusOnRunSystem, bind it to PlatformsSharedModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Systems/UpdatePlatformsAreaParticleSystemRadiusOnRunSystem.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Runtime.Features.Graphics.Particles;
+using _Project.Scripts.Runtime.Features.Platforms.Shared.Services;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Systems
+{
+    public sealed class UpdatePlatformsAreaParticleSystemRadiusOnRunSystem : IProtoRunSystem
+    {
+        [DI] private readonly PlatformsSharedAspect _psAspect;
+        [DI] private readonly ParticlesAspect _pAspect;
+        private readonly PlatformsAreaService _paService;
+        
+        public void Run()
+        {
+            foreach (var e in _psAspect.PlatformsAreaParticleSystems)
+            {
+                ref var psComponent = ref _pAspect.ParticleSystemComponentPool.Get(e);
+                var shape = psComponent.ParticleSystem.shape;
+                shape.radius = _paService.Radius;
+            }
+        }
+    }
+}
