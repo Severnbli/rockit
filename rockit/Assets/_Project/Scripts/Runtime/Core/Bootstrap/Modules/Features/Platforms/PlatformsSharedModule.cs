e721510715019382846038bd285a20743b90810b
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 23:11:57 +0300

    Create PlayPlatformsAreaParticleSystemOnLevelSpawnedRequestSystem, bind it to PlatformsSharedModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Platforms/PlatformsSharedModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Platforms/PlatformsSharedModule.cs
@@ -15,6 +15,7 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Platforms
             
             BindSystem<LoadPlatformStartStatesOnInitializeRequestSystem>();
             BindSystem<TranslatePlatformTriggeredRequestsIntoAnyPlatformTriggeredRequestSystem>();
+            BindSystem<PlayPlatformsAreaParticleSystemOnLevelSpawnedRequestSystem>();
         }
     }
 }
---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Systems/PlayPlatformsAreaParticleSystemOnLevelSpawnedRequestSystem.cs
@@ -0,0 +1,26 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Graphics.Particles;
+using _Project.Scripts.Runtime.Features.World.Levels;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Systems
+{
+    public sealed class PlayPlatformsAreaParticleSystemOnLevelSpawnedRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly LevelsRequestsAspect _lrAspect;
+        [DI] private readonly PlatformsSharedAspect _psAspect;
+        [DI] private readonly ParticlesAspect _pAspect;
+        
+        public void Run()
+        {
+            if (_lrAspect.LevelSpawnedRequests.IsEmptySlow()) return;
+
+            foreach (var e in _psAspect.PlatformsAreaParticleSystems)
+            {
+                ref var psComponent = ref _pAspect.ParticleSystemComponentPool.Get(e);
+                psComponent.ParticleSystem.Play();
+            }
+        }
+    }
+}
