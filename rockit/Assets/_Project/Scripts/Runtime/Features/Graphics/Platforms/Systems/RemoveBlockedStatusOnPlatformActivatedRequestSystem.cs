9103ebe1338bba2efd20ad29ac55a553c4adabdb
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-23 01:25:06 +0300

    Create RemoveBlockedStatusOnPlatformActivatedRequestSystem, bind it to PlatformsGraphicsModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/Platforms/Systems/RemoveBlockedStatusOnPlatformActivatedRequestSystem.cs
@@ -0,0 +1,34 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Platforms.Shared;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.Platforms.Systems
+{
+    public sealed class RemoveBlockedStatusOnPlatformActivatedRequestSystem : IProtoInitSystem, IProtoRunSystem
+    {
+        [DIRequests] private readonly RequestsAspect _rAspect;
+        [DIRequests] private readonly PlatformsSharedRequestsAspect _psrAspect;
+        [DI] private readonly PlatformsGraphicsAspect _pgAspect;
+        private ProtoWorld _world;
+        
+        public void Init(IProtoSystems systems)
+        {
+            _world = systems.World();
+        }
+
+        public void Run()
+        {
+            foreach (var reqE in _psrAspect.PlatformActivatedRequests)
+            {
+                if (!_rAspect.TryCompareRequestWorld(reqE, _world, out var tarE)) continue;
+                if (!_pgAspect.PlatformColorChanges.Has(tarE)) continue;
+
+                ref var pccComponent = ref _pgAspect.PlatformColorChangeComponentPool.Get(tarE);
+                pccComponent.Blocked = false;
+            }
+        }
+    }
+}
