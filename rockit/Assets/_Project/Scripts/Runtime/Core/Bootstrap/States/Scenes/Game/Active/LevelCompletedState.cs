ea2d38ffdfcb7976a05513d58fd145c19abac208
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 04:22:09 +0300

    Switch to menus camera at "OnEnter" of LevelCompletedState

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/LevelCompletedState.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/States/Scenes/Game/Active/LevelCompletedState.cs
@@ -1,4 +1,6 @@
-﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Monos;
+﻿using _Project.Scripts.Runtime.Features.Graphics.Cameras.Monos;
+using _Project.Scripts.Runtime.Features.Graphics.Cameras.Tools.Switcher;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Windows.Scenes.Game.Monos;
 using Cysharp.Threading.Tasks;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
@@ -6,14 +8,19 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active
     public class LevelCompletedState : ISceneState
     {
         private readonly LevelCompletedWindow _lcWindow;
+        private readonly MenusCamera _mCamera;
+        private readonly ICameraSwitcher _cSwitcher;
 
-        public LevelCompletedState(LevelCompletedWindow lcWindow)
+        public LevelCompletedState(LevelCompletedWindow lcWindow, MenusCamera mCamera, ICameraSwitcher cSwitcher)
         {
             _lcWindow = lcWindow;
+            _mCamera = mCamera;
+            _cSwitcher = cSwitcher;
         }
 
         public async UniTask OnEnter(IStateMachine stateMachine)
         {
+            await _cSwitcher.SwitchTo(_mCamera.Camera);
             await _lcWindow.OpenAwait();
         }
 
