5cb87ebfb7d42162281e24a26cd52cfe00c5dc48
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 23:58:06 +0300

    Create EnablePlatformsAreaServiceOnLevelSpawnedRequestSystem, bind it to PlatformsSharedModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/Systems/EnablePlatformsAreaServiceOnLevelSpawnedRequestSystem.cs
@@ -0,0 +1,29 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using _Project.Scripts.Runtime.Features.Platforms.Shared.Services;
+using _Project.Scripts.Runtime.Features.World.Levels;
+using _Project.Scripts.Runtime.Shared.Utils.Features;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.Platforms.Shared.Systems
+{
+    public sealed class EnablePlatformsAreaServiceOnLevelSpawnedRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly RequestsAspect _rAspect;
+        [DIRequests] private readonly LevelsRequestsAspect _lrAspect;
+        private readonly PlatformsAreaService _paService;
+
+        public EnablePlatformsAreaServiceOnLevelSpawnedRequestSystem(PlatformsAreaService paService)
+        {
+            _paService = paService;
+        }
+
+        public void Run()
+        {
+            if (_lrAspect.LevelSpawnedRequests.IsEmptySlow()) return;
+
+            _paService.Enabled = true;
+            PlatformsUtils.CreatePlatformsAreaServiceEnabledRequests(_rAspect);
+        }
+    }
+}
