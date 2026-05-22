61164badad47308fa1208e758fb826714bf3754c
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-23 00:13:50 +0300

    Rename DeactivatePlatformRequest into PlatformDeactivatedRequest

--- rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Platforms/Shared/PlatformsSharedRequestsAspect.cs
@@ -29,7 +29,7 @@ namespace _Project.Scripts.Runtime.Features.Platforms.Shared
         public readonly ProtoPool<ActivatePlatformRequest> ActivatePlatformRequestPool;
         public readonly ProtoIt ActivatePlatformRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, ActivatePlatformRequest>());
         
-        public readonly ProtoPool<DeactivatePlatformRequest> DeactivatePlatformRequestPool;
-        public readonly ProtoIt DeactivatePlatformRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, DeactivatePlatformRequest>());
+        public readonly ProtoPool<PlatformDeactivatedRequest> PlatformDeactivatedRequestPool;
+        public readonly ProtoIt PlatformDeactivatedRequests = new (It.Inc<RequestComponent, ActiveRequestTag, RunRequestTag, PlatformDeactivatedRequest>());
     }
 }
