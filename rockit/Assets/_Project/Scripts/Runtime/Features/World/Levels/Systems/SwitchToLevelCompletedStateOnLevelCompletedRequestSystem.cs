f98ff400ace52d2601a1013592efe30b647cbd0f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-22 03:33:29 +0300

    Create SwitchToLevelCompletedStateOnLevelCompletedRequestSystem, bind it to LevelsSceneModule

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/World/Levels/Systems/SwitchToLevelCompletedStateOnLevelCompletedRequestSystem.cs
@@ -0,0 +1,26 @@
+﻿using _Project.Scripts.Runtime.Core.Bootstrap.States;
+using _Project.Scripts.Runtime.Core.Bootstrap.States.Scenes.Game.Active;
+using _Project.Scripts.Runtime.Core.Infrastructure.Requests.World;
+using Cysharp.Threading.Tasks;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Features.World.Levels.Systems
+{
+    public sealed class SwitchToLevelCompletedStateOnLevelCompletedRequestSystem : IProtoRunSystem
+    {
+        [DIRequests] private readonly LevelsRequestsAspect _lrAspect;
+        private readonly IStateMachine _sMachine;
+
+        public SwitchToLevelCompletedStateOnLevelCompletedRequestSystem(IStateMachine sMachine)
+        {
+            _sMachine = sMachine;
+        }
+
+        public void Run()
+        {
+            if (_lrAspect.LevelCompletedRequests.IsEmptySlow()) return;
+
+            _sMachine.ChangeState<LevelCompletedState>().Forget();
+        }
+    }
+}
