52e62c49ef8ffb38ff03634d2dd733d2c459bcaa
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-24 01:41:03 +0300

    Create NotificationsUtils with only method "CreateShowCenterNotificationRequest"

---
+++ rockit/Assets/_Project/Scripts/Runtime/Shared/Utils/Features/Graphics/NotificationsUtils.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Requests;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Requests;
+using _Project.Scripts.Runtime.Shared.Extensions.Infrastructure;
+using Leopotam.EcsProto;
+
+namespace _Project.Scripts.Runtime.Shared.Utils.Features.Graphics
+{
+    public static class NotificationsUtils
+    {
+        public static ProtoEntity CreateShowCenterNotificationRequest(RequestsAspect aspect,
+            ShowCenterNotificationRequest prepared)
+        {
+            return aspect.CreateRequest(
+                aspect.UIRequestsAspect.NotificationsRequestsAspect.ShowCenterNotificationRequestPool,
+                prepared: prepared);
+        }
+    }
+}
