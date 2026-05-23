08421607f5577f6576f578f59d5ed959f8e8814f
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-24 00:39:35 +0300

    Create NotificationsRequestsAspect, add it to UIRequestsAspect

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Notifications/NotificationsRequestsAspect.cs
@@ -0,0 +1,9 @@
+﻿using Leopotam.EcsProto.QoL;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Notifications
+{
+    public sealed class NotificationsRequestsAspect : ProtoAspectInject
+    {
+        
+    }
+}
