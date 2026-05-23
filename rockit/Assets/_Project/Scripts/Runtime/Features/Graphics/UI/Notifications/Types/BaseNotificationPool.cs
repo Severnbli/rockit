504fa349086f6ec42da733f499d3a7fc17d2132d
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-24 01:06:36 +0300

    Add "NConfig" field, initialize from constructor

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Notifications/Types/BaseNotificationPool.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Notifications/Types/BaseNotificationPool.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Configs;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Monos;
 using Leopotam.EcsProto;
 using UnityEngine;
@@ -12,10 +13,13 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Types
         where TDespawnSettings : struct
     {
         protected readonly NotificationsArea NArea;
+        protected readonly NotificationsConfig NConfig;
         
-        protected BaseNotificationPool(ProtoWorld world, NotificationsArea nArea) : base(world)
+        protected BaseNotificationPool(ProtoWorld world, NotificationsArea nArea, NotificationsConfig nConfig) 
+            : base(world)
         {
             NArea = nArea;
+            NConfig = nConfig;
         }
 
         protected override void PostDespawn(TItem instance, TDespawnSettings settings = default)
