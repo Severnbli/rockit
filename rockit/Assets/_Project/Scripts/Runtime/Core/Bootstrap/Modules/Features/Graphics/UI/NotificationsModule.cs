124d8f035bd12843c553d4cbf31433b7a75e8126
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-24 01:01:46 +0300

    Bind MonoNotificationPool to NotificationsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/NotificationsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/NotificationsModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Types;
 
 namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI
 {
@@ -6,6 +7,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI
     {
         public NotificationsModule(IDomain domain) : base(domain)
         {
+        }
+
+        protected override void RegisterBindings()
+        {
+            base.RegisterBindings();
+
+            Container.Bind<MonoNotificationPool>().ToSelf().AsSingle();
         }
     }
 }
