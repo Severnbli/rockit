f9794d1d587340832a985843ba2b557522ed594e
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-24 00:39:35 +0300

    Create NotificationsRequestsAspect, add it to UIRequestsAspect

--- rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UIRequestsAspect.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Shared/UIRequestsAspect.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Features.Graphics.UI.Buttons;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications;
 using Leopotam.EcsProto.QoL;
 
 namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared
@@ -6,5 +7,6 @@ namespace _Project.Scripts.Runtime.Features.Graphics.UI.Shared
     public sealed class UIRequestsAspect : ProtoAspectInject
     {
         public readonly ButtonsRequestsAspect ButtonsRequestsAspect;
+        public readonly NotificationsRequestsAspect NotificationsRequestsAspect;
     }
 }
