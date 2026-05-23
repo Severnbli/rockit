2b043e5a0f027a5520956eae3e433beae3eec432
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-24 00:57:10 +0300

    Create MonoNotificationPool that inherited from BasePrefabPool with defined "PostSpawn" and "PostDespawn" methods overriding

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Notifications/Types/MonoNotificationPool.cs
@@ -0,0 +1,44 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Configs;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Monos;
+using Leopotam.EcsProto;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Types
+{
+    public class MonoNotificationPool : BasePrefabPool<MonoNotification, MonoNotificationPoolSpawnSettings, MonoNotificationPoolDespawnSettings>
+    {
+        protected readonly NotificationsConfig NConfig;
+        
+        public MonoNotificationPool(ProtoWorld world, NotificationsConfig nConfig) : base(world)
+        {
+            NConfig = nConfig;
+        }
+
+        protected override void PostSpawn(MonoNotification instance, Transform at = null, MonoNotificationPoolSpawnSettings settings = default)
+        {
+            base.PostSpawn(instance, at, settings);
+
+            instance.Text.text = settings.Text;
+        }
+
+        protected override void PostDespawn(MonoNotification instance, MonoNotificationPoolDespawnSettings settings = default)
+        {
+            base.PostDespawn(instance, settings);
+            
+            instance.Text.text = string.Empty;
+        }
+
+        protected override GameObject GetPrefab() => NConfig.MonoNotificationPrefab;
+    }
+
+    public struct MonoNotificationPoolSpawnSettings
+    {
+        public string Text;
+    }
+
+    public struct MonoNotificationPoolDespawnSettings
+    {
+        
+    }
+}
