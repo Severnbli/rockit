e0791fb2c946bf3fb5726a607d1b9faff65e34d3
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-24 01:05:31 +0300

    Create BaseNotificationPool where define "PostDespawn" behaviour

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Notifications/Types/BaseNotificationPool.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Runtime.Core.Infrastructure.Objects.Lifecycle.Pools;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Monos;
+using Leopotam.EcsProto;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Types
+{
+    public abstract class BaseNotificationPool<TItem, TSpawnSettings, TDespawnSettings> : 
+        BasePrefabPool<TItem, TSpawnSettings, TDespawnSettings>
+        where TItem : Component
+        where TSpawnSettings : struct
+        where TDespawnSettings : struct
+    {
+        protected readonly NotificationsArea NArea;
+        
+        protected BaseNotificationPool(ProtoWorld world, NotificationsArea nArea) : base(world)
+        {
+            NArea = nArea;
+        }
+
+        protected override void PostDespawn(TItem instance, TDespawnSettings settings = default)
+        {
+            base.PostDespawn(instance, settings);
+            
+            instance.gameObject.transform.SetParent(NArea.transform);
+        }
+    }
+}
