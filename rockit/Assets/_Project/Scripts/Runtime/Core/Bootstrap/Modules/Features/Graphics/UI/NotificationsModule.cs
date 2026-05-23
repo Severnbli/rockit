725d19b2a317823daf8550ee912efdde6d2fc2e0
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-24 01:57:12 +0300

    Create ShowCenterNotificationOnShowCenterNotificationRequest, bind it to NotificationsModule

--- rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/NotificationsModule.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Bootstrap/Modules/Features/Graphics/UI/NotificationsModule.cs
@@ -1,4 +1,5 @@
 ﻿using _Project.Scripts.Runtime.Core.Bootstrap.Domain;
+using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Systems;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Tools;
 using _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Types;
 
@@ -16,6 +17,13 @@ namespace _Project.Scripts.Runtime.Core.Bootstrap.Modules.Features.Graphics.UI
 
             Container.Bind<MonoNotificationPool>().ToSelf().AsSingle();
             Container.Bind<INotifier>().To<Notifier>().AsSingle();
+        }
+
+        protected override void BindSystems()
+        {
+            base.BindSystems();
+            
+            BindSystem<ShowCenterNotificationOnShowCenterNotificationRequest>();
         }
     }
 }
