7140d45880628d7fbcb018c8aee45a47db4993b4
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-24 00:38:16 +0300

    Create NotificationsConfig with only "_center" field and its property getter

---
+++ rockit/Assets/_Project/Scripts/Runtime/Features/Graphics/UI/Notifications/Configs/NotificationsConfig.cs
@@ -0,0 +1,13 @@
+﻿using _Project.Scripts.Modules.Zenject;
+using _Project.Scripts.Runtime.Core.Infrastructure.Animations.Tools.Pipeline.Core;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Features.Graphics.UI.Notifications.Configs
+{
+    public sealed class NotificationsConfig : ScriptableObjectAutoInstaller<NotificationsConfig>
+    {
+        [SerializeField] private TweenPipeline _center;
+        
+        public TweenPipeline Center => _center;
+    }
+}
