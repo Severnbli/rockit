e19892ed14daa2ea666dd6e1444c310219a5d324
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-26 19:03:38 +0300

    Add "_buttonClip" field with its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Configs/SfxConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Configs/SfxConfig.cs
@@ -1,7 +1,11 @@
-﻿namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Configs
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Configs
 {
     public sealed class SfxConfig : AudioConfig<SfxConfig>
     {
+        [SerializeField] private AudioClip _buttonClip;
         
+        public AudioClip ButtonClip => _buttonClip;
     }
 }
