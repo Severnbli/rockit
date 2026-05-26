d7ff01ce343b16140aff8ceacdc717f931ed6fb3
Usevalad Buben <usevaladbuben@gmail.com>
2026-05-26 19:35:27 +0300

    Add "_errorClip" field and its property getter

--- rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Configs/SfxConfig.cs
+++ rockit/Assets/_Project/Scripts/Runtime/Core/Infrastructure/Audio/Configs/SfxConfig.cs
@@ -5,7 +5,9 @@ namespace _Project.Scripts.Runtime.Core.Infrastructure.Audio.Configs
     public sealed class SfxConfig : AudioConfig<SfxConfig>
     {
         [SerializeField] private AudioClip _clickClip;
+        [SerializeField] private AudioClip _errorClip;
         
         public AudioClip ClickClip => _clickClip;
+        public AudioClip ErrorClip => _errorClip;
     }
 }
